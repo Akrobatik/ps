ae64e6417ee3dcc3578deb4aa94b586c027c2acf
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-01-22 00:01:13 +0900

    [Gold V] Title: 최소비용 구하기, Time: 24 ms, Memory: 4752 KB -BaekjoonHub

---
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.cc
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct City {
+  City() : min(INT_MAX) {}
+
+  int min;
+  vector<pair<int, int>> bus;
+};
+
+struct Path {
+  auto operator<=>(const Path& rhs) const {
+    return weight <=> rhs.weight;
+  }
+
+  int pos;
+  int weight;
+};
+
+City nodes[1001];
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n, m;
+  cin >> n >> m;
+  for (int i = 0; i < m; i++) {
+    int a, b, c;
+    cin >> a >> b >> c;
+    nodes[a].bus.push_back(make_pair(b, c));
+  }
+  int s, e;
+  cin >> s >> e;
+  priority_queue<Path, vector<Path>, greater<Path>> pq;
+  pq.push(Path{s, 0});
+  while (!pq.empty()) {
+    auto v = pq.top();
+    pq.pop();
+
+    if (v.pos == e) {
+      cout << v.weight << endl;
+      break;
+    }
+
+    if (v.weight < nodes[v.pos].min) {
+      nodes[v.pos].min = v.weight;
+      for (auto [id, weight] : nodes[v.pos].bus) {
+        if (v.weight + weight < nodes[id].min) {
+          pq.push(Path{id, v.weight + weight});
+        }
+      }
+    }
+  }
+
+  return 0;
+}
