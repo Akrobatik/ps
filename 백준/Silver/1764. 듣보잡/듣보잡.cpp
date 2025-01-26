01a83e08716c30bc9339536c55f2f85d5c22a106
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-01-27 04:03:34 +0900

    [Silver IV] Title: 듣보잡, Time: 40 ms, Memory: 7572 KB -BaekjoonHub

---
+++ 백준/Silver/1764. 듣보잡/듣보잡.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  string s;
+  map<string, bool> st;
+  int n, m;
+  cin >> n >> m;
+
+  while (n--) {
+    cin >> s;
+    st.insert({s, false});
+  }
+
+  int cnt = 0;
+  while (m--) {
+    cin >> s;
+    if (auto it = st.find(s); it != st.end()) {
+      ++cnt;
+      it->second = true;
+    }
+  }
+
+  cout << cnt << "\n";
+  for (auto& [s2, ok] : st) {
+    if (ok) cout << s2 << "\n";
+  }
+
+  return 0;
+}
