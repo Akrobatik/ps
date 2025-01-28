c9775e169afeb7af51d1fdfaa3e227a163f33aee
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-01-28 22:43:04 +0900

    [Gold III] Title: 앱, Time: 0 ms, Memory: 2100 KB -BaekjoonHub

--- 백준/Gold/7579. 앱/앱.cpp
+++ 백준/Gold/7579. 앱/앱.cpp
@@ -2,14 +2,7 @@
 
 using namespace std;
 
-int mem[101];
-int cost[101];
-int memo[101][10001];
-// [app][cost-sum] = w min
-
-// Aij = Ai-1j
-//     = Ai-1j-cost + mem
-// Ai-1j, Ai-1j-cost 가 있는지 체크
+int _memo[2][10001];
 
 int main() {
   ios::sync_with_stdio(false);
@@ -17,37 +10,28 @@ int main() {
 
   int n, m;
   cin >> n >> m;
-  for (int i = 1; i <= n; i++) cin >> mem[i];
-  for (int i = 1; i <= n; i++) cin >> cost[i];
+
+  int mems[100];
+  int costs[100];
+
+  for (int i = 0; i < n; i++) cin >> mems[i];
+  for (int i = 0; i < n; i++) cin >> costs[i];
 
   int maxx = 0;
-  for (int i = 1; i <= n; i++) {
-    int mm = mem[i];
-    int c = cost[i];
-    int maxx_old = maxx;
-    maxx += c;
-    for (int j = maxx_old + 1; j <= maxx; j++) memo[i - 1][j] = -1;
-
-    int j = 0;
-    for (; j < c; j++) memo[i][j] = memo[i - 1][j];
-    for (; j <= maxx; j++) {
-      if (memo[i - 1][j] != -1 && memo[i - 1][j - c] != -1) {
-        memo[i][j] = max<int>(memo[i - 1][j], memo[i - 1][j - c] + mm);
-      } else if (memo[i - 1][j] != -1) {
-        memo[i][j] = memo[i - 1][j];
-      } else if (memo[i - 1][j - c] != -1) {
-        memo[i][j] = memo[i - 1][j - c] + mm;
-      } else {
-        memo[i][j] = -1;
-      }
-    }
+  int* memo = _memo[1];
+  int* memo_old = _memo[0];
+  for (int i = 0; i < n; i++) {
+    int mem = mems[i];
+    int cost = costs[i];
+    copy(memo_old, memo_old + cost, memo);
+    for (int j = 0; j <= maxx; j++) memo[j + cost] = max<int>(memo_old[j + cost], memo_old[j] + mem);
+    maxx += cost;
+    swap(memo, memo_old);
   }
 
-  for (int i = 0; i <= maxx; i++) {
-    if (memo[n][i] < m) continue;
-    cout << i << endl;
-    break;
-  }
+  int c = 0;
+  while (memo_old[c] < m) c++;
+  cout << c << endl;
 
   return 0;
 }
