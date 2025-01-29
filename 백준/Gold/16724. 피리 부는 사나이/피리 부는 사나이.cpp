a31b7c044ca5a5c5e13ce41e786b9d08887c8386
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-01-30 00:05:51 +0900

    [Gold III] Title: 피리 부는 사나이, Time: 64 ms, Memory: 10212 KB -BaekjoonHub

--- 백준/Gold/16724. 피리 부는 사나이/피리 부는 사나이.cpp
+++ 백준/Gold/16724. 피리 부는 사나이/피리 부는 사나이.cpp
@@ -2,55 +2,52 @@
 
 using namespace std;
 
-int foothold[1000][1000];
-int memo[1000][1000];
+constexpr char kDeltaY[] =
+    "\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff"
+    "\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff"
+    "\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff"
+    "\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff"
+    "\xff\xff\xff\xff\x01\xff\xff\xff\xff\xff\xff\xff\x00\xff\xff\xff"
+    "\xff\xff\x00\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff";
+
+constexpr char kDeltaX[] =
+    "\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff"
+    "\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff"
+    "\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff"
+    "\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff"
+    "\xff\xff\xff\xff\x00\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff"
+    "\xff\xff\x01\xff\xff\x00\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff";
+
+int memo[1 << 20][2];
 
 int main() {
-  ios::sync_with_stdio(false);
-  cin.tie(nullptr);
-
-  int r, c;
-  cin >> r >> c;
-  for (int i = 0; i < r; i++) {
-    for (int j = 0; j < c; j++) {
+  int rows, cols;
+  cin >> rows >> cols;
+  for (int i = 0; i < rows; i++) {
+    for (int j = 0; j < cols; j++) {
       char c;
       cin >> c;
-      switch (c) {
-        case 'U':
-          foothold[i][j] = ((i - 1) << 16) | j;
-          break;
-        case 'D':
-          foothold[i][j] = ((i + 1) << 16) | j;
-          break;
-        case 'L':
-          foothold[i][j] = (i << 16) | (j - 1);
-          break;
-        case 'R':
-          foothold[i][j] = (i << 16) | (j + 1);
-          break;
-      }
+      memo[(i << 10) | j][0] = ((i + kDeltaY[c]) << 10) | (j + kDeltaX[c]);
     }
   }
 
   int ans = 0;
   int cnt = 1;
-  for (int i = 0; i < r; i++) {
-    for (int j = 0; j < c; j++) {
-      if (memo[i][j]) continue;
+  for (int i = 0; i < rows; i++) {
+    for (int j = 0; j < cols; j++) {
+      auto cur = memo[(i << 10) | j];
+      if (cur[1]) continue;
 
-      int cr = i, cc = j;
-      while (memo[cr][cc] == 0) {
-        int v = foothold[cr][cc];
-        memo[cr][cc] = cnt;
-        cr = v >> 16;
-        cc = v & 0xFFFF;
-      }
+      do {
+        cur[1] = cnt;
+        cur = (int*)memo + (cur[0] << 1);
+      } while (cur[1] == 0);
 
-      if (memo[cr][cc] == cnt++) ++ans;
+      if ((cur[1] ^ cnt++) == 0) ++ans;
     }
   }
 
-  cout << ans << endl;
+  cout << ans;
 
   return 0;
 }
