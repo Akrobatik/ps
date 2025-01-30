929a3c4d8bd8339866c9cb8738396b55a686d48c
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-01-31 02:57:28 +0900

    [Platinum V] Title: Parcel, Time: 36 ms, Memory: 5288 KB -BaekjoonHub

--- 백준/Platinum/16287. Parcel/Parcel.cpp
+++ 백준/Platinum/16287. Parcel/Parcel.cpp
@@ -2,16 +2,7 @@
 
 using namespace std;
 
-struct Subset {
-  Subset(int _a, int _b) : s(_a + _b), a(_a), b(_b) {}
-  Subset(int _s) : s(_s), a(0), b(0) {}
-
-  constexpr auto operator<=>(const Subset& other) const {
-    return s <=> other.s;
-  }
-
-  int s, a, b;
-};
+int memo[400001][2];
 
 int main() {
   ios::sync_with_stdio(false);
@@ -23,38 +14,23 @@ int main() {
   for (int i = 0; i < n; i++) cin >> a[i];
   sort(a.begin(), a.end());
 
-  vector<Subset> ab;
-  ab.reserve((n * (n - 1)) / 2);
   for (int i = 0; i < n - 1; i++) {
-    for (int j = i + 1; j < n; j++) ab.emplace_back(a[i], a[j]);
-  }
-  sort(ab.begin(), ab.end());
-  vector<Subset> cd(ab);
-
-  auto ab_it = ab.begin(), ab_end = ab.end();
-  auto cd_it = cd.rbegin(), cd_end = cd.rend();
-  while (ab_it != ab_end && cd_it != cd_end) {
-    int abv = ab_it->s;
-    int cdv = cd_it->s;
-    if (abv + cdv == w) {
-      vector<pair<int, int>> ab_cands;
-      while (ab_it != ab_end && ab_it->s == abv) ab_cands.push_back({ab_it->a, ab_it->b}), ++ab_it;
-      vector<pair<int, int>> cd_cands;
-      while (cd_it != cd_end && cd_it->s == cdv) cd_cands.push_back({cd_it->a, cd_it->b}), ++cd_it;
-      for (auto [aa, bb] : ab_cands) {
-        for (auto [cc, dd] : cd_cands) {
-          if (aa != cc && aa != bb && aa != dd && bb != cc && bb != cc && bb != dd && cc != dd) {
-            cout << "YES" << endl;
-            return 0;
-          }
-        }
-      }
-    } else if (abv + cdv < w) {
-      ab_it = lower_bound(ab_it + 1, ab_end, Subset(w - cdv));
-    } else {
-      cd_it = lower_bound(cd_it + 1, cd_end, Subset(w - abv), greater<>());
+    for (int j = i + 1; j < n; j++) {
+      int sum = a[i] + a[j];
+      if (memo[sum][0] == 0 || memo[sum][0] > j) memo[sum][0] = j;
+      if (memo[sum][1] == 0 || memo[sum][1] < i) memo[sum][1] = i;
     }
   }
-  cout << "NO" << endl;
+
+  bool found = false;
+  for (int i = max<int>(3, w - 400000); i <= w / 2; i++) {
+    if (memo[i][0] && memo[w - i][1] && memo[i][0] < memo[w - i][1]) {
+      found = true;
+      break;
+    }
+  }
+
+  cout << (found ? "YES" : "NO") << endl;
+
   return 0;
 }
