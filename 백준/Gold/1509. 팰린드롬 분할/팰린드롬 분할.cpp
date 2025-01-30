05e138f81cb571fd940b02ce293392d014ec150a
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-01-31 08:36:54 +0900

    [Gold I] Title: 팰린드롬 분할, Time: 8 ms, Memory: 2056 KB -BaekjoonHub

--- 백준/Gold/1509. 팰린드롬 분할/팰린드롬 분할.cpp
+++ 백준/Gold/1509. 팰린드롬 분할/팰린드롬 분할.cpp
@@ -2,58 +2,52 @@
 
 using namespace std;
 
-constexpr uint64_t kPrime = 1e9 + 9;
-constexpr uint64_t kBase = 91;
+char s[5002];
+int memo[5001];
+int ans[2501];
 
-template <int N>
-struct PolyHashArray {
-  constexpr PolyHashArray() : arr() {
-    for (int i = 0; i < N; i++) {
-      if (i == 0) {
-        arr[i] = 1;
-      } else {
-        arr[i] = (arr[i - 1] * kBase) % kPrime;
-      }
-    }
-  }
-
-  uint64_t arr[N];
-};
-
-uint64_t forwards[2501];
-uint64_t backwards[2501];
-constexpr PolyHashArray<2501> _polyhashes;
-const uint64_t* polyhashes = _polyhashes.arr;
-
-int memo[2501];
-
-bool Check(int i, int j) {
-  return (forwards[j + 1] + polyhashes[j + 1] * backwards[i]) % kPrime ==
-         (forwards[i] + polyhashes[i] * backwards[j + 1]) % kPrime;
+bool IsPalindrom(int i, int j) {
+  return j - i < memo[i + j + 1];
 }
 
 int main() {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
 
-  string s;
-  cin >> s;
-  int n = s.size();
-  for (int i = 0; i < n; i++) {
-    forwards[i + 1] = (forwards[i] + s[i] * polyhashes[i]) % kPrime;
-    backwards[i + 1] = (backwards[i] * kBase + s[i]) % kPrime;
-  }
+  char c;
+  int n = 0;
+  while (cin >> c) s[(n++ << 1) + 1] = c;
 
-  for (int i = 0; i < n; i++) {
-    memo[i + 1] = i + 1;
-    for (int j = 0; j <= i; j++) {
-      if ((j == i || Check(j, i)) && memo[i + 1] > memo[j] + 1) {
-        memo[i + 1] = memo[j] + 1;
+  int p, r = 0;
+  int nn = n * 2 + 1;
+  for (int i = 1; i < nn - 1; i++) {
+    if (i > r) {
+      p = r = i;
+      while (r < nn && r <= p * 2 && s[r] == s[p * 2 - r]) ++r;
+      memo[i] = --r - p;
+    } else {
+      if (memo[p * 2 - i] < r - i) {
+        memo[i] = memo[p * 2 - i];
+      } else if (memo[p * 2 - i] > r - i) {
+        memo[i] = r - i;
+      } else {
+        p = i;
+        while (r < nn && r <= p * 2 && s[r] == s[p * 2 - r]) ++r;
+        memo[i] = --r - p;
       }
     }
   }
 
-  cout << memo[n];
+  for (int i = 0; i < n; i++) {
+    ans[i + 1] = i + 1;
+    for (int j = 0; j <= i; j++) {
+      if (IsPalindrom(j, i) && ans[i + 1] > ans[j] + 1) {
+        ans[i + 1] = ans[j] + 1;
+      }
+    }
+  }
+
+  cout << ans[n];
 
   return 0;
 }
