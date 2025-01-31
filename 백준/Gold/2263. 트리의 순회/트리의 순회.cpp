826562781b6e10c71240b7b8a55a758c277758a1
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-01-31 13:41:30 +0900

    [Gold I] Title: 트리의 순회, Time: 28 ms, Memory: 16736 KB -BaekjoonHub

---
+++ 백준/Gold/2263. 트리의 순회/트리의 순회.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int in[1000000];
+int post[1000000];
+int memo[1000001];
+
+void Solve(int ib, int ie, int pb, int pe) {
+  if (ib > ie || pb > pe) return;
+
+  int root = post[pe];
+  int idx = memo[root];
+
+  cout << root << " ";
+  Solve(ib, idx - 1, pb, pb + idx - ib - 1);
+  Solve(idx + 1, ie, pb + idx - ib, pe - 1);
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  cin >> n;
+  for (int i = 0; i < n; i++) cin >> in[i], memo[in[i]] = i;
+  for (int i = 0; i < n; i++) cin >> post[i];
+
+  Solve(0, n - 1, 0, n - 1);
+
+  return 0;
+}
