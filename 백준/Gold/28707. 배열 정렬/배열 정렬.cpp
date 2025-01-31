68ac11601fd555e6f037b872a788a937d192fbf9
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-01-31 18:32:03 +0900

    [Gold I] Title: 배열 정렬, Time: 56 ms, Memory: 4220 KB -BaekjoonHub

--- 백준/Gold/28707. 배열 정렬/배열 정렬.cpp
+++ 백준/Gold/28707. 배열 정렬/배열 정렬.cpp
@@ -2,42 +2,64 @@
 
 using namespace std;
 
+uint32_t Swap(uint32_t bits, int l, int r) {
+  uint32_t lb = bits & (0xF << (l << 2));
+  uint32_t rb = bits & (0xF << (r << 2));
+  uint32_t d = (r - l) << 2;
+  return bits ^ lb ^ rb ^ (lb << d) ^ (rb >> d);
+}
+
+uint32_t Sort(uint32_t bits) {
+  for (int i = 0; i < 8; i++) {
+    for (int j = 0; j < 7 - i; j++) {
+      uint32_t lb = (bits >> (j << 2)) & 0xF;
+      uint32_t rb = (bits >> ((j + 1) << 2)) & 0xF;
+      if (lb && rb && lb > rb) bits = Swap(bits, j, j + 1);
+    }
+  }
+  return bits;
+}
+
 int main() {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
 
+  uint32_t seq = 0;
   int n;
   cin >> n;
-  vector<int> seq(n);
-  for (int i = 0; i < n; i++) cin >> seq[i];
-  vector<int> ans(seq);
-  sort(ans.begin(), ans.end());
+  int v;
+  for (int i = 0; i < n; i++) cin >> v, seq |= v << (i << 2);
+  uint32_t ans = Sort(seq);
+
   int m;
   cin >> m;
   vector<tuple<int, int, int>> edges(m);
-  for (int i = 0; i < m; i++) cin >> get<1>(edges[i]) >> get<2>(edges[i]) >> get<0>(edges[i]);
-  set<vector<int>> memo;
-  priority_queue<pair<int, vector<int>>, vector<pair<int, vector<int>>>, greater<>> pq;
+  for (int i = 0; i < m; i++) {
+    int l, r, c;
+    cin >> l >> r >> c;
+    edges[i] = {c, l - 1, r - 1};
+  }
+
+  unordered_set<uint32_t> memo;
+  priority_queue<pair<int, uint32_t>, vector<pair<int, uint32_t>>, greater<>> pq;
   pq.push({0, seq});
+
   while (!pq.empty()) {
-    auto [c, v] = pq.top();
+    auto [cost, sbits] = pq.top();
     pq.pop();
 
-    auto it = memo.find(v);
-    if (it != memo.end()) continue;
-    memo.insert(v);
-
-    if (v == ans) {
-      cout << c;
+    if (ans == sbits) {
+      cout << cost;
       return 0;
     }
 
-    for (auto& edge : edges) {
-      auto [w, a, b] = edge;
-      auto vv = v;
-      swap(vv[a - 1], vv[b - 1]);
-      if (memo.find(vv) != memo.end()) continue;
-      pq.push({c + w, vv});
+    if (memo.contains(sbits)) continue;
+    memo.insert(sbits);
+
+    for (auto [c, l, r] : edges) {
+      uint32_t tmp = Swap(sbits, l, r);
+      if (memo.contains(tmp)) continue;
+      pq.push({cost + c, tmp});
     }
   }
   cout << -1;
