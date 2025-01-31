8da77ee28dfa63bd49e260ad38c378061a9f2e1d
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-01-31 10:27:59 +0900

    [Gold I] Title: 계단 수, Time: 4 ms, Memory: 2100 KB -BaekjoonHub

--- 백준/Gold/1562. 계단 수/계단 수.cpp
+++ 백준/Gold/1562. 계단 수/계단 수.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 constexpr int kMod = 1000000000;
 
-int memo[100][10][1 << 10];
+int memo[2][10][1 << 10];
 
 int main() {
   ios::sync_with_stdio(false);
@@ -14,8 +14,8 @@ int main() {
   cin >> n;
   for (int i = 1; i <= 9; i++) memo[0][i][1 << i] = 1;
 
+  auto m = memo[1], m_old = memo[0];
   for (int i = 1; i < n; i++) {
-    auto m = memo[i], m_old = memo[i - 1];
     for (int j = 0; j < 1024; j++) {
       m[0][j | (1 << 0)] = (m[0][j | (1 << 0)] + m_old[1][j]) % kMod;
       m[9][j | (1 << 9)] = (m[9][j | (1 << 9)] + m_old[8][j]) % kMod;
@@ -24,11 +24,13 @@ int main() {
         m[k][j | (1 << k)] = (m[k][j | (1 << k)] + m_old[k + 1][j]) % kMod;
       }
     }
+    swap(m, m_old);
+    memset(m, 0, sizeof(memo[0]));
   }
 
   int64_t sum = 0;
   for (int i = 0; i <= 9; i++) {
-    sum += memo[n - 1][i][1023];
+    sum += m_old[i][1023];
   }
   cout << sum % kMod;
 
