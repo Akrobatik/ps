0a451fd87d2f2dd31c627b5e1c2ee1b78c6e5cb8
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-01-31 19:59:55 +0900

    [Gold I] Title: 배열 정렬, Time: 24 ms, Memory: 3848 KB -BaekjoonHub

--- 백준/Gold/28707. 배열 정렬/배열 정렬.cpp
+++ 백준/Gold/28707. 배열 정렬/배열 정렬.cpp
@@ -2,6 +2,22 @@
 
 using namespace std;
 
+uint32_t Compress(uint32_t* nums, int n) {
+  for (int i = 0; i < n; i++) {
+    while (find(nums, nums + n, i) == nums + n) {
+      bool skip = true;
+      for (int j = 0; j < n; j++) {
+        if (nums[j] > i) --nums[j];
+      }
+      if (skip) break;
+    }
+  }
+
+  uint32_t bits = 0;
+  for (int i = 0; i < n; i++) bits |= nums[i] << (i << 2);
+  return bits;
+}
+
 uint32_t Swap(uint32_t bits, int l, int r) {
   uint32_t lb = bits & (0xF << (l << 2));
   uint32_t rb = bits & (0xF << (r << 2));
@@ -9,12 +25,12 @@ uint32_t Swap(uint32_t bits, int l, int r) {
   return bits ^ lb ^ rb ^ (lb << d) ^ (rb >> d);
 }
 
-uint32_t Sort(uint32_t bits) {
-  for (int i = 0; i < 8; i++) {
-    for (int j = 0; j < 7 - i; j++) {
+uint32_t Sort(uint32_t bits, int n) {
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n - i - 1; j++) {
       uint32_t lb = (bits >> (j << 2)) & 0xF;
       uint32_t rb = (bits >> ((j + 1) << 2)) & 0xF;
-      if (lb && rb && lb > rb) bits = Swap(bits, j, j + 1);
+      if (lb > rb) bits = Swap(bits, j, j + 1);
     }
   }
   return bits;
@@ -24,12 +40,12 @@ int main() {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
 
-  uint32_t seq = 0;
+  uint32_t nums[8];
   int n;
   cin >> n;
-  int v;
-  for (int i = 0; i < n; i++) cin >> v, seq |= v << (i << 2);
-  uint32_t ans = Sort(seq);
+  for (int i = 0; i < n; i++) cin >> nums[i];
+  uint32_t compressed = Compress(nums, n);
+  uint32_t ans = Sort(compressed, n);
 
   int m;
   cin >> m;
@@ -42,7 +58,7 @@ int main() {
 
   unordered_map<uint32_t, int> memo;
   priority_queue<pair<int, uint32_t>, vector<pair<int, uint32_t>>, greater<>> pq;
-  pq.push({0, seq});
+  pq.push({0, compressed});
 
   while (!pq.empty()) {
     auto [cost, sbits] = pq.top();
