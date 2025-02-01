15b4a8ee0d07374d5e9af45fe978c922e49fce62
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-02-01 18:23:04 +0900

    [Gold I] Title: 2048 (Easy), Time: 4 ms, Memory: 2152 KB -BaekjoonHub

--- 백준/Gold/12100. 2048 （Easy）/2048 （Easy）.cpp
+++ 백준/Gold/12100. 2048 （Easy）/2048 （Easy）.cpp
@@ -2,15 +2,6 @@
 
 using namespace std;
 
-struct Board {
-  Board(int n) : size(n), m(n * n) {}
-
-  int* operator[](int row) { return m.data() + row * size; }
-
-  int size;
-  vector<int> m;
-};
-
 void Arrange(span<int> sp) {
   if (sp.size() == 0) return;
   auto it = remove(sp.begin(), sp.end(), 0);
@@ -25,23 +16,23 @@ void Arrange(span<int> sp) {
   }
 }
 
-int Solve(const Board& board, int depth) {
-  int maxx = *max_element(board.m.begin(), board.m.end());
+int Solve(const vector<int>& board, int size, int depth) {
+  int maxx = *max_element(board.begin(), board.end());
   if (depth == 5) return maxx;
 
   for (int i = 0; i < 4; i++) {
-    Board b(board);
-    int s1 = i < 2 ? board.size : 1;
-    int s2 = (i < 2 ? 1 : board.size) * (i & 1 ? -1 : 1);
-    int* sp = b.m.data() + (i == 1 ?  board.size - 1 : i == 3 ? board.size * (board.size - 1) : 0);
-    vector<int> vec(board.size);
-    for (int j = 0; j < board.size; j++) {
+    vector<int> b(board);
+    int s1 = i < 2 ? size : 1;
+    int s2 = (i < 2 ? 1 : size) * (i & 1 ? -1 : 1);
+    int* sp = b.data() + (i & 1 ? (size - 1) * (0 - s2) : 0);
+    vector<int> tmp(size);
+    for (int j = 0; j < size; j++) {
       int* p = sp + s1 * j;
-      for (int k = 0; k < board.size; k++) vec[k] = *(p + s2 * k);
-      Arrange(vec);
-      for (int k = 0; k < board.size; k++) *(p + s2 * k) = vec[k];
+      for (int k = 0; k < size; k++) tmp[k] = *(p + s2 * k);
+      Arrange(tmp);
+      for (int k = 0; k < size; k++) *(p + s2 * k) = tmp[k];
     }
-    int v = Solve(b, depth + 1);
+    int v = Solve(b, size, depth + 1);
     maxx = max<int>(maxx, v);
   }
 
@@ -54,12 +45,11 @@ int main() {
 
   int n;
   cin >> n;
-  Board board(n);
-  for (int i = 0; i < n; i++) {
-    for (int j = 0; j < n; j++) cin >> board[i][j];
-  }
+  vector<int> board(n * n);
+  int* p = board.data();
+  for (int i = 0; i < n * n; i++) cin >> *p++;
 
-  cout << Solve(board, 0);
+  cout << Solve(board, n, 0);
 
   return 0;
 }
