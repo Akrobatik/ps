2b96ce9c52f602ac094b47f6af9e1a6fa667e52e
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-02-11 22:20:10 +0900

    [Platinum V] Title: 찾기, Time: 12 ms, Memory: 14788 KB -BaekjoonHub

--- 백준/Platinum/1786. 찾기/찾기.cpp
+++ 백준/Platinum/1786. 찾기/찾기.cpp
@@ -1,38 +1,67 @@
-#include <bits/stdc++.h>
+#pragma GCC optimize("O3")
+// #include <bits/stdc++.h>
+#include <stdio.h>
 
 using namespace std;
 
+char in[3000000];
+char out[7000000];
+auto bo = out + 7;
+auto eo = out + 8;
+int table[1000000];
+
+int readString(char*& out) {
+  static auto it = in;
+  while (*it < 32) ++it;
+  out = it;
+  while (*it >= 32) ++it;
+  return it - out;
+}
+
+void writeSize(int x) {
+  *bo-- = '\n';
+  if (x == 0) {
+    *bo-- = '0';
+  } else {
+    while (x) *bo-- = '0' + x % 10, x /= 10;
+  }
+  ++bo;
+}
+
+void writeIndex(int x) {
+  int n = 0;
+  char s[8];
+  while (x) s[n++] = '0' + x % 10, x /= 10;
+  while (n--) *eo++ = s[n];
+  *eo++ = ' ';
+}
+
 int main() {
-  ios::sync_with_stdio(false);
-  cin.tie(nullptr);
+  fread(in, 1, sizeof(in), stdin);
+  char *t, *p;
+  int nt = readString(t), np = readString(p);
 
-  string t, p;
-  getline(cin, t, '\n');
-  getline(cin, p, '\n');
-
-  int nt = t.size();
-  int np = p.size();
   int idx = 0;
-  vector<int> table(np, 0);
   for (int i = 1; i < np; i++) {
     while (idx && p[i] != p[idx]) idx = table[idx - 1];
     if (p[i] == p[idx]) table[i] = ++idx;
   }
 
-  vector<int> ans;
+	int cnt = 0;
   idx = 0;
   for (int i = 0; i < nt; i++) {
     while (idx && t[i] != p[idx]) idx = table[idx - 1];
     if (t[i] == p[idx]) {
       if (++idx == np) {
-        ans.push_back(i - np + 2);
+				++cnt;
+				writeIndex(i - np + 2);
         idx = table[idx - 1];
       }
     }
   }
+	writeSize(cnt);
 
-  cout << ans.size() << "\n";
-  for (auto v : ans) cout << v << " ";
+	fwrite(bo, 1, eo - bo, stdout);
 
   return 0;
 }
