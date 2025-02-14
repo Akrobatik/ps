2a34082ab4179d04a1405efc7f202db97d958437
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-02-15 03:32:24 +0900

    [Silver V] Title: 영화감독 숌, Time: 4 ms, Memory: 2100 KB -BaekjoonHub

--- 백준/Silver/1436. 영화감독 숌/영화감독 숌.cpp
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.cpp
@@ -2,31 +2,36 @@
 
 using namespace std;
 
-bool Check(int v) {
-  int cnt = 3;
-  while (v) {
-    auto dv = div(v, 10);
-    if (dv.rem == 6) {
-      if (--cnt == 0) return true;
-    } else {
-      cnt = 3;
-    }
-    v = dv.quot;
-  }
-  return false;
-}
-
 int main() {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
 
+  int table[50000];
+  auto ptr = table;
+  for (int i = 0; i <= 9999; i++) {
+    char s[4];
+    int v = i;
+    for (int j = 3; j >= 0; j--) {
+      s[j] = '0' + v % 10;
+      v /= 10;
+    }
+
+    string_view sv(s, s + 4);
+    for (int j = 0; j < 5; j++) {
+      string ss;
+      ss.reserve(7);
+      ss.append(sv.substr(0, j));
+      ss.append("666");
+      ss.append(sv.substr(j));
+      table[i * 5 + j] = stoi(ss);
+    }
+  }
+  sort(table, table + 50000);
+  unique(table, table + 50000);
+
   int n;
   cin >> n;
-  int v = 666;
-  while (n) {
-    if (Check(v++)) --n;
-  }
-  cout << --v;
+  cout << table[n - 1];
 
   return 0;
 }
