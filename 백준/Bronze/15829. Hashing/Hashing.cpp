89f14d78d4dd1291dbc5634221ea6b1778eb03c8
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-02-14 19:57:59 +0900

    [Bronze II] Title: Hashing, Time: 0 ms, Memory: 2024 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/15829. Hashing/Hashing.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+constexpr int64_t kMod = 1234567891;
+constexpr int64_t kBase = 31;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  string s;
+  cin >> n >> s;
+  int64_t hash = 0;
+  for (auto it = s.rbegin(); it != s.rend(); ++it) {
+    hash = ((hash * kBase) + *it - 'a' + 1) % kMod;
+  }
+  cout << hash;
+
+  return 0;
+}
