a4c8148ecf7deb09a9fd9e37aaded13a556c66a8
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-02-14 19:13:11 +0900

    [Bronze III] Title: 웰컴 키트, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/30802. 웰컴 키트/웰컴 키트.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  cin >> n;
+  int nums[6];
+  for (int i = 0; i < 6; i++) cin >> nums[i];
+  int t, p;
+  cin >> t >> p;
+
+  int ans_t = 0;
+  for (int i = 0; i < 6; i++) {
+    auto dv = div(nums[i], t);
+    ans_t += dv.quot + (dv.rem != 0);
+  }
+  auto ans_p = div(n, p);
+
+  cout << ans_t << "\n"
+       << ans_p.quot << " " << ans_p.rem;
+
+  return 0;
+}
