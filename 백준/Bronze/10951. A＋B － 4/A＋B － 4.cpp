ec7f4e3ab84c202b752a96d2cc46a50952702c9c
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-02-14 17:31:55 +0900

    [Bronze V] Title: A+B - 4, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  for (;;) {
+    int a, b;
+    cin >> a >> b;
+    if (cin.fail()) break;
+    cout << a + b << "\n";
+  }
+
+  return 0;
+}
