3b53a65967093e1fb539800feaf972fc93bf1daf
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-02-14 19:33:30 +0900

    [Bronze II] Title: 분해합, Time: 0 ms, Memory: 2116 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.cpp
+++ 백준/Bronze/2231. 분해합/분해합.cpp
@@ -2,27 +2,6 @@
 
 using namespace std;
 
-template <int N>
-  requires(N >= 1)
-struct PrimeTable {
-  constexpr PrimeTable() {
-    fill(table, table + N + 1, true);
-    table[0] = table[1] = false;
-    for (int i = 2; i <= 32; i++) {
-      if (!table[i]) continue;
-      for (int j = (i << 1); j <= N; j += i) table[j] = false;
-    }
-  }
-
-  constexpr bool operator()(int n) const {
-    return table[n];
-  }
-
-  bool table[N + 1];
-};
-
-constexpr PrimeTable<1000> kPrime;
-
 int main() {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
