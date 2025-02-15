996ee6ba19a7c1e0488300cfb822d42dc11972ed
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-02-15 15:55:32 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 228 ms, Memory: 13744 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.cpp
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.cpp
@@ -2,34 +2,42 @@
 
 using namespace std;
 
+int nums[500000];
+int ans[500000];
+pair<int, int*> queries[500000];
+
 int main() {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
 
   int n;
   cin >> n;
-  map<int, int> mp;
-  while (n--) {
-    int v;
-    cin >> v;
-    if (auto it = mp.find(v); it != mp.end()) {
-      ++it->second;
-    } else {
-      mp.insert({v, 1});
-    }
-  }
+  for (int i = 0; i < n; i++) cin >> nums[i];
+  sort(nums, nums + n);
 
   int m;
   cin >> m;
-  while (m--) {
+  for (int i = 0; i < m; i++) {
     int v;
     cin >> v;
-    if (auto it = mp.find(v); it != mp.end()) {
-      cout << it->second << " ";
-    } else {
-      cout << "0 ";
-    }
+    queries[i] = {v, ans + i};
   }
+  sort(queries, queries + m, [](const pair<int, int*>& lhs, const pair<int, int*>& rhs) {
+    return lhs.first < rhs.first;
+  });
+
+  auto it = nums;
+  for (int i = 0; i < m; i++) {
+    auto b = lower_bound(it, nums + n, queries[i].first);
+    auto e = lower_bound(b, nums + n, queries[i].first + 1);
+    *queries[i].second = e - b;
+    while (i + 1 != m && queries[i].first == queries[i + 1].first) {
+      *queries[++i].second = e - b;
+    }
+    it = e;
+  }
+
+  for (int i = 0; i < m; i++) cout << ans[i] << " ";
 
   return 0;
 }
