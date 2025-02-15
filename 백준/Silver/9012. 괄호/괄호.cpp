24a7deb5bdb1033020e51f35fe2a3115368afd55
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-02-15 15:13:17 +0900

    [Silver IV] Title: 괄호, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/9012. 괄호/괄호.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  cin >> n;
+  cin.ignore(numeric_limits<streamsize>::max(), '\n');
+  while (n--) {
+    string s;
+    getline(cin, s);
+
+    bool ok = true;
+    stack<char> st;
+    for (char c : s) {
+      if (c == '(') {
+        st.push(c);
+      } else if (c == ')') {
+        if (st.empty()) {
+          ok = false;
+          break;
+        }
+        st.pop();
+      }
+    }
+
+    cout << (ok && st.empty() ? "YES\n" : "NO\n");
+  }
+
+  return 0;
+}
