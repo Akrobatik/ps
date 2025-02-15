a8dbbf9865764e8f21d4b2e543d5adc7a8de3220
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-02-15 15:21:18 +0900

    [Silver IV] Title: 제로, Time: 8 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/10773. 제로/제로.cpp
+++ 백준/Silver/10773. 제로/제로.cpp
@@ -2,28 +2,28 @@
 
 using namespace std;
 
+int nums[100000];
+
 int main() {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
 
   int n;
   cin >> n;
-  stack<int> st;
+  auto e = nums;
   while (n--) {
     int v;
     cin >> v;
     if (v) {
-      st.push(v);
+      *e++ = v;
     } else {
-      st.pop();
+      --e;
     }
   }
 
   int sum = 0;
-  while (!st.empty()) {
-    sum += st.top();
-    st.pop();
-  }
+  auto it = nums;
+  while (it != e) sum += *it++;
   cout << sum;
 
   return 0;
