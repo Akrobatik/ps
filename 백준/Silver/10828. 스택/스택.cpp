b32e440d166b0e30f1e56106d7c57a279f6519a6
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-02-15 16:03:44 +0900

    [Silver IV] Title: 스택, Time: 0 ms, Memory: 2060 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+char op[8];
+int st[10000];
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  cin >> n;
+  auto ptr = st;
+  while (n--) {
+    cin >> op;
+    switch (*(uint16_t*)op) {
+      case 'p' | ('u' << 8): {
+        cin >> *ptr++;
+      } break;
+      case 'p' | ('o' << 8): {
+        cout << (ptr != st ? *--ptr : -1) << "\n";
+      } break;
+      case 's' | ('i' << 8): {
+        cout << ptr - st << "\n";
+      } break;
+      case 'e' | ('m' << 8): {
+        cout << (char)('0' + (ptr == st)) << "\n";
+      } break;
+      case 't' | ('o' << 8): {
+        cout << (ptr != st ? *(ptr - 1) : -1) << "\n";
+      } break;
+    }
+  }
+
+  return 0;
+}
