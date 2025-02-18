95f1a4e602c01b81281900bd593e196666c52d2d
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-02-19 08:37:32 +0900

    [Silver II] Title: 랭킹전 대기열, Time: 0 ms, Memory: 2312 KB -BaekjoonHub

--- 백준/Silver/20006. 랭킹전 대기열/랭킹전 대기열.cpp
+++ 백준/Silver/20006. 랭킹전 대기열/랭킹전 대기열.cpp
@@ -3,9 +3,13 @@
 using namespace std;
 
 struct User {
+  User(int _lv) : lv(_lv) {}
   int lv;
   char name[17];
 };
+
+pair<int, vector<User>> rooms[500];
+auto rse = rooms;
 
 int main() {
   ios::sync_with_stdio(false);
@@ -13,31 +17,29 @@ int main() {
 
   int p, m;
   cin >> p >> m;
-
-  vector<pair<int, vector<User>>> vec;
   while (p--) {
-    User user;
-    cin >> user.lv >> user.name;
+    int lv;
+    cin >> lv;
     bool found = false;
-    for (int i = 0; i < vec.size(); i++) {
-      auto& [lv, room] = vec[i];
-      if (room.size() == m) continue;
-      if (abs(lv - user.lv) <= 10) {
-        found = true;
-        room.emplace_back(user);
-        break;
-      }
+    for (auto it = rooms; it != rse; ++it) {
+      if (it->second.size() == m || abs(lv - it->first) > 10) continue;
+      auto& room = it->second;
+      auto& user = room.emplace_back(lv);
+      cin >> user.name;
+      found = true;
+      break;
     }
-    if (!found) {
-      vec.push_back({user.lv, vector<User>()});
-      auto& room = vec.back();
-      room.second.reserve(m);
-      room.second.emplace_back(user);
-    }
+    if (found) continue;
+
+    rse->first = lv;
+    rse->second.reserve(m);
+    auto& user = rse->second.emplace_back(lv);
+    cin >> user.name;
+    ++rse;
   }
 
-  for (auto& v : vec) {
-    auto& room = v.second;
+  for (auto it = rooms; it != rse; ++it) {
+    auto& room = it->second;
     sort(room.begin(), room.end(), [](const User& lhs, const User& rhs) {
       return strcmp(lhs.name, rhs.name) < 0;
     });
