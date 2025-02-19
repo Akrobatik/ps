701a3aa913c0cf32822f3ab19d959b6fe2c602dc
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-02-19 09:22:28 +0900

    [Silver IV] Title: 팬케익 먹기, Time: 0 ms, Memory: 2020 KB, Score: 3 point -BaekjoonHub

---
+++ 백준/Silver/2545. 팬케익 먹기/팬케익 먹기.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int t;
+  cin >> t;
+  while (t--) {
+    int64_t a, b, c, d;
+    cin >> a >> b >> c >> d;
+    if (a > b) swap(a, b);
+    if (b > c) swap(b, c);
+    if (a > b) swap(a, b);
+    int64_t sum = a + b + c - d;
+    a = min<int64_t>(a, sum / 3ll);
+    sum -= a;
+    b = min<int64_t>(b, sum / 2ll);
+    c = sum - b;
+    int64_t ans = a * b * c;
+    cout << ans << "\n";
+  }
+
+  return 0;
+}
