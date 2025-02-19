bbaa8a699a1c1755d96a4d645ef96d04a3090a75
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-02-20 01:09:20 +0900

    [Silver II] Title: 하이퍼 가지 따기, Time: 4 ms, Memory: 2108 KB -BaekjoonHub

---
+++ 백준/Silver/27941. 하이퍼 가지 따기/하이퍼 가지 따기.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int pts[2047][11];
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  auto ptr = (int*)pts;
+  for (int i = 0; i < 2047 * 11; i++) cin >> *ptr++;
+
+  int cnt[11] = {}, pt1[11], pt2[11];
+  for (int i = 0; i < 11; i++) pt1[i] = pts[0][i];
+  for (int i = 0; i < 11; i++) {
+    for (int j = 1; j < 2047; j++) {
+      if (pt1[i] == pts[j][i]) continue;
+      pt2[i] = pts[j][i];
+      break;
+    }
+  }
+
+  for (int i = 0; i < 2047; i++) {
+    for (int j = 0; j < 11; j++) {
+      if (pts[i][j] == pt1[j]) ++cnt[j];
+    }
+  }
+
+  for (int i = 0; i < 11; i++) {
+    cout << (!(cnt[i] >> 10) ? pt1[i] : pt2[i]) << " ";
+  }
+
+  return 0;
+}
