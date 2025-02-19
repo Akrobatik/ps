8d856c2bb079d2490f9d5838092ef35f1bd0dd79
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-02-19 10:11:34 +0900

    [Silver III] Title: 점호, Time: 8 ms, Memory: 2680 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/31747. 점호/점호.cpp
+++ 백준/Silver/31747. 점호/점호.cpp
@@ -3,39 +3,28 @@
 using namespace std;
 
 int main() {
-  ios::sync_with_stdio(false);
-  cin.tie(nullptr);
-
-  priority_queue<int, vector<int>, greater<>> pq1, pq2;
   int n, k;
   cin >> n >> k;
+  int h1[100000], h2[100000];
+  auto hit1 = h1, he1 = h1;
+  auto hit2 = h2, he2 = h2;
   for (int i = 0; i < n; i++) {
     char c;
     cin >> c;
     if (c == '1') {
-      pq1.push(i);
+      *he1++ = i;
     } else {
-      pq2.push(i);
+      *he2++ = i;
     }
   }
 
   int out = 0;
   int cnt = 0;
   while (out != n) {
-    int c = 0;
-    if (!pq1.empty()) {
-      if (pq1.top() - out < k) {
-        pq1.pop();
-        ++c;
-      }
-    }
-    if (!pq2.empty()) {
-      if (pq2.top() - out < k) {
-        pq2.pop();
-        ++c;
-      }
-    }
-    out += c;
+    int o = 0;
+    if (hit1 != he1 && *hit1 - out < k) ++hit1, ++o;
+    if (hit2 != he2 && *hit2 - out < k) ++hit2, ++o;
+    out += o;
     ++cnt;
   }
   cout << cnt;
