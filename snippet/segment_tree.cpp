abeb4aeb5de04c890f2f411d0a3f1cd41a5f48ef
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-02-21 06:37:27 +0900

    Create segment_tree.cpp

---
+++ snippet/segment_tree.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct SegmentTree {
+  SegmentTree(int n) : nmax(GetMaxN(n)), tree(nmax, 0) {}
+
+  void Update(int idx, int value) {
+    int node = idx + nmax;
+    int delta = value - tree[node];
+    tree[node] = value;
+    while ((node >>= 1)) tree[node] += delta;
+  }
+
+  int Query(int l, int r) {
+    int res = 0;
+    for (l += nmax, r += nmax + 1; l < r; l >>= 1, r >>= 1) {
+      if (l & 1) res += tree[l++];
+      if (r & 1) res += tree[--r];
+    }
+    return res;
+  }
+
+  int GetValue(int idx) {
+    return tree[idx + nmax];
+  }
+
+  static int GetMaxN(int n) {
+    if (has_single_bit((uint32_t)n)) return n;
+    return 1 << (32 - countl_zero((uint32_t)n));
+  }
+
+  int nmax;
+  vector<int> tree;
+};
