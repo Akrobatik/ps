f543023ec60b8b002ea48ba0d53077ef4882e401
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-02-21 07:59:33 +0900

    [Gold I] Title: 구간 합 구하기, Time: 200 ms, Memory: 17652 KB -BaekjoonHub

--- 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.cpp
+++ 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.cpp
@@ -2,56 +2,32 @@
 
 using namespace std;
 
-vector<vector<int64_t>> MakeTree(const vector<int64_t>& vec) {
-  int n = vec.size();
-  int l = (int)log2(n) + 1;
-  vector<vector<int64_t>> tree(l);
-  tree[0].resize(n);
-  copy(vec.begin(), vec.end(), tree[0].begin());
-  for (int i = 1; i < l; i++) {
-    n >>= 1;
-    tree[i].resize(n);
-    for (int j = 0; j < n; j++) {
-      tree[i][j] = tree[i - 1][j << 1] + tree[i - 1][(j << 1) + 1];
-    }
+struct FenwickTree {
+  using T = int64_t;
+  FenwickTree(int n) : tree(n + 1) {}
+
+  // Inverse element required
+  void Update(int idx, T delta) {
+    int n = tree.size();
+    while (idx < n) tree[idx] += delta, idx += (idx & -idx);
   }
 
-  return tree;
-}
-
-void UpdateTree(vector<vector<int64_t>>& tree, int pos, int64_t v) {
-  int64_t diff = v - tree[0][pos];
-  for (auto& t : tree) {
-    if (pos >= t.size()) break;
-    t[pos] += diff;
-    pos >>= 1;
+  T Query(int idx) {
+    T res = 0;
+    while (idx) res += tree[idx], idx -= (idx & -idx);
+    return res;
   }
-}
 
-int64_t Pick(vector<vector<int64_t>>& tree, int a, int b) {
-  int d = log2(b - a + 1);
-  return tree[d][a >> d];
-}
-
-vector<pair<int, int>> Split(int n, int m) {
-  vector<pair<int, int>> result;
-
-  int cur = n;
-  while (cur <= m) {
-    int step = 1;
-    while (cur % (step << 1) == 0 && cur + (step << 1) - 1 <= m) step <<= 1;
-    result.emplace_back(cur, min(cur + step - 1, m));
-    cur += step;
+  T Query(int l, int r) {
+    T res = 0;
+    --l;
+    while (l) res -= tree[l], l -= (l & -l);
+    while (r) res += tree[r], r -= (r & -r);
+    return res;
   }
-  return result;
-}
 
-int64_t Sum(vector<vector<int64_t>>& tree, int a, int b) {
-  int64_t sum = 0;
-  auto splits = Split(a, b);
-  for (auto [aa, bb] : splits) sum += Pick(tree, aa, bb);
-  return sum;
-}
+  vector<T> tree;
+};
 
 int main() {
   ios::sync_with_stdio(false);
@@ -59,19 +35,22 @@ int main() {
 
   int n, m, k;
   cin >> n >> m >> k;
-  vector<int64_t> vec(n);
-  for (int i = 0; i < n; i++) cin >> vec[i];
-  auto tree = MakeTree(vec);
+  FenwickTree tree(n);
+  vector<int64_t> arr(n + 1);
+  for (int i = 1; i <= n; i++) cin >> arr[i];
+  for (int i = 1; i <= n; i++) tree.Update(i, arr[i]);
 
-  int nn = m + k;
-  for (int i = 0; i < nn; i++) {
-    int a, b;
-    int64_t c;
+  int q = m + k;
+  while (q--) {
+    char a;
+    int64_t b, c;
     cin >> a >> b >> c;
-    if (a == 1) {
-      UpdateTree(tree, b - 1, c);
-    } else {
-      cout << Sum(tree, b - 1, c - 1) << "\n";
+    if (a & 1) {  // '1'
+      int64_t delta = c - arr[b];
+      arr[b] = c;
+      tree.Update(b, delta);
+    } else {  // '2'
+      cout << tree.Query(b, c) << "\n";
     }
   }
 
