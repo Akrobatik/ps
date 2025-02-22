487b41fb15d02a035dbeb51df4a59144a44d56c7
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-02-23 02:53:08 +0900

    [Silver III] Title: 계단 오르기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int _memo[302][2];
+  auto memo = _memo + 1;
+  memset(_memo, 0, sizeof(_memo));
+
+  int n;
+  cin >> n;
+  for (int i = 1; i <= n; i++) {
+    int v;
+    cin >> v;
+    memo[i][0] = max<int>(memo[i - 2][0], memo[i - 2][1]) + v;
+    memo[i][1] = memo[i - 1][0] + v;
+  }
+  cout << max<int>(memo[n][0], memo[n][1]);
+
+  return 0;
+}
