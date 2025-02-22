7cd2b47df9b49bb736903e9df0e315ccd420fcbc
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-02-23 07:50:55 +0900

    [Silver II] Title: 헌내기는 친구가 필요해, Time: 12 ms, Memory: 3340 KB -BaekjoonHub

---
+++ 백준/Silver/21736. 헌내기는 친구가 필요해/헌내기는 친구가 필요해.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+constexpr pair<int, int> kDelta[] = {
+    {-1, 0},
+    {1, 0},
+    {0, -1},
+    {0, 1}};
+
+bool walls[600][600];
+bool mans[600][600];
+bool visited[600][600];
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n, m;
+  cin >> n >> m;
+  int cy, cx;
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < m; j++) {
+      char c;
+      cin >> c;
+      walls[i][j] = (c == 'X');
+      mans[i][j] = (c == 'P');
+      if (c == 'I') cy = i, cx = j;
+    }
+  }
+
+  int ans = 0;
+  queue<pair<int, int>> q;
+  visited[cy][cx] = true;
+  q.push({cy, cx});
+  while (!q.empty()) {
+    auto [y, x] = q.front();
+    q.pop();
+
+    ans += mans[y][x];
+    for (auto [dy, dx] : kDelta) {
+      int yy = y + dy, xx = x + dx;
+      if (0 <= yy && yy < n && 0 <= xx && xx < m && !walls[yy][xx] && !visited[yy][xx]) {
+        visited[yy][xx] = true;
+        q.push({yy, xx});
+      }
+    }
+  }
+
+  if (ans) {
+    cout << ans;
+  } else {
+    cout << "TT";
+  }
+
+  return 0;
+}
