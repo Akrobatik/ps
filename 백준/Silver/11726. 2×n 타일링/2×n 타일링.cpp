7cd2363967f6d7d4f39b6ea96606d83f82cb3bf5
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-02-23 03:47:25 +0900

    [Silver III] Title: 2×n 타일링, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.cpp
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.cpp
@@ -2,20 +2,37 @@
 
 using namespace std;
 
+constexpr int kMod = 10007;
+
+struct Matrix {
+  friend Matrix operator*(const Matrix& m1, const Matrix& m2) {
+    Matrix m;
+    m.m[0][0] = (m1.m[0][0] * m2.m[0][0] + m1.m[0][1] * m2.m[1][0]) % kMod;
+    m.m[0][1] = (m1.m[0][0] * m2.m[0][1] + m1.m[0][1] * m2.m[1][1]) % kMod;
+    m.m[1][0] = (m1.m[1][0] * m2.m[0][0] + m1.m[1][1] * m2.m[1][0]) % kMod;
+    m.m[1][1] = (m1.m[1][0] * m2.m[0][1] + m1.m[1][1] * m2.m[1][1]) % kMod;
+    return m;
+  }
+
+  int m[2][2];
+};
+
+Matrix Exp(int n, const Matrix& m) {
+  if (n == 1) return m;
+  auto exp = Exp(n / 2, m);
+  auto mp = exp * exp;
+  if (n & 1) mp = mp * m;
+  return mp;
+}
+
 int main() {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
 
-  int memo[1001];
-  memo[1] = 1, memo[2] = 2;
-
   int n;
   cin >> n;
-  for (int i = 3; i <= n; i++) {
-    memo[i] = memo[i - 1] + memo[i - 2];
-    if (memo[i] >= 10007) memo[i] -= 10007;
-  }
-  cout << memo[n];
+  auto m = Exp(n + 1, {{{1, 1}, {1, 0}}});
+  cout << m.m[0][1] << endl;
 
   return 0;
 }
