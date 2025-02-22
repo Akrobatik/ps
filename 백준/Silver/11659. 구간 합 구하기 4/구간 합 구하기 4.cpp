863bac342f1a3f65c4aed9316c74f78472323e49
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-02-23 03:34:42 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 36 ms, Memory: 2288 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.cpp
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.cpp
@@ -2,52 +2,25 @@
 
 using namespace std;
 
-struct FenwickTree {
-  void Init(int n) {
-    tree.clear(), tree.resize(n);
-  }
-
-  // Inverse element required
-  void Update(int idx, int delta) {
-    int n = tree.size();
-    while (idx < n) tree[idx] += delta, idx += (idx & -idx);
-  }
-
-  int Query(int idx) {
-    int res = 0;
-    while (idx) res += tree[idx], idx -= (idx & -idx);
-    return res;
-  }
-
-  int Query(int l, int r) {
-    int res = 0;
-    --l;
-    while (l) res -= tree[l], l -= (l & -l);
-    while (r) res += tree[r], r -= (r & -r);
-    return res;
-  }
-
-  vector<int> tree;
-};
-
 int main() {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
 
+  int psum[100001];
+  psum[0] = 0;
+
   int n, m;
   cin >> n >> m;
-  FenwickTree tree;
-  tree.Init(n + 1);
   for (int i = 1; i <= n; i++) {
     int v;
     cin >> v;
-    tree.Update(i, v);
+    psum[i] = psum[i - 1] + v;
   }
 
   while (m--) {
     int i, j;
     cin >> i >> j;
-    cout << tree.Query(i, j) << "\n";
+    cout << psum[j] - psum[i - 1] << "\n";
   }
 
   return 0;
