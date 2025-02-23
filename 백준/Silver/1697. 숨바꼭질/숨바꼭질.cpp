014eedf794d0ff3af65e16b84b07c5a0e0502d1f
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-02-23 21:18:21 +0900

    [Silver I] Title: 숨바꼭질, Time: 0 ms, Memory: 2480 KB -BaekjoonHub

---
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+bool memo[200001];
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n, k;
+  cin >> n >> k;
+  queue<int> q;
+  memo[n] = true;
+  q.push(n);
+  int i;
+  for (i = 0; !memo[k]; i++) {
+    int nq = q.size();
+    while (nq--) {
+      int cur = q.front();
+      q.pop();
+
+      int arr[] = {cur - 1, cur + 1, cur * 2};
+      for (auto e : arr) {
+        if (0 <= e && e <= 200000 && !memo[e]) {
+          memo[e] = true;
+          q.push(e);
+        }
+      }
+    }
+  }
+  cout << i;
+
+  return 0;
+}
