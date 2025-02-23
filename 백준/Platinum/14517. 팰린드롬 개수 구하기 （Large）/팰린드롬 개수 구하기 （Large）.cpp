a7a99ed49e2b5ff5ac55a49478c2dc6bd9805c32
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-02-24 06:07:40 +0900

    [Platinum V] Title: 팰린드롬 개수 구하기 (Large), Time: 32 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Platinum/14517. 팰린드롬 개수 구하기 （Large）/팰린드롬 개수 구하기 （Large）.cpp
+++ 백준/Platinum/14517. 팰린드롬 개수 구하기 （Large）/팰린드롬 개수 구하기 （Large）.cpp
@@ -4,30 +4,65 @@ using namespace std;
 
 constexpr int kMod = 10007;
 
-int memo[1000];
+struct SegmentTree {
+  void Init(int n) {
+    nmax = has_single_bit((uint32_t)n) ? n : (1 << (32 - countl_zero((uint32_t)n)));
+    tree.clear(), tree.resize(nmax << 1);
+  }
+
+  void Update(int idx, int delta) {
+    int node = idx + nmax;
+    tree[node] += delta;
+    while ((node >>= 1)) {
+      tree[node] += delta;
+      if (tree[node] >= kMod) tree[node] -= kMod;
+    }
+  }
+
+  int Query(int l, int r) {
+    int res = 0;
+    for (l += nmax, r += nmax + 1; l < r; l >>= 1, r >>= 1) {
+      if (l & 1) res += tree[l++];
+      if (r & 1) res += tree[--r];
+    }
+    return res % kMod;
+  }
+
+  static int GetMaxN(int n) {
+    if (has_single_bit((uint32_t)n)) return n;
+    return 1 << (32 - countl_zero((uint32_t)n));
+  }
+
+  int nmax;
+  vector<int> tree;
+};
 
 int main() {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
 
+  SegmentTree tree;
+
   string s;
   cin >> s;
   int n = s.size();
-  fill_n(memo, n, 1);
+  tree.Init(n);
+  for (int i = 0; i < n; i++) tree.Update(i, 1);
+
   for (int i = 1; i < n; i++) {
     for (int j = 0; j < i; j++) {
       if (s[i] == s[j]) {
-        ++memo[j];
-        for (int k = j + 1; k < i; k++) {
-          memo[j] = (memo[j] + memo[k]) % kMod;
+        int delta = 1;
+        if (j + 1 != i) {
+          delta += tree.Query(j + 1, i - 1);
+          if (delta == kMod) delta = 0;
         }
+        tree.Update(j, delta);
       }
     }
   }
 
-  int ans = 0;
-  for (int i = 0; i < n; i++) ans += memo[i];
-  cout << ans % kMod;
+  cout << tree.tree[1];
 
   return 0;
 }
