220e478ed4d91ee4ba67bf202ba98468ba534ec0
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-02-24 08:45:24 +0900

    [Platinum V] Title: 팰린드롬 개수 구하기 (Large), Time: 12 ms, Memory: 5932 KB -BaekjoonHub

--- 백준/Platinum/14517. 팰린드롬 개수 구하기 （Large）/팰린드롬 개수 구하기 （Large）.cpp
+++ 백준/Platinum/14517. 팰린드롬 개수 구하기 （Large）/팰린드롬 개수 구하기 （Large）.cpp
@@ -4,67 +4,29 @@ using namespace std;
 
 constexpr int kMod = 10007;
 
-struct SegmentTree {
-  void Init(int n) {
-    nmax = has_single_bit((uint32_t)n) ? n : (1 << (32 - countl_zero((uint32_t)n)));
-    tree.clear(), tree.resize(nmax << 1);
-  }
+string s;
+int memo[1000][1000];
 
-  void Update(int idx, int delta) {
-    int node = idx + nmax;
-    tree[node] += delta;
-    while ((node >>= 1)) {
-      tree[node] += delta;
-      if (tree[node] >= kMod) tree[node] -= kMod;
-    }
-  }
+int Traverse(int b, int e) {
+  if (b > e) return 0;
+  if (b == e) return 1;
+  if (memo[b][e]) return memo[b][e];
 
-  int Query(int l, int r) {
-    int res = 0;
-    for (l += nmax, r += nmax + 1; l < r; l >>= 1, r >>= 1) {
-      if (l & 1) res += tree[l++];
-      if (r & 1) res += tree[--r];
-    }
-    return res % kMod;
+  memo[b][e] = (Traverse(b + 1, e) + Traverse(b, e - 1)) % kMod;
+  if (s[b] == s[e]) {
+    if (++memo[b][e] == kMod) memo[b][e] = 0;
+  } else {
+    memo[b][e] = (memo[b][e] - Traverse(b + 1, e - 1) + kMod) % kMod;
   }
-
-  static int GetMaxN(int n) {
-    if (has_single_bit((uint32_t)n)) return n;
-    return 1 << (32 - countl_zero((uint32_t)n));
-  }
-
-  int nmax;
-  vector<int> tree;
-};
+  return memo[b][e];
+}
 
 int main() {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
 
-  SegmentTree tree;
-
-  string s;
   cin >> s;
-  int n = s.size();
-  tree.Init(n);
-
-  vector<int> memo[26];
-
-  for (int i = 0; i < n; i++) {
-    tree.Update(i, 1);
-    int v = s[i] - 'a';
-    for (auto j : memo[v]) {
-      int delta = 1;
-      if (j + 1 != i) {
-        delta += tree.Query(j + 1, i - 1);
-        if (delta == kMod) delta = 0;
-      }
-      tree.Update(j, delta);
-    }
-    memo[v].push_back(i);
-  }
-
-  cout << tree.tree[1];
+  cout << Traverse(0, s.size() - 1);
 
   return 0;
 }
