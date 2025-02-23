5045e8040e438d4ac3d64c8a02014e49e37127ef
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-02-24 06:40:20 +0900

    Create combination.cpp

---
+++ snippet/combination.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+template <int MAXN, int64_t MOD>
+  requires(MAXN >= 0, MOD >= 3)
+struct Combination {
+  constexpr Combination() {
+    fact[0] = 1;
+    for (int i = 1; i <= MAXN; i++) fact[i] = (fact[i - 1] * i) % MOD;
+    inv[MAXN] = Power(fact[MAXN], MOD - 2);
+    for (int i = MAXN; i > 0; i--) inv[i - 1] = (inv[i] * i) % MOD;
+  }
+
+  int64_t operator()(int n, int r) const {
+    return (((fact[n] * inv[r]) % MOD) * inv[n - r]) % MOD;
+  }
+
+  static constexpr int64_t Power(int64_t n, int exp) {
+    if (exp == 1) return n;
+    int64_t nn = Power(n, exp >> 1);
+    nn = (nn * nn) % MOD;
+    if (exp & 1) nn = (nn * n) % MOD;
+    return nn;
+  }
+
+  int64_t fact[MAXN + 1], inv[MAXN + 1];
+};
