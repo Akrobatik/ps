7f8d1a6e6d604ad6a15463dd65a7fcbdcbc59cd0
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-02-25 23:00:01 +0900

    [Silver I] Title: 행렬, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/1080. 행렬/행렬.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+char m1[50][50], m2[50][50];
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n, m;
+  cin >> n >> m;
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < m; j++) cin >> m1[i][j];
+  }
+
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < m; j++) cin >> m2[i][j];
+  }
+
+  int cnt = 0;
+  for (int i = 0; i < n - 2; i++) {
+    for (int j = 0; j < m - 2; j++) {
+      if (m1[i][j] != m2[i][j]) {
+        ++cnt;
+        for (int k = 0; k < 3; k++) {
+          for (int l = 0; l < 3; l++) {
+            m1[i + k][j + l] ^= 1;
+          }
+        }
+      }
+    }
+  }
+
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < m; j++) {
+      if (m1[i][j] != m2[i][j]) {
+        cout << "-1";
+        return 0;
+      }
+    }
+  }
+  cout << cnt;
+
+  return 0;
+}
