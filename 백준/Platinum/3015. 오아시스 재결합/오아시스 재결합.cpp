a65bb34d65e3b26fbbe85a8f8d0bb3f44349bc40
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-02-25 11:16:06 +0900

    [Platinum V] Title: 오아시스 재결합, Time: 80 ms, Memory: 3976 KB -BaekjoonHub

--- 백준/Platinum/3015. 오아시스 재결합/오아시스 재결합.cpp
+++ 백준/Platinum/3015. 오아시스 재결합/오아시스 재결합.cpp
@@ -16,7 +16,7 @@ int main() {
     cin >> now;
     auto it = lower_bound(stk.begin(), stk.end(), now, greater<>());
     sum += (stk.end() - it) + (it != stk.begin());
-    stk.resize(upper_bound(it, stk.end(), now, greater<>()) - stk.begin());
+    while (!stk.empty() && stk.back() < now) stk.pop_back();
     stk.push_back(now);
   }
   cout << sum;
