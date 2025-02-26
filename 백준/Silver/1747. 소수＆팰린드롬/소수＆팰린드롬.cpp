54ebf1e6928127355541fb4062583a351c1565e0
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-02-27 05:58:01 +0900

    [Silver I] Title: 소수&팰린드롬, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1747. 소수＆팰린드롬/소수＆팰린드롬.cpp
+++ 백준/Silver/1747. 소수＆팰린드롬/소수＆팰린드롬.cpp
@@ -2,32 +2,15 @@
 
 using namespace std;
 
-bool memo[1003002];
-
-bool IsPalindrome(int n) {
-  vector<int> digits;
-  while (n) digits.push_back(n % 10), n /= 10;
-  int size = digits.size();
-  for (int i = 0; i < (size >> 1); i++) {
-    if (digits[i] != digits[size - i - 1]) return false;
-  }
-  return true;
-}
+constexpr array<int, 114> kTable = {2, 3, 5, 7, 11, 101, 131, 151, 181, 191, 313, 353, 373, 383, 727, 757, 787, 797, 919, 929, 10301, 10501, 10601, 11311, 11411, 12421, 12721, 12821, 13331, 13831, 13931, 14341, 14741, 15451, 15551, 16061, 16361, 16561, 16661, 17471, 17971, 18181, 18481, 19391, 19891, 19991, 30103, 30203, 30403, 30703, 30803, 31013, 31513, 32323, 32423, 33533, 34543, 34843, 35053, 35153, 35353, 35753, 36263, 36563, 37273, 37573, 38083, 38183, 38783, 39293, 70207, 70507, 70607, 71317, 71917, 72227, 72727, 73037, 73237, 73637, 74047, 74747, 75557, 76367, 76667, 77377, 77477, 77977, 78487, 78787, 78887, 79397, 79697, 79997, 90709, 91019, 93139, 93239, 93739, 94049, 94349, 94649, 94849, 94949, 95959, 96269, 96469, 96769, 97379, 97579, 97879, 98389, 98689, 1003001};
 
 int main() {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
 
-  vector<int> vec;
-  for (int i = 2; i <= 1003001; i++) {
-    if (memo[i]) continue;
-    for (int j = (i << 1); j <= 1003001; j += i) memo[j] = true;
-    if (IsPalindrome(i)) vec.push_back(i);
-  }
-
   int n;
   cin >> n;
-  cout << *lower_bound(vec.begin(), vec.end(), n);
+  cout << *lower_bound(kTable.begin(), kTable.end(), n);
 
   return 0;
 }
