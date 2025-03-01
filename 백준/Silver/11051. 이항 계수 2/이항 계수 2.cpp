71a5eac1a680ab04c71f55c226f0798a86bfd510
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-03-01 18:32:34 +0900

    [Silver II] Title: 이항 계수 2, Time: 0 ms, Memory: 2036 KB -BaekjoonHub

---
+++ 백준/Silver/11051. 이항 계수 2/이항 계수 2.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+template <int MAXN, int64_t MOD>
+  requires(MAXN >= 0, MOD >= 3)
+struct Combination {
+  constexpr Combination() {
+    fact[0] = 1;
+    for (int i = 1; i <= MAXN; i++) fact[i] = (fact[i - 1] * i) % MOD;
+    inv[MAXN] = Power(fact[MAXN], MOD - 2);
+    for (int i = MAXN; i > 0; i--) inv[i - 1] = (inv[i] * i) % MOD;
+  }
+
+  int64_t operator()(int n, int r) const {
+    return (((fact[n] * inv[r]) % MOD) * inv[n - r]) % MOD;
+  }
+
+  static constexpr int64_t Power(int64_t n, int exp) {
+    int64_t res = 1;
+    while (exp) {
+      if (exp & 1) res = (res * n) % MOD;
+      n = (n * n) % MOD;
+      exp >>= 1;
+    }
+    return res;
+  }
+
+  int64_t fact[MAXN + 1], inv[MAXN + 1];
+};
+
+constexpr Combination<1000, 10007> kComb;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n, k;
+  cin >> n >> k;
+  cout << kComb(n, k);
+
+  return 0;
+}
