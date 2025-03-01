d09db2de891b21157137cddd6af61a9a8e226d7a
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-03-02 06:43:19 +0900

    [Silver I] Title: 개미, Time: 28 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/4307. 개미/개미.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int t;
+  cin >> t;
+  while (t--) {
+    int l, n;
+    cin >> l >> n;
+    int minn = 0, maxx = 0;
+    while (n--) {
+      int x;
+      cin >> x;
+      int xmax = max<int>(x, l - x);
+      int xmin = min<int>(x, l - x);
+      if (maxx < xmax) maxx = xmax;
+      if (minn < xmin) minn = xmin;
+    }
+    cout << minn << " " << maxx << "\n";
+  }
+
+  return 0;
+}
