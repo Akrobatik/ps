29d57865e9f6b9446ee9bec6976c255b5a1f96bd
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-03-01 18:52:44 +0900

    [Silver I] Title: 기타리스트, Time: 0 ms, Memory: 2068 KB -BaekjoonHub

--- 백준/Silver/1495. 기타리스트/기타리스트.cpp
+++ 백준/Silver/1495. 기타리스트/기타리스트.cpp
@@ -2,38 +2,33 @@
 
 using namespace std;
 
+int ans = -1;
+int n, s, m;
+bool memo[50][1001];
+int arr[50];
+
+void Traverse(int idx, int cur) {
+  if (idx == n) {
+    if (ans < cur) ans = cur;
+    return;
+  }
+
+  for (auto e : {cur - arr[idx], cur + arr[idx]}) {
+    if (0 <= e && e <= m && !memo[idx][e]) {
+      memo[idx][e] = true;
+      Traverse(idx + 1, e);
+    }
+  }
+}
+
 int main() {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
 
-  int n, s, m;
   cin >> n >> s >> m;
-  queue<int> q;
-  q.push(s);
-  for (int i = 0; i < n; i++) {
-    int v;
-    cin >> v;
-    set<int> st;
-    int nq = q.size();
-    while (nq--) {
-      auto x = q.front();
-      q.pop();
-      for (auto e : {x - v, x + v}) {
-        if (0 <= e && e <= m && !st.contains(e)) {
-          st.insert(e);
-          q.push(e);
-        }
-      }
-    }
-  }
-
-  int maxx = -1;
-  while (!q.empty()) {
-    auto x = q.front();
-    q.pop();
-    if (maxx < x) maxx = x;
-  }
-  cout << maxx;
+  for (int i = 0; i < n; i++) cin >> arr[i];
+  Traverse(0, s);
+  cout << ans;
 
   return 0;
 }
