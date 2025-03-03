579b6777f6e2dcd82cfe2014d5ae123686bb39de
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-03-03 13:12:24 +0900

    [Silver I] Title: 팩토리얼5, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1564. 팩토리얼5/팩토리얼5.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+constexpr int64_t kMod = 1e12;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  cin >> n;
+  int64_t ans = 1;
+  for (int i = 2; i <= n; i++) {
+    ans *= i;
+    while (ans % 10 == 0) ans /= 10;
+    ans %= kMod;
+  }
+  cout << setw(5) << setfill('0') << ans % 100000;
+
+  return 0;
+}
