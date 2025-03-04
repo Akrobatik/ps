ab8a45edb3b3e4058962d6a9ff93e85ee6353ab3
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-03-04 16:12:50 +0900

    [Platinum II] Title: Finite Array Swaps, Time: 864 ms, Memory: 94204 KB, Score: 100 point -BaekjoonHub

--- 백준/Platinum/33567. Finite Array Swaps/Finite Array Swaps.cpp
+++ 백준/Platinum/33567. Finite Array Swaps/Finite Array Swaps.cpp
@@ -2,43 +2,58 @@
 
 using namespace std;
 
-int cnt;
+int n, k, cnt;
 vector<int> a, b;
-map<int, set<int>> ma, mb;
-map<int64_t, set<int>> mab;
+unordered_map<int, unordered_set<int>> ma, mb;
+unordered_map<int64_t, unordered_set<int>> mab;
 vector<pair<int, int>> swaps;
+
+void Swap(int i, int j) {
+  int res = 0;
+  res -= (a[i] == b[i]) + (a[j] == b[j]);
+
+  ma[a[i]].erase(i), ma[a[j]].erase(j);
+  mb[b[i]].erase(i), mb[b[j]].erase(j);
+  mab[((int64_t)a[i] << 32) | b[i]].erase(i);
+  mab[((int64_t)a[j] << 32) | b[j]].erase(j);
+  swap(a[i], a[j]);
+  --k;
+  swaps.push_back({i, j});
+
+  for (auto e : {i, j}) {
+    if (a[e] == b[e]) continue;
+    ma[a[e]].insert(e);
+    mb[b[e]].insert(e);
+    mab[((int64_t)a[e] << 32) | b[e]].insert(e);
+  }
+
+  res += (a[i] == b[i]) + (a[j] == b[j]);
+  cnt += res;
+}
 
 bool Single() {
   for (auto& [x, is] : ma) {
-    if (!mb.contains(x)) continue;
-    int i = *is.begin();
-    int j = *mb[x].begin();
-    ma[a[i]].erase(i);
-    ma[a[j]].erase(j);
-    mb[b[i]].erase(i);
-    swap(a[i], a[j]);
-    swaps.push_back({i, j});
-    ma[a[j]].insert(j);
+    if (is.empty() || mb[x].empty()) continue;
+    Swap(*is.begin(), *mb[x].begin());
     return true;
   }
   return false;
 }
 
 bool Triple() {
+  set<int64_t> visited;
   for (auto& [x, is] : ma) {
-    if (!mb.contains(x)) continue;
-    for (int i : is) {
-      for (int j : mb[x]) {
-        if (!mb.contains(a[j])) continue;
-        for (int k : mb[a[j]]) {
-          if (a[k] == b[i]) {
-            swap(a[i], a[j]);
-            swap(a[j], a[k]);
-            swaps.push_back({i, j});
-            swaps.push_back({j, k});
-            return true;
-          }
-        }
+    if (is.empty()) continue;
+    int64_t ab = ((int64_t)x << 32);
+    for (int j : mb[x]) {
+      if (visited.contains(ab | a[j])) continue;
+      visited.insert(ab | a[j]);
+      for (int k : mb[a[j]]) {
+        if (mab[((int64_t)x << 32) | a[k]].empty()) continue;
+        int i = *mab[((int64_t)x << 32) | a[k]].begin();
+        Swap(i, k);
+        Swap(j, k);
+        return true;
       }
     }
   }
@@ -49,42 +64,27 @@ int main() {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
 
-  int n, k;
   cin >> n >> k;
-
   a.resize(n), b.resize(n);
   for (int i = 0; i < n; i++) cin >> a[i];
   for (int i = 0; i < n; i++) cin >> b[i];
 
   for (int i = 0; i < n; i++) {
-    if (a[i] == b[i]) {
-      ++cnt;
-    } else {
+    cnt += (a[i] == b[i]);
+    if (k && a[i] != b[i]) {
       int64_t ab = ((int64_t)a[i] << 32) | b[i];
       int64_t ba = ((int64_t)b[i] << 32) | a[i];
-      if (k && mab.contains(ba) && !mab[ba].empty()) {
-        int j = *mab[ba].begin();
-        --k;
-        cnt += 2;
-        ma[a[j]].erase(j);
-        mb[b[j]].erase(j);
-        mab[ba].erase(j);
-        swap(a[i], a[j]);
-        swaps.push_back({i, j});
-      } else {
-        ma[a[i]].insert(i);
-        mb[b[i]].insert(i);
-        mab[ab].insert(i);
+      ma[a[i]].insert(i);
+      mb[b[i]].insert(i);
+      mab[ab].insert(i);
+      if (!mab[ba].empty()) {
+        Swap(i, *mab[ba].begin());
       }
     }
   }
 
-  if (k == 2 && Triple()) {
-    k = 0;
-    cnt += 3;
-  }
-
-  while (k-- && Single()) ++cnt;
+  if (k == 2) Triple();
+  while (k && Single());
 
   cout << cnt << "\n";
   for (auto e : a) cout << e << " ";
