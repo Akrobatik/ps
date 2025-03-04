7dd6c2a5523aef66e6073ac4b64e3cfda83e2f62
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-03-04 22:41:32 +0900

    [Silver I] Title: 인형들, Time: 24 ms, Memory: 2040 KB -BaekjoonHub

---
+++ 백준/Silver/15954. 인형들/인형들.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int64_t arr[500], psum[501];
+
+long double Std(int i, int k) {
+  long double res = 0;
+  long double m = (long double)(psum[i + k] - psum[i]) / k;
+  for (int j = 0; j < k; j++) {
+    long double x = m - arr[i + j];
+    res += x * x;
+  }
+  return sqrtl(res / k);
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n, k;
+  cin >> n >> k;
+  for (int i = 0; i < n; i++) cin >> arr[i];
+  partial_sum(arr, arr + n, psum + 1);
+
+  long double minn = LDBL_MAX;
+  for (int i = 0; i < n; i++) {
+    for (int j = i + k - 1; j < n; j++) {
+      long double s = Std(i, j - i + 1);
+      if (minn > s) minn = s;
+    }
+  }
+  cout.precision(6);
+  cout << fixed << minn;
+
+  return 0;
+}
