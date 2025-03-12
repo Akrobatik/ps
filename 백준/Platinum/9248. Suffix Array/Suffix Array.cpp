02c0f1108b17d7b850204c36f145f070877f38c3
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-03-13 04:13:37 +0900

    [Platinum III] Title: Suffix Array, Time: 120 ms, Memory: 8976 KB -BaekjoonHub

--- 백준/Platinum/9248. Suffix Array/Suffix Array.cpp
+++ 백준/Platinum/9248. Suffix Array/Suffix Array.cpp
@@ -3,49 +3,162 @@
 using namespace std;
 
 struct LCP {
-  void Init(string_view sv) {
-    int n = sv.size();
-    sa.resize(n), lcp.resize(n);
-    vector<int> p(n), pn(n), cnts(max<int>(n, 256));
-
-    auto Sort = [&]() {
-      fill(cnts.begin(), cnts.end(), 0);
-      for (auto e : p) ++cnts[e];
-      exclusive_scan(cnts.begin(), cnts.end(), cnts.begin(), 0);
-      for (auto e : pn) sa[cnts[p[e]]++] = e;
-    };
-
-    // SA
-    for (int i = 0; i < n; i++) sa[i] = pn[i] = i, p[i] = sv[i];
-    Sort();
-    for (int k = 1; p[sa.back()] + 1 != n; k <<= 1) {
-      iota(pn.begin(), pn.begin() + k, n - k);
-      for (int i = 0, j = k; i < n; i++) {
-        if (sa[i] < k) continue;
-        pn[j++] = sa[i] - k;
-      }
-      Sort();
-      int o1, o2;
-      o1 = sa[0], pn[o1] = 0;
-      for (int i = 1; i < n; i++) {
-        o2 = o1, o1 = sa[i];
-        pn[o1] = pn[o2];
-        if (o1 + k < n && o2 + k < n && p[o1] == p[o2] && p[o1 + k] == p[o2 + k]) continue;
-        ++pn[o1];
-      }
-      swap(p, pn);
+ private:
+  struct Buckets : public vector<int> {
+    template <typename T>
+    Buckets(span<const T> sv) : vector<int>(*max_element(sv.begin(), sv.end()) + 1) {
+      for (auto e : sv) ++operator[](e);
     }
 
-    // LCP
+    vector<int> Incl() const {
+      vector<int> incl(size());
+      partial_sum(begin(), end(), incl.begin());
+      return incl;
+    }
+
+    vector<int> Excl() const {
+      vector<int> excl(size());
+      partial_sum(begin(), end() - 1, excl.begin() + 1);
+      excl[0] = 0;
+      return excl;
+    }
+  };
+
+  struct LTypes : public vector<bool> {
+    template <typename T>
+    LTypes(span<const T> sv) : vector<bool>(sv.size()) {
+      int n = sv.size();
+      for (int i = n - 2; i >= 0; i--) {
+        operator[](i) = sv[i] > sv[i + 1] || (sv[i] == sv[i + 1] && operator[](i + 1));
+      }
+    }
+  };
+
+ public:
+  LCP() : buf(nullptr) {}
+  ~LCP() {
+    if (buf) delete[] buf;
+  }
+
+  void Init(const string_view sv) {
+    int n = sv.size();
+    if (buf) delete[] buf;
+    buf = new int[n + 1];
+    sa = span<int>(buf + 1, n);
+    lcp.resize(n);
+    SAIS(span<const char>(sv.data(), n + 1));
+    Kasai(sv);
+  }
+
+  span<int> sa;
+  vector<int> lcp;
+
+ private:
+  void Kasai(const string_view sv) {
+    int n = sv.size();
+    vector<int> ranks(n);
+    for (int i = 0; i < n; i++) ranks[sa[i]] = i + 1;
     for (int i = 0, k = 0; i < n; i++, k && --k) {
-      if (p[i] == 0) continue;
-      int j = sa[p[i] - 1];
-      while (i + k < n && j + k < n && sv[i + k] == sv[j + k]) ++k;
-      lcp[p[i]] = k;
+      if (ranks[i] == n) {
+        k = 0;
+        continue;
+      }
+      int j = sa[ranks[i]];
+      while (i + k < n && j + k < n && sv[i + k] == sv[j + k]) k++;
+      lcp[ranks[i]] = k;
     }
   }
 
-  vector<int> sa, lcp;
+  template <typename T>
+  void SAIS(span<const T> sv) {
+    int n = sv.size();
+    fill_n(buf, n, 0);
+    Buckets bkts(sv);
+    LTypes ltypes(sv);
+    Sort(sv, ltypes, bkts);
+
+    int *sub, sub_len;
+    if (!Reduce(sv, ltypes, bkts, sub, sub_len)) {
+      SAIS(span<const int>(sub, sub_len));
+      for (int i = 0; i < sub_len; i++) sub[buf[i]] = i;
+    }
+
+    for (int i = 1, j = 0; i < n; i++) {
+      if (IsLMS(ltypes, i)) buf[sub[j++]] = -i;
+    }
+    fill_n(buf + sub_len, n - sub_len, 0);
+    Sort(sv, ltypes, sub_len, bkts);
+  }
+
+  template <typename T>
+  void Sort(span<const T> sv, const LTypes& ltypes, Buckets& bkts) {
+    int n = sv.size();
+    auto incl = bkts.Incl();
+    for (int i = 1; i < n; i++) {
+      if (IsLMS(ltypes, i)) buf[--incl[sv[i]]] = i;
+    }
+    SortLS(sv, ltypes, bkts);
+  }
+
+  template <typename T>
+  void Sort(span<const T> sv, const LTypes& ltypes, int lms_len, Buckets& bkts) {
+    auto incl = bkts.Incl();
+    for (int i = lms_len - 1; i >= 0; i--) buf[--incl[sv[-buf[i]]]] = -buf[i];
+    SortLS(sv, ltypes, bkts);
+  }
+
+  template <typename T>
+  void SortLS(span<const T> sv, const LTypes& ltypes, Buckets& bkts) {
+    int n = sv.size();
+    auto incl = bkts.Incl(), excl = bkts.Excl();
+
+    for (int i = 0; i < n; i++) {
+      int pos = buf[i] - 1;
+      if (pos >= 0 && ltypes[pos]) buf[excl[sv[pos]]++] = pos;
+    }
+
+    for (int i = n - 1; i > 0; i--) {
+      int pos = buf[i] - 1;
+      if (pos >= 0 && !ltypes[pos]) buf[--incl[sv[pos]]] = pos;
+    }
+  }
+
+  template <typename T>
+  bool Reduce(span<const T> sv, const LTypes& ltypes, const Buckets& bkts, int*& sub, int& sub_len) {
+    int n = sv.size();
+    sub = buf + (n >> 1);
+    sub_len = n - (n >> 1);
+
+    int rcnt = 0;
+    for (int i = 0; i < n; i++) {
+      if (buf[i] && IsLMS(ltypes, buf[i])) buf[rcnt++] = buf[i];
+    }
+    fill_n(sub, sub_len, -1);
+
+    int rank = sub[(buf[0] - 1) >> 1] = 0;
+    for (int i = 1; i < rcnt; i++) {
+      if (!IsEqual(sv, ltypes, buf[i - 1], buf[i])) ++rank;
+      sub[(buf[i] - 1) >> 1] = rank;
+    }
+
+    sub_len = remove(sub, sub + sub_len, -1) - sub;
+    return rank + 1 == sub_len;
+  }
+
+  bool IsLMS(const LTypes& ltypes, int i) {
+    return ltypes[i] < ltypes[i - 1];
+  }
+
+  template <typename T>
+  bool IsEqual(span<const T> sv, const LTypes& ltypes, int i, int j) {
+    for (;; i++, j++) {
+      if (sv[i] != sv[j] || ltypes[i] != ltypes[j]) return false;
+      if (IsLMS(ltypes, i + 1) && IsLMS(ltypes, j + 1)) return sv[i + 1] == sv[j + 1];
+    }
+  }
+
+ private:
+  int* buf;
 };
 
 int main() {
