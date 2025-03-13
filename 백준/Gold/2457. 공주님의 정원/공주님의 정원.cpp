d83d52830c5386962981fb3b98de9814cdf09824
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-03-13 22:57:58 +0900

    [Gold III] Title: 공주님의 정원, Time: 28 ms, Memory: 2812 KB -BaekjoonHub

---
+++ 백준/Gold/2457. 공주님의 정원/공주님의 정원.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int memo[1232];
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  cin >> n;
+  vector<pair<int, int>> arr;
+  arr.reserve(n);
+  for (int i = 0; i < n; i++) {
+    int sm, sd, dm, dd;
+    cin >> sm >> sd >> dm >> dd;
+    int s = sm * 100 + sd, d = dm * 100 + dd;
+    if (s >= 1201 || d <= 300) continue;
+    arr.push_back({s, d});
+  }
+  sort(arr.begin(), arr.end(), [](const pair<int, int>& lhs, const pair<int, int>& rhs) {
+    return lhs.second > rhs.second;
+  });
+
+  int last = 1201;
+  for (auto [s, d] : arr) {
+    if (s < last) {
+      for (int i = s; i < last; i++) memo[i] = d;
+      last = s;
+    }
+  }
+
+  int cur = 301, ans = 0;
+  while (cur < 1201) {
+    if (memo[cur] <= cur) {
+      ans = 0;
+      break;
+    }
+
+    cur = memo[cur];
+    ++ans;
+  }
+  cout << ans;
+
+  return 0;
+}
