9a0963ad7ee9608e409f9673a1392009769a3b63
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-03-13 17:32:33 +0900

    [Gold V] Title: 아름다운 문자열, Time: 8 ms, Memory: 3680 KB -BaekjoonHub

--- 백준/Gold/24524. 아름다운 문자열/아름다운 문자열.cpp
+++ 백준/Gold/24524. 아름다운 문자열/아름다운 문자열.cpp
@@ -8,35 +8,15 @@ int main() {
 
   string s, t;
   cin >> s >> t;
-
-  int memo[256] = {};
-  int nt = t.size();
-  for (int i = 0; i < nt; i++) {
-    memo[t[i]] = i + 1;
+  int table[256] = {};
+  int nt = 0;
+  for (auto c : t) table[c] = ++nt;
+  int memo[27] = {INT_MAX, };
+  for (auto c : s) {
+    int id = table[c];
+    if (id && memo[id - 1] > memo[id]) ++ memo[id];
   }
-
-  int ans = 0;
-  vector<vector<int>> tables;
-  for (char c : s) {
-    int id = memo[c];
-    if (!id) continue;
-
-    if (id == 1) {
-      tables.emplace_back(1, 1);
-      if (id == nt) ++ans;
-      continue;
-    }
-
-    auto it = lower_bound(tables.begin(), tables.end(), id - 1, [](const vector<int>& table, int x) {
-      return table.back() > x;
-    });
-
-    if (it != tables.end() && it->back() == id - 1) {
-      it->push_back(id);
-      if (id == nt) ++ans;
-    }
-  }
-  cout << ans;
+  cout << memo[nt];
 
   return 0;
 }
