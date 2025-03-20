8b92e8a9cfbb1a63734ee7593a66e980309a59c3
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-03-21 04:24:02 +0900

    [Silver II] Title: Parentheses Tree, Time: 88 ms, Memory: 26720 KB -BaekjoonHub

---
+++ 백준/Silver/26111. Parentheses Tree/Parentheses Tree.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  string s;
+  cin >> s;
+  int64_t ans = 0;
+  int n = s.size(), cnt = 0;
+  for (int i = 0; i < n - 1; i++) {
+    if (s[i] == '(' && s[i + 1] == ')') {
+      ans += cnt;
+    }
+
+    if (s[i] == '(') {
+      ++cnt;
+    } else {
+      --cnt;
+    }
+  }
+  cout << ans;
+
+  return 0;
+}
