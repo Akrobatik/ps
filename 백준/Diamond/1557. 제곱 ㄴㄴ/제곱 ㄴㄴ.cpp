97cb61204b1c6bcb741e83dfd6369edffc531eac
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-03-20 22:21:52 +0900

    [Diamond V] Title: 제곱 ㄴㄴ, Time: 12 ms, Memory: 2180 KB -BaekjoonHub

---
+++ 백준/Diamond/1557. 제곱 ㄴㄴ/제곱 ㄴㄴ.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+constexpr int kMax = 40558;
+int mu[kMax + 1];
+
+int64_t Count(int64_t n) {
+  int64_t cnt = 0;
+  for (int64_t i = 1; i * i <= n; i++) {
+    cnt += mu[i] * (n / (i * i));
+  }
+  return cnt;
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  mu[1] = 1;
+  for (int i = 1; i <= kMax; i++) {
+    for (int j = (i << 1); j <= kMax; j += i) {
+      mu[j] -= mu[i];
+    }
+  }
+
+  int64_t k;
+  cin >> k;
+
+  int64_t lo = 0, hi = 1644934082;
+  while (lo + 1 < hi) {
+    int64_t mid = (lo + hi) >> 1;
+    if (Count(mid) < k) {
+      lo = mid;
+    } else {
+      hi = mid;
+    }
+  };
+  cout << lo + 1;
+
+  return 0;
+}
