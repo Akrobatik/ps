5baa722b19f290b4888d19b8df678b7610c7e412
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-03-21 08:24:13 +0900

    [Platinum III] Title: 해밍 거리와 쿼리, Time: 504 ms, Memory: 3192 KB -BaekjoonHub

--- 백준/Platinum/14851. 해밍 거리와 쿼리/해밍 거리와 쿼리.cpp
+++ 백준/Platinum/14851. 해밍 거리와 쿼리/해밍 거리와 쿼리.cpp
@@ -6,14 +6,14 @@
 
 using namespace std;
 
-uint32_t b1[32][6250], b2[32][6250];
+uint8_t b1[8][25000], b2[8][25000];
 
 int Count(int p1, int p2, int len) {
-  auto bp1 = b1[p1 % 32], bp2 = b2[p2 % 32];
-  p1 -= p1 % 32, p2 -= p2 % 32;
+  auto bp1 = b1[p1 % 8], bp2 = b2[p2 % 8];
+  p1 -= p1 % 8, p2 -= p2 % 8;
 
-  int cnt = 0, i = 0, pi1 = p1 / 32, pi2 = p2 / 32;
-  for (; i + 128 <= len; i += 128, pi1 += 4, pi2 += 4) {
+  int cnt = 0, i = 0, pi1 = p1 / 8, pi2 = p2 / 8;
+  for (; i + 128 <= len; i += 128, pi1 += 16, pi2 += 16) {
     __m128i v1 = _mm_loadu_si128((const __m128i *)(bp1 + pi1));
     __m128i v2 = _mm_loadu_si128((const __m128i *)(bp2 + pi2));
     __m128i vx = _mm_xor_si128(v1, v2);
@@ -21,10 +21,10 @@ int Count(int p1, int p2, int len) {
     cnt += __builtin_popcountll(_mm_extract_epi64(vx, 1));
   }
 
-  for (; i + 32 <= len; i += 32, pi1++, pi2++) {
+  for (; i + 8 <= len; i += 8, pi1++, pi2++) {
     cnt += __builtin_popcount(bp1[pi1] ^ bp2[pi2]);
   }
-  for (int j = 0; j < 32 && i < len; i++, j++) {
+  for (int j = 0; j < 8 && i < len; i++, j++) {
     cnt += (((bp1[pi1] ^ bp2[pi2]) >> j) & 1);
   }
   return cnt;
@@ -39,19 +39,19 @@ int main() {
   int n1 = s1.size(), n2 = s2.size();
   for (int i = 0; i < n1; i++) {
     if (s1[i] == '0') continue;
-    for (int j = 0; j < 32; j++) {
+    for (int j = 0; j < 8; j++) {
       int p = i - j;
       if (p < 0) continue;
-      int pi = p / 32, pb = p % 32;
+      int pi = p / 8, pb = p % 8;
       b1[j][pi] |= (1ull << pb);
     }
   }
   for (int i = 0; i < n2; i++) {
     if (s2[i] == '0') continue;
-    for (int j = 0; j < 32; j++) {
+    for (int j = 0; j < 8; j++) {
       int p = i - j;
       if (p < 0) continue;
-      int pi = p / 32, pb = p % 32;
+      int pi = p / 8, pb = p % 8;
       b2[j][pi] |= (1ull << pb);
     }
   }
