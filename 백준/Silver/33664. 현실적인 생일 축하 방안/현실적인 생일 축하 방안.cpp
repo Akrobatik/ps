7a8dbcff06849efeca1f5d48bac7697ea19aecf5
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-03-26 06:41:59 +0900

    [Silver V] Title: 현실적인 생일 축하 방안, Time: 0 ms, Memory: 2292 KB -BaekjoonHub

---
+++ 백준/Silver/33664. 현실적인 생일 축하 방안/현실적인 생일 축하 방안.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int64_t b, n, m;
+  cin >> b >> n >> m;
+  unordered_map<string, int64_t> hm;
+  while (n--) {
+    string s;
+    int64_t x;
+    cin >> s >> x;
+    hm[s] = x;
+  }
+  int64_t sum = 0;
+  while (m--) {
+    string s;
+    cin >> s;
+    sum += hm[s];
+  }
+  cout << (sum <= b ? "acceptable" : "unacceptable");
+
+  return 0;
+}
