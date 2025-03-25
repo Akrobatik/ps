83ecea0389e8f034c1ba162f601ac299079a2451
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-03-26 02:13:07 +0900

    [Gold III] Title: 4교시: 국어 (Easy), Time: 0 ms, Memory: 2164 KB -BaekjoonHub

--- 백준/Gold/33634. 4교시： 국어 （Easy）/4교시： 국어 （Easy）.cpp
+++ 백준/Gold/33634. 4교시： 국어 （Easy）/4교시： 국어 （Easy）.cpp
@@ -2,65 +2,100 @@
 
 using namespace std;
 
-int ans;
-vector<int> trr, arr, brr, crr;
-bool visited[1000][11][11][11];
+constexpr int kMask = (1 << 9) - 1;
 
-void Traverse(int a, int b, int c, int idx) {
-  if (idx == trr.size()) {
-    ans = max<int>(ans, a + b + c);
-    return;
-  }
+int MakeKey(int a, int b, int c) {
+  return (a << 18) | (b << 9) | c;
+}
 
-  if (visited[idx][a][b][c]) return;
-  visited[idx][a][b][c] = true;
-
-  int t = trr[idx];
-  auto ab = arr.begin() + a;
-  auto ae = upper_bound(ab, arr.end(), arr[a] + t);
-  for (auto ait = ae; ait != ab;) {
-    int ad = *--ait - arr[a];
-    auto bb = brr.begin() + b;
-    auto be = upper_bound(bb, brr.end(), brr[b] + t - ad);
-    int cmax = -1;
-    for (auto bit = be; bit != bb;) {
-      int bd = *--bit - brr[b];
-      auto cb = crr.begin() + c;
-      auto ce = upper_bound(cb, crr.end(), crr[c] + t - ad - bd);
-      if (cb != ce && cmax < (ce - cb) - 1) {
-        cmax = (ce - cb) - 1;
-        auto cit = ce - 1;
-        int cd = *cit - crr[c];
-        Traverse(ait - arr.begin(), bit - brr.begin(), cit - crr.begin(), idx + 1);
-      }
-    }
-  }
+tuple<int, int, int> SplitKey(int key) {
+  int a = (key >> 18) & kMask;
+  int b = (key >> 9) & kMask;
+  int c = key & kMask;
+  return {a, b, c};
 }
 
 int main() {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
 
-  int n, t;
-  cin >> n >> t;
-  trr.resize(n);
+  int nt, t;
+  cin >> nt >> t;
+  vector<int> trr(nt);
   for (auto& e : trr) cin >> e;
-  for (int i = 1; i < n; i++) {
-    trr[i - 1] = trr[i] - trr[i - 1] - 1;
-  }
-  trr.pop_back();
 
   int na, nb, nc;
   cin >> na >> nb >> nc;
-  arr.resize(na + 1), brr.resize(nb + 1), crr.resize(nc + 1);
-  for (int i = 1; i <= na; i++) cin >> arr[i];
-  for (int i = 1; i <= nb; i++) cin >> brr[i];
-  for (int i = 1; i <= nc; i++) cin >> crr[i];
-  partial_sum(arr.begin(), arr.end(), arr.begin());
-  partial_sum(brr.begin(), brr.end(), brr.begin());
-  partial_sum(crr.begin(), crr.end(), crr.begin());
+  vector<int> arr(na), brr(nb), crr(nc);
+  for (auto& e : arr) cin >> e;
+  for (auto& e : brr) cin >> e;
+  for (auto& e : crr) cin >> e;
 
-  Traverse(0, 0, 0, 0);
+  vector<int> axr(na + 1), bxr(nb + 1), cxr(nc + 1);
+  partial_sum(arr.begin(), arr.end(), axr.begin() + 1);
+  partial_sum(brr.begin(), brr.end(), bxr.begin() + 1);
+  partial_sum(crr.begin(), crr.end(), cxr.begin() + 1);
+
+  vector<pair<int, int>> cands;
+  vector<vector<pair<int, int>>> perms;
+
+  unordered_map<int, int> cur;
+  cur[MakeKey(0, 0, 0)] = 0;
+
+  for (int i = 1; i < nt; i++) {
+    int trem = trr[i] - trr[i - 1] - 1;
+    if (trem == 0) continue;
+    unordered_map<int, int> nxt;
+
+    for (auto [key, cnt] : cur) {
+      auto [a, b, c] = SplitKey(key);
+      cands.clear();
+      if (a < na) cands.push_back({0, a});
+      if (b < nb) cands.push_back({1, b});
+      if (c < nc) cands.push_back({2, c});
+
+      perms.clear();
+      do {
+        perms.push_back(cands);
+      } while (next_permutation(cands.begin(), cands.end()));
+
+      for (auto& perm : perms) {
+        int used = 0, added = 0;
+        int aa = a, bb = b, cc = c;
+        for (auto [id, start] : perm) {
+          auto& sxr = id == 0 ? axr : (id == 1 ? bxr : cxr);
+          int limit = id == 0 ? na : (id == 1 ? nb : nc);
+          int maxx = start;
+          while (maxx < limit && used + sxr[maxx + 1] - sxr[start] <= trem) ++maxx;
+
+          used += sxr[maxx] - sxr[start];
+          added += maxx - start;
+          if (id == 0) {
+            aa = maxx;
+          } else if (id == 1) {
+            bb = maxx;
+          } else {
+            cc = maxx;
+          }
+        }
+
+        int nkey = MakeKey(aa, bb, cc);
+        int total = cnt + added;
+        if (auto it = nxt.find(nkey); it != nxt.end()) {
+          it->second = max<int>(it->second, total);
+        } else {
+          nxt[nkey] = total;
+        }
+      }
+    }
+
+    cur.swap(nxt);
+  }
+
+  int ans = 0;
+  for (auto [key, cnt] : cur) {
+    ans = max<int>(ans, cnt);
+  }
   cout << ans;
 
   return 0;
