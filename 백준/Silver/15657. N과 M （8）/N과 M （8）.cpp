3478a9b64edd1e40e4e7142e8ff82e6a61a832ad
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-03-30 02:45:41 +0900

    [Silver III] Title: N과 M (8), Time: 4 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int memo[8];
+vector<int> arr;
+
+void Print(int i, int j, int m) {
+  if (i == m) {
+    for (int i = 0; i < m; i++) cout << memo[i] << " ";
+    cout << "\n";
+    return;
+  }
+
+  int na = arr.size();
+  for (int k = j; k < na; k++) {
+    memo[i] = arr[k];
+    Print(i + 1, k, m);
+  }
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n, m;
+  cin >> n >> m;
+  arr.resize(n);
+  for (auto& e : arr) cin >> e;
+  sort(arr.begin(), arr.end());
+  arr.erase(unique(arr.begin(), arr.end()), arr.end());
+  Print(0, 0, m);
+
+  return 0;
+}
