0cde0248c26d4842cc22aa6df209dd9564571bde
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-03-30 20:20:30 +0900

    [Silver III] Title: 우주 여행, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/33676. 우주 여행/우주 여행.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n, m, k, l;
+  cin >> n >> m >> k >> l;
+
+  if (n + m - 2 > l || ((n + m - l) & 1)) {
+    cout << "-1";
+    return 0;
+  }
+
+  int lm = l - (n + m - 2);
+  for (int i = 0; i < lm; i += 2) {
+    cout << "DU";
+  }
+  for (int i = 1; i < n; i++) {
+    cout << "D";
+  }
+  for (int i = 1; i < m; i++) {
+    cout << "R";
+  }
+
+  return 0;
+}
