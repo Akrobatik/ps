40ef5019a37e5c59258fcc999f5c6bb00b6264d4
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-03-30 20:19:05 +0900

    [Silver V] Title: 포린드롬, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/33690. 포린드롬/포린드롬.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  string s;
+  cin >> s;
+  int n = s.size();
+  if (n == 1) {
+    cout << stoi(s) + 1;
+    return 0;
+  }
+
+  int ans = (n - 1) * 9 + 1;
+  string tmp(n, s[0]);
+  ans += s[0] - '1' + (s.compare(tmp) >= 0);
+  cout << ans;
+
+  return 0;
+}
