b5571b7fba6539f613f9d357f8f1ad35f7b77ce9
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-03-31 03:11:39 +0900

    [Gold III] Title: 하늘에서 떨어지는 $\infty$개의 별, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/33682. 하늘에서 떨어지는 $＼infty$개의 별/하늘에서 떨어지는 $＼infty$개의 별.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int64_t s0, x, m, d, k;
+  cin >> s0 >> x >> m >> d >> k;
+  int64_t g = gcd(x, m);
+  int64_t maxx = (((m - s0 - 1)) / g) * g + s0;
+
+  int64_t lo = 1, hi = d + 1;
+  while (lo + 1 < hi) {
+    int64_t mid = (lo + hi) >> 1;
+    __int128_t r = (__int128_t)maxx * mid;
+    if (r <= (__int128_t)k) {
+      lo = mid;
+    } else {
+      hi = mid;
+    }
+  }
+  cout << (d - 1) / lo;
+
+  return 0;
+}
