7bc56139825859bbd614f54bbe063c1eb79448e0
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-04-01 08:34:37 +0900

    [Platinum V] Title: 가장 긴 팰린드롬 부분 문자열, Time: 0 ms, Memory: 3256 KB -BaekjoonHub

---
+++ 백준/Platinum/14444. 가장 긴 팰린드롬 부분 문자열/가장 긴 팰린드롬 부분 문자열.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct Manacher {
+  void Init(string_view sv) {
+    int n = (sv.size() << 1) + 1;
+    table.clear(), table.resize(n);
+
+    string s(n, '\0');
+    auto ptr = s.data() + 1;
+    for (char c : sv) *ptr = c, ptr += 2;
+
+    int p, r = 0;
+    for (int i = 1; i < n - 1; i++) {
+      if (i > r) {
+        p = r = i;
+        while (r < n && r <= (p << 1) && s[r] == s[(p << 1) - r]) ++r;
+        table[i] = --r - p;
+      } else {
+        if (table[(p << 1) - i] < r - i) {
+          table[i] = table[(p << 1) - i];
+        } else if (table[(p << 1) - i] > r - i) {
+          table[i] = r - i;
+        } else {
+          p = i;
+          while (r < n && r <= (p << 1) && s[r] == s[(p << 1) - r]) ++r;
+          table[i] = --r - p;
+        }
+      }
+    }
+  }
+
+  vector<int> table;
+};
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  string s;
+  cin >> s;
+
+  Manacher mncher;
+  mncher.Init(s);
+
+  auto& table = mncher.table;
+  int maxx = 0;
+  for (auto e : table) maxx = max<int>(maxx, e);
+  cout << maxx;
+
+  return 0;
+}
