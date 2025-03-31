b6fd4a23d0131946cd966673808165ab36f6d1fa
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-03-31 09:52:06 +0900

    [Gold I] Title: 타일 밟기, Time: 32 ms, Memory: 3128 KB -BaekjoonHub

---
+++ 백준/Gold/6988. 타일 밟기/타일 밟기.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+bool memo[1000001];
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  cin >> n;
+  vector<int> arr(n);
+  for (auto& e : arr) cin >> e, memo[e] = true;
+
+  int64_t ans = 0;
+  for (int i = 1; i < n; i++) {
+    int r = arr[i];
+    for (int j = 0; j < i; j++) {
+      int d = r - arr[j];
+      int cnt = 2, nxt = r + d;
+      while (nxt <= 1000000 && memo[nxt]) nxt += d, ++cnt;
+      if (cnt >= 3) {
+        int64_t x = arr[j] + nxt - d;
+        ans = max<int64_t>(ans, x * cnt / 2);
+      }
+    }
+  }
+  cout << ans;
+
+  return 0;
+}
