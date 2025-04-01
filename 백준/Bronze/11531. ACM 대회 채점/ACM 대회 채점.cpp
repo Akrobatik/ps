aee3ffd9ce800e5758f4ec338b73b7f48b490c54
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-04-02 01:02:19 +0900

    [Bronze II] Title: ACM 대회 채점, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Bronze/11531. ACM 대회 채점/ACM 대회 채점.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  unordered_map<string, int> hm;
+  int64_t m;
+  string name, result;
+  int64_t a1 = 0, a2 = 0;
+  while (cin >> m >> name >> result) {
+    auto& x = hm[name];
+    if (x == -1) continue;
+    if (result == "right") {
+      ++a1;
+      a2 += m + x * 20;
+      x = -1;
+    } else {
+      ++x;
+    }
+  }
+  cout << a1 << " " << a2;
+
+  return 0;
+}
