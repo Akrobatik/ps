d28b801d0fda285a750039dbb87d7a8d5c3f1ef0
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-04-01 09:00:15 +0900

    [Gold IV] Title: 알파벳, Time: 204 ms, Memory: 27264 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.cpp
+++ 백준/Gold/1987. 알파벳/알파벳.cpp
@@ -7,8 +7,17 @@ constexpr pair<int, int> kDelta[] = {
 
 int ans, r, c;
 char m[20][20];
+unordered_set<int64_t> visited;
+
+int64_t MakeKey(int64_t y, int64_t x, int64_t mask) {
+  return (y << 48) | (x << 32) | mask;
+}
 
 void Traverse(int y, int x, int mask, int cnt) {
+  auto key = MakeKey(y, x, mask);
+  if (visited.contains(key)) return;
+  visited.insert(key);
+
   ans = max<int>(ans, cnt + 1);
   mask |= (1 << (m[y][x] ^ 0x40));
 
