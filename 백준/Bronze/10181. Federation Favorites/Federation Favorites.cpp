ea096301f46839e9acc5538d6c2788660e70cef1
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-04-02 00:05:01 +0900

    [Bronze I] Title: Federation Favorites, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10181. Federation Favorites/Federation Favorites.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  vector<int> elems;
+  int n;
+  while (cin >> n && n != -1) {
+    elems.clear();
+    int half = n >> 1;
+    int64_t sum = 1;
+    for (int i = 2; i <= half; i++) {
+      if (n % i == 0) elems.push_back(i), sum += i;
+    }
+    if (sum == n) {
+      cout << n << " = 1";
+      for (auto e : elems) cout << " + " << e;
+      cout << "\n";
+    } else {
+      cout << n << " is NOT perfect.\n";
+    }
+  }
+
+  return 0;
+}
