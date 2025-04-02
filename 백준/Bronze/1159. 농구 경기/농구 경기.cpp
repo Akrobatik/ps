85636e8cf699e47101092f1c76342b436cde4632
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-04-02 23:14:46 +0900

    [Bronze II] Title: 농구 경기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/1159. 농구 경기/농구 경기.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int memo[26] = {};
+  int n;
+  cin >> n;
+  while (n--) {
+    string s;
+    cin >> s;
+    ++memo[s.front() - 'a'];
+  }
+
+  int cnt = 0;
+  for (int i = 0; i < 26; i++) {
+    if (memo[i] < 5) continue;
+    cout << (char)(i + 'a');
+    ++cnt;
+  }
+  if (cnt == 0) cout << "PREDAJA";
+
+  return 0;
+}
