63f261e15da93732c253724c94afb9be38abea60
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-04-03 00:27:30 +0900

    [Bronze II] Title: 추론, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1731. 추론/추론.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n, s1, s2, s3;
+  cin >> n >> s1 >> s2 >> s3;
+  if (s2 - s1 == s3 - s2) {
+    cout << s1 + (s2 - s1) * n;
+  } else {
+    int64_t x = s1;
+    while (n--) x = x * s2 / s1;
+    cout << x;
+  }
+
+
+  return 0;
+}
