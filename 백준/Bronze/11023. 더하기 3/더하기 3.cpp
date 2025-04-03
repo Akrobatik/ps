3279cc9b3b00cbbfb6399269edba3bc299c3a932
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-04-04 04:22:21 +0900

    [Bronze III] Title: 더하기 3, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11023. 더하기 3/더하기 3.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int64_t sum = 0, n;
+  while (cin >> n) sum += n;
+  cout << sum;
+
+  return 0;
+}
