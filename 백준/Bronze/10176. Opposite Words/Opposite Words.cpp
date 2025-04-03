4a5d98919088a9632c75bd5d26efb71c3abd03c5
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-04-04 03:54:48 +0900

    [Bronze I] Title: Opposite Words, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Bronze/10176. Opposite Words/Opposite Words.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  cin >> n;
+  while (n--) {
+    string s, s2, s3;
+    cin >> s;
+    for (auto c : s) {
+      if (isalpha(c)) {
+        c = tolower(c);
+        s2.push_back(c);
+        s3.push_back('z' - (c - 'a'));
+      }
+    }
+    sort(s2.begin(), s2.end());
+    sort(s3.begin(), s3.end());
+
+    bool ok = true;
+    for (int i = 0; ok && i < s2.size(); i++) {
+      ok = (s2[i] == s3[i]);
+    }
+    cout << (ok ? "Yes\n" : "No\n");
+  }
+
+  return 0;
+}
