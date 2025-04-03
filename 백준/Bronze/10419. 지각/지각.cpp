73646933e93e5eaac170b506717a5801d566a242
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-04-04 04:12:03 +0900

    [Bronze III] Title: 지각, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10419. 지각/지각.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int t;
+  cin >> t;
+  while (t--) {
+    int64_t x;
+    cin >> x;
+    int64_t res = 0;
+    while (res * res + res <= x) ++res;
+    cout << res - 1 << "\n";
+  }
+
+  return 0;
+}
