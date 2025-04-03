feb2e89dbcfb9d4b66fb295ffffdc704f3d3137e
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-04-04 04:44:27 +0900

    [Bronze III] Title: Report Card Time, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11367. Report Card Time/Report Card Time.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const char* GetGrade(int x) {
+  if (x >= 97) return "A+";
+  if (x >= 90) return "A";
+  if (x >= 87) return "B+";
+  if (x >= 80) return "B";
+  if (x >= 77) return "C+";
+  if (x >= 70) return "C";
+  if (x >= 67) return "D+";
+  if (x >= 60) return "D";
+  return "F";
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int t;
+  cin >> t;
+  while (t--) {
+    string s;
+    int n;
+    cin >> s >> n;
+    cout << s << " " << GetGrade(n) << "\n";
+  }
+
+  return 0;
+}
