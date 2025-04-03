f6ca0f0ba71895d51fe345e37af5766565c0572c
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-04-04 05:36:19 +0900

    [Bronze I] Title: Grades, Time: 24 ms, Memory: 2532 KB -BaekjoonHub

---
+++ 백준/Bronze/14194. Grades/Grades.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int t;
+  cin >> t;
+  while (t--) {
+    int n;
+    cin >> n;
+    vector<int64_t> arr(n);
+    int64_t y = 0;
+    for (auto& e : arr) cin >> e, y += e;
+    sort(arr.begin(), arr.end());
+    int64_t x = (arr.front() + arr.back()) * n;
+    y <<= 1;
+    cout << (abs(x - y) <= n * 2 ? "Yes\n" : "No\n");
+  }
+
+  return 0;
+}
