a5e4997a783f32ea15b612018a5bdeb15563965b
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-04-03 15:02:18 +0900

    [Silver V] Title: 건덕이의 돌탑, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/33703. 건덕이의 돌탑/건덕이의 돌탑.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int64_t n;
+  cin >> n;
+  cout << n * (n + 1) / 2;
+
+  return 0;
+}
