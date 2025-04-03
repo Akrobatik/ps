29919771afa81eb3ce78fe2d5e8d1273cdb51a06
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-04-04 05:07:45 +0900

    [Bronze IV] Title: 뜨거운 붕어빵, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11945. 뜨거운 붕어빵/뜨거운 붕어빵.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n, m;
+  cin >> n >> m;
+  while (n--) {
+    string s;
+    cin >> s;
+    for (auto c : views::reverse(s)) cout << c;
+    cout << "\n";
+  }
+
+  return 0;
+}
