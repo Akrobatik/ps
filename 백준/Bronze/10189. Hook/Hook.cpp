3bc70ff136676f2bfa8ed6dae1d528de4dba8a29
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-04-04 04:02:48 +0900

    [Bronze V] Title: Hook, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10189. Hook/Hook.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+constexpr const char* kStr =
+"#  # #### #### #  #\n"
+"#### #  # #  # # #\n"
+"#### #  # #  # # #\n"
+"#  # #### #### #  #\n";
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  cout << kStr;
+
+  return 0;
+}
