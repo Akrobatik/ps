ee9ec2f55bf52a90efe08dc981c4d75f8280bf20
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-04-03 15:17:30 +0900

    [Gold V] Title: 마스코트 정하기, Time: 16 ms, Memory: 2804 KB -BaekjoonHub

--- 백준/Gold/33705. 마스코트 정하기/마스코트 정하기.cpp
+++ 백준/Gold/33705. 마스코트 정하기/마스코트 정하기.cpp
@@ -8,21 +8,20 @@ int main() {
 
   int n;
   cin >> n;
-  vector<int> arr(n);
-  for (auto& e : arr) cin >> e;
-
-  vector<int> psum(n + 1);
-  for (int i = 0; i < n; i++) {
-    psum[i + 1] = psum[i] + (arr[i] == 1);
+  vector<int> arr(n + 1);
+  for (int i = 1; i <= n; i++) {
+    int x;
+    cin >> x;
+    arr[i] = arr[i - 1] + (x == 1);
   }
 
-  if (psum[n] >= ((n + 1) >> 1)) {
+  if (arr[n] >= ((n + 1) >> 1)) {
     cout << "0";
     return 0;
   }
 
   for (int i = 1; i <= n; i++) {
-    if (psum[i] >= ((i + 1) >> 1) || psum[n] - psum[n - i] >= ((i + 1) >> 1)) {
+    if (arr[i] >= ((i + 1) >> 1) || arr[n] - arr[n - i] >= ((i + 1) >> 1)) {
       cout << "1";
       return 0;
     }
