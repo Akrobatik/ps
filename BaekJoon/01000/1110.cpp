6243e2955c338592c774a68c0d4e6147d0fe49b4
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-04-05 20:48:47 +0900

    1110 - 더하기 사이클

---
+++ BaekJoon/01000/1110.cpp
@@ -0,0 +1,30 @@
+// Title : 더하기 사이클
+// Link  : https://www.acmicpc.net/problem/1110 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int Calc(int n) {
+  int l = n % 10, r = (l + n / 10) % 10;
+  return l * 10 + r;
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  cin >> n;
+
+  int nxt = Calc(n), cnt = 1;
+  while (nxt != n) {
+    nxt = Calc(nxt);
+    ++cnt;
+  }
+  cout << cnt;
+
+  return 0;
+}
