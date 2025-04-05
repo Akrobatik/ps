16206eac69231b91aa75f2c9cd2e14ed679232fc
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-04-05 20:38:21 +0900

    1000 - A+B

---
+++ BaekJoon/01000/1000.cpp
@@ -0,0 +1,19 @@
+// Title : A+B
+// Link  : https://www.acmicpc.net/problem/1000 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int a, b;
+  cin >> a >> b;
+  cout << a + b;
+
+  return 0;
+}
