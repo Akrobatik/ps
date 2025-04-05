c20c34c0b21aa284baea1a338382ec7d3ae4836e
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-04-05 20:51:19 +0900

    1179 - 마지막 요세푸스 문제

---
+++ BaekJoon/01000/1179.cpp
@@ -0,0 +1,35 @@
+// Title : 마지막 요세푸스 문제
+// Link  : https://www.acmicpc.net/problem/1179 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int64_t n, k;
+  cin >> n >> k;
+  int64_t ans = 1, idx = 1;
+  if (k == 1) {
+    cout << n;
+    return 0;
+  }
+
+  for (;;) {
+    int64_t step = (idx - ans) / (k - 1) + 1;
+    if (idx + step > n) {
+      ans += (n - idx) * k;
+      break;
+    }
+    idx += step;
+    ans = (ans + step * k) % idx;
+    if (ans == 0) ans = idx;
+  }
+  cout << ans;
+
+  return 0;
+}
