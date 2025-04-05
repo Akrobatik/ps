7a10166b6771dec1f9d88734e2944e5462741cb1
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-04-05 20:43:28 +0900

    1038 - 감소하는 수

---
+++ BaekJoon/01000/1038.cpp
@@ -0,0 +1,41 @@
+// Title : 감소하는 수
+// Link  : https://www.acmicpc.net/problem/1038 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  cin >> n;
+
+  queue<int64_t> q;
+  for (int i = 0; i < 10; i++) {
+    q.push(i);
+  }
+
+  int64_t result = -1;
+  int nn = 0;
+  while (!q.empty()) {
+    int64_t v = q.front();
+    q.pop();
+
+    if (nn++ == n) {
+      result = v;
+      break;
+    }
+
+    for (int i = 0; i < v % 10; i++) {
+      q.push(v * 10 + i);
+    }
+  }
+
+  cout << result << endl;
+
+  return 0;
+}
