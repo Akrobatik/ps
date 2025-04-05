ab9a4f57f38b36ae85f2387977f57d1924a9717b
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-04-05 20:40:22 +0900

    1008 - A/B

---
+++ BaekJoon/01000/1008.cpp
@@ -0,0 +1,21 @@
+// Title : A/B
+// Link  : https://www.acmicpc.net/problem/1008 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int a, b;
+  cin >> a >> b;
+  cout << fixed;
+  cout.precision(10);
+  cout << (double)a / (double)b;
+
+  return 0;
+}
