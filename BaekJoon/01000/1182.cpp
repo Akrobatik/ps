819a202f99212ea3595138c422be24ae0a129b00
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-04-05 20:51:43 +0900

    1182 - 부분수열의 합

---
+++ BaekJoon/01000/1182.cpp
@@ -0,0 +1,39 @@
+// Title : 부분수열의 합
+// Link  : https://www.acmicpc.net/problem/1182 
+// Time  : 0 ms
+// Memory: 2292 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n, s;
+  cin >> n >> s;
+  vector<int> arr(n);
+  for (int i = 0; i < n; i++) cin >> arr[i];
+
+  map<int, int> m1, m2;
+  ++m1[0], ++m2[0];
+  int mid = n >> 1;
+  for (int i = 0; i < mid; i++) {
+    vector<pair<int, int>> vec(m1.begin(), m1.end());
+    for (auto [e, cnt] : vec) m1[e + arr[i]] += cnt;
+  }
+
+  for (int i = mid; i < n; i++) {
+    vector<pair<int, int>> vec(m2.begin(), m2.end());
+    for (auto [e, cnt] : vec) m2[e + arr[i]] += cnt;
+  }
+
+  int ans = 0;
+  for (auto [e, cnt] : m1) {
+    ans += cnt * m2[s - e];
+  }
+  cout << ans - (s == 0);
+
+  return 0;
+}
