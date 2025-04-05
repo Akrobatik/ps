dd288444238719c0df062f659990d2e94e35dbcc
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-04-05 20:44:31 +0900

    1052 - 물병

---
+++ BaekJoon/01000/1052.cpp
@@ -0,0 +1,38 @@
+// Title : 물병
+// Link  : https://www.acmicpc.net/problem/1052 
+// Time  : 0 ms
+// Memory: 2208 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  uint32_t n, k;
+  cin >> n >> k;
+  bitset<32> b(n);
+
+  uint32_t result = 0;
+  if (b.count() > k) {
+    int nn = 0;
+    uint32_t v = 0;
+    for (int i = 31; i >= 0; i--) {
+      if (b.test(i)) {
+        if (++nn == k) {
+          v += (uint32_t)pow(2, i + 1);
+          result = v - n;
+          break;
+        } else {
+          v += (uint32_t)pow(2, i);
+        }
+      }
+    }
+  }
+
+  cout << result << endl;
+
+  return 0;
+}
