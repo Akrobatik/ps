bb9c8d939fa5b4118e5a4b460d3db26ca053580c
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-04-05 20:56:05 +0900

    1259 - 팰린드롬수

---
+++ BaekJoon/01000/1259.cpp
@@ -0,0 +1,30 @@
+// Title : 팰린드롬수
+// Link  : https://www.acmicpc.net/problem/1259 
+// Time  : 0 ms
+// Memory: 2024 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  for (;;) {
+    string s;
+    cin >> s;
+    if (s == "0") break;
+    auto b = s.data();
+    auto e = b + s.size() - 1;
+    bool ok = true;
+    while (b <= e) {
+      if (*b++ == *e--) continue;
+      ok = false;
+      break;
+    }
+    cout << (ok ? "yes\n" : "no\n");
+  }
+
+  return 0;
+}
