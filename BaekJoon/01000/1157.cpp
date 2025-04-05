061ffe7e232504ff1c0bf315f8f1cd132d16c533
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-04-05 20:50:00 +0900

    1157 - 단어 공부

---
+++ BaekJoon/01000/1157.cpp
@@ -0,0 +1,35 @@
+// Title : 단어 공부
+// Link  : https://www.acmicpc.net/problem/1157 
+// Time  : 4 ms
+// Memory: 3680 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int memo[256];
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  string s;
+  cin >> s;
+  for (auto c : s) {
+    ++memo[c & 0xDF];
+  }
+
+  char ans;
+  int maxx = 0;
+  for (int i = 0; i < 256; i++) {
+    if (maxx < memo[i]) {
+      ans = i;
+      maxx = memo[i];
+    } else if (maxx == memo[i]) {
+      ans = '?';
+    }
+  }
+  cout << ans;
+
+  return 0;
+}
