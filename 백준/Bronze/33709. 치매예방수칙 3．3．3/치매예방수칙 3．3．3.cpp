9d79b4f2e375fdea9aa191f2e8ddb9ca10fb191a
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-04-05 10:12:56 +0900

    [Bronze I] Title: 치매예방수칙 3.3.3, Time: 0 ms, Memory: 2160 KB -BaekjoonHub

---
+++ 백준/Bronze/33709. 치매예방수칙 3．3．3/치매예방수칙 3．3．3.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<string> Tokenize(string str) {
+  vector<string> tokens;
+  size_t b = 0, e;
+  while ((e = str.find_first_of(".|:#", b)) != string::npos) {
+    tokens.push_back(str.substr(b, e - b));
+    b = e + 1;
+  }
+  tokens.push_back(str.substr(b));
+  return tokens;
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  string s;
+  int n;
+  cin >> n >> s;
+  auto tokens = Tokenize(s);
+  int64_t ans = 0;
+  for (auto token : tokens) ans += stoi(token);
+  cout << ans;
+
+  return 0;
+}
