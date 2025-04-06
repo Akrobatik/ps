d85cf3bbed92936f71a64601f5dd152dd1307244
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-04-06 17:44:38 +0900

    study

---
+++ snippet/hopcroft_karp.cpp
@@ -0,0 +1,79 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct HopcroftKarp {
+  void Init(int n) {
+    nmax = n;
+    used.clear(), used.resize(n);
+    left.clear(), left.resize(n, -1);
+    right.clear(), right.resize(n, -1);
+    dist.clear(), dist.resize(n);
+    edges.clear(), edges.resize(n);
+  }
+
+  void AddEdge(int u, int v) {
+    edges[u].push_back(v);
+  }
+
+  int Solve() {
+    int res = 0;
+    for (;;) {
+      BFS();
+      int add = 0;
+      for (int i = 0; i < nmax; i++) {
+        add += (!used[i] && DFS(i));
+      }
+
+      if (add == 0) break;
+      res += add;
+    }
+    return res;
+  }
+
+ private:
+  static constexpr int kInf = INT_MAX >> 1;
+
+  void BFS() {
+    queue<int> q;
+    for (int i = 0; i < nmax; i++) {
+      if (used[i]) {
+        dist[i] = kInf;
+      } else {
+        dist[i] = 0;
+        q.push(i);
+      }
+    }
+
+    while (!q.empty()) {
+      int l = q.front();
+      q.pop();
+
+      for (auto r : edges[l]) {
+        int ll = right[r];
+        if (ll != -1 && dist[ll] == kInf) {
+          dist[ll] = dist[l] + 1;
+          q.push(ll);
+        }
+      }
+    }
+  }
+
+  bool DFS(int l) {
+    for (auto r : edges[l]) {
+      int ll = right[r];
+      if (ll == -1 || (dist[ll] == dist[l] + 1 && DFS(ll))) {
+        used[l] = true;
+        left[l] = r;
+        right[r] = l;
+        return true;
+      }
+    }
+    return false;
+  }
+
+  int nmax;
+  vector<bool> used;
+  vector<int> left, right, dist;
+  vector<vector<int>> edges;
+};
