f1bf09a2d9c1c05f9bd98ca855c7f40b25f70c9a
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-04-06 18:55:22 +0900

    33634 - 4교시: 국어 (Easy)

--- BaekJoon/33000/33634.cpp
+++ BaekJoon/33000/33634.cpp
@@ -1,88 +1,71 @@
 // Title : 4교시: 국어 (Easy)
 // Link  : https://www.acmicpc.net/problem/33634 
-// Time  : 492 ms
-// Memory: 644920 KB
+// Time  : 20 ms
+// Memory: 3360 KB
 
 #include <bits/stdc++.h>
 
 using namespace std;
 
-int memo[401][401][401];
-int nxt[1001][100001];
+int ans;
+vector<int> trr, arr, brr, crr;
+bool visited[1000][11][11][11];
+
+void Traverse(int a, int b, int c, int idx) {
+  if (idx == trr.size()) {
+    ans = max<int>(ans, a + b + c);
+    return;
+  }
+
+  if (visited[idx][a][b][c]) return;
+  visited[idx][a][b][c] = true;
+
+  int t = trr[idx];
+  auto ab = arr.begin() + a;
+  auto ae = upper_bound(ab, arr.end(), arr[a] + t);
+  for (auto ait = ae; ait != ab;) {
+    int ad = *--ait - arr[a];
+    auto bb = brr.begin() + b;
+    auto be = upper_bound(bb, brr.end(), brr[b] + t - ad);
+    int cmax = -1;
+    for (auto bit = be; bit != bb;) {
+      int bd = *--bit - brr[b];
+      auto cb = crr.begin() + c;
+      auto ce = upper_bound(cb, crr.end(), crr[c] + t - ad - bd);
+      if (cb != ce && cmax < (ce - cb) - 1) {
+        cmax = (ce - cb) - 1;
+        auto cit = ce - 1;
+        int cd = *cit - crr[c];
+        Traverse(ait - arr.begin(), bit - brr.begin(), cit - crr.begin(), idx + 1);
+      }
+    }
+  }
+}
 
 int main() {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
 
-  int nt, te;
-  cin >> nt >> te;
-  vector<int> trr(nt);
+  int n, t;
+  cin >> n >> t;
+  trr.resize(n);
   for (auto& e : trr) cin >> e;
+  for (int i = 1; i < n; i++) {
+    trr[i - 1] = trr[i] - trr[i - 1] - 1;
+  }
+  trr.pop_back();
 
   int na, nb, nc;
   cin >> na >> nb >> nc;
-  vector<int> arr(na), brr(nb), crr(nc);
-  for (auto& e : arr) cin >> e;
-  for (auto& e : brr) cin >> e;
-  for (auto& e : crr) cin >> e;
+  arr.resize(na + 1), brr.resize(nb + 1), crr.resize(nc + 1);
+  for (int i = 1; i <= na; i++) cin >> arr[i];
+  for (int i = 1; i <= nb; i++) cin >> brr[i];
+  for (int i = 1; i <= nc; i++) cin >> crr[i];
+  partial_sum(arr.begin(), arr.end(), arr.begin());
+  partial_sum(brr.begin(), brr.end(), brr.begin());
+  partial_sum(crr.begin(), crr.end(), crr.begin());
 
-  for (int i = 1; i <= 1000; i++) {
-    int r = te;
-    for (auto l : views::reverse(trr)) {
-      for (int j = r; j > l; j--) nxt[i][j] = j;
-      r = min<int>(r, l - i);
-    }
-    r = 0;
-    for (int j = te; j >= 0; j--) {
-      if (nxt[i][j]) {
-        r = j;
-      } else {
-        nxt[i][j] = r;
-      }
-    }
-  }
-
-  int ans;
-  int nabc = na + nb + nc;
-  fill_n((int*)memo, 401 * 401 * 401, INT_MAX);
-  memo[0][0][0] = 0;
-  for (ans = 0; ans < nabc; ans++) {
-    bool none = true;
-    for (int a = 0; a <= na && a <= ans; a++) {
-      for (int b = 0; b <= nb && a + b <= ans; b++) {
-        int c = ans - a - b;
-        if (c > nc) continue;
-
-        int cur = memo[a][b][c];
-        if (cur == INT_MAX) continue;
-
-        if (a < na) {
-          int axt = nxt[arr[a]][cur];
-          if (axt) {
-            none = false;
-            memo[a + 1][b][c] = min<int>(memo[a + 1][b][c], axt + arr[a]);
-          }
-        }
-
-        if (b < nb) {
-          int bxt = nxt[brr[b]][cur];
-          if (bxt) {
-            none = false;
-            memo[a][b + 1][c] = min<int>(memo[a][b + 1][c], bxt + brr[b]);
-          }
-        }
-
-        if (c < nc) {
-          int cxt = nxt[crr[c]][cur];
-          if (cxt) {
-            none = false;
-            memo[a][b][c + 1] = min<int>(memo[a][b][c + 1], cxt + crr[c]);
-          }
-        }
-      }
-    }
-    if (none) break;
-  }
+  Traverse(0, 0, 0, 0);
   cout << ans;
 
   return 0;
