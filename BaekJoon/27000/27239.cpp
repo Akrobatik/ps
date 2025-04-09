5ec182cb25791257949befafd0c5408e1be649d8
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-04-09 15:46:23 +0900

    27239 - Шахматная доска

---
+++ BaekJoon/27000/27239.cpp
@@ -0,0 +1,21 @@
+// Title : Шахматная доска
+// Link  : https://www.acmicpc.net/problem/27239 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  cin >> n;
+  --n;
+  int quot = n / 8, rem = n % 8;
+  cout << (char)(rem + 'a') << (char)(quot + '1');
+
+  return 0;
+}
