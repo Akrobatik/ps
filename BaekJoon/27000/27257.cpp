341b079d5dbbe1cb77d38f3dcf977fd1a2c59d1c
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-04-09 15:42:58 +0900

    27257 - Любитель нулей

---
+++ BaekJoon/27000/27257.cpp
@@ -0,0 +1,23 @@
+// Title : Любитель нулей
+// Link  : https://www.acmicpc.net/problem/27257 
+// Time  : 0 ms
+// Memory: 2024 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  string s;
+  cin >> s;
+  while (!s.empty() && s.back() == '0') s.pop_back();
+
+  int cnt = 0;
+  for (auto c : s) cnt += (c == '0');
+  cout << cnt;
+
+  return 0;
+}
