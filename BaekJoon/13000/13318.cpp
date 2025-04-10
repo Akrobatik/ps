396b34b5a408480c956d777e487db73dac6e4524
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-04-10 20:49:09 +0900

    13318 - 위험한 해싱

---
+++ BaekJoon/13000/13318.cpp
@@ -0,0 +1,21 @@
+// Title : 위험한 해싱
+// Link  : https://www.acmicpc.net/problem/13318 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+constexpr const char* kS1 = "oaaahaadfabaagaadfiagokacdaajaalaagaagecddjdacafadeaaaalggaaaaacacaabaafaeaeaaacabaaaaadacaaaaaaaaaa";
+constexpr const char* kS2 = "amiharcaacaggaadaaaaaaafaagiabcaacafhaaaaaaaraaajaaaggiaaahehbcacadfajfacabaakdaeaeeabbaaacaaaaaaaaa";
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  cout << kS1 << "\n"
+       << kS2;
+
+  return 0;
+}
