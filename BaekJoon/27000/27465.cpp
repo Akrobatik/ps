c338e140257a1c76a5a3570537e0b27960dc89b2
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-04-10 12:08:29 +0900

    27465 - 소수가 아닌 수

---
+++ BaekJoon/27000/27465.cpp
@@ -0,0 +1,17 @@
+// Title : 소수가 아닌 수
+// Link  : https://www.acmicpc.net/problem/27465 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  cout << (int)(1e9);
+
+  return 0;
+}
