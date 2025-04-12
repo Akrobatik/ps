1cfc0016761ec805b1f31242b2e8885a1828a0ec
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-04-12 13:43:49 +0900

    1105 - 팔

---
+++ BaekJoon/01000/1105.cpp
@@ -0,0 +1,30 @@
+// Title : 팔
+// Link  : https://www.acmicpc.net/problem/1105 
+// Time  : 0 ms
+// Memory: 2024 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  string l, r;
+  cin >> l >> r;
+  if (l.size() != r.size()) {
+    cout << "0";
+    return 0;
+  }
+
+  int n = l.size();
+  int cnt = 0;
+  for (int i = 0; i < n; i++) {
+    if (l[i] != r[i]) break;
+    cnt += (l[i] == '8');
+  }
+  cout << cnt;
+
+  return 0;
+}
