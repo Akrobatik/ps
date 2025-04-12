80fb3abf3caa52483f105161f8097c94047f4352
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-04-13 01:51:05 +0900

    17610 - 양팔저울

---
+++ BaekJoon/17000/17610.cpp
@@ -0,0 +1,40 @@
+// Title : 양팔저울
+// Link  : https://www.acmicpc.net/problem/17610 
+// Time  : 8 ms
+// Memory: 4560 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int arr[13];
+bool memo[2600001];
+
+void Traverse(int i, int e, int sum) {
+  if (i == e) {
+    memo[abs(sum)] = true;
+    return;
+  }
+
+  Traverse(i + 1, e, sum);
+  Traverse(i + 1, e, sum + arr[i]);
+  Traverse(i + 1, e, sum - arr[i]);
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n, sum = 0;
+  cin >> n;
+  for (int i = 0; i < n; i++) cin >> arr[i], sum += arr[i];
+
+  Traverse(0, n, 0);
+  int cnt = sum;
+  for (int i = 1; i <= sum; i++) {
+    cnt -= memo[i];
+  }
+  cout << cnt;
+
+  return 0;
+}
