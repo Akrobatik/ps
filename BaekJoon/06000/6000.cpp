cb24a4211dcbecd3d28cd271ba4a7dfb4288de1a
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-04-12 13:29:07 +0900

    6000 - 동전 게임

--- BaekJoon/06000/6000.cpp
+++ BaekJoon/06000/6000.cpp
@@ -1,6 +1,6 @@
 // Title : 동전 게임
 // Link  : https://www.acmicpc.net/problem/6000 
-// Time  : 28 ms
+// Time  : 20 ms
 // Memory: 17668 KB
 
 #include <bits/stdc++.h>
@@ -20,13 +20,16 @@ int main() {
   partial_sum(arr, arr + n + 1, arr);
 
   for (int i = 1; i <= n; i++) {
-    for (int j = 1; j <= n; j++) {
-      memo[i][j] = max<int>(memo[i][j], memo[i][j - 1]);
+    for (int j = 1; j <= ((i + 1) >> 1); j++) {
+      memo[i][j] = memo[i][j - 1];
       for (auto step : {j * 2 - 1, j * 2}) {
         if (step <= i) {
           memo[i][j] = max<int>(memo[i][j], arr[i] - memo[i - step][step]);
         }
       }
+    }
+    for (int j = ((i + 1) >> 1) + 1; j <= n; j++) {
+      memo[i][j] = memo[i][j - 1];
     }
   }
   cout << memo[n][1];
