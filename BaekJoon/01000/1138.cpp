630dfe9cf4c81c176a97ff0359175b97d6d7ea4d
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-04-12 14:01:49 +0900

    1138 - 한 줄로 서기

---
+++ BaekJoon/01000/1138.cpp
@@ -0,0 +1,35 @@
+// Title : 한 줄로 서기
+// Link  : https://www.acmicpc.net/problem/1138 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  cin >> n;
+  int memo[10] = {};
+  for (int i = 1; i <= n; i++) {
+    int x;
+    cin >> x;
+    int cnt = 0;
+    for (int j = 0; j < n; j++) {
+      if (memo[j]) continue;
+      if (++cnt > x) {
+        memo[j] = i;
+        break;
+      }
+    }
+  }
+
+  for (int i = 0; i < n; i++) {
+    cout << memo[i] << " ";
+  }
+
+  return 0;
+}
