256dabfaebdf10a8dd8cbf064ad271323a2e974c
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-04-12 15:57:04 +0900

    7140 - 데이터 만들기 1

---
+++ BaekJoon/07000/7140.cpp
@@ -0,0 +1,22 @@
+// Title : 데이터 만들기 1
+// Link  : https://www.acmicpc.net/problem/7140 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  cout << "101\n";
+  for (int i = 0; i < 101; i++) {
+    cout << "0\n";
+  }
+  cout << "1\n";
+  cout << "0 1";
+
+  return 0;
+}
