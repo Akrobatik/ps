fa30c347d8bf083f909e7036deff837b5eb6569d
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-04-13 14:53:21 +0900

    21314 - 민겸 수

---
+++ BaekJoon/21000/21314.cpp
@@ -0,0 +1,62 @@
+// Title : 민겸 수
+// Link  : https://www.acmicpc.net/problem/21314 
+// Time  : 0 ms
+// Memory: 2024 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string MaxMK(const string& s) {
+  string res;
+  res.reserve(s.size());
+  int cnt = 0;
+  for (auto c : s) {
+    if (c == 'M') {
+      ++cnt;
+    } else {
+      res.push_back('5');
+      res.resize(res.size() + cnt, '0');
+      cnt = 0;
+    }
+  }
+  if (cnt) {
+    res.resize(res.size() + cnt, '1');
+  }
+  return res;
+}
+
+string MinMK(const string& s) {
+  string res;
+  res.reserve(s.size());
+  int cnt = 0;
+  for (auto c : s) {
+    if (c == 'M') {
+      ++cnt;
+    } else {
+      if (cnt != 0) {
+        res.push_back('1');
+        res.resize(res.size() + cnt - 1, '0');
+      }
+      res.push_back('5');
+      cnt = 0;
+    }
+  }
+  if (cnt) {
+    res.push_back('1');
+    res.resize(res.size() + cnt - 1, '0');
+  }
+  return res;
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  string s;
+  cin >> s;
+  cout << MaxMK(s) << "\n"
+       << MinMK(s);
+
+  return 0;
+}
