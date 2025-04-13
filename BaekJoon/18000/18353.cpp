c4cd277ded55f022ef909eaf564d9e39756ed0e2
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-04-13 13:21:49 +0900

    18353 - 병사 배치하기

---
+++ BaekJoon/18000/18353.cpp
@@ -0,0 +1,31 @@
+// Title : 병사 배치하기
+// Link  : https://www.acmicpc.net/problem/18353 
+// Time  : 0 ms
+// Memory: 2156 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  cin >> n;
+  vector<int> arr;
+  for (int i = 0; i < n; i++) {
+    int x;
+    cin >> x;
+    auto it = lower_bound(arr.begin(), arr.end(), x, greater<>());
+    if (it != arr.end()) {
+      *it = x;
+    } else {
+      arr.push_back(x);
+    }
+  }
+
+  cout << n - arr.size();
+
+  return 0;
+}
