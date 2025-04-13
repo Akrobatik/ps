2b7e91de4f4a79959231adcb33c0b82d4b80d8d3
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-04-13 21:42:50 +0900

    30999 - 민주주의

---
+++ BaekJoon/30000/30999.cpp
@@ -0,0 +1,29 @@
+// Title : 민주주의
+// Link  : https://www.acmicpc.net/problem/30999 
+// Time  : 0 ms
+// Memory: 2024 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n, m;
+  cin >> n >> m;
+  int ans = 0;
+  while (n--) {
+    string s;
+    cin >> s;
+    int cnt = 0;
+    for (int i = 0; i < m; i++) {
+      cnt += (s[i] == 'O');
+    }
+    ans += (cnt >= ((m + 1) >> 1));
+  }
+  cout << ans;
+
+  return 0;
+}
