417f2570ab66c24ce5a5b540b9cc0a58754ad30f
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-04-13 21:11:08 +0900

    31005 - 귤나무

---
+++ BaekJoon/31000/31005.cpp
@@ -0,0 +1,39 @@
+// Title : 귤나무
+// Link  : https://www.acmicpc.net/problem/31005 
+// Time  : 32 ms
+// Memory: 6784 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int64_t n, m;
+  cin >> n >> m;
+  vector<int64_t> arr(n);
+  int64_t sum = 0;
+  for (auto& e : arr) {
+    cin >> e;
+    sum += e;
+  }
+
+  while (!arr.empty()) {
+    m %= sum;
+    vector<int64_t> nxt;
+    sum = 0;
+    for (auto e: arr) {
+      if (e <= m) {
+        m -= e;
+        sum += e;
+        nxt.push_back(e);
+      }
+    }
+    arr.swap(nxt);
+  }
+  cout << m;
+
+  return 0;
+}
