3e341a1a6ded34d04653d12d956edae800fe9d20
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-04-15 17:31:31 +0900

    24313 - 알고리즘 수업 - 점근적 표기 1

---
+++ BaekJoon/24000/24313.cpp
@@ -0,0 +1,20 @@
+// Title : 알고리즘 수업 - 점근적 표기 1
+// Link  : https://www.acmicpc.net/problem/24313 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int a1, a0, c, n0;
+  cin >> a1 >> a0 >> c >> n0;
+
+  cout << (a0 + a1 * n0 <= c * n0 && a1 <= c);
+
+  return 0;
+}
