bd08e6776f639e81258a0e534535b24a5795cecf
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-04-15 17:33:17 +0900

    10926 - ??!

---
+++ BaekJoon/10000/10926.cpp
@@ -0,0 +1,19 @@
+// Title : ??!
+// Link  : https://www.acmicpc.net/problem/10926 
+// Time  : 0 ms
+// Memory: 2024 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  string s;
+  cin >> s;
+  cout << s << "??!";
+
+  return 0;
+}
