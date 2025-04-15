fa1cd5cea02cd149d4aeef10aad820fe456d0ec0
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-04-15 17:35:39 +0900

    10430 - 나머지

---
+++ BaekJoon/10000/10430.cpp
@@ -0,0 +1,22 @@
+// Title : 나머지
+// Link  : https://www.acmicpc.net/problem/10430 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int a, b, c;
+  cin >> a >> b >> c;
+  cout << (a + b) % c << "\n"
+       << (a % c + b % c) % c << "\n"
+       << a * b % c << "\n"
+       << (a % c) * (b % c) % c;
+
+  return 0;
+}
