cb15ff0c0d4c27d0cc0c30232421c4111ebfd0ee
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-04-17 00:37:34 +0900

    2504 - 괄호의 값

---
+++ BaekJoon/02000/2504.cpp
@@ -0,0 +1,59 @@
+// Title : 괄호의 값
+// Link  : https://www.acmicpc.net/problem/2504 
+// Time  : 0 ms
+// Memory: 2024 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  string s;
+  cin >> s;
+
+  int64_t x = 1, ans = 0;
+  vector<char> memo;
+  char last;
+  for (auto c : s) {
+    switch (c) {
+      case '(': {
+        x *= 2;
+        memo.push_back(c);
+      } break;
+      case ')': {
+        if (memo.empty() || memo.back() != '(') {
+          cout << "0";
+          return 0;
+        }
+        if (last == '(') ans += x;
+        x /= 2;
+        memo.pop_back();
+      } break;
+      case '[': {
+        x *= 3;
+        memo.push_back(c);
+      } break;
+      case ']': {
+        if (memo.empty() || memo.back() != '[') {
+          cout << "0";
+          return 0;
+        }
+        if (last == '[') ans += x;
+        x /= 3;
+        memo.pop_back();
+      } break;
+    }
+    last = c;
+  }
+
+  if (memo.empty()) {
+    cout << ans;
+  } else {
+    cout << "0";
+  }
+
+  return 0;
+}
