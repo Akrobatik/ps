7f5c1573c312252520ab8610ec7971e2646650da
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-04-17 05:07:35 +0900

    25311 - UCPC에서 가장 쉬운 문제 번호는?

---
+++ BaekJoon/25000/25311.cpp
@@ -0,0 +1,17 @@
+// Title : UCPC에서 가장 쉬운 문제 번호는?
+// Link  : https://www.acmicpc.net/problem/25311 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  cout << 'A';
+
+  return 0;
+}
