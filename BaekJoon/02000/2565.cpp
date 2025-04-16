e4b3f9dd386fb84c898a3fc14bf87aef481d8b0e
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-04-17 01:06:14 +0900

    2565 - 전깃줄

---
+++ BaekJoon/02000/2565.cpp
@@ -0,0 +1,28 @@
+// Title : 전깃줄
+// Link  : https://www.acmicpc.net/problem/2565 
+// Time  : 0 ms
+// Memory: 2028 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int memo[501];
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  cin >> n;
+  vector<pair<int, int>> arr(n);
+  for (auto& [l, r] : arr) cin >> l >> r;
+  sort(arr.begin(), arr.end());
+
+  for (auto [l, r] : arr) {
+    memo[r] = max<int>(memo[r], *max_element(memo, memo + r) + 1);
+  }
+  cout << n - *max_element(memo, memo + 501);
+
+  return 0;
+}
