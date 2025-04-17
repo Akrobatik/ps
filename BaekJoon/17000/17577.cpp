b9c33b7da1434a3fa34a7c6b11e39f95cad14d98
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-04-17 21:24:45 +0900

    17577 - Scores of Final Examination

---
+++ BaekJoon/17000/17577.cpp
@@ -0,0 +1,33 @@
+// Title : Scores of Final Examination
+// Link  : https://www.acmicpc.net/problem/17577 
+// Time  : 8 ms
+// Memory: 2272 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n, m;
+  while (cin >> n >> m && n && m) {
+    vector<vector<int>> arr(m, vector<int>(n));
+    for (auto& vec : arr) {
+      for (auto& e : vec) cin >> e;
+    }
+
+    int ans = 0;
+    for (int i = 0; i < n; i++) {
+      int sum = 0;
+      for (int j = 0; j < m; j++) {
+        sum += arr[j][i];
+      }
+      ans = max<int>(ans, sum);
+    }
+    cout << ans << "\n";
+  }
+
+  return 0;
+}
