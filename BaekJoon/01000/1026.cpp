8bd191a73d0e3acee58fbc8cb0a081b1b9654a21
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-04-17 21:27:04 +0900

    1026 - 보물

---
+++ BaekJoon/01000/1026.cpp
@@ -0,0 +1,29 @@
+// Title : 보물
+// Link  : https://www.acmicpc.net/problem/1026 
+// Time  : 0 ms
+// Memory: 2024 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  cin >> n;
+  vector<int> arr(n), brr(n);
+  for (auto& e : arr) cin >> e;
+  for (auto& e : brr) cin >> e;
+  sort(arr.begin(), arr.end());
+  sort(brr.begin(), brr.end(), greater<>());
+
+  int sum = 0;
+  for (int i = 0; i < n; i++) {
+    sum += arr[i] * brr[i];
+  }
+  cout << sum;
+
+  return 0;
+}
