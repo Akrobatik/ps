7c1f7463c57e39c6143bb9ad31a1cf1a9b4f91c4
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-04-18 04:03:33 +0900

    13975 - 파일 합치기 3

---
+++ BaekJoon/13000/13975.cpp
@@ -0,0 +1,41 @@
+// Title : 파일 합치기 3
+// Link  : https://www.acmicpc.net/problem/13975 
+// Time  : 644 ms
+// Memory: 14436 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  priority_queue<int64_t, vector<int64_t>, greater<>> pq;
+
+  int t;
+  cin >> t;
+  while (t--) {
+    int n;
+    cin >> n;
+    while (n--) {
+      int x;
+      cin >> x;
+      pq.push(x);
+    }
+
+    int64_t ans = 0;
+    while (pq.size() != 1) {
+      int64_t a = pq.top();
+      pq.pop();
+      int64_t b = pq.top();
+      pq.pop();
+      ans += a + b;
+      pq.push(a + b);
+    }
+    pq.pop();
+    cout << ans << "\n";
+  }
+
+  return 0;
+}
