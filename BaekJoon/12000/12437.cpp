d0352943b766693f91376152c02ea29b4cb6e7c3
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-04-17 21:41:20 +0900

    12437 - 새로운 달력 (Small)

---
+++ BaekJoon/12000/12437.cpp
@@ -0,0 +1,37 @@
+// Title : 새로운 달력 (Small)
+// Link  : https://www.acmicpc.net/problem/12437 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int Calc(int a, int b, int c) {
+  int res = 0, x = 0;
+  for (int i = 0; i < a; i ++) {
+    res += (b + x) / c;
+    if ((b + x) % c == 0) {
+      x = 0;
+    } else {
+      ++res;
+      x = (b + x) % c;
+    }
+  }
+  return res;
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int t;
+  cin >> t;
+  for (int i = 1; i <= t; i++) {
+    int a, b, c;
+    cin >> a >> b >> c;
+    cout << "Case #" << i << ": " << Calc(a, b, c) << "\n";
+  }
+
+  return 0;
+}
