a2073bdf3e2e0241793bd34c7b44fb4d12380b0f
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-04-18 01:43:42 +0900

    1456 - 거의 소수

---
+++ BaekJoon/01000/1456.cpp
@@ -0,0 +1,39 @@
+// Title : 거의 소수
+// Link  : https://www.acmicpc.net/problem/1456 
+// Time  : 124 ms
+// Memory: 18060 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+constexpr int kMax = 1e7;
+
+bool seive[kMax + 1];
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int64_t a, b;
+  cin >> a >> b;
+
+  vector<int> primes;
+  for (int64_t i = 2; i * i <= b; i++) {
+    if (!seive[i]) primes.push_back(i);
+    for (auto p : primes) {
+      if (i * p > kMax) break;
+      seive[i * p] = true;
+      if (i % p == 0) break;
+    }
+  }
+
+  int64_t sum = 0;
+  for (auto p : primes) {
+    __int128_t x = (__int128_t)p * p;
+    while (x <= b) sum += (x >= a), x *= p;
+  }
+  cout << sum;
+
+  return 0;
+}
