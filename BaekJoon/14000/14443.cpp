676c60ad3369fd541c45bbaadee9d2e58980ba90
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-04-18 18:10:25 +0900

    14443 - 채점 소수 번호

---
+++ BaekJoon/14000/14443.cpp
@@ -0,0 +1,17 @@
+// Title : 채점 소수 번호
+// Link  : https://www.acmicpc.net/problem/14443 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  cout << "https://github.com/Akrobatik/ps";
+
+  return 0;
+}
