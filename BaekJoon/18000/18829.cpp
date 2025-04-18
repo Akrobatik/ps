38871e33a7969dbebefae8798abc58dc4e149353
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-04-18 18:15:00 +0900

    18829 - 0초 후에 제출할 수 있습니다.

---
+++ BaekJoon/18000/18829.cpp
@@ -0,0 +1,17 @@
+// Title : 0초 후에 제출할 수 있습니다.
+// Link  : https://www.acmicpc.net/problem/18829 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  cout << "https://github.com/Akrobatik/ps";
+
+  return 0;
+}
