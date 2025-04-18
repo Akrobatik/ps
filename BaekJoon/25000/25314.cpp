a13c41663f993c0173ddbd882a06adf390b026df
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-04-18 21:28:57 +0900

    25314 - 코딩은 체육과목 입니다

---
+++ BaekJoon/25000/25314.cpp
@@ -0,0 +1,23 @@
+// Title : 코딩은 체육과목 입니다
+// Link  : https://www.acmicpc.net/problem/25314 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  cin >> n;
+  n = (n + 3) / 4;
+  while (n--) {
+    cout << "long ";
+  }
+  cout << "int";
+
+  return 0;
+}
