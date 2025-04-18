dc4ee6143dc729fc0a1211c49e6f039096e2e61d
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-04-18 18:27:38 +0900

    20499 - Darius님 한타 안 함?

---
+++ BaekJoon/20000/20499.cpp
@@ -0,0 +1,25 @@
+// Title : Darius님 한타 안 함?
+// Link  : https://www.acmicpc.net/problem/20499 
+// Time  : 0 ms
+// Memory: 2024 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  string s;
+  getline(cin, s, '/');
+  int k = stoi(s);
+  getline(cin, s, '/');
+  int d = stoi(s);
+  getline(cin, s, '/');
+  int a = stoi(s);
+
+  cout << ((k + a < d || d == 0) ? "hasu" : "gosu");
+
+  return 0;
+}
