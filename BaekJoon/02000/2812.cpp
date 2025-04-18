8e1b1ac66647e14feaf69e6ce0f0eee03fc7852f
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-04-18 16:42:52 +0900

    2812 - 크게 만들기

---
+++ BaekJoon/02000/2812.cpp
@@ -0,0 +1,29 @@
+// Title : 크게 만들기
+// Link  : https://www.acmicpc.net/problem/2812 
+// Time  : 4 ms
+// Memory: 2952 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n, k;
+  string s;
+  cin >> n >> k >> s;
+
+  int ds = n - k;
+  string dst;
+  dst.reserve(ds + 1);
+  for (auto c : s) {
+    while (k && !dst.empty() && dst.back() < c) --k, dst.pop_back();
+    dst.push_back(c);
+  }
+  dst.resize(ds);
+  cout << dst;
+
+  return 0;
+}
