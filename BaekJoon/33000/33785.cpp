ac66f2523f534e30d2162020e7283dc8d00b2c8b
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-04-21 07:59:40 +0900

    33785 - High Towers

---
+++ BaekJoon/33000/33785.cpp
@@ -0,0 +1,19 @@
+// Title : High Towers
+// Link  : https://www.acmicpc.net/problem/33785 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int64_t n;
+  cin >> n;
+  cout << n * (n + 1) * (n + 2) / 6;
+
+  return 0;
+}
