dbd0266370f6eee4b48e8884ffe4713d3394ca29
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-04-21 07:47:07 +0900

    33788 - Stone Jump

---
+++ BaekJoon/33000/33788.cpp
@@ -0,0 +1,28 @@
+// Title : Stone Jump
+// Link  : https://www.acmicpc.net/problem/33788 
+// Time  : 0 ms
+// Memory: 2648 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int t;
+  cin >> t;
+  while (t--) {
+    int n;
+    string s;
+    cin >> n >> s;
+    if (n == 1 || (s.front() != 'L' || s.back() != 'R')) {
+      cout << "YES\n";
+    } else {
+      cout << "NO\n";
+    }
+  }
+
+  return 0;
+}
