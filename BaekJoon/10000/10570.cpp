44cf59ca14a351af474233fd3ac96c9833c5f249
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-04-20 23:01:21 +0900

    10570 - Favorite Number

---
+++ BaekJoon/10000/10570.cpp
@@ -0,0 +1,35 @@
+// Title : Favorite Number
+// Link  : https://www.acmicpc.net/problem/10570 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int t;
+  cin >> t;
+  while (t--) {
+    int n;
+    cin >> n;
+    int cnts[1001] = {};
+    int maxx = 0, ans;
+    while (n--) {
+      int x;
+      cin >> x;
+      int cnt = ++cnts[x];
+      if (cnt == maxx) {
+        ans = min<int>(ans, x);
+      } else if (cnt == maxx + 1) {
+        ++maxx, ans = x;
+      }
+    }
+    cout << ans << "\n";
+  }
+
+  return 0;
+}
