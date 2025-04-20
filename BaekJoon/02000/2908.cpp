d88b6fa69a59adc7ac855dc688f7eb43d801d239
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-04-20 23:13:24 +0900

    2908 - 상수

---
+++ BaekJoon/02000/2908.cpp
@@ -0,0 +1,22 @@
+// Title : 상수
+// Link  : https://www.acmicpc.net/problem/2908 
+// Time  : 0 ms
+// Memory: 2024 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  string s1, s2;
+  cin >> s1 >> s2;
+  reverse(s1.begin(), s1.end());
+  reverse(s2.begin(), s2.end());
+  int n1 = stoi(s1), n2 = stoi(s2);
+  cout << max<int>(n1, n2);
+
+  return 0;
+}
