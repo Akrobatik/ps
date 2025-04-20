f0331e55f0e254a8ed717d4504f3d0db539c4b9c
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-04-20 22:11:30 +0900

    13319 - 가짜 소수

---
+++ BaekJoon/13000/13319.cpp
@@ -0,0 +1,17 @@
+// Title : 가짜 소수
+// Link  : https://www.acmicpc.net/problem/13319 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  cout << 464052305161ll << " " << 8521;
+
+  return 0;
+}
