a6e1d9d1f6c2778ab3092e49ca26943758bbe0b6
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-04-22 07:10:23 +0900

    10813 - 공 바꾸기

---
+++ BaekJoon/10000/10813.cpp
@@ -0,0 +1,28 @@
+// Title : 공 바꾸기
+// Link  : https://www.acmicpc.net/problem/10813 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int memo[101] = {};
+  iota(memo, memo + 101, 0);
+
+  int n, m;
+  cin >> n >> m;
+  while (m--) {
+    int a, b;
+    cin >> a >> b;
+    swap(memo[a], memo[b]);
+  }
+
+  for (int i = 1; i <= n; i++) cout << memo[i] << " ";
+
+  return 0;
+}
