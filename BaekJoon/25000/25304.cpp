70ebd39e0024f81c28dd6f475e00ec1749e59f0c
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-04-22 07:03:39 +0900

    25304 - 영수증

---
+++ BaekJoon/25000/25304.cpp
@@ -0,0 +1,25 @@
+// Title : 영수증
+// Link  : https://www.acmicpc.net/problem/25304 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int x, n, sum = 0;
+  cin >> x >> n;
+  while (n--) {
+    int a, b;
+    cin >> a >> b;
+    sum += a * b;
+  }
+
+  cout << (sum == x ? "Yes" : "No");
+
+  return 0;
+}
