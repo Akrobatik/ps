de64117f25d491c7e0fb102b011a44796a61c1ed
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-04-22 06:51:43 +0900

    18108 - 1998년생인 내가 태국에서는 2541년생?!

---
+++ BaekJoon/18000/18108.cpp
@@ -0,0 +1,19 @@
+// Title : 1998년생인 내가 태국에서는 2541년생?!
+// Link  : https://www.acmicpc.net/problem/18108 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int y;
+  cin >> y;
+  cout << y - 2541 + 1998;
+
+  return 0;
+}
