a22383482ef10e19a57785acca277e255b9a8c4e
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-04-22 07:11:40 +0900

    5597 - 과제 안 내신 분..?

---
+++ BaekJoon/05000/5597.cpp
@@ -0,0 +1,27 @@
+// Title : 과제 안 내신 분..?
+// Link  : https://www.acmicpc.net/problem/5597 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  bool check[31] = {};
+  for (int i = 0; i < 28; i++) {
+    int x;
+    cin >> x;
+    check[x] = true;
+  }
+
+  for (int i = 1; i <= 30; i++) {
+    if (check[i]) continue;
+    cout << i << "\n";
+  }
+
+  return 0;
+}
