142f1e6d66b5cad6ee7e63836471800689a268cb
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-04-22 07:12:23 +0900

    2743 - 단어 길이 재기

---
+++ BaekJoon/02000/2743.cpp
@@ -0,0 +1,19 @@
+// Title : 단어 길이 재기
+// Link  : https://www.acmicpc.net/problem/2743 
+// Time  : 0 ms
+// Memory: 2024 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  string s;
+  cin >> s;
+  cout << s.size();
+
+  return 0;
+}
