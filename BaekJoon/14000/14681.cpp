8f93b470a76b4b61d5b38e94bdcbee87bba14cf4
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-04-22 06:55:04 +0900

    14681 - 사분면 고르기

---
+++ BaekJoon/14000/14681.cpp
@@ -0,0 +1,27 @@
+// Title : 사분면 고르기
+// Link  : https://www.acmicpc.net/problem/14681 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int x, y;
+  cin >> x >> y;
+  if (x > 0 && y > 0) {
+    cout << 1;
+  } else if (x > 0 && y < 0) {
+    cout << 4;
+  } else if (x < 0 && y > 0) {
+    cout << 2;
+  } else {
+    cout << 3;
+  }
+
+  return 0;
+}
