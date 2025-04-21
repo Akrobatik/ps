0f2fd48d031b037c10d46ca25a50613a48e64c1a
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-04-22 06:26:30 +0900

    3009 - 네 번째 점

---
+++ BaekJoon/03000/3009.cpp
@@ -0,0 +1,19 @@
+// Title : 네 번째 점
+// Link  : https://www.acmicpc.net/problem/3009 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int y1, x1, y2, x2, y3, x3;
+  cin >> y1 >> x1 >> y2 >> x2 >> y3 >> x3;
+  cout << (y1 ^ y2 ^ y3) << " " << (x1 ^ x2 ^ x3);
+
+  return 0;
+}
