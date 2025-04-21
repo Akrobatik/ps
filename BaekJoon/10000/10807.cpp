1ceae212ff8b50f1038622d4e526b6b8d7b3a3a2
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-04-22 07:06:03 +0900

    10807 - 개수 세기

---
+++ BaekJoon/10000/10807.cpp
@@ -0,0 +1,28 @@
+// Title : 개수 세기
+// Link  : https://www.acmicpc.net/problem/10807 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int cnt[201] = {};
+
+  int n;
+  cin >> n;
+  while (n--) {
+    int x;
+    cin >> x;
+    ++cnt[x + 100];
+  }
+  int v;
+  cin >> v;
+  cout << cnt[v + 100];
+
+  return 0;
+}
