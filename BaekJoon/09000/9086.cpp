a011581107582349580f6d2d5fb1c3bf66281e52
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-04-22 07:13:07 +0900

    9086 - 문자열

---
+++ BaekJoon/09000/9086.cpp
@@ -0,0 +1,23 @@
+// Title : 문자열
+// Link  : https://www.acmicpc.net/problem/9086 
+// Time  : 0 ms
+// Memory: 2024 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int t;
+  cin >> t;
+  while (t--) {
+    string s;
+    cin >> s;
+    cout << s.front() << s.back() << "\n";
+  }
+
+  return 0;
+}
