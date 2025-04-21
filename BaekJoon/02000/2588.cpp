b56a27dcb5d2653c27a0ef80858e2dd4c278b2f1
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-04-22 06:53:11 +0900

    2588 - 곱셈

---
+++ BaekJoon/02000/2588.cpp
@@ -0,0 +1,22 @@
+// Title : 곱셈
+// Link  : https://www.acmicpc.net/problem/2588 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int a, b;
+  cin >> a >> b;
+  cout << a * (b % 10) << "\n"
+       << a * (b / 10 % 10) << "\n"
+       << a * (b / 100) << "\n"
+       << a * b;
+
+  return 0;
+}
