b8265a6abcf1e6583bb3222b8c6208a90455fafc
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-04-22 06:56:37 +0900

    2525 - 오븐 시계

---
+++ BaekJoon/02000/2525.cpp
@@ -0,0 +1,20 @@
+// Title : 오븐 시계
+// Link  : https://www.acmicpc.net/problem/2525 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int a, b, c;
+  cin >> a >> b >> c;
+  int t = a * 60 + b + c;
+  cout << (t / 60) % 24 << " " << t % 60;
+
+  return 0;
+}
