a67a970047d8ea793f873fe29589b58a2e00a513
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-04-22 07:19:32 +0900

    5622 - 다이얼

---
+++ BaekJoon/05000/5622.cpp
@@ -0,0 +1,23 @@
+// Title : 다이얼
+// Link  : https://www.acmicpc.net/problem/5622 
+// Time  : 0 ms
+// Memory: 2024 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int memo[] = {3, 3, 3, 4, 4, 4, 5, 5, 5, 6, 6, 6, 7, 7, 7, 8, 8, 8, 8, 9, 9, 9, 10, 10, 10, 10};
+
+  string s;
+  cin >> s;
+  int sum = 0;
+  for (auto c : s) sum += memo[c - 'A'];
+  cout << sum;
+
+  return 0;
+}
