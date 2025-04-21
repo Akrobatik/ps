2945ae293ee8776d478582e29f8c85ce91797d2e
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-04-22 07:01:58 +0900

    8393 - 합

---
+++ BaekJoon/08000/8393.cpp
@@ -0,0 +1,19 @@
+// Title : 합
+// Link  : https://www.acmicpc.net/problem/8393 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  cin >> n;
+  cout << n * (n + 1) / 2;
+
+  return 0;
+}
