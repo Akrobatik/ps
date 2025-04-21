d6badc70c1fc405434912e54a8cb837df60d0282
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-04-21 22:48:36 +0900

    27433 - 팩토리얼 2

---
+++ BaekJoon/27000/27433.cpp
@@ -0,0 +1,21 @@
+// Title : 팩토리얼 2
+// Link  : https://www.acmicpc.net/problem/27433 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int64_t x = 1;
+  int n;
+  cin >> n;
+  for (int i = 2; i <= n; i++) x *= i;
+  cout << x;
+
+  return 0;
+}
