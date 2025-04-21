4c9bf24ac13126355d4477c89a7cb07459d8686b
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-04-22 07:04:33 +0900

    15552 - 빠른 A+B

---
+++ BaekJoon/15000/15552.cpp
@@ -0,0 +1,23 @@
+// Title : 빠른 A+B
+// Link  : https://www.acmicpc.net/problem/15552 
+// Time  : 192 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int t;
+  cin >> t;
+  while (t--) {
+    int a, b;
+    cin >> a >> b;
+    cout << a + b << "\n";
+  }
+
+  return 0;
+}
