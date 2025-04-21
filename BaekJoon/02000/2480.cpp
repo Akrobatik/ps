c94e7a2cf5c960323cbbb1067149296f0d04f92e
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-04-22 07:00:12 +0900

    2480 - 주사위 세개

---
+++ BaekJoon/02000/2480.cpp
@@ -0,0 +1,31 @@
+// Title : 주사위 세개
+// Link  : https://www.acmicpc.net/problem/2480 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int a, b, c;
+  cin >> a >> b >> c;
+  int d = (a == b) + (b == c) + (c == a);
+
+  if (d == 3) {
+    cout << 10000 + a * 1000;
+  } else if (d == 1) {
+    if (a == b || b == c) {
+      cout << 1000 + 100 * b;
+    } else {
+      cout << 1000 + 100 * a;
+    }
+  } else {
+    cout << max<int>({a, b, c}) * 100;
+  }
+
+  return 0;
+}
