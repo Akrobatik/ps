b4042de8c753e5ac947ff4f8bf53662cc07805f2
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-04-22 23:40:35 +0900

    13206 - Professor KCM

--- BaekJoon/13000/13206.cpp
+++ BaekJoon/13000/13206.cpp
@@ -1,7 +1,7 @@
 // Title : Professor KCM
 // Link  : https://www.acmicpc.net/problem/13206 
-// Time  : 220 ms
-// Memory: 2180 KB
+// Time  : 120 ms
+// Memory: 2052 KB
 
 #include <bits/stdc++.h>
 
@@ -9,42 +9,63 @@ using namespace std;
 
 constexpr int kMod = 1e9 + 7;
 
-vector<pair<int, int>> memo[1001];
+constexpr array<int, 168> kPrimes = []() {
+  array<int, 168> primes{};
+  bool seive[1001] = {};
+  int idx = 0;
+  for (int i = 2; i <= 1000; i++) {
+    if (!seive[i]) primes[idx++] = i;
+    for (auto p : primes) {
+      if (p == 0 || i * p > 1000) break;
+      seive[i * p] = true;
+      if (i % p == 0) break;
+    }
+  }
+  return primes;
+}();
+
+constexpr array<array<pair<int, int>, 4>, 1001> kFacts = []() {
+  array<array<pair<int, int>, 4>, 1001> facts{};
+  for (int i = 2; i <= 1000; i++) {
+    int x = i;
+    auto it = facts[i].begin();
+    for (auto p : kPrimes) {
+      if (x % p == 0) {
+        int cnt = 0;
+        while (x % p == 0) x /= p, ++cnt;
+        *it++ = {p, cnt};
+        if (x == 1) break;
+      }
+    }
+  }
+  return facts;
+}();
 
 int main() {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
-
-  for (int i = 2; i <= 1000; i++) {
-    int x = i;
-    for (int j = 2; x != 1 && j * j <= i; j++) {
-      if (x % j == 0) {
-        int cnt = 0;
-        while (x % j == 0) x /= j, ++cnt;
-        memo[i].push_back({j, cnt});
-      }
-    }
-    if (x != 1) memo[i].push_back({x, 1});
-  }
 
   int t;
   cin >> t;
   while (t--) {
     int n;
     cin >> n;
-    map<int, int> facts;
+
+    int memo[1001] = {};
     while (n--) {
       int x;
       cin >> x;
-      for (auto [p, cnt] : memo[x]) {
-        facts[p] = max<int>(facts[p], cnt);
+      for (auto [p, cnt] : kFacts[x]) {
+        if (p == 0) break;
+        memo[p] = max<int>(memo[p], cnt);
       }
     }
 
     int ans = 1;
-    for (auto [p, cnt] : facts) {
+    for (auto p : kPrimes) {
+      if (memo[p] == 0) continue;
       int x = 1;
-      while (cnt--) x *= p;
+      while (memo[p]--) x *= p;
       ans = (int64_t)ans * x % kMod;
     }
     cout << ans << "\n";
