425eeb1e88f147f7d169b9fbc4d7df774f22ad93
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-04-22 17:48:24 +0900

    1316 - 그룹 단어 체커

---
+++ BaekJoon/01000/1316.cpp
@@ -0,0 +1,38 @@
+// Title : 그룹 단어 체커
+// Link  : https://www.acmicpc.net/problem/1316 
+// Time  : 0 ms
+// Memory: 2024 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int t, cnt = 0;
+  cin >> t;
+  while (t--) {
+    string s;
+    cin >> s;
+
+    bool check[256] = {};
+    bool ok = true;
+    int idx = 0;
+    while (idx < s.size()) {
+      int x = s[idx];
+      if (check[x]) {
+        ok = false;
+        break;
+      }
+      check[x] = true;
+
+      while (idx < s.size() && s[idx] == x) ++idx;
+    }
+    cnt += ok;
+  }
+  cout << cnt;
+
+  return 0;
+}
