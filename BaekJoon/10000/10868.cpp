8e8df8cd618ff8548a7ba6fc742ab528da2b38ab
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-04-22 17:24:22 +0900

    10868 - 최솟값

--- BaekJoon/10000/10868.cpp
+++ BaekJoon/10000/10868.cpp
@@ -1,6 +1,6 @@
 // Title : 최솟값
 // Link  : https://www.acmicpc.net/problem/10868 
-// Time  : 52 ms
+// Time  : 48 ms
 // Memory: 3044 KB
 
 #include <bits/stdc++.h>
@@ -11,11 +11,16 @@ constexpr int kMax = 1 << 17;
 
 int tree[kMax << 1];
 
+int Get(int node) {
+  int x = tree[node];
+  return x ? x : INT_MAX;
+}
+
 int Query(int l, int r) {
   int res = INT_MAX;
   for (l += kMax, r += kMax + 1; l < r; l >>= 1, r >>= 1) {
-    if (l & 1) res = min<int>(res, tree[l++]);
-    if (r & 1) res = min<int>(res, tree[--r]);
+    if (l & 1) res = min<int>(res, Get(l++));
+    if (r & 1) res = min<int>(res, Get(--r));
   }
   return res;
 }
@@ -24,15 +29,13 @@ int main() {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
 
-  fill_n(tree, kMax << 1, INT_MAX);
-
   int n, m;
   cin >> n >> m;
   for (int i = 0; i < n; i++) {
     int x;
     cin >> x;
     int node = i + kMax;
-    while (node && tree[node] > x) tree[node] = x, node >>= 1;
+    while (node && Get(node) > x) tree[node] = x, node >>= 1;
   }
 
   while (m--) {
