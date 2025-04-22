f4cca017184dbf40baa84eb4f26f595491a27679
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-04-22 22:11:54 +0900

    25378 - 조약돌

---
+++ BaekJoon/25000/25378.cpp
@@ -0,0 +1,39 @@
+// Title : 조약돌
+// Link  : https://www.acmicpc.net/problem/25378 
+// Time  : 0 ms
+// Memory: 2156 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  cin >> n;
+  vector<int> arr(n);
+  for (auto& e : arr) cin >> e;
+
+  int sum = 0, cnt = 0;
+  set<int> memo;
+  for (int i = 0; i < n; i++) {
+    ++cnt;
+    if (memo.contains(arr[i])) {
+      memo.clear();
+      sum += cnt - 1;
+      cnt = 0;
+    } else {
+      set<int> nxt;
+      for (auto x : memo) {
+        if (arr[i] - x > 0) nxt.insert(arr[i] - x);
+      }
+      nxt.insert(arr[i]);
+      memo.swap(nxt);
+    }
+  }
+  cout << sum + cnt;
+
+  return 0;
+}
