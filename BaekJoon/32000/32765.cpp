441eabe021f6ee356ad7985ca3552897c2897c82
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-04-23 18:06:20 +0900

    32765 - 연봉 998244353원 주세요

---
+++ BaekJoon/32000/32765.cpp
@@ -0,0 +1,34 @@
+// Title : 연봉 998244353원 주세요
+// Link  : https://www.acmicpc.net/problem/32765 
+// Time  : 28 ms
+// Memory: 2916 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int q;
+  vector<int64_t> arr{0};
+  cin >> arr[0] >> q;
+
+  int64_t x, y;
+  do {
+    x = arr.size(), y = (arr.back() / x + 1) * x;
+    arr.push_back(y);
+  } while (x * x != y);
+
+  while (q--) {
+    cin >> y;
+    if (y < arr.size()) {
+      cout << arr[y] << "\n";
+    } else {
+      cout << x * y << "\n";
+    }
+  }
+
+  return 0;
+}
