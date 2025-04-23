27e67f7522b1d2d694e5290aa393fdbfbddd6dce
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-04-24 03:14:23 +0900

    14241 - 슬라임 합치기

---
+++ BaekJoon/14000/14241.cpp
@@ -0,0 +1,36 @@
+// Title : 슬라임 합치기
+// Link  : https://www.acmicpc.net/problem/14241 
+// Time  : 0 ms
+// Memory: 2024 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  priority_queue<int, vector<int>, greater<int>> pq;
+
+  int n;
+  cin >> n;
+  while (n--) {
+    int x;
+    cin >> x;
+    pq.push(x);
+  }
+
+  int sum = 0;
+  while (pq.size() != 1) {
+    int a = pq.top();
+    pq.pop();
+    int b = pq.top();
+    pq.pop();
+    sum += a * b;
+    pq.push(a + b);
+  }
+  cout << sum;
+
+  return 0;
+}
