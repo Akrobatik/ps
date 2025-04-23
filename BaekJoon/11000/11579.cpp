8d26971ba50be0e9cde4322d5356f0bebd455536
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-04-23 18:22:20 +0900

    11579 - 초차원전쟁 이나

---
+++ BaekJoon/11000/11579.cpp
@@ -0,0 +1,49 @@
+// Title : 초차원전쟁 이나
+// Link  : https://www.acmicpc.net/problem/11579 
+// Time  : 692 ms
+// Memory: 3128 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int memo[500][500];
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  vector<int64_t> arr;
+
+  int t;
+  cin >> t;
+  while (t--) {
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < n; j++) {
+        cin >> memo[i][j];
+      }
+    }
+
+    arr.resize(n);
+    for (auto& e : arr) cin >> e;
+
+    int64_t sum = 0;
+    int idx = 0;
+    while (idx < n && arr[idx] >= 0) {
+      int64_t x = arr[idx];
+      sum += x;
+      for (int i = idx; i < n; i++) arr[i] -= x * memo[idx][i];
+      ++idx;
+    }
+
+    if (idx == n && sum <= (int)2e10) {
+      cout << "1 " << sum << "\n";
+    } else {
+      cout << "0\n";
+    }
+  }
+
+  return 0;
+}
