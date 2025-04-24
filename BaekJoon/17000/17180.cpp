7bea1a8d377f0b2938070f3fce0142a566900d6d
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-04-24 19:45:54 +0900

    17180 - 문자열 비교하기

---
+++ BaekJoon/17000/17180.cpp
@@ -0,0 +1,31 @@
+// Title : 문자열 비교하기
+// Link  : https://www.acmicpc.net/problem/17180 
+// Time  : 0 ms
+// Memory: 2380 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int memo[301][301];
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  fill_n((int*)memo, 301 * 301, INT_MAX >> 1);
+
+  int n1, n2;
+  string s1, s2;
+  cin >> n1 >> n2 >> s1 >> s2;
+
+  memo[0][0] = 0;
+  for (int i = 1; i <= n1; i++) {
+    for (int j = 1; j <= n2; j++) {
+      memo[i][j] = min<int>({memo[i - 1][j - 1], memo[i - 1][j], memo[i][j - 1]}) + abs(s1[i - 1] - s2[j - 1]);
+    }
+  }
+  cout << memo[n1][n2];
+
+  return 0;
+}
