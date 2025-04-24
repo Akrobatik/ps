50d4dd48f3d0634a17d9b849ab0e511d42853758
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-04-24 20:48:16 +0900

    1374 - 강의실

---
+++ BaekJoon/01000/1374.cpp
@@ -0,0 +1,30 @@
+// Title : 강의실
+// Link  : https://www.acmicpc.net/problem/1374 
+// Time  : 48 ms
+// Memory: 3704 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  cin >> n;
+  vector<pair<int, int>> arr(n);
+  for (auto& [b, e] : arr) cin >> b >> b >> e;
+  sort(arr.begin(), arr.end());
+
+  int maxx = 0;
+  priority_queue<int, vector<int>, greater<int>> pq;
+  for (auto [b, e] : arr) {
+    while (!pq.empty() && pq.top() <= b) pq.pop();
+    pq.push(e);
+    maxx = max<int>(maxx, pq.size());
+  }
+  cout << maxx;
+
+  return 0;
+}
