00e681987048b785133e74e285ba04c09b84c6d4
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-04-24 19:07:39 +0900

    33753 - 주차 요금 정산하기

---
+++ BaekJoon/33000/33753.cpp
@@ -0,0 +1,25 @@
+// Title : 주차 요금 정산하기
+// Link  : https://www.acmicpc.net/problem/33753 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int a, b, c, t;
+  cin >> a >> b >> c >> t;
+  t -= 30;
+
+  if (t <= 0) {
+    cout << a;
+  } else {
+    cout << a + ((t + b - 1) / b) * c;
+  }
+
+  return 0;
+}
