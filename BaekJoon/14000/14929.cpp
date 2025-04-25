029405f91f0492ae70c14384d962dc8e31b05051
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-04-25 23:32:03 +0900

    14929 - 귀찮아 (SIB)

---
+++ BaekJoon/14000/14929.cpp
@@ -0,0 +1,26 @@
+// Title : 귀찮아 (SIB)
+// Link  : https://www.acmicpc.net/problem/14929 
+// Time  : 8 ms
+// Memory: 2412 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n, sum = 0;
+  cin>> n;
+  vector<int> arr(n);
+  for (auto& e : arr) cin >> e, sum += e;
+
+  int64_t ans = 0;
+  for (auto e : arr) {
+    ans += e * (sum - e);
+  }
+  cout << ans / 2;
+
+  return 0;
+}
