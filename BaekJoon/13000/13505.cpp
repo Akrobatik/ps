03bfaefcd9e9a2ec74653fe90b69aa6f979427f2
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-04-25 20:45:58 +0900

    13505 - 두 수 XOR

---
+++ BaekJoon/13000/13505.cpp
@@ -0,0 +1,68 @@
+// Title : 두 수 XOR
+// Link  : https://www.acmicpc.net/problem/13505 
+// Time  : 72 ms
+// Memory: 37176 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct Node {
+  Node() : cnt(0), nxt{} {}
+
+  int cnt;
+  array<int, 2> nxt;
+};
+
+int nidx;
+Node nodes[3000001];
+
+void Insert(int x) {
+  bitset<30> bits(x);
+
+  Node* node = nodes;
+  int idx = 30;
+  while (idx--) {
+    int bit = bits.test(idx);
+    if (node->nxt[bit] == 0) node->nxt[bit] = ++nidx;
+    node = nodes + node->nxt[bit];
+    ++node->cnt;
+  }
+}
+
+int Query(int x) {
+  int res = 0;
+  bitset<30> bits(x);
+
+  Node* node = nodes;
+  int idx = 30, mask = 1 << 29;
+  while (idx--) {
+    int bit = bits.test(idx);
+    if (node->nxt[bit ^ 1]) {
+      res |= mask;
+      node = nodes + node->nxt[bit ^ 1];
+    } else {
+      node = nodes + node->nxt[bit];
+    }
+    mask >>= 1;
+  }
+  return res;
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n, x;
+  cin >> n >> x;
+  Insert(x);
+  int maxx = 0;
+  while (--n) {
+    cin >> x;
+    maxx = max<int>(maxx, Query(x));
+    Insert(x);
+  }
+  cout << maxx;
+
+  return 0;
+}
