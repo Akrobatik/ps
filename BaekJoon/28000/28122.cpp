d3aba93ffc4ebe283a1782f483227812c6b1326f
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-04-25 20:38:47 +0900

    28122 - 아이템

--- BaekJoon/28000/28122.cpp
+++ BaekJoon/28000/28122.cpp
@@ -1,34 +1,24 @@
 // Title : 아이템
 // Link  : https://www.acmicpc.net/problem/28122 
-// Time  : 212 ms
-// Memory: 5148 KB
+// Time  : 172 ms
+// Memory: 3584 KB
 
 #include <bits/stdc++.h>
 
 using namespace std;
 
-int64_t tmp[200000];
-
 int Traverse(span<int64_t> arr, int rem, int add) {
   if (arr.empty()) return 0;
   if (rem == 0) return arr.size();
 
-  int cnt[2] = {};
-  for (auto e : arr) ++cnt[e & 1];
+  auto ab = arr.data(), ae = ab + arr.size();
+  auto am = partition(ab, ae, [](int64_t x) {
+    return x & 1;
+  });
+  for (auto& e : arr) e >>= 1;
 
-  auto brr = arr.subspan(0, cnt[0]);
-  auto crr = arr.subspan(cnt[0]);
-
-  cnt[1] += cnt[0];
-  for (auto e : views::reverse(arr)) {
-    tmp[--cnt[e & 1]] = e;
-  }
-
-  int n = arr.size();
-  for (int i = 0; i < n; i++) {
-    arr[i] = tmp[i] >> 1;
-  }
-
+  auto brr = span<int64_t>(ab, am);
+  auto crr = span<int64_t>(am, ae);
   int maxx = max<int>(Traverse(brr, rem - 1, 1), Traverse(crr, rem - 1, 1)) + add;
   return min<int>(maxx, arr.size());
 }
