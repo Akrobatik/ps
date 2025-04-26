ddc743cdc6f301645b876a37cf15efbb2595443e
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-04-27 04:49:51 +0900

    5217 - 쌍의 합

---
+++ BaekJoon/05000/5217.cpp
@@ -0,0 +1,29 @@
+// Title : 쌍의 합
+// Link  : https://www.acmicpc.net/problem/5217 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int t;
+  cin >> t;
+  while (t--) {
+    int n;
+    cin >> n;
+    cout << "Pairs for " << n << ":";
+
+    int half = (n + 1) >> 1;
+    for (int i = 1; i < half; i++) {
+      cout << (i == 1 ? " " : ", ") << i << " " << n - i;
+    }
+    cout << "\n";
+  }
+
+  return 0;
+}
