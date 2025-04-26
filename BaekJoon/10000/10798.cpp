2c1004c0305a3b4d80011f4efcc76a0a3d54720b
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-04-27 05:21:05 +0900

    10798 - 세로읽기

---
+++ BaekJoon/10000/10798.cpp
@@ -0,0 +1,28 @@
+// Title : 세로읽기
+// Link  : https://www.acmicpc.net/problem/10798 
+// Time  : 0 ms
+// Memory: 2024 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string s[5];
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  for (int i = 0; i < 5; i++) {
+    cin >> s[i];
+  }
+
+  for (int i = 0; i < 15; i++) {
+    for (int j = 0; j < 5; j++) {
+      if (s[j].size() <= i) continue;
+      cout << s[j][i];
+    }
+  }
+
+  return 0;
+}
