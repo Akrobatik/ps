5bc6f38d9ddeec7d8c0fe83d78042ac8ef13bd0d
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-04-26 23:48:48 +0900

    10872 - 팩토리얼

---
+++ BaekJoon/10000/10872.cpp
@@ -0,0 +1,21 @@
+// Title : 팩토리얼
+// Link  : https://www.acmicpc.net/problem/10872 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  cin >> n;
+  int64_t x = 1;
+  for (int i = 2; i <= n; i++) x *= i;
+  cout << x;
+
+  return 0;
+}
