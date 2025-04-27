64d771a0e8e0048fd8314f3d6b4b8d9140143758
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-04-27 23:58:02 +0900

    10464 - XOR

---
+++ BaekJoon/10000/10464.cpp
@@ -0,0 +1,32 @@
+// Title : XOR
+// Link  : https://www.acmicpc.net/problem/10464 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+constexpr int kMax = 1 << 30;
+
+int Calc(int n) {
+  int res = 0;
+  if (!(n & 1)) res = n;
+  if (bitset<2>(n % 4).count() == 1) ++res;
+  return res;
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int t;
+  cin >> t;
+  while (t--) {
+    int a, b;
+    cin >> a >> b;
+    cout << (Calc(b) ^ Calc(a - 1)) << "\n";
+  }
+
+  return 0;
+}
