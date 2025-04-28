66b3a224d5f4762cf0c812a0eec9de9485b488be
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-04-29 07:49:58 +0900

    13034 - 다각형 게임

---
+++ BaekJoon/13000/13034.cpp
@@ -0,0 +1,35 @@
+// Title : 다각형 게임
+// Link  : https://www.acmicpc.net/problem/13034 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int memo[1001] = {0, 0, 1};
+
+  int n;
+  cin >> n;
+  for (int i = 3; i <= n; i++) {
+    int nxt[1001] = {};
+    int mid = i >> 1;
+    for (int j = 0; j <= mid; j++) {
+      int l = j, r = i - l - 2;
+      nxt[memo[l] ^ memo[r]] = 1;
+    }
+    for (int j = 0; j < i; j++) {
+      if (nxt[j]) continue;
+      memo[i] = j;
+      break;
+    }
+  }
+
+  cout << (char)('1' + (memo[n] == 0));
+
+  return 0;
+}
