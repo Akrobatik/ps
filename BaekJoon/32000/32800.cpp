dfbbb08f0798908b000e810fe40c43812b9f3e96
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-04-29 08:12:14 +0900

    32800 - Bus Assignment

---
+++ BaekJoon/32000/32800.cpp
@@ -0,0 +1,27 @@
+// Title : Bus Assignment
+// Link  : https://www.acmicpc.net/problem/32800 
+// Time  : 24 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int64_t x = 0, maxx = 0;
+
+  int n;
+  cin >> n;
+  for (int i = 0; i < n; i++) {
+    int a, b;
+    cin >> a >> b;
+    x += b - a;
+    maxx = max<int64_t>(maxx, x);
+  }
+  cout << maxx;
+
+  return 0;
+}
