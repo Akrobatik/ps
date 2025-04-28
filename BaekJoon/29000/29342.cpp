42d2e56ca3fa0b71332db296308b13cdc3914fef
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-04-29 08:08:40 +0900

    29342 - Война клонов

---
+++ BaekJoon/29000/29342.cpp
@@ -0,0 +1,29 @@
+// Title : Война клонов
+// Link  : https://www.acmicpc.net/problem/29342 
+// Time  : 8 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  cin >> n;
+  int a = 0, b = 0;
+  for (int i = 0; i < n; i++) {
+    int x;
+    cin >> x;
+    if (x & 1) {
+      ++a;
+    } else {
+      ++b;
+    }
+  }
+  cout << (int64_t)a * b;
+
+  return 0;
+}
