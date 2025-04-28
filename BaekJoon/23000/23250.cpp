3466217ba0b8df30129a7800fdc33ad3846da1a0
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-04-29 07:38:04 +0900

    23250 - 하노이 탑 K

---
+++ BaekJoon/23000/23250.cpp
@@ -0,0 +1,31 @@
+// Title : 하노이 탑 K
+// Link  : https://www.acmicpc.net/problem/23250 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void Traverse(int n, int64_t r, int a, int b, int c) {
+  int64_t mid = 1ll << (n - 1);
+  if (r < mid) {
+    Traverse(n - 1, r, a, c, b);
+  } else if (r == mid) {
+    cout << a << " " << c;
+    exit(0);
+  } else {
+    Traverse(n - 1, r - mid, b, a, c);
+  }
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int64_t n, k;
+  cin >> n >> k;
+  Traverse(n, k, 1, 2, 3);
+
+  return 0;
+}
