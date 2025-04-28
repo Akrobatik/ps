117fe9e5b0e370b2a1e3b72d9ab17ee070341b0c
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-04-29 04:17:31 +0900

    1010 - 다리 놓기

---
+++ BaekJoon/01000/1010.cpp
@@ -0,0 +1,28 @@
+// Title : 다리 놓기
+// Link  : https://www.acmicpc.net/problem/1010 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int t;
+  cin >> t;
+  while (t--) {
+    int l, r;
+    cin >> l >> r;
+    if (l > r - l) l = r - l;
+    int64_t x = 1;
+    for (int i = 0; i < l; i++) {
+      x = x * (r - i) / (i + 1);
+    }
+    cout << x << "\n";
+  }
+
+  return 0;
+}
