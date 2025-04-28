64a7d19bf38f9deacd3521c178e1dd9b803722cc
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-04-29 07:03:28 +0900

    28278 - 스택 2

---
+++ BaekJoon/28000/28278.cpp
@@ -0,0 +1,52 @@
+// Title : 스택 2
+// Link  : https://www.acmicpc.net/problem/28278 
+// Time  : 128 ms
+// Memory: 8292 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  vector<int> stk;
+
+  int n;
+  cin >> n;
+  while (n--) {
+    int op;
+    cin >> op;
+    switch (op) {
+      case 1: {
+        int x;
+        cin >> x;
+        stk.push_back(x);
+      } break;
+
+      case 2: {
+        if (stk.empty()) {
+          cout << "-1\n";
+        } else {
+          cout << stk.back() << "\n";
+          stk.pop_back();
+        }
+      } break;
+
+      case 3: {
+        cout << stk.size() << "\n";
+      } break;
+
+      case 4: {
+        cout << stk.empty() << "\n";
+      } break;
+
+      case 5: {
+        cout << (stk.empty() ? -1 : stk.back()) << "\n";
+      } break;
+    }
+  }
+
+  return 0;
+}
