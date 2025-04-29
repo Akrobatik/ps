6174291e48f797a38bf17d7f2139693ad345e992
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-04-29 15:16:36 +0900

    20490 - Рыцарский щит

---
+++ BaekJoon/20000/20490.cpp
@@ -0,0 +1,20 @@
+// Title : Рыцарский щит
+// Link  : https://www.acmicpc.net/problem/20490 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int a, b, c, d, e, f;
+  cin >> a >> b >> c >> d >> e >> f;
+  int g = min<int>(max<int>({a, b, c}), max<int>({d, e, f}));
+  cout << a + b + c + d + e + f - (g << 1);
+
+  return 0;
+}
