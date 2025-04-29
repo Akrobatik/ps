9c37844b6ba918c2013787f29e0edd17cfbda3af
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-04-29 09:54:43 +0900

    9933 - 민균이의 비밀번호

---
+++ BaekJoon/09000/9933.cpp
@@ -0,0 +1,33 @@
+// Title : 민균이의 비밀번호
+// Link  : https://www.acmicpc.net/problem/9933 
+// Time  : 0 ms
+// Memory: 2160 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  set<string> memo;
+
+  int n;
+  cin >> n;
+  while (n--) {
+    string s;
+    cin >> s;
+    if (!memo.insert(s).second) {
+      cout << s.size() << " " << s[s.size() >> 1];
+      return 0;
+    }
+    reverse(s.begin(), s.end());
+    if (!memo.insert(s).second) {
+      cout << s.size() << " " << s[s.size() >> 1];
+      return 0;
+    }
+  }
+
+  return 0;
+}
