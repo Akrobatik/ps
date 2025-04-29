309be1fda5dded4ec3772fba397ba77be8826502
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-04-29 14:05:05 +0900

    1427 - 소트인사이드

---
+++ BaekJoon/01000/1427.cpp
@@ -0,0 +1,20 @@
+// Title : 소트인사이드
+// Link  : https://www.acmicpc.net/problem/1427 
+// Time  : 0 ms
+// Memory: 2024 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  string s;
+  cin >> s;
+  sort(s.begin(), s.end(), greater<>());
+  cout << s;
+
+  return 0;
+}
