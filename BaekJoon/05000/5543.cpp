149f7351fc00120af8304b8c5a0c2d6537c1b170
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-04-30 11:50:34 +0900

    5543 - 상근날드

---
+++ BaekJoon/05000/5543.cpp
@@ -0,0 +1,19 @@
+// Title : 상근날드
+// Link  : https://www.acmicpc.net/problem/5543 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int a, b, c, d, e;
+  cin >> a >> b >> c >> d >> e;
+  cout << min<int>({a, b, c}) + min<int>(d, e) - 50;
+
+  return 0;
+}
