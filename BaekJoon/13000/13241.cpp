b27b0e7d025d864bc0a861a46c080f70a7373c8c
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-04-30 16:16:29 +0900

    13241 - 최소공배수

---
+++ BaekJoon/13000/13241.cpp
@@ -0,0 +1,24 @@
+// Title : 최소공배수
+// Link  : https://www.acmicpc.net/problem/13241 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int GCD(int a, int b) {
+  while (b) a %= b, swap(a, b);
+  return a;
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  int a, b;
+  cin >> a >> b;
+  cout << (int64_t)a * b / GCD(a, b);
+
+  return 0;
+}
