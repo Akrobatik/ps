89b65deef49689034322e81633723977e0e9166e
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-05-01 06:17:29 +0900

    23969 - 알고리즘 수업 - 버블 정렬 2

---
+++ BaekJoon/23000/23969.cpp
@@ -0,0 +1,33 @@
+// Title : 알고리즘 수업 - 버블 정렬 2
+// Link  : https://www.acmicpc.net/problem/23969 
+// Time  : 116 ms
+// Memory: 2180 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n, k;
+  cin >> n >> k;
+  vector<int> arr(n);
+  for (auto& e : arr) cin >> e;
+
+  for (int i = n - 1; i > 0; i--) {
+    for (int j = 0; j < i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        swap(arr[j], arr[j + 1]);
+        if (--k == 0) {
+          for (auto e : arr) cout << e << " ";
+          return 0;
+        }
+      }
+    }
+  }
+  cout << "-1";
+
+  return 0;
+}
