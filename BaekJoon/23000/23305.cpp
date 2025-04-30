155caa6b74fa1c7cbebcc914470e369f4dcce391
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-04-30 12:03:49 +0900

    23305 - 수강변경

---
+++ BaekJoon/23000/23305.cpp
@@ -0,0 +1,36 @@
+// Title : 수강변경
+// Link  : https://www.acmicpc.net/problem/23305 
+// Time  : 200 ms
+// Memory: 5928 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int memo[1000001];
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  cin >> n;
+  for (int i = 0; i < n; i++) {
+    int x;
+    cin >> x;
+    ++memo[x];
+  }
+  for (int i = 0; i < n; i++) {
+    int x;
+    cin >> x;
+    --memo[x];
+  }
+
+  int sum = 0;
+  for (int i = 1; i <= 1000000; i++) {
+    sum += max<int>(memo[i], 0);
+  }
+  cout << sum;
+
+  return 0;
+}
