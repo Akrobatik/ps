7bcd56c24131d77bf21c942b1c51e3cb8a4fe27d
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-04-30 13:28:30 +0900

    9627 - 문장

---
+++ BaekJoon/09000/9627.cpp
@@ -0,0 +1,96 @@
+// Title : 문장
+// Link  : https://www.acmicpc.net/problem/9627 
+// Time  : 0 ms
+// Memory: 2108 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const string kStr[] = {
+    "",
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine",
+    "ten",
+    "eleven",
+    "twelve",
+    "thirteen",
+    "fourteen",
+    "fifteen",
+    "sixteen",
+    "seventeen",
+    "eigthteen",
+    "nineteen",
+};
+
+const string kStr2[] = {
+    "",
+    "",
+    "twenty",
+    "thirty",
+    "forty",
+    "fifty",
+    "sixty",
+    "seventy",
+    "eighty",
+    "ninety",
+};
+
+const string kStr3[] = {
+    "",
+    "onehundred",
+    "twohundred",
+    "threehundred",
+    "fourhundred",
+    "fivehundred",
+    "sixhundred",
+    "sevenhundred",
+    "eighthundred",
+    "ninehundred",
+};
+
+string Get(int n) {
+  if (n >= 100) return kStr3[n / 100] + Get(n % 100);
+  if (n < 20) return kStr[n];
+  return kStr2[n / 10] + kStr[n % 10];
+}
+
+string Calc(int sum) {
+  int n = sum;
+  while (n++) {
+    string s = Get(n);
+    if (sum + s.size() == n) return s;
+  }
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  cin >> n;
+
+  int sum = 0;
+  ostringstream oss;
+  while (n--) {
+    string s;
+    cin >> s;
+    if (s == "$") {
+      oss << "%s ";
+    } else {
+      sum += s.size();
+      oss << s << " ";
+    }
+  }
+
+  printf(oss.str().c_str(), Calc(sum).c_str());
+
+  return 0;
+}
