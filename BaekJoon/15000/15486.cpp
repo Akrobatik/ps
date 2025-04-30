dfae8a4ac257732276ac03c442db65d1a04eb3ff
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-04-30 11:33:42 +0900

    15486 - 퇴사 2

---
+++ BaekJoon/15000/15486.cpp
@@ -0,0 +1,29 @@
+// Title : 퇴사 2
+// Link  : https://www.acmicpc.net/problem/15486 
+// Time  : 192 ms
+// Memory: 7880 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int memo[1500002];
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  cin >> n;
+  for (int i = 1; i <= n; i++) {
+    int t, p;
+    cin >> t >> p;
+    memo[i] = max<int>(memo[i], memo[i - 1]);
+    if (i + t - 1 <= n) {
+      memo[i + t] = max<int>(memo[i + t], memo[i] + p);
+    }
+  }
+  cout << max<int>(memo[n], memo[n + 1]);
+
+  return 0;
+}
