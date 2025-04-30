de8aab84a12df6fd03b32e377202ec505e0e4d0f
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-04-30 20:06:01 +0900

    31963 - 두 배

--- BaekJoon/31000/31963.cpp
+++ BaekJoon/31000/31963.cpp
@@ -1,7 +1,7 @@
 // Title : 두 배
 // Link  : https://www.acmicpc.net/problem/31963 
 // Time  : 28 ms
-// Memory: 3976 KB
+// Memory: 4960 KB
 
 #include <bits/stdc++.h>
 
@@ -11,24 +11,26 @@ int main() {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
 
-  vector<int64_t> brr(62);
-  for (int i = 0; i < brr.size(); i++) brr[i] = 1ll << i;
+  vector<int8_t> lrr(1000001);
+  for (int i = 2; i <= 1000000; i++) lrr[i] = lrr[i >> 1] + 1;
 
   int n;
   cin >> n;
-  vector<int64_t> arr(n);
+  vector<int> arr(n), brr(n);
   for (auto& e : arr) cin >> e;
 
-  int sum = 0;
+  int64_t ans = 0;
   for (int i = 1; i < n; i++) {
-    if (arr[i - 1] <= arr[i]) continue;
-    int64_t x = arr[i - 1] / arr[i];
-    auto it = lower_bound(brr.begin(), brr.end(), x);
-    sum += it - brr.begin();
-    arr[i] *= *it;
-    if (arr[i - 1] > arr[i]) arr[i] <<= 1, ++sum;
+    if (arr[i - 1] <= arr[i]) {
+      int l = lrr[arr[i] / arr[i - 1]];
+      brr[i] = max<int>(brr[i - 1] - l, 0);
+    } else {
+      int l = lrr[arr[i - 1] / arr[i]];
+      brr[i] = brr[i - 1] + l + (arr[i - 1] != (arr[i] << l));
+    }
+    ans += brr[i];
   }
-  cout << sum;
+  cout << ans;
 
   return 0;
 }
