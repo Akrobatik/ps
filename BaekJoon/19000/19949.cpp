17fe92602d9acd61d8d9c33da60a6dbf93adfd1c
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-05-02 07:48:09 +0900

    19949 - 영재의 시험

---
+++ BaekJoon/19000/19949.cpp
@@ -0,0 +1,35 @@
+// Title : 영재의 시험
+// Link  : https://www.acmicpc.net/problem/19949 
+// Time  : 28 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int cnt;
+int ans[10], memo[10];
+
+void Traverse(int idx, int score) {
+  if (idx == 10) {
+    cnt += (score >= 5);
+    return;
+  }
+
+  for (int i = 1; i <= 5; i++) {
+    if (idx >= 2 && memo[idx - 2] == i && memo[idx - 1] == i) continue;
+    memo[idx] = i;
+    Traverse(idx + 1, score + (ans[idx] == memo[idx]));
+  }
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  for (auto& e : ans) cin >> e;
+  Traverse(0, 0);
+  cout << cnt;
+
+  return 0;
+}
