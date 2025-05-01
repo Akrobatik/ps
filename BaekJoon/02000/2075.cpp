ad5672a5ba739e40925dc44a62af72bb234a77a0
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-05-02 02:22:08 +0900

    2075 - N번째 큰 수

---
+++ BaekJoon/02000/2075.cpp
@@ -0,0 +1,22 @@
+// Title : N번째 큰 수
+// Link  : https://www.acmicpc.net/problem/2075 
+// Time  : 396 ms
+// Memory: 10812 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  cin >> n;
+  vector<int> arr(n * n);
+  for (auto& e : arr) cin >> e;
+  sort(arr.begin(), arr.end(), greater<>());
+  cout << arr[n - 1];
+
+  return 0;
+}
