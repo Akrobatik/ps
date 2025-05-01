db810bc8c381c1d5d5c3ac627cf43a66a31394d1
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-05-01 12:41:20 +0900

    8245 - Obraz

---
+++ BaekJoon/08000/8245.cpp
@@ -0,0 +1,29 @@
+// Title : Obraz
+// Link  : https://www.acmicpc.net/problem/8245 
+// Time  : 28 ms
+// Memory: 3088 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n, m;
+  cin >> n >> m;
+  vector<vector<char>> board(n, vector<char>(m));
+  for (auto& vec : board) {
+    for (auto& e : vec) cin >> e;
+  }
+
+  for (int i = 0; i < m; i++) {
+    for (int j = n - 1; j >= 0; j--) {
+      cout << board[j][i];
+    }
+    cout << "\n";
+  }
+
+  return 0;
+}
