f6afe89eeef7d7055fdbd4e3be074ab1ea443434
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-05-01 12:23:21 +0900

    13411 - 하늘에서 정의가 빗발친다!

--- BaekJoon/13000/13411.cpp
+++ BaekJoon/13000/13411.cpp
@@ -1,7 +1,7 @@
 // Title : 하늘에서 정의가 빗발친다!
 // Link  : https://www.acmicpc.net/problem/13411 
 // Time  : 36 ms
-// Memory: 3612 KB
+// Memory: 3080 KB
 
 #include <bits/stdc++.h>
 
@@ -22,10 +22,11 @@ int main() {
     arr[i] = {a * a + b * b, c * c, i + 1};
   }
 
-  stable_sort(arr.begin(), arr.end(), [](const tup& lhs, const tup& rhs) {
+  sort(arr.begin(), arr.end(), [](const tup& lhs, const tup& rhs) {
     auto [ld, ls, li] = lhs;
     auto [rd, rs, ri] = rhs;
-    return (int64_t)ld * rs < (int64_t)rd * ls;
+    int64_t l = (int64_t)ld * rs, r = (int64_t)rd * ls;
+    return l < r || (l == r && li < ri);
   });
 
   for (auto [d, s, i] : arr) cout << i << "\n";
