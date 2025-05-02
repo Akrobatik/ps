ae938ad8862671b06c260fb52cb6427ddf5969c9
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-05-03 04:02:17 +0900

    13263 - 나무 자르기

--- BaekJoon/13000/13263.cpp
+++ BaekJoon/13000/13263.cpp
@@ -1,42 +1,56 @@
-// Title : 나무 자르기
-// Link  : https://www.acmicpc.net/problem/13263
+// Title : 나무 자르기
+// Link  : https://www.acmicpc.net/problem/13263 
 // Time  : 28 ms
-// Memory: 2808 KB
+// Memory: 2804 KB
 
 #include <bits/stdc++.h>
 
 using namespace std;
 
-// Convex Hull Trick
-// https://m.blog.naver.com/jinhan814/222689836982
-using i64 = long long;
-
-struct PLL {
-  i64 x, y;
-  PLL(const i64 x = 0, const i64 y = 0) : x(x), y(y) {}
-  bool operator<=(const PLL& i) const { return 1. * x / y <= 1. * i.x / i.y; }
-};
-
-struct ConvexHull {
+struct CHT {
  private:
-  static i64 F(const PLL& i, const i64 x) { return i.x * x + i.y; }
-  static PLL C(const PLL& a, const PLL& b) { return {a.y - b.y, b.x - a.x}; }
-  deque<PLL> S;
+  using Line = pair<int64_t, int64_t>;
 
  public:
-  void Insert(const i64 a, const i64 b) {
-    while (S.size() > 1 && C(S.back(), PLL(a, b)) <= C(S[S.size() - 2], S.back())) S.pop_back();
-    S.push_back(PLL(a, b));
+  void Init() {
+    lines.clear();
+    ptr = 0;
   }
-  i64 Query(const i64 x) {
-    while (S.size() > 1 && F(S[0], x) >= F(S[1], x)) S.pop_front();
-    return F(S[0], x);
+
+  void AddLine(int64_t a, int64_t b) {
+    Line line = {a, b};
+    while (ptr + 1 < lines.size() && Check(lines[lines.size() - 2], lines.back(), line)) lines.pop_back();
+    lines.push_back(line);
   }
-} CHT;
+
+  int64_t Query(int64_t x) {
+    auto F = [&](int i) {
+      auto [a, b] = lines[i];
+      return a * x + b;
+    };
+
+    while (ptr + 1 < lines.size() && F(ptr) >= F(ptr + 1)) ++ptr;
+    return F(ptr);
+  }
+
+ private:
+  bool Check(const Line& a, const Line& b, const Line& c) {
+    auto [aa, ab] = a;
+    auto [ba, bb] = b;
+    auto [ca, cb] = c;
+    return (__int128_t)(bb - cb) * (ca - aa) <= (__int128_t)(ca - ba) * (ab - cb);
+  }
+
+  vector<Line> lines;
+  int ptr;
+};
 
 int main() {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
+
+  CHT cht;
+  cht.Init();
 
   int n;
   cin >> n;
@@ -45,10 +59,10 @@ int main() {
   for (auto& e : b) cin >> e;
 
   int64_t ans = 0;
-  CHT.Insert(b[0], ans);
+  cht.AddLine(b[0], ans);
   for (int i = 1; i < n; i++) {
-    ans = CHT.Query(a[i]);
-    CHT.Insert(b[i], ans);
+    ans = cht.Query(a[i]);
+    cht.AddLine(b[i], ans);
   }
   cout << ans;
 
