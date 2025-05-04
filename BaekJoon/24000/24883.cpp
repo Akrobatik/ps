2c272862a979ff6af8349cf8cbf2b275b153b531
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-05-04 11:47:37 +0900

    24883 - 자동완성

---
+++ BaekJoon/24000/24883.cpp
@@ -0,0 +1,23 @@
+// Title : 자동완성
+// Link  : https://www.acmicpc.net/problem/24883 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  char c;
+  cin >> c;
+  if (tolower(c) == 'n') {
+    cout << "Naver D2";
+  } else {
+    cout << "Naver Whale";
+  }
+
+  return 0;
+}
