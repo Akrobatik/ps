d4a86a3f3d259daef7f7bf2d7ee7724505b76c27
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-05-04 11:47:29 +0900

    1940 - 주몽

---
+++ BaekJoon/01000/1940.cpp
@@ -0,0 +1,34 @@
+// Title : 주몽
+// Link  : https://www.acmicpc.net/problem/1940 
+// Time  : 0 ms
+// Memory: 2200 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n, m;
+  cin >> n >> m;
+  vector<int> arr(n);
+  for (auto& e : arr) cin >> e;
+  sort(arr.begin(), arr.end());
+
+  int l = 0, r = n - 1, cnt = 0;
+  while (l < r) {
+    int sum = arr[l] + arr[r];
+    if (sum == m) {
+      ++cnt, ++l, --r;
+    } else if (sum > m) {
+      --r;
+    } else {
+      ++l;
+    }
+  }
+  cout << cnt;
+
+  return 0;
+}
