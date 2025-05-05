003006331495c8ac24a7996e512bcd371d17e5e9
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-05-05 10:03:05 +0900

    33811 - Split The GSHS 4

---
+++ BaekJoon/33000/33811.cpp
@@ -0,0 +1,29 @@
+// Title : Split The GSHS 4
+// Link  : https://www.acmicpc.net/problem/33811 
+// Time  : 4 ms
+// Memory: 2912 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  string s;
+  cin >> n >> s;
+
+  string_view sv = s;
+  int cur = 0, maxx = 0;
+  for (int i = 3; i < n; i++) {
+    auto sub = sv.substr(i - 3, 4);
+    int add = (sub == "SSHS" ? 1 : sub == "GSHS" ? -1 : 0);
+    cur = max<int>(cur + add, 0);
+    maxx = max<int>(maxx, cur);
+  }
+  cout << maxx;
+
+  return 0;
+}
