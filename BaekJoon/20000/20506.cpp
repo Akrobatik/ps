7750363a4caaf07c486eb121baf46f4400fff9eb
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-05-05 19:32:04 +0900

    20506 - Kaisar - 생존

--- BaekJoon/20000/20506.cpp
+++ BaekJoon/20000/20506.cpp
@@ -1,47 +1,43 @@
 // Title : Kaisar - 생존
 // Link  : https://www.acmicpc.net/problem/20506 
-// Time  : 48 ms
-// Memory: 11572 KB
+// Time  : 28 ms
+// Memory: 5928 KB
 
 #include <bits/stdc++.h>
 
 using namespace std;
 
-vector<int64_t> memo;
-vector<int> edges[200001];
-
-int Traverse(int cur) {
-  int64_t all = 0;
-  int sum = 0;
-  for (auto nxt : edges[cur]) {
-    int cnt = Traverse(nxt);
-    all -= (int64_t)cnt * cnt;
-    sum += cnt;
-  }
-
-  ++sum;
-  all += (int64_t)sum * sum;
-  memo[cur] = all;
-  return sum;
-}
+int par[200001], deg[200001], cnts[200001];
+int64_t memo[200001];
 
 int main() {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
 
-  int n, r;
+  int n;
   cin >> n;
-  memo.resize(n + 1);
+  deg[0] = 1;
   for (int i = 1; i <= n; i++) {
     int x;
     cin >> x;
-    if (x) {
-      edges[x].push_back(i);
-    } else {
-      r = i;
-    }
+    par[i] = x;
+    ++deg[x];
   }
-  Traverse(r);
+
+  for (int i = 1; i <= n; i++) {
+    if (deg[i]) continue;
+
+    int cur = i;
+    do {
+      --deg[cur];
+      int nxt = par[cur];
+      int64_t x = ++cnts[cur];
+      cnts[nxt] += x;
+      memo[cur] += x * x;
+      memo[nxt] -= x * x;
+      cur = nxt;
+    } while (--deg[cur] == 0);
+  }
 
   int64_t o = 0, e = 0, cur = 0;
   for (int i = 1; i <= n; i++) {
