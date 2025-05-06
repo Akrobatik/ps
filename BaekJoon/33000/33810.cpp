7837bb1a89c981693c872d53c61e6e90af39ec2a
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-05-06 13:10:39 +0900

    33810 - SciComLove (2025)

---
+++ BaekJoon/33000/33810.cpp
@@ -0,0 +1,25 @@
+// Title : SciComLove (2025)
+// Link  : https://www.acmicpc.net/problem/33810 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+constexpr const char* kStr = "SciComLove";
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  char s[12];
+  cin >> s;
+  int cnt = 0;
+  for (int i = 0; s[i]; i++) {
+    cnt += (s[i] != kStr[i]);
+  }
+  cout << cnt;
+
+  return 0;
+}
