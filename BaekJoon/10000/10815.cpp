27ea82ec30ba86ada32561cbc67f930287aa7b98
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-05-06 10:42:16 +0900

    10815 - 숫자 카드

---
+++ BaekJoon/10000/10815.cpp
@@ -0,0 +1,35 @@
+// Title : 숫자 카드
+// Link  : https://www.acmicpc.net/problem/10815 
+// Time  : 140 ms
+// Memory: 4460 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+constexpr int kMax = 1e7;
+
+bitset<kMax * 2 + 1> memo;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  cin >> n;
+  while (n--) {
+    int x;
+    cin >> x;
+    memo.set(x + kMax);
+  }
+
+  int m;
+  cin >> m;
+  while (m--) {
+    int x;
+    cin >> x;
+    cout << memo.test(x + kMax) << " ";
+  }
+
+  return 0;
+}
