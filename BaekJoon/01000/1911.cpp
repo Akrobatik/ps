5567bdd06004d71e8fe6ab9a55b9558db359362d
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-05-06 22:18:52 +0900

    1911 - 흙길 보수하기

---
+++ BaekJoon/01000/1911.cpp
@@ -0,0 +1,32 @@
+// Title : 흙길 보수하기
+// Link  : https://www.acmicpc.net/problem/1911 
+// Time  : 4 ms
+// Memory: 2220 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n, m;
+  cin >> n >> m;
+  vector<pair<int, int>> arr(n);
+  for (auto& [l, r] : arr) cin >> l >> r;
+  sort(arr.begin(), arr.end());
+
+  int cur = 0, cnt = 0;
+  for (auto [l, r] : arr) {
+    l = max<int>(l, cur);
+    int w = r - l;
+    if (w <= 0) continue;
+    int x = (w + m - 1) / m;
+    cur = l + x * m;
+    cnt += x;
+  }
+  cout << cnt;
+
+  return 0;
+}
