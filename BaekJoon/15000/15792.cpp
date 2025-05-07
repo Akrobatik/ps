ec97263e0374e1ddb2e237a68ae9a5e8c5986207
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-05-07 10:14:54 +0900

    15792 - A/B - 2

---
+++ BaekJoon/15000/15792.cpp
@@ -0,0 +1,23 @@
+// Title : A/B - 2
+// Link  : https://www.acmicpc.net/problem/15792 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int a, b;
+  cin >> a >> b;
+  cout << a / b << ".";
+  for (int i = 0; i < 1000; i++) {
+    a = (a % b) * 10;
+    cout << a / b; 
+  }
+
+  return 0;
+}
