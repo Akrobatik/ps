fba6a057a5fdde0f7c9fcc68a729f1821d31a285
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-05-07 10:14:44 +0900

    15733 - 나는 누구인가

---
+++ BaekJoon/15000/15733.cpp
@@ -0,0 +1,17 @@
+// Title : 나는 누구인가
+// Link  : https://www.acmicpc.net/problem/15733 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  cout << "I'm Sexy";
+
+  return 0;
+}
