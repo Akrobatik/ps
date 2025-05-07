cab062ed882bf9217423f12f237599bcf8c215b7
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-05-07 10:15:07 +0900

    30823 - 건공문자열

---
+++ BaekJoon/30000/30823.cpp
@@ -0,0 +1,26 @@
+// Title : 건공문자열
+// Link  : https://www.acmicpc.net/problem/30823 
+// Time  : 12 ms
+// Memory: 2912 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n, k;
+  string s;
+  cin >> n >> k >> s;
+  --k;
+  for (int i = k; i < n; i++) cout << s[i];
+  if ((n - k + 1) & 1) {
+    for (int i = 0; i < k; i++) cout << s[i];
+  } else {
+    while (k--) cout << s[k];
+  }
+
+  return 0;
+}
