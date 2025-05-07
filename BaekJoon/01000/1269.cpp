ebed297db7d9b6fe2b584253c5a9bbcc5f1bdf90
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-05-07 17:03:01 +0900

    1269 - 대칭 차집합

---
+++ BaekJoon/01000/1269.cpp
@@ -0,0 +1,36 @@
+// Title : 대칭 차집합
+// Link  : https://www.acmicpc.net/problem/1269 
+// Time  : 76 ms
+// Memory: 26436 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+constexpr int kMax = 1e8;
+
+bitset<kMax + 1> s1, s2;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n, m;
+  cin >> n >> m;
+  int ans = n + m;
+  while (n--) {
+    int x;
+    cin >> x;
+    s1.set(x);
+  }
+  while (m--) {
+    int x;
+    cin >> x;
+    s2.set(x); 
+  }
+  s1 &= s2;
+  ans -= s1.count() * 2;
+  cout << ans;
+
+  return 0;
+}
