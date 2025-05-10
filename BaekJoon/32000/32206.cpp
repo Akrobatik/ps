7b3ade74d1407c136f57e7fcc158e9850c403222
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-05-10 22:51:24 +0900

    32206 - 아보와 킨텍스

---
+++ BaekJoon/32000/32206.cpp
@@ -0,0 +1,19 @@
+// Title : 아보와 킨텍스
+// Link  : https://www.acmicpc.net/problem/32206 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int64_t n;
+  cin >> n;
+  cout << (n + 1) * 26 - n;
+
+  return 0;
+}
