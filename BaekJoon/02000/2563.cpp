86063b028dfe75b73c0b990c7bd6846224b0011b
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-05-10 12:40:24 +0900

    2563 - 색종이

---
+++ BaekJoon/02000/2563.cpp
@@ -0,0 +1,37 @@
+// Title : 색종이
+// Link  : https://www.acmicpc.net/problem/2563 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  bool board[100][100] = {};
+
+  int n;
+  cin >> n;
+  while (n--) {
+    int y, x;
+    cin >> y >> x;
+    for (int i = 0; i < 10; i++) {
+      for (int j = 0; j < 10; j++) {
+        board[y + i][x + j] = true;
+      }
+    }
+  }
+
+  int cnt = 0;
+  for (int i = 0; i < 100; i++) {
+    for (int j = 0; j < 100; j++) {
+      cnt += board[i][j];
+    }
+  }
+  cout << cnt;
+
+  return 0;
+}
