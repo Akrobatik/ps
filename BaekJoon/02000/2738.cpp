a302cf95cf09483468ca81397ea1d10fdba77e81
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-05-10 12:37:35 +0900

    2738 - 행렬 덧셈

---
+++ BaekJoon/02000/2738.cpp
@@ -0,0 +1,37 @@
+// Title : 행렬 덧셈
+// Link  : https://www.acmicpc.net/problem/2738 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int board[100][100] = {};
+
+  int n, m;
+  cin >> n >> m;
+
+  for (int k = 0; k < 2; k++) {
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < m; j++) {
+        int x;
+        cin >> x;
+        board[i][j] += x;
+      }
+    }
+  }
+
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < m; j++) {
+      cout << board[i][j] << " ";
+    }
+    cout << "\n";
+  }
+
+  return 0;
+}
