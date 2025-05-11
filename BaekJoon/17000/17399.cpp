dc5fef8a7753369065f600c8bd9085211583ddc7
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-05-11 23:10:11 +0900

    17399 - 트리의 외심

---
+++ BaekJoon/17000/17399.cpp
@@ -0,0 +1,270 @@
+// Title : 트리의 외심
+// Link  : https://www.acmicpc.net/problem/17399 
+// Time  : 484 ms
+// Memory: 6828 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct LCT {
+  struct Node {
+    Node() : l(nullptr), r(nullptr), p(nullptr), sz(1), sum(1), rev(0) {}
+
+    Node *l, *r, *p;
+    int sz, sum, rev;
+  };
+
+  LCT(int n) : nodes(n + 1) {}
+
+  void MakeRoot(int x) {
+    MakeRoot(&nodes[x]);
+  }
+
+  // set node value for DP
+  void Set(int x, int w) {
+    Access(&nodes[x]);
+    //
+    Pull(&nodes[x]);
+  }
+
+  void Link(int u, int v) {
+    Link(&nodes[u], &nodes[v]);
+  }
+
+  void Cut(int x) {
+    Cut(&nodes[x]);
+  }
+
+  void Cut(int u, int v) {
+    Cut(&nodes[u], &nodes[v]);
+  }
+
+  bool IsConnected(int u, int v) {
+    return GetRoot(&nodes[u]) == GetRoot(&nodes[v]);
+  }
+
+  int GetParent(int x) {
+    auto p = GetParent(&nodes[x]);
+    return p ? p - nodes.data() : 0;
+  }
+
+  // v = ancestor of u
+  // output: child of v, on u -> v path
+  int GetChild(int u, int v) {
+    auto p = GetChild(&nodes[u], &nodes[v]);
+    return p ? p - nodes.data() : 0;
+  }
+
+  int GetLCA(int u, int v) {
+    return GetLCA(&nodes[u], &nodes[v]) - nodes.data();
+  }
+
+  int GetDistance(int u, int v) {
+    MakeRoot(u);
+    Access(&nodes[v]);
+    return nodes[v].sum - 1;
+  }
+
+  int GetNth(int u, int v, int n) {
+    MakeRoot(u);
+    Access(&nodes[v]);
+    Node* x = &nodes[v];
+    if (n < 1 || n > x->sum) return 0;
+    Node* y = GetNth(x, n);
+    Splay(y);
+    return y - nodes.data();
+  }
+
+ private:
+  bool IsRoot(Node* x) {
+    return !x->p || (x->p->l != x && x->p->r != x);
+  }
+
+  // lazy propagation
+  void Pull(Node* x) {
+    x->sum = x->sz;
+    if (x->l) x->sum += x->l->sum;
+    if (x->r) x->sum += x->r->sum;
+  }
+
+  void Push(Node* x) {
+    if (!x || !x->rev) return;
+    swap(x->l, x->r);
+    if (x->l) x->l->rev ^= 1;
+    if (x->r) x->r->rev ^= 1;
+    x->rev = 0;
+  }
+
+  void Rotate(Node* x) {
+    Node *p = x->p, *g = p->p;
+    Push(p);
+    Push(x);
+    bool r = (x == p->r);
+    Node* b = (r ? x->l : x->r);
+    if (!IsRoot(p)) (p == g->r ? g->r : g->l) = x;
+    x->p = g;
+    (r ? x->l : x->r) = p;
+    p->p = x;
+    (r ? p->r : p->l) = b;
+    if (b) b->p = p;
+    Pull(p);
+    Pull(x);
+  }
+
+  void Splay(Node* x) {
+    Node* y = x;
+    do stk.push_back(y);
+    while (!IsRoot(y) && (y = y->p));
+    while (!stk.empty()) Push(stk.back()), stk.pop_back();
+
+    while (!IsRoot(x)) {
+      Node *p = x->p, *g = p->p;
+      if (!IsRoot(p)) {
+        if ((p->l == x) != (g->l == p)) {
+          Rotate(x);
+        } else {
+          Rotate(p);
+        }
+      }
+      Rotate(x);
+    }
+  }
+
+  Node* Access(Node* x) {
+    Node* o = nullptr;
+    for (Node* y = x; y; y = y->p) {
+      Splay(y);
+      y->r = o;
+      Pull(y);
+      o = y;
+    }
+    Splay(x);
+    return o;
+  }
+
+  void MakeRoot(Node* x) {
+    Access(x);
+    x->rev ^= 1;
+  }
+
+  void Link(Node* u, Node* v) {
+    MakeRoot(u);
+    if (Access(v) != u) u->p = v;
+  }
+
+  void Cut(Node* x) {
+    Access(x);
+    x->l->p = nullptr;
+    x->l = nullptr;
+    Pull(x);
+  }
+
+  void Cut(Node* u, Node* v) {
+    MakeRoot(u);
+    Access(v);
+    if (v->l == u) v->l = u->p = nullptr;
+    Pull(v);
+  }
+
+  Node* GetRoot(Node* x) {
+    Access(x);
+    for (;;) {
+      Push(x);
+      if (!x->l) break;
+      x = x->l;
+    }
+    Splay(x);
+    return x;
+  }
+
+  Node* GetParent(Node* x) {
+    Access(x);
+    if (!x->l) return nullptr;
+
+    x = x->l;
+    Push(x);
+    while (x->r) {
+      x = x->r;
+      Push(x);
+    }
+    Splay(x);
+    return x;
+  }
+
+  Node* GetChild(Node* u, Node* v) {
+    MakeRoot(v);
+    Access(u);
+    Splay(v);
+    if (!v->r) return nullptr;
+
+    Node* x = v->r;
+    Push(x);
+    while (x->l) {
+      x = x->l;
+      Push(x);
+    }
+    Splay(x);
+    return x;
+  }
+
+  Node* GetLCA(Node* u, Node* v) {
+    if (u == v) return u;
+    Access(u);
+    return Access(v);
+  }
+
+  Node* GetNth(Node* x, int n) {
+    Push(x);
+    int sz = x->l ? x->l->sum : 0;
+    if (n == sz + 1) return x;
+    if (n <= sz) return GetNth(x->l, n);
+    return GetNth(x->r, n - sz - 1);
+  }
+
+  vector<Node> nodes;
+  vector<Node*> stk;
+};
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  cin >> n;
+  LCT lct(n);
+  for (int i = 1; i < n; i++) {
+    int u, v;
+    cin >> u >> v;
+    lct.Link(u, v);
+  }
+
+  auto Find = [&](int a, int b, int c) {
+    array<tuple<int, int, int>, 3> arr = {{
+        {lct.GetDistance(a, b), a, b},
+        {lct.GetDistance(b, c), b, c},
+        {lct.GetDistance(c, a), c, a},
+    }};
+
+    sort(arr.begin(), arr.end());
+    auto [d, x, y] = arr[2];
+    if (d & 1) return -1;
+
+    int w = lct.GetNth(x, y, (d >> 1) + 1);
+    if (w == 0) return -1;
+
+    d = lct.GetDistance(w, a);
+    if (d == lct.GetDistance(w, b) && d == lct.GetDistance(w, c)) return w;
+    return -1;
+  };
+
+  int q;
+  cin >> q;
+  while (q--) {
+    int a, b, c;
+    cin >> a >> b >> c;
+    cout << Find(a, b, c) << "\n";
+  }
+
+  return 0;
+}
