f6648a7e10bc66c583288ee8197ed1ce907bff44
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-05-11 19:45:19 +0900

    10817 - 세 수

---
+++ BaekJoon/10000/10817.cpp
@@ -0,0 +1,20 @@
+// Title : 세 수
+// Link  : https://www.acmicpc.net/problem/10817 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int arr[3];
+  for (auto& e : arr) cin >> e;
+  sort(arr, arr + 3);
+  cout << arr[1];
+
+  return 0;
+}
