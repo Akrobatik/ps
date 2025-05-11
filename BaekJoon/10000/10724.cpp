7ccd179e152432e806fa249835ba200dbfe53259
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-05-11 20:41:55 +0900

    10724 - 판게아 2

---
+++ BaekJoon/10000/10724.cpp
@@ -0,0 +1,254 @@
+// Title : 판게아 2
+// Link  : https://www.acmicpc.net/problem/10724 
+// Time  : 3848 ms
+// Memory: 16208 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct LCT {
+  struct Node {
+    Node() : l(nullptr), r(nullptr), p(nullptr), maxx(this), val(0), rev(0) {}
+
+    Node *l, *r, *p, *maxx;
+    int val, rev;
+  };
+
+  LCT(int n) : nodes(n + 1) {}
+
+  void MakeRoot(int x) {
+    MakeRoot(&nodes[x]);
+  }
+
+  void Set(int x, int w) {
+    nodes[x].val = w;
+    nodes[x].maxx = &nodes[x];
+  }
+
+  void Link(int u, int v) {
+    Link(&nodes[u], &nodes[v]);
+  }
+
+  void Cut(int x) {
+    Cut(&nodes[x]);
+  }
+
+  void Cut(int u, int v) {
+    Cut(&nodes[u], &nodes[v]);
+  }
+
+  bool IsConnected(int u, int v) {
+    return GetRoot(&nodes[u]) == GetRoot(&nodes[v]);
+  }
+
+  int GetParent(int x) {
+    auto p = GetParent(&nodes[x]);
+    return p ? p - nodes.data() : 0;
+  }
+
+  int GetChild(int u, int v) {
+    auto p = GetChild(&nodes[u], &nodes[v]);
+    return p ? p - nodes.data() : 0;
+  }
+
+  int GetLCA(int u, int v) {
+    return GetLCA(&nodes[u], &nodes[v]) - nodes.data();
+  }
+
+  pair<Node*, int> GetMaxEdge(int u, int v) {
+    MakeRoot(u);
+    Access(&nodes[v]);
+    return {nodes[v].maxx, nodes[v].maxx - nodes.data()};
+  }
+
+ private:
+  bool IsRoot(Node* x) {
+    return !x->p || (x->p->l != x && x->p->r != x);
+  }
+
+  void Pull(Node* x) {
+    x->maxx = x;
+    if (x->l && x->maxx->val < x->l->maxx->val) x->maxx = x->l->maxx;
+    if (x->r && x->maxx->val < x->r->maxx->val) x->maxx = x->r->maxx;
+  }
+
+  void Push(Node* x) {
+    if (!x || !x->rev) return;
+    swap(x->l, x->r);
+    if (x->l) x->l->rev ^= 1;
+    if (x->r) x->r->rev ^= 1;
+    x->rev = 0;
+  }
+
+  void Rotate(Node* x) {
+    Node *p = x->p, *g = p->p;
+    Push(p);
+    Push(x);
+    bool r = (x == p->r);
+    Node* b = (r ? x->l : x->r);
+    if (!IsRoot(p)) (p == g->r ? g->r : g->l) = x;
+    x->p = g;
+    (r ? x->l : x->r) = p;
+    p->p = x;
+    (r ? p->r : p->l) = b;
+    if (b) b->p = p;
+    Pull(p);
+    Pull(x);
+  }
+
+  void Splay(Node* x) {
+    Node* y = x;
+    do stk.push_back(y);
+    while (!IsRoot(y) && (y = y->p));
+    while (!stk.empty()) Push(stk.back()), stk.pop_back();
+
+    while (!IsRoot(x)) {
+      Node *p = x->p, *g = p->p;
+      if (!IsRoot(p)) {
+        if ((p->l == x) != (g->l == p)) {
+          Rotate(x);
+        } else {
+          Rotate(p);
+        }
+      }
+      Rotate(x);
+    }
+  }
+
+  Node* Access(Node* x) {
+    Node* o = nullptr;
+    for (Node* y = x; y; y = y->p) {
+      Splay(y);
+      y->r = o;
+      Pull(y);
+      o = y;
+    }
+    Splay(x);
+    return o;
+  }
+
+  void MakeRoot(Node* x) {
+    Access(x);
+    x->rev ^= 1;
+  }
+
+  void Link(Node* u, Node* v) {
+    MakeRoot(u);
+    if (Access(v) != u) u->p = v;
+  }
+
+  void Cut(Node* x) {
+    Access(x);
+    x->l->p = nullptr;
+    x->l = nullptr;
+    Pull(x);
+  }
+
+  void Cut(Node* u, Node* v) {
+    MakeRoot(u);
+    Access(v);
+    if (v->l == u) v->l = u->p = nullptr;
+    Pull(v);
+  }
+
+  Node* GetRoot(Node* x) {
+    Access(x);
+    for (;;) {
+      Push(x);
+      if (!x->l) break;
+      x = x->l;
+    }
+    Splay(x);
+    return x;
+  }
+
+  Node* GetParent(Node* x) {
+    Access(x);
+    if (!x->l) return nullptr;
+
+    x = x->l;
+    Push(x);
+    while (x->r) {
+      x = x->r;
+      Push(x);
+    }
+    Splay(x);
+    return x;
+  }
+
+  Node* GetChild(Node* u, Node* v) {
+    MakeRoot(v);
+    Access(u);
+    Splay(v);
+    if (!v->r) return nullptr;
+
+    Node* x = v->r;
+    Push(x);
+    while (x->l) {
+      x = x->l;
+      Push(x);
+    }
+    Splay(x);
+    return x;
+  }
+
+  Node* GetLCA(Node* u, Node* v) {
+    if (u == v) return u;
+    Access(u);
+    return Access(v);
+  }
+
+  vector<Node> nodes;
+  vector<Node*> stk;
+};
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int t;
+  cin >> t;
+  while (t--) {
+    int n, m;
+    cin >> n >> m;
+    LCT lct((n << 1) + m);
+    vector<pair<int, int>> memo((n << 1) + m);
+    int64_t sum = 0;
+    for (int i = 1; i < n; i++) {
+      int u, c, x = i + n;
+      cin >> u >> c;
+      ++u;
+      memo[x] = {i + 1, u};
+      sum += c;
+      lct.Set(x, c);
+      lct.Link(x, i + 1);
+      lct.Link(x, u);
+    }
+
+    int idx = n << 1;
+    int64_t ans = 0;
+    while (m--) {
+      int u, v, c;
+      cin >> u >> v >> c;
+      ++u, ++v;
+      int x = idx++;
+      memo[x] = {u, v};
+      lct.Set(x, c);
+
+      auto [maxx, id] = lct.GetMaxEdge(u, v);
+      if (maxx->val > c) {
+        auto [a, b] = memo[id];
+        sum += (c - maxx->val);
+        lct.Cut(id, a);
+        lct.Cut(id, b);
+        lct.Link(x, u);
+        lct.Link(x, v);
+      }
+      ans ^= sum;
+    }
+    cout << ans << "\n";
+  }
+
+  return 0;
+}
