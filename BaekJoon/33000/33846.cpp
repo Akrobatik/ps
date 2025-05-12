ff1af05cc09ee58e99b9f133dae768cf58004fda
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-05-13 01:00:10 +0900

    33846 - 삽입 정렬을 해볼까

---
+++ BaekJoon/33000/33846.cpp
@@ -0,0 +1,23 @@
+// Title : 삽입 정렬을 해볼까
+// Link  : https://www.acmicpc.net/problem/33846 
+// Time  : 272 ms
+// Memory: 5928 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n, t;
+  cin >> n >> t;
+  vector<int> arr(n);
+  for (auto& e : arr) cin >> e;
+  sort(arr.begin(), arr.begin() + t);
+  for (auto e : arr) cout << e << " ";
+
+  return 0;
+}
