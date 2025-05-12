50c590247816bf9de17448bd165a14f196018f31
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-05-12 23:18:24 +0900

    17449 - 순위 계산

---
+++ BaekJoon/17000/17449.cpp
@@ -0,0 +1,35 @@
+// Title : 순위 계산
+// Link  : https://www.acmicpc.net/problem/17449 
+// Time  : 24 ms
+// Memory: 2804 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int r, n;
+  cin >> r >> n;
+  vector<int> arr(n);
+  for (auto& e : arr) cin >> e;
+
+  int minn = r, maxx = r, cnt = 0;
+  for (auto e : arr) {
+    maxx += (e <= maxx);
+    if (e < minn) {
+      ++minn;
+    } else if (e == minn) {
+      ++cnt;
+    } else if (e <= minn + cnt) {
+      int d = e - minn;
+      cnt -= d - 1;
+      minn += d;
+    }
+  }
+  cout << minn << " " << maxx;
+
+  return 0;
+}
