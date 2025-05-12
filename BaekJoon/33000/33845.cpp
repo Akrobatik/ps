e1678478aced9b0d5a463203bc9b7f0eb53fdb88
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-05-13 00:58:22 +0900

    33845 - PNUPC에 한 번도 빠지지 않고 출연한 산지니가 새삼 대단하다고 느껴지네

---
+++ BaekJoon/33000/33845.cpp
@@ -0,0 +1,27 @@
+// Title : PNUPC에 한 번도 빠지지 않고 출연한 산지니가 새삼 대단하다고 느껴지네
+// Link  : https://www.acmicpc.net/problem/33845 
+// Time  : 0 ms
+// Memory: 2168 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  string s1, s2;
+  cin >> s1 >> s2;
+
+  bool check[256] = {};
+  for (auto c : s1) check[c] = true;
+
+  for (auto c : s2) {
+    if (check[c]) continue;
+    cout << c;
+  }
+
+  return 0;
+}
