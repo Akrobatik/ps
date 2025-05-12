a4223d6a2695e4a04021a1a49cf7f4437880c5d5
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-05-12 22:38:06 +0900

    6198 - 옥상 정원 꾸미기

---
+++ BaekJoon/06000/6198.cpp
@@ -0,0 +1,29 @@
+// Title : 옥상 정원 꾸미기
+// Link  : https://www.acmicpc.net/problem/6198 
+// Time  : 8 ms
+// Memory: 2916 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  cin >> n;
+
+  vector<int> stk;
+  int64_t ans = 0;
+  for (int i = 0; i < n; i++) {
+    int x;
+    cin >> x;
+    while (!stk.empty() && stk.back() <= x) stk.pop_back();
+    ans += stk.size();
+    stk.push_back(x);
+  }
+  cout << ans;
+
+  return 0;
+}
