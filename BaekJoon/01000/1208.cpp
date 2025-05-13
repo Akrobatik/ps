62b5e6661b619249eb51ab2dedcfab1d4d394679
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-05-13 17:03:21 +0900

    1208 - 부분수열의 합 2

---
+++ BaekJoon/01000/1208.cpp
@@ -0,0 +1,47 @@
+// Title : 부분수열의 합 2
+// Link  : https://www.acmicpc.net/problem/1208 
+// Time  : 500 ms
+// Memory: 58784 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+map<int, int> Count(span<const int> arr) {
+  map<int, int> memo;
+  memo[0] = 1;
+  for (auto e : arr) {
+    map<int, int> mnxt;
+    for (auto [x, cnt] : memo) {
+      mnxt[x] += cnt;
+      mnxt[x + e] += cnt;
+    }
+    memo.swap(mnxt);
+  }
+  return memo;
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n, s;
+  cin >> n >> s;
+
+  vector<int> arr(n);
+  for (auto& e : arr) cin >> e;
+
+  int half = n >> 1;
+  span<int> lrr(arr.begin(), arr.begin() + half);
+  span<int> rrr(arr.begin() + half, arr.end());
+
+  auto lm = Count(lrr), rm = Count(rrr);
+
+  int64_t cnt = 0;
+  for (auto [x, c] : lm) {
+    if (auto it = rm.find(s - x); it != rm.end()) cnt += (int64_t)c * it->second;
+  }
+  cout << cnt - (s == 0);
+
+  return 0;
+}
