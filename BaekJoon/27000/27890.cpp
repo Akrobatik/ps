30b6c5e218c25d3493df6fa788ac67d1a626fca0
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-05-14 16:51:06 +0900

    27890 - 특별한 작은 분수

---
+++ BaekJoon/27000/27890.cpp
@@ -0,0 +1,26 @@
+// Title : 특별한 작은 분수
+// Link  : https://www.acmicpc.net/problem/27890 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int x, n;
+  cin >> x >> n;
+  while (n--) {
+    if (x & 1) {
+      x = (x << 1) ^ 6;
+    } else {
+      x = (x >> 1) ^ 6;
+    }
+  }
+  cout << x;
+
+  return 0;
+}
