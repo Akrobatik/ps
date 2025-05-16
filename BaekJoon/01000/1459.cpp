5f3ef1304a63d1efdde27bbff1aab1ddb3af29fd
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-05-16 20:29:49 +0900

    1459 - 걷기

---
+++ BaekJoon/01000/1459.cpp
@@ -0,0 +1,27 @@
+// Title : 걷기
+// Link  : https://www.acmicpc.net/problem/1459 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int64_t x, y, w, s;
+  cin >> x >> y >> w >> s;
+  int64_t minn = min<int64_t>(x, y);
+  int64_t maxx = x ^ y ^ minn;
+  int64_t ans = min<int64_t>((x + y) * w, minn * s + (maxx - minn) * w);
+  if ((maxx - minn) & 1) {
+    ans = min<int64_t>(ans, maxx * s + w - s);
+  } else {
+    ans = min<int64_t>(ans, maxx * s);
+  }
+  cout << ans;
+
+  return 0;
+}
