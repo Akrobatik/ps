604d33b1e2011affe2e640422ca940b4bc1519c5
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-05-17 02:31:44 +0900

    33842 - 돌멩이 배치

---
+++ BaekJoon/33000/33842.cpp
@@ -0,0 +1,50 @@
+// Title : 돌멩이 배치
+// Link  : https://www.acmicpc.net/problem/33842 
+// Time  : 28 ms
+// Memory: 2180 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  vector<bool> arr;
+
+  int t;
+  cin >> t;
+  while (t--) {
+    int n, k, l, r, d;
+    cin >> n >> k >> l >> r;
+    arr.assign(n, false);
+
+    int s = ((int64_t)n * l + k - 1) / k;
+    int x = 0;
+    for (int i = 0; i < n; i++) {
+      x += s;
+      if (x >= n) x -= n, arr[i] = true;
+    }
+
+    int sum = 0;
+    for (int i = n - k; i < n; i++) {
+      sum += arr[i];
+    }
+
+    bool ok = true;
+    for (int i = 0; ok && i < n; i++) {
+      sum += arr[i] - arr[(i + n - k) % n];
+      ok = (l <= sum && sum <= r);
+    }
+
+    if (ok) {
+      for (auto e : arr) cout << e << " ";
+      cout << "\n";
+    } else {
+      cout << "-1\n";
+    }
+  }
+
+  return 0;
+}
