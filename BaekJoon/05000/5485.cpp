1354bf31143817dc0892c5331785b311b379a1b4
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-05-16 20:20:05 +0900

    5485 - 평균값 수열

---
+++ BaekJoon/05000/5485.cpp
@@ -0,0 +1,28 @@
+// Title : 평균값 수열
+// Link  : https://www.acmicpc.net/problem/5485 
+// Time  : 528 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  cin >> n;
+
+  int l = -2000000000, r = 2000000000;
+  while (n-- && l <= r) {
+    int x;
+    cin >> x;
+    r = min<int>(r, x);
+    x <<= 1;
+    tie(l, r) = make_pair(x - r, x - l);
+  }
+  cout << max<int>(r - l + 1, 0);
+
+  return 0;
+}
