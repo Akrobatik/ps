7c559c11cf1b1e808a059337c54cf87a3e75270e
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-05-18 02:49:38 +0900

    27294 - 몇개고?

---
+++ BaekJoon/27000/27294.cpp
@@ -0,0 +1,24 @@
+// Title : 몇개고?
+// Link  : https://www.acmicpc.net/problem/27294 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int t, s;
+  cin >> t >> s;
+
+  if (12 <= t && t <= 16 && s == 0) {
+    cout << "320";
+  } else {
+    cout << "280";
+  }
+
+  return 0;
+}
