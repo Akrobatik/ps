48f8804835b29a95b0f595468e4f90ac77760ea4
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-05-20 04:38:28 +0900

    33924 - 신묘마루의 요술망치

---
+++ BaekJoon/33000/33924.cpp
@@ -0,0 +1,52 @@
+// Title : 신묘마루의 요술망치
+// Link  : https://www.acmicpc.net/problem/33924 
+// Time  : 0 ms
+// Memory: 2024 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n, m, k;
+  string s;
+  cin >> n >> m >> k >> s;
+
+  --n, --m;
+  for (auto c : s) {
+    switch (c) {
+      case 'A': {
+        n = (n + 2) % 4;
+      } break;
+      case 'B': {
+        n ^= 1, m ^= 1;
+      } break;
+      case 'C': {
+        n = (n + 1) % 4;
+        n ^= 1, m ^= 1;
+        n = (n + 3) % 4;
+      } break;
+      case 'D': {
+        if (m == 0) {
+          if (n == 0) {
+            m = 1;
+          } else {
+            --n;
+          }
+        } else {
+          if (n == 3) {
+            m = 0;
+          } else {
+            ++n;
+          }
+        }
+      } break;
+    }
+  }
+  cout << n + 1 << " " << m + 1;
+
+  return 0;
+}
