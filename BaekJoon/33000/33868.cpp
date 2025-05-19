7b978b275806471a21594d6d4ca7e2521f585074
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-05-19 23:25:01 +0900

    33868 - 스티커 나눠주기

---
+++ BaekJoon/33000/33868.cpp
@@ -0,0 +1,27 @@
+// Title : 스티커 나눠주기
+// Link  : https://www.acmicpc.net/problem/33868 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  cin >> n;
+  int bt = 0, bb = INT_MAX;
+  while (n--) {
+    int t, b;
+    cin >> t >> b;
+    bt = max<int>(bt, t);
+    bb = min<int>(bb, b);
+  }
+
+  cout << (bt * bb % 7) + 1;
+
+  return 0;
+}
