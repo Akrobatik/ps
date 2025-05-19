0f4a66a5ed3c757dbc628b51e6eaa01998206d15
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-05-20 01:28:25 +0900

    33910 - 합의 최소

---
+++ BaekJoon/33000/33910.cpp
@@ -0,0 +1,28 @@
+// Title : 합의 최소
+// Link  : https://www.acmicpc.net/problem/33910 
+// Time  : 48 ms
+// Memory: 3976 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  cin >> n;
+  vector<int> arr(n);
+  for (auto& e : arr) cin >> e;
+
+  int64_t sum = 0;
+  int cur = INT32_MAX;
+  while (n--) {
+    cur = min<int>(cur, arr[n]);
+    sum += cur;
+  }
+  cout << sum;
+
+  return 0;
+}
