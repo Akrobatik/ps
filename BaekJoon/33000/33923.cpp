61223427865b309f65a0b1097ef083acaab37ce2
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-05-20 04:38:07 +0900

    33923 - 인경호 울타리 공사

---
+++ BaekJoon/33000/33923.cpp
@@ -0,0 +1,26 @@
+// Title : 인경호 울타리 공사
+// Link  : https://www.acmicpc.net/problem/33923 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n, m;
+  cin >> n >> m;
+  if (n > m) swap(n, m);
+
+  --n, --m;
+  if (n == m) {
+    cout << (n - 1) * (n - 1) + 1;
+  } else {
+    cout << n * n;
+  }
+
+  return 0;
+}
