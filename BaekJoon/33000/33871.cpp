bf7150769f2d93c34c3747cfe19ba3c9dcc36b0b
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-05-19 22:48:41 +0900

    33871 - 서로소 게임

---
+++ BaekJoon/33000/33871.cpp
@@ -0,0 +1,24 @@
+// Title : 서로소 게임
+// Link  : https://www.acmicpc.net/problem/33871 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  cin >> n;
+
+  if (n & 1) {
+    cout << "Soomin";
+  } else {
+    cout << "Song";
+  }
+
+  return 0;
+}
