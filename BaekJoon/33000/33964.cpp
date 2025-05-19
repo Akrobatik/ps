4bae8ff71bc2da381cd790b89bc596870a922262
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-05-20 04:37:59 +0900

    33964 - 레퓨닛의 덧셈

---
+++ BaekJoon/33000/33964.cpp
@@ -0,0 +1,20 @@
+// Title : 레퓨닛의 덧셈
+// Link  : https://www.acmicpc.net/problem/33964 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int x, y;
+  cin >> x >> y;
+  string s1(x, '1'), s2(y, '1');
+  cout << stoi(s1) + stoi(s2);
+
+  return 0;
+}
