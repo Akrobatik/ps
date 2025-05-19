03a85fef86b798057d557c6244ccef4023cb15bc
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-05-20 04:36:55 +0900

    33913 - even 하게 익은 SCON

---
+++ BaekJoon/33000/33913.cpp
@@ -0,0 +1,32 @@
+// Title : even 하게 익은 SCON
+// Link  : https://www.acmicpc.net/problem/33913 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+constexpr int kMod = 1e9 + 7;
+constexpr int kInv2 = 500000004;
+
+int64_t Power(int64_t x, int64_t exp) {
+  int64_t res = 1;
+  while (exp) {
+    if (exp & 1) res = res * x % kMod;
+    x = x * x % kMod;
+    exp >>= 1;
+  }
+  return res;
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  cin >> n;
+  cout << (Power(26, n) + Power(22, n)) * kInv2 % kMod;
+
+  return 0;
+}
