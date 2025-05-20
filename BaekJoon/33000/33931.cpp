066ff20cfcc06d2802ed96a8c60990feba9511e6
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-05-20 13:52:28 +0900

    33931 - 체크박스 누르기

---
+++ BaekJoon/33000/33931.cpp
@@ -0,0 +1,20 @@
+// Title : 체크박스 누르기
+// Link  : https://www.acmicpc.net/problem/33931 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n, m;
+  cin >> n >> m;
+  int q = m / n, r = m % n;
+  cout << ((q & 1) ? n - r : r);
+
+  return 0;
+}
