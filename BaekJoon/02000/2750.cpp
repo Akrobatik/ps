2fc64a92cd0bde347fa8a3d76c33fb21582ddd97
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-05-21 06:15:21 +0900

    2750 - 수 정렬하기

---
+++ BaekJoon/02000/2750.cpp
@@ -0,0 +1,22 @@
+// Title : 수 정렬하기
+// Link  : https://www.acmicpc.net/problem/2750 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  cin >> n;
+  vector<int> arr(n);
+  for (auto& e : arr) cin >> e;
+  sort(arr.begin(), arr.end());
+  for (auto e : arr) cout << e << "\n";
+
+  return 0;
+}
