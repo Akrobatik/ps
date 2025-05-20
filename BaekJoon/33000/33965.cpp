23b326f05b8e2db276d80978fec69424aaf5fdd1
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-05-21 04:37:33 +0900

    33965 - 주사위 피라미드

---
+++ BaekJoon/33000/33965.cpp
@@ -0,0 +1,19 @@
+// Title : 주사위 피라미드
+// Link  : https://www.acmicpc.net/problem/33965 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int64_t n;
+  cin >> n;
+  cout << (n * (n + 1) / 2) * 35;
+
+  return 0;
+}
