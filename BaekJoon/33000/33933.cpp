84916641e47e2eff216a4df650adcb9658a80f20
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-05-22 04:49:11 +0900

    33933 - 인덕이와 산책

---
+++ BaekJoon/33000/33933.cpp
@@ -0,0 +1,66 @@
+// Title : 인덕이와 산책
+// Link  : https://www.acmicpc.net/problem/33933 
+// Time  : 32 ms
+// Memory: 2312 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<int> edges[1001];
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n, m, t;
+  cin >> n >> m >> t;
+  while (m--) {
+    int u, v;
+    cin >> u >> v;
+    --u, --v;
+    edges[u].push_back(v);
+    edges[v].push_back(u);
+  }
+
+  vector<int> cycle(t);
+  for (auto& e : cycle) cin >> e, --e;
+
+  vector<vector<bool>> visited(t, vector<bool>(n));
+
+  queue<int> q;
+
+  auto Push = [&](int x, int r) {
+    if (visited[r][x]) return;
+    visited[r][x] = true;
+    q.push(x);
+  };
+
+  Push(0, 0);
+  for (int i = 0; !q.empty(); i++) {
+    int nq = q.size();
+    while (nq--) {
+      int cur = q.front();
+      q.pop();
+
+      if (cur + 1 == n) {
+        cout << i;
+        return 0;
+      }
+
+      int cr = i % t, nr = (cr + 1) % t;
+      if (cycle[cr] == cur) {
+        Push(cycle[nr], nr);
+        continue;
+      }
+
+      Push(cur, nr);
+      for (auto nxt : edges[cur]) {
+        Push(nxt, nr);
+      }
+    }
+  }
+  cout << "-1";
+
+  return 0;
+}
