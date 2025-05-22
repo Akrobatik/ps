e106b92219cf7c958dd6f34034399ed9c0f58c3d
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-05-23 07:14:56 +0900

    2033 - 반올림

---
+++ BaekJoon/02000/2033.cpp
@@ -0,0 +1,25 @@
+// Title : 반올림
+// Link  : https://www.acmicpc.net/problem/2033 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  cin >> n;
+  int x = 1;
+  for (;;) {
+    if (n < x * 10) break;
+    n = (n / (x * 10) + ((n % (x * 10)) / x >= 5)) * (x * 10);
+    x *= 10;
+  }
+  cout << n;
+
+  return 0;
+}
