45780d884ec915ecfe20710fcd22698f98dfaf11
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-05-23 07:15:03 +0900

    17393 - 다이나믹 롤러

---
+++ BaekJoon/17000/17393.cpp
@@ -0,0 +1,27 @@
+// Title : 다이나믹 롤러
+// Link  : https://www.acmicpc.net/problem/17393 
+// Time  : 304 ms
+// Memory: 9836 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  cin >> n;
+  vector<int64_t> arr(n), brr(n);
+  for (auto& e : arr) cin >> e;
+  for (auto& e : views::reverse(brr)) cin >> e;
+
+  for (int i = 0; i < n; i++) {
+    brr.pop_back();
+    auto it = lower_bound(brr.begin(), brr.end(), arr[i], greater<int64_t>());
+    cout << brr.end() - it << " ";
+  }
+
+  return 0;
+}
