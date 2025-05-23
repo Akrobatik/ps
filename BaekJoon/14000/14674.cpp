ea9c4d9f7a10abbde0a5a97ab813f91a838f562a
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-05-23 09:40:50 +0900

    14674 - STOP USING MONEY

---
+++ BaekJoon/14000/14674.cpp
@@ -0,0 +1,33 @@
+// Title : STOP USING MONEY
+// Link  : https://www.acmicpc.net/problem/14674 
+// Time  : 0 ms
+// Memory: 2156 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using tup = tuple<int, int, int>;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n, k;
+  cin >> n >> k;
+  vector<tup> arr(n);
+  for (auto& [i, c, h] : arr) cin >> i >> c >> h;
+
+  sort(arr.begin(), arr.end(), [](const tup& lhs, const tup& rhs) {
+    auto [li, lc, lh] = lhs;
+    auto [ri, rc, rh] = rhs;
+    int64_t lv = (int64_t)lh * rc, rv = (int64_t)rh * lc;
+    if (lv != rv) return lv > rv;
+    if (lc != rc) return lc < rc;
+    return li < ri;
+  });
+
+  for (int i = 0; i < k; i++) cout << get<0>(arr[i]) << "\n";
+
+  return 0;
+}
