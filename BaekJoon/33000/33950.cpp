eac24c1a15d15d241308c335223d0755cb48b9ed
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-05-26 04:46:06 +0900

    33950 - 어게인 포닉스

---
+++ BaekJoon/33000/33950.cpp
@@ -0,0 +1,32 @@
+// Title : 어게인 포닉스
+// Link  : https://www.acmicpc.net/problem/33950 
+// Time  : 0 ms
+// Memory: 2024 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int t;
+  cin >> t;
+  while (t--) {
+    string s;
+    cin >> s;
+    int n = s.size();
+    for (int i = 0; i < n; i++) {
+      if (i + 1 != n && s[i] == 'P' && s[i + 1] == 'O') {
+        cout << "PHO";
+        ++i;
+      } else {
+        cout << s[i];
+      }
+    }
+    cout << "\n";
+  }
+
+  return 0;
+}
