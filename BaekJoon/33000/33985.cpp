2cbaae89e2810444255b7daff1fb14ea2c830246
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-05-26 04:46:26 +0900

    33985 - 그거 왜 말해!

---
+++ BaekJoon/33000/33985.cpp
@@ -0,0 +1,20 @@
+// Title : 그거 왜 말해!
+// Link  : https://www.acmicpc.net/problem/33985 
+// Time  : 0 ms
+// Memory: 2912 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  string s;
+  cin >> n >> s;
+  cout << (s[0] == 'A' && s[n - 1] == 'B' ? "Yes" : "No");
+
+  return 0;
+}
