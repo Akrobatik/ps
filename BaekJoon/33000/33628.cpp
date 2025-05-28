8ffa17e5b8ec60ad2aed0f8376a1b803380e6647
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-05-28 16:45:02 +0900

    33628 - Connect the GSHS

---
+++ BaekJoon/33000/33628.cpp
@@ -0,0 +1,247 @@
+// Title : Connect the GSHS
+// Link  : https://www.acmicpc.net/problem/33628 
+// Time  : 748 ms
+// Memory: 7208 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct LCT {
+  struct Node {
+    Node() : l(nullptr), r(nullptr), p(nullptr), rev(0) {}
+
+    Node *l, *r, *p;
+    int rev;
+  };
+
+  LCT(int n) : nodes(n + 1) {}
+
+  void MakeRoot(int x) {
+    MakeRoot(&nodes[x]);
+  }
+
+  void Set(int x) {
+    Access(&nodes[x]);
+    //
+    Pull(&nodes[x]);
+  }
+
+  void Link(int u, int v) {
+    Link(&nodes[u], &nodes[v]);
+  }
+
+  void Cut(int x) {
+    Cut(&nodes[x]);
+  }
+
+  void Cut(int u, int v) {
+    Cut(&nodes[u], &nodes[v]);
+  }
+
+  bool IsConnected(int u, int v) {
+    return GetRoot(&nodes[u]) == GetRoot(&nodes[v]);
+  }
+
+  int GetParent(int x) {
+    auto p = GetParent(&nodes[x]);
+    return p ? p - nodes.data() : 0;
+  }
+
+  // v = ancestor of u
+  // output: child of v, on u -> v path
+  int GetChild(int u, int v) {
+    auto p = GetChild(&nodes[u], &nodes[v]);
+    return p ? p - nodes.data() : 0;
+  }
+
+  int GetLCA(int u, int v) {
+    return GetLCA(&nodes[u], &nodes[v]) - nodes.data();
+  }
+
+ private:
+  bool IsRoot(Node* x) {
+    return !x->p || (x->p->l != x && x->p->r != x);
+  }
+
+  void Pull(Node* x) {
+  }
+
+  void Push(Node* x) {
+    if (!x || !x->rev) return;
+    swap(x->l, x->r);
+    if (x->l) x->l->rev ^= 1;
+    if (x->r) x->r->rev ^= 1;
+    x->rev = 0;
+  }
+
+  void Rotate(Node* x) {
+    Node *p = x->p, *g = p->p;
+    Push(p);
+    Push(x);
+    bool r = (x == p->r);
+    Node* b = (r ? x->l : x->r);
+    if (!IsRoot(p)) (p == g->r ? g->r : g->l) = x;
+    x->p = g;
+    (r ? x->l : x->r) = p;
+    p->p = x;
+    (r ? p->r : p->l) = b;
+    if (b) b->p = p;
+    Pull(p);
+    Pull(x);
+  }
+
+  void Splay(Node* x) {
+    Node* y = x;
+    do stk.push_back(y);
+    while (!IsRoot(y) && (y = y->p));
+    while (!stk.empty()) Push(stk.back()), stk.pop_back();
+
+    while (!IsRoot(x)) {
+      Node *p = x->p, *g = p->p;
+      if (!IsRoot(p)) {
+        if ((p->l == x) != (g->l == p)) {
+          Rotate(x);
+        } else {
+          Rotate(p);
+        }
+      }
+      Rotate(x);
+    }
+  }
+
+  Node* Access(Node* x) {
+    Node* o = nullptr;
+    for (Node* y = x; y; y = y->p) {
+      Splay(y);
+      y->r = o;
+      Pull(y);
+      o = y;
+    }
+    Splay(x);
+    return o;
+  }
+
+  void MakeRoot(Node* x) {
+    Access(x);
+    x->rev ^= 1;
+  }
+
+  void Link(Node* u, Node* v) {
+    MakeRoot(u);
+    if (Access(v) != u) u->p = v;
+  }
+
+  void Cut(Node* x) {
+    Access(x);
+    x->l->p = nullptr;
+    x->l = nullptr;
+    Pull(x);
+  }
+
+  void Cut(Node* u, Node* v) {
+    MakeRoot(u);
+    Access(v);
+    if (v->l == u) v->l = u->p = nullptr;
+    Pull(v);
+  }
+
+  Node* GetRoot(Node* x) {
+    Access(x);
+    for (;;) {
+      Push(x);
+      if (!x->l) break;
+      x = x->l;
+    }
+    Splay(x);
+    return x;
+  }
+
+  Node* GetParent(Node* x) {
+    Access(x);
+    if (!x->l) return nullptr;
+
+    x = x->l;
+    Push(x);
+    while (x->r) {
+      x = x->r;
+      Push(x);
+    }
+    Splay(x);
+    return x;
+  }
+
+  Node* GetChild(Node* u, Node* v) {
+    MakeRoot(v);
+    Access(u);
+    Splay(v);
+    if (!v->r) return nullptr;
+
+    Node* x = v->r;
+    Push(x);
+    while (x->l) {
+      x = x->l;
+      Push(x);
+    }
+    Splay(x);
+    return x;
+  }
+
+  Node* GetLCA(Node* u, Node* v) {
+    if (u == v) return u;
+    Access(u);
+    return Access(v);
+  }
+
+  vector<Node> nodes;
+  vector<Node*> stk;
+};
+
+int memo[100001];
+
+int Find(int id) {
+  while (id != memo[id]) {
+    int par = memo[id];
+    id = memo[id] = memo[par];
+  }
+  return id;
+}
+
+void Union(int a, int b) {
+  a = Find(a), b = Find(b);
+  if (a == b) return;
+
+  if (a > b) swap(a, b);
+  memo[b] = a;
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n, q;
+  cin >> n >> q;
+  LCT lct(n);
+  iota(memo, memo + n + 1, 0);
+
+  int ans = 0;
+  while (q--) {
+    int cmd, a, b;
+    cin >> cmd >> a >> b;
+    a ^= ans, b ^= ans;
+    if (cmd == 1) {
+      lct.Link(a, b);
+      Union(a, b);
+    } else {
+      if (lct.IsConnected(a, b)) {
+        lct.MakeRoot(Find(a));
+        ans = lct.GetLCA(a, b);
+      } else {
+        ans = 0;
+      }
+      cout << ans << "\n";
+    }
+  }
+
+  return 0;
+}
