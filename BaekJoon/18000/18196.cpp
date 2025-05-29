dd1470902940cae91b1a30dbef1c8e97dbc6a432
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-05-29 18:51:52 +0900

    18196 - 정기 모임

---
+++ BaekJoon/18000/18196.cpp
@@ -0,0 +1,270 @@
+// Title : 정기 모임
+// Link  : https://www.acmicpc.net/problem/18196 
+// Time  : 724 ms
+// Memory: 29564 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct LCT {
+  struct Node {
+    Node() : l(nullptr), r(nullptr), p(nullptr), rev(0), val(0), maxx(0) {}
+
+    Node *l, *r, *p;
+    int rev, val, maxx;
+  };
+
+  LCT(int n) : nodes(n + 1) {}
+
+  void MakeRoot(int x) {
+    MakeRoot(&nodes[x]);
+  }
+
+  void Set(int x, int v) {
+    Access(&nodes[x]);
+    nodes[x].val = v;
+    Pull(&nodes[x]);
+  }
+
+  void Link(int u, int v) {
+    Link(&nodes[u], &nodes[v]);
+  }
+
+  void Cut(int x) {
+    Cut(&nodes[x]);
+  }
+
+  void Cut(int u, int v) {
+    Cut(&nodes[u], &nodes[v]);
+  }
+
+  bool IsConnected(int u, int v) {
+    return GetRoot(&nodes[u]) == GetRoot(&nodes[v]);
+  }
+
+  int GetParent(int x) {
+    auto p = GetParent(&nodes[x]);
+    return p ? p - nodes.data() : 0;
+  }
+
+  // v = ancestor of u
+  // output: child of v, on u -> v path
+  int GetChild(int u, int v) {
+    auto p = GetChild(&nodes[u], &nodes[v]);
+    return p ? p - nodes.data() : 0;
+  }
+
+  int GetLCA(int u, int v, int r = 0) {
+    if (r) MakeRoot(r);
+    return GetLCA(&nodes[u], &nodes[v]) - nodes.data();
+  }
+
+  int Query(int u, int v) {
+    MakeRoot(u);
+    Access(&nodes[v]);
+    return nodes[v].maxx;
+  }
+
+ private:
+  bool IsRoot(Node* x) {
+    return !x->p || (x->p->l != x && x->p->r != x);
+  }
+
+  void Pull(Node* x) {
+    x->maxx = x->val;
+    if (x->l) x->maxx = max<int>(x->maxx, x->l->maxx);
+    if (x->r) x->maxx = max<int>(x->maxx, x->r->maxx);
+  }
+
+  void Push(Node* x) {
+    if (!x || !x->rev) return;
+    swap(x->l, x->r);
+    if (x->l) x->l->rev ^= 1;
+    if (x->r) x->r->rev ^= 1;
+    x->rev = 0;
+  }
+
+  void Rotate(Node* x) {
+    Node *p = x->p, *g = p->p;
+    Push(p);
+    Push(x);
+    bool r = (x == p->r);
+    Node* b = (r ? x->l : x->r);
+    if (!IsRoot(p)) (p == g->r ? g->r : g->l) = x;
+    x->p = g;
+    (r ? x->l : x->r) = p;
+    p->p = x;
+    (r ? p->r : p->l) = b;
+    if (b) b->p = p;
+    Pull(p);
+    Pull(x);
+  }
+
+  void Splay(Node* x) {
+    Node* y = x;
+    do stk.push_back(y);
+    while (!IsRoot(y) && (y = y->p));
+    while (!stk.empty()) Push(stk.back()), stk.pop_back();
+
+    while (!IsRoot(x)) {
+      Node *p = x->p, *g = p->p;
+      if (!IsRoot(p)) {
+        if ((p->l == x) != (g->l == p)) {
+          Rotate(x);
+        } else {
+          Rotate(p);
+        }
+      }
+      Rotate(x);
+    }
+  }
+
+  Node* Access(Node* x) {
+    Node* o = nullptr;
+    for (Node* y = x; y; y = y->p) {
+      Splay(y);
+      y->r = o;
+      Pull(y);
+      o = y;
+    }
+    Splay(x);
+    return o;
+  }
+
+  void MakeRoot(Node* x) {
+    Access(x);
+    x->rev ^= 1;
+  }
+
+  void Link(Node* u, Node* v) {
+    MakeRoot(u);
+    if (Access(v) != u) u->p = v;
+  }
+
+  void Cut(Node* x) {
+    Access(x);
+    x->l->p = nullptr;
+    x->l = nullptr;
+    Pull(x);
+  }
+
+  void Cut(Node* u, Node* v) {
+    MakeRoot(u);
+    Access(v);
+    if (v->l == u) v->l = u->p = nullptr;
+    Pull(v);
+  }
+
+  Node* GetRoot(Node* x) {
+    Access(x);
+    for (;;) {
+      Push(x);
+      if (!x->l) break;
+      x = x->l;
+    }
+    Splay(x);
+    return x;
+  }
+
+  Node* GetParent(Node* x) {
+    Access(x);
+    if (!x->l) return nullptr;
+
+    x = x->l;
+    Push(x);
+    while (x->r) {
+      x = x->r;
+      Push(x);
+    }
+    Splay(x);
+    return x;
+  }
+
+  Node* GetChild(Node* u, Node* v) {
+    MakeRoot(v);
+    Access(u);
+    Splay(v);
+    if (!v->r) return nullptr;
+
+    Node* x = v->r;
+    Push(x);
+    while (x->l) {
+      x = x->l;
+      Push(x);
+    }
+    Splay(x);
+    return x;
+  }
+
+  Node* GetLCA(Node* u, Node* v) {
+    if (u == v) return u;
+    Access(u);
+    return Access(v);
+  }
+
+  vector<Node> nodes;
+  vector<Node*> stk;
+};
+
+struct SegmentTree {
+  void Init(int n) {
+    nmax = has_single_bit((uint32_t)n) ? n : (1 << (32 - countl_zero((uint32_t)n)));
+    tree.clear(), tree.resize(nmax << 1);
+  }
+
+  void Update(int idx, int value) {
+    int node = idx + nmax;
+    tree[node] = value;
+    while ((node >>= 1)) tree[node] = max<int>(tree[node << 1], tree[(node << 1) + 1]);
+  }
+
+  int Query(int l, int r) {
+    int res = 0;
+    for (l += nmax, r += nmax + 1; l < r; l >>= 1, r >>= 1) {
+      if (l & 1) res = max<int>(tree[l++], res);
+      if (r & 1) res = max<int>(tree[--r], res);
+    }
+    return res;
+  }
+
+  int nmax;
+  vector<int> tree;
+};
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n, q;
+  cin >> n >> q;
+
+  LCT lct((n << 1) - 1);
+  for (int i = 1; i < n; i++) {
+    int a, b, c;
+    cin >> a >> b >> c;
+
+    int id = n + i;
+    lct.Set(id, c);
+    lct.Link(a, id);
+    lct.Link(id, b);
+  }
+
+  SegmentTree st;
+  st.Init(n);
+  for (int i = 1; i < n; i++) {
+    st.Update(i, lct.Query(i, i + 1));
+  }
+
+  while (q--) {
+    int s, e;
+    cin >> s >> e;
+    if (s == e) {
+      cout << "0\n";
+    } else {
+      cout << st.Query(s, e - 1) << "\n";
+    }
+  }
+
+  return 0;
+}
