8894583a3853648110625e972c86638a0c551e00
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-05-30 12:20:12 +0900

    4889 - 안정적인 문자열

---
+++ BaekJoon/04000/4889.cpp
@@ -0,0 +1,27 @@
+// Title : 안정적인 문자열
+// Link  : https://www.acmicpc.net/problem/4889 
+// Time  : 0 ms
+// Memory: 2024 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int t = 0;
+
+  string s;
+  while (cin >> s && count(s.begin(), s.end(), '-') == 0) {
+    int cnt = 0, ans = 0;
+    for (int i = 0; i < s.size(); i++) {
+      cnt += (s[i] == '{' ? 1 : -1);
+      if (cnt < 0) cnt += 2, ++ans;
+    }
+    cout << ++t << ". " << ans + (cnt / 2) << "\n";
+  }
+
+  return 0;
+}
