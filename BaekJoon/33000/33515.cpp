ae90fc5c6f318e03465d49f79ec9b9e220dcc096
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-05-31 19:58:38 +0900

    33515 - 노트북 세 대를 가지고 왔다

---
+++ BaekJoon/33000/33515.cpp
@@ -0,0 +1,19 @@
+// Title : 노트북 세 대를 가지고 왔다
+// Link  : https://www.acmicpc.net/problem/33515 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int a, b;
+  cin >> a >> b;
+  cout << min<int>(a, b);
+
+  return 0;
+}
