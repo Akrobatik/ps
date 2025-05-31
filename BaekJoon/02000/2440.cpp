c73e1d6f70b557158dd71d6952b0c03918f5a2e7
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-05-31 19:50:05 +0900

    2440 - 별 찍기 - 3

---
+++ BaekJoon/02000/2440.cpp
@@ -0,0 +1,20 @@
+// Title : 별 찍기 - 3
+// Link  : https://www.acmicpc.net/problem/2440 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  cin >> n;
+  string s(n, '*');
+  while (!s.empty()) cout << s << "\n", s.pop_back();
+
+  return 0;
+}
