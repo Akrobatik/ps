bcd43fbdacc7abb4ceff1fec1724785f78e647df
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-06-02 08:32:04 +0900

    34010 - e-코너 시스템 테스트 (Easy)

---
+++ BaekJoon/34000/34010.cpp
@@ -0,0 +1,21 @@
+// Title : e-코너 시스템 테스트 (Easy)
+// Link  : https://www.acmicpc.net/problem/34010 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  cin >> n;
+
+  int d = (n - 1) << 1;
+  cout << d << " " << d - 1;
+
+  return 0;
+}
