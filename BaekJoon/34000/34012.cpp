e71f4d44f3dd1d07a901c606cb44230d7910ab00
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-06-02 08:32:38 +0900

    34012 - DagDag구리

---
+++ BaekJoon/34000/34012.cpp
@@ -0,0 +1,35 @@
+// Title : DagDag구리
+// Link  : https://www.acmicpc.net/problem/34012 
+// Time  : 88 ms
+// Memory: 2412 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int deg[100001];
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n, m;
+  cin >> n >> m;
+  while (m--) {
+    int u, v;
+    cin >> u >> v;
+    ++deg[u];
+  }
+
+  int e = 1;
+  while (deg[e]) ++e;
+
+  int ans = 0;
+  for (int i = 1; i <= n; i++) {
+    if (i == e) continue;
+    ans += (deg[i] < 2);
+  }
+  cout << ans;
+
+  return 0;
+}
