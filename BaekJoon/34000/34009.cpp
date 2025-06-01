9422a83f4c18f226aad2969bf828ff09b4bb9a2a
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-06-02 08:31:55 +0900

    34009 - Bob부 멍충이

---
+++ BaekJoon/34000/34009.cpp
@@ -0,0 +1,20 @@
+// Title : Bob부 멍충이
+// Link  : https://www.acmicpc.net/problem/34009 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  cin >> n;
+
+  cout << ((n & 1) ? "Bob" : "Alice");
+
+  return 0;
+}
