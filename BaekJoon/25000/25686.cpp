00fd560b8aea5cca82600cf320964da8f4ac80fb
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-06-02 21:53:10 +0900

    25686 - 1차원

---
+++ BaekJoon/25000/25686.cpp
@@ -0,0 +1,34 @@
+// Title : 1차원
+// Link  : https://www.acmicpc.net/problem/25686 
+// Time  : 8 ms
+// Memory: 2532 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+constexpr int kMax = 1 << 17;
+
+int arr[kMax];
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  cin >> n;
+
+  iota(arr, arr + kMax, 1);
+  for (int i = 1, j = 0; i < kMax; i++) {
+    int b = kMax >> 1;
+    while (j & b) j ^= b, b >>= 1;
+    j |= b;
+    if (i < j) swap(arr[i], arr[j]);
+  }
+
+  for (int i = 0; i < kMax; i++) {
+    if (arr[i] <= n) cout << arr[i] << " ";
+  }
+
+  return 0;
+}
