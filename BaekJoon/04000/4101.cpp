1e6e20b67a343cc23f1289e5076c43ce8675dab5
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-06-06 22:41:29 +0900

    4101 - 크냐?

---
+++ BaekJoon/04000/4101.cpp
@@ -0,0 +1,20 @@
+// Title : 크냐?
+// Link  : https://www.acmicpc.net/problem/4101 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int a, b;
+  while (cin >> a >> b && a && b) {
+    cout << (a > b ? "Yes\n" : "No\n");
+  }
+
+  return 0;
+}
