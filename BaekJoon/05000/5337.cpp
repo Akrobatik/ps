2cf17bea6096ad389beda7cbf115705e707959a0
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-06-06 22:41:17 +0900

    5337 - 웰컴

---
+++ BaekJoon/05000/5337.cpp
@@ -0,0 +1,22 @@
+// Title : 웰컴
+// Link  : https://www.acmicpc.net/problem/5337 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+constexpr const char* kStr =
+R"AKRBT(.  .   .
+|  | _ | _. _ ._ _  _
+|/\|(/.|(_.(_)[ | )(/.)AKRBT";
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  cout << kStr;
+
+  return 0;
+}
