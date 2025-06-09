0a9ec4e2c19e512a2d80a27355bbfecd62653cb1
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-06-09 16:21:04 +0900

    14626 - ISBN

---
+++ BaekJoon/14000/14626.cpp
@@ -0,0 +1,36 @@
+// Title : ISBN
+// Link  : https://www.acmicpc.net/problem/14626 
+// Time  : 0 ms
+// Memory: 2024 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+bool Check(const string& s) {
+  int x = 0;
+  for (int i = 0; i < 12; i++) {
+    x += (s[i] - '0') * ((i & 1) ? 3 : 1);
+  }
+  return s[12] - '0' == (10 - x % 10) % 10;
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  string s;
+  cin >> s;
+  int idx = 0;
+  while (s[idx] != '*') ++idx;
+
+  for (int i = 0; i < 10; i++) {
+    s[idx] = i + '0';
+    if (Check(s)) {
+      cout << i;
+      break;
+    }
+  }
+
+  return 0;
+}
