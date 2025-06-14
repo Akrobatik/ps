589dad863cff7dfea05d67e5d689c3915751fbf2
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-06-15 04:07:03 +0900

    15596 - 정수 N개의 합

---
+++ BaekJoon/15000/15596.cpp
@@ -0,0 +1,11 @@
+// Title : 정수 N개의 합
+// Link  : https://www.acmicpc.net/problem/15596 
+// Time  : 4 ms
+// Memory: 13740 KB
+
+#include <vector>
+long long sum(std::vector<int> &a) {
+	long long ans = 0;
+    for (auto e : a) ans += e;
+	return ans;
+}
