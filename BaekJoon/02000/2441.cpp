f4d3653fdc1bd7ba01a086b1d4cc836f83767bfd
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-06-15 04:06:34 +0900

    2441 - 별 찍기 - 4

---
+++ BaekJoon/02000/2441.cpp
@@ -0,0 +1,23 @@
+// Title : 별 찍기 - 4
+// Link  : https://www.acmicpc.net/problem/2441 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  cin >> n;
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < i; j++) cout << ' ';
+    for (int j = i; j < n; j++) cout << '*';
+    cout << '\n';
+  }
+
+  return 0;
+}
