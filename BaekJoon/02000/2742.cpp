e03915d9468c013a84bbe6fb364d484328b927e4
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-06-17 18:42:40 +0900

    2742 - 기찍 N

---
+++ BaekJoon/02000/2742.cpp
@@ -0,0 +1,21 @@
+// Title : 기찍 N
+// Link  : https://www.acmicpc.net/problem/2742 
+// Time  : 8 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  cin >> n;
+  for (int i = n; i > 0; i--) {
+    cout << i << "\n";
+  }
+
+  return 0;
+}
