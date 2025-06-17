0acf0dc66edc402a10b6f5737dc9eef0e4cf3cd2
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-06-17 18:42:33 +0900

    4999 - 아!

---
+++ BaekJoon/04000/4999.cpp
@@ -0,0 +1,20 @@
+// Title : 아!
+// Link  : https://www.acmicpc.net/problem/4999 
+// Time  : 0 ms
+// Memory: 2024 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  string s1, s2;
+  cin >> s1 >> s2;
+
+  cout << (s1.size() >= s2.size() ? "go" : "no");
+
+  return 0;
+}
