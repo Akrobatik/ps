9d821b45667b724f56aacd77f9db939f952379e8
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-06-17 18:43:09 +0900

    34027 - 제곱 수?

---
+++ BaekJoon/34000/34027.cpp
@@ -0,0 +1,28 @@
+// Title : 제곱 수?
+// Link  : https://www.acmicpc.net/problem/34027 
+// Time  : 0 ms
+// Memory: 2288 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+constexpr int kMax = 1e9;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  vector<int> arr;
+  for (int i = 1; i * i <= kMax; i++) arr.push_back(i * i);
+
+  int t;
+  cin >> t;
+  while (t--) {
+    int n;
+    cin >> n;
+    cout << binary_search(arr.begin(), arr.end(), n) << "\n";
+  }
+
+  return 0;
+}
