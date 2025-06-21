611ada6ffd1dd130445a06e97cc4dcb85e31e1c0
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-06-21 23:18:11 +0900

    2003 - 수들의 합 2

---
+++ BaekJoon/02000/2003.cpp
@@ -0,0 +1,30 @@
+// Title : 수들의 합 2
+// Link  : https://www.acmicpc.net/problem/2003 
+// Time  : 0 ms
+// Memory: 2180 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n, m;
+  cin >> n >> m;
+  vector<int> arr(n);
+  for (auto& e : arr) cin >> e;
+
+  int l = 0, r = 0;
+  int ls = 0, rs = 0;
+  int cnt = 0;
+  while (l < n) {
+    while (r < n && ls + m > rs) rs += arr[r++];
+    if (ls + m == rs) ++cnt;
+    ls += arr[l++];
+  }
+  cout << cnt;
+
+  return 0;
+}
