0fa80f64054db37bd896affe633de66c2880dc13
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-06-23 07:41:27 +0900

    34019 - [G] Grounded Number

---
+++ BaekJoon/34000/34019.cpp
@@ -0,0 +1,19 @@
+// Title : [G] Grounded Number
+// Link  : https://www.acmicpc.net/problem/34019 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int64_t n;
+  cin >> n;
+  cout << ((n & 1) ? "No" : "Yes");
+
+  return 0;
+}
