78ae806069c3e8c50a514d22c2267748d26b7c37
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-06-23 00:31:34 +0900

    14445 - 케이크(?) 자르기

---
+++ BaekJoon/14000/14445.cpp
@@ -0,0 +1,24 @@
+// Title : 케이크(?) 자르기
+// Link  : https://www.acmicpc.net/problem/14445 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int64_t n;
+  cin >> n;
+
+  if (n == 1) {
+    cout << "0";
+  } else {
+    cout << (n >> 1) + (n & 1);
+  }
+
+  return 0;
+}
