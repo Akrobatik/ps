ee03b566e8c80a9e0978939548cc053830967cb8
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-06-23 11:11:16 +0900

    2447 - 별 찍기 - 10

---
+++ BaekJoon/02000/2447.cpp
@@ -0,0 +1,44 @@
+// Title : 별 찍기 - 10
+// Link  : https://www.acmicpc.net/problem/2447 
+// Time  : 68 ms
+// Memory: 6836 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<string> board;
+
+void Traverse(int y, int x, int n) {
+  if (n == 3) {
+    for (int i = 0; i < 3; i++) {
+      for (int j = 0; j < 3; j++) {
+        if (i == 1 && j == 1) continue;
+        board[y + i][x + j] = '*';
+      }
+    }
+    return;
+  }
+
+  n /= 3;
+  for (int i = 0; i < 3; i++) {
+    for (int j = 0; j < 3; j++) {
+      if (i == 1 && j == 1) continue;
+      Traverse(y + i * n, x + j * n, n);
+    }
+  }
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  cin >> n;
+  board.resize(n, string(n, ' '));
+  Traverse(0, 0, n);
+
+  for (auto& s : board) cout << s << "\n";
+
+  return 0;
+}
