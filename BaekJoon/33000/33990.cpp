4de85c826efdd1956e85f5d32b1f58f1cab1eab0
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-06-25 09:24:19 +0900

    33990 - 3대 512

---
+++ BaekJoon/33000/33990.cpp
@@ -0,0 +1,27 @@
+// Title : 3대 512
+// Link  : https://www.acmicpc.net/problem/33990 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  cin >> n;
+  int best = INT_MAX;
+  while (n--) {
+    int a, b, c;
+    cin >> a >> b >> c;
+    int sum = a + b + c;
+    if (sum < 512) continue;
+    best = min<int>(best, sum);
+  }
+  cout << (best != INT_MAX ? best : -1);
+
+  return 0;
+}
