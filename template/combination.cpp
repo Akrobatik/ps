fd8313120e7e42bba18052760d6dbc68062c4aba
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-06-25 13:50:14 +0900

    rollback

--- template/combination.cpp
+++ template/combination.cpp
@@ -2,20 +2,21 @@
 
 using namespace std;
 
-template <int MAXN, int MOD>
+template <int MAXN, int64_t MOD>
+  requires(MAXN >= 0, MOD >= 3)
 struct Combination {
   constexpr Combination() {
     fact[0] = 1;
-    for (int i = 1; i <= MAXN; i++) fact[i] = (int64_t)fact[i - 1] * i % MOD;
+    for (int i = 1; i <= MAXN; i++) fact[i] = (fact[i - 1] * i) % MOD;
     inv[MAXN] = Power(fact[MAXN], MOD - 2);
-    for (int i = MAXN; i > 0; i--) inv[i - 1] = (int64_t)inv[i] * i % MOD;
+    for (int i = MAXN; i > 0; i--) inv[i - 1] = (inv[i] * i) % MOD;
   }
 
-  constexpr int operator()(int n, int r) const {
-    return ((int64_t)fact[n] * inv[r] % MOD) * inv[n - r] % MOD;
+  int64_t operator()(int n, int r) const {
+    return (((fact[n] * inv[r]) % MOD) * inv[n - r]) % MOD;
   }
 
-  static constexpr int Power(int64_t n, int exp) {
+  static constexpr int64_t Power(int64_t n, int exp) {
     int64_t res = 1;
     while (exp) {
       if (exp & 1) res = (res * n) % MOD;
@@ -25,5 +26,5 @@ struct Combination {
     return res;
   }
 
-  int fact[MAXN + 1], inv[MAXN + 1];
+  int64_t fact[MAXN + 1], inv[MAXN + 1];
 };
