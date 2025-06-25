49fd074a1227dcaa0a445515f5493f4ee7a8a04a
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-06-26 07:45:38 +0900

    34032 - /gg

---
+++ BaekJoon/34000/34032.cpp
@@ -0,0 +1,21 @@
+// Title : /gg
+// Link  : https://www.acmicpc.net/problem/34032 
+// Time  : 0 ms
+// Memory: 2912 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  string s;
+  cin >> n >> s;
+  int half = (n + 1) >> 1;
+  cout << (count(s.begin(), s.end(), 'O') >= half ? "Yes" : "No");
+
+  return 0;
+}
