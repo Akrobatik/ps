1a2b3e0ad349f428ae463f36b17e29e7a1aa1315
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-06-26 07:15:57 +0900

    17943 - 도미노 예측

---
+++ BaekJoon/17000/17943.cpp
@@ -0,0 +1,39 @@
+// Title : 도미노 예측
+// Link  : https://www.acmicpc.net/problem/17943 
+// Time  : 68 ms
+// Memory: 2804 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n, q;
+  cin >> n >> q;
+  vector<int> arr(n);
+  for (int i = 1; i < n; i++) {
+    cin >> arr[i];
+    arr[i] ^= arr[i - 1];
+  }
+
+  while (q--) {
+    int cmd;
+    cin >> cmd;
+    if (cmd == 0) {
+      int x, y;
+      cin >> x >> y;
+      int key = arr[x - 1] ^ arr[y - 1];
+      cout << key << "\n";
+    } else {
+      int x, y, d;
+      cin >> x >> y >> d;
+      int key = arr[x - 1] ^ arr[y - 1];
+      cout << (key ^ d) << "\n";
+    }
+  }
+
+  return 0;
+}
