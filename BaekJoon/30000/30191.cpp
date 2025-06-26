ab1a02dcda2cd7d8fd3ac2b04ed2b32ec41189aa
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-06-27 05:19:37 +0900

    30191 - 문자열 만들기 1

---
+++ BaekJoon/30000/30191.cpp
@@ -0,0 +1,38 @@
+// Title : 문자열 만들기 1
+// Link  : https://www.acmicpc.net/problem/30191 
+// Time  : 16 ms
+// Memory: 6152 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  string s;
+  cin >> n >> s;
+
+  string ans;
+  ans.reserve(n << 1);
+
+  vector<char> stk;
+  constexpr char kXor = 'S' ^ 'U';
+  for (auto c : views::reverse(s)) {
+    if (stk.empty() || c == stk.back()) {
+      ans.push_back(c ^ kXor);
+      ans.push_back('N');
+      stk.push_back(c);
+    } else {
+      ans.push_back('N');
+      stk.pop_back();
+    }
+  }
+
+  cout << ans.size() << "\n"
+       << ans;
+
+  return 0;
+}
