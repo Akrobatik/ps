4cb13c3fdf5ccb04c3d3c334278d8385195889e3
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-06-28 04:53:34 +0900

    1956 - 운동

---
+++ BaekJoon/01000/1956.cpp
@@ -0,0 +1,37 @@
+// Title : 운동
+// Link  : https://www.acmicpc.net/problem/1956 
+// Time  : 84 ms
+// Memory: 2680 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int v, e;
+  cin >> v >> e;
+  vector<vector<int>> dist(v + 1, vector<int>(v + 1, INT_MAX >> 1));
+  while (e--) {
+    int a, b, c;
+    cin >> a >> b >> c;
+    dist[a][b] = c;
+  }
+
+  for (int k = 0; k < v; k++) {
+    for (int i = 1; i <= v; i++) {
+      for (int j = 1; j <= v; j++) {
+        dist[i][j] = min<int>(dist[i][j], dist[i][k] + dist[k][j]);
+      }
+    }
+  }
+
+  int minn = INT_MAX;
+  for (int i = 1; i <= v; i++) minn = min<int>(minn, dist[i][i]);
+
+  cout << (minn < (INT_MAX >> 1) ? minn : -1);
+
+  return 0;
+}
