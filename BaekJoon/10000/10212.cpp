9001829e382942577c2f84d9693878632383d9c1
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-06-27 12:03:32 +0900

    10212 - Mystery

---
+++ BaekJoon/10000/10212.cpp
@@ -0,0 +1,23 @@
+// Title : Mystery
+// Link  : https://www.acmicpc.net/problem/10212 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+constexpr const char* kStr[2] = {
+    "Yonsei",
+    "Korea",
+};
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  mt19937 gen(random_device{}());
+  cout << kStr[gen() & 1];
+
+  return 0;
+}
