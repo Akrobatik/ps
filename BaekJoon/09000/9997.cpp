092f401f152edeb7c66b7465962a6aeb78022a31
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-06-27 13:20:13 +0900

    9997 - 폰트

---
+++ BaekJoon/09000/9997.cpp
@@ -0,0 +1,39 @@
+// Title : 폰트
+// Link  : https://www.acmicpc.net/problem/9997 
+// Time  : 536 ms
+// Memory: 2024 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  cin >> n;
+  vector<bitset<32>> arr(n);
+  for (auto& e : arr) {
+    string s;
+    cin >> s;
+    for (auto c : s) e.set(c - 'a');
+  }
+
+  int ans = 0;
+  int limit = 1 << n;
+  for (int b = 1; b < limit; b++) {
+    int x = b;
+    bitset<32> cnt;
+    while (x) {
+      int lsb = x & (-x);
+      cnt |= arr[countr_zero((uint32_t)lsb)];
+      x -= lsb;
+    }
+
+    ans += (cnt.count() == 26);
+  }
+  cout << ans;
+
+  return 0;
+}
