94671754e269c715bfbbe65426413356029f52b6
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-06-27 09:36:29 +0900

    25343 - 최장 최장 증가 부분 수열

---
+++ BaekJoon/25000/25343.cpp
@@ -0,0 +1,40 @@
+// Title : 최장 최장 증가 부분 수열
+// Link  : https://www.acmicpc.net/problem/25343 
+// Time  : 68 ms
+// Memory: 2152 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  cin >> n;
+
+  vector<vector<int>> board(n, vector<int>(n));
+  for (auto& vec : board) {
+    for (auto& e : vec) cin >> e;
+  }
+
+  vector<vector<int>> memo(n, vector<int>(n));
+  int maxx = 0;
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n; j++) {
+      int x = board[i][j], cur = 0;
+      for (int k = 0; k <= i; k++) {
+        for (int l = 0; l <= j; l++) {
+          if (x <= board[k][l]) continue;
+          cur = max<int>(cur, memo[k][l]);
+        }
+      }
+      memo[i][j] = cur + 1;
+      maxx = max<int>(maxx, memo[i][j]);
+    }
+  }
+  cout << maxx;
+
+  return 0;
+}
