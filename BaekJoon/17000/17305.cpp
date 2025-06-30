c6d9631b5fde94db397b2e9037397282c52db5db
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-06-30 23:29:58 +0900

    17305 - 사탕 배달

---
+++ BaekJoon/17000/17305.cpp
@@ -0,0 +1,49 @@
+// Title : 사탕 배달
+// Link  : https://www.acmicpc.net/problem/17305 
+// Time  : 60 ms
+// Memory: 5128 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n, w;
+  cin >> n >> w;
+
+  vector<int> arr, brr;
+  for (int i = 0; i < n; i++) {
+    int t, s;
+    cin >> t >> s;
+    if (t == 3) {
+      arr.push_back(s);
+    } else {
+      brr.push_back(s);
+    }
+  }
+
+  sort(arr.begin(), arr.end(), greater<int>());
+  sort(brr.begin(), brr.end(), greater<int>());
+
+  vector<int64_t> axr(arr.size() + 1);
+  for (int i = 0; i < arr.size(); i++) {
+    axr[i + 1] = axr[i] + arr[i];
+  }
+
+  auto Calc = [&](int rem) {
+    return axr[min<int>(arr.size(), rem / 3)] - axr[0];
+  };
+
+  int64_t maxx = Calc(w), sum = 0;
+  for (auto e : brr) {
+    if ((w -= 5) < 0) break;
+    sum += e;
+    maxx = max<int64_t>(maxx, sum + Calc(w));
+  }
+  cout << maxx;
+
+  return 0;
+}
