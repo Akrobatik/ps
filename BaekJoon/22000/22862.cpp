3e426230ff56474139c15dedebff02c7c509a6d6
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-06-30 21:45:10 +0900

    22862 - 가장 긴 짝수 연속한 부분 수열 (large)

--- BaekJoon/22000/22862.cpp
+++ BaekJoon/22000/22862.cpp
@@ -1,7 +1,7 @@
 // Title : 가장 긴 짝수 연속한 부분 수열 (large)
 // Link  : https://www.acmicpc.net/problem/22862 
 // Time  : 96 ms
-// Memory: 5220 KB
+// Memory: 5928 KB
 
 #include <bits/stdc++.h>
 
@@ -14,31 +14,14 @@ int main() {
   int n, k;
   cin >> n >> k;
 
-  vector<int> arr;
-  int pb = 0, cnt = 0;
-  while (n--) {
-    int x;
-    cin >> x;
-    if ((x & 1) == pb) {
-      ++cnt;
-    } else {
-      arr.push_back(cnt);
-      pb ^= 1, cnt = 1;
-    }
-  }
+  vector<int> arr(n);
+  for (auto& e : arr) cin >> e, e &= 1;
 
-  int na = arr.size();
-  if (!(na & 1)) arr.push_back(cnt), ++na;
-
-  int maxx = 0, r = -1, odd = 0, even = arr[0];
-  for (int i = 0; i < na; i += 2) {
-    while (r + 2 < na && odd + arr[r + 2] <= k) {
-      odd += arr[r + 2];
-      even += arr[r + 3];
-      r += 2;
-    }
-    maxx = max<int>(maxx, even);
-    if (i <= r) even -= arr[i], odd -= arr[i + 1];
+  int maxx = 0, l = 0, r = 0, sum[2] = {};
+  while (l < n) {
+    while (r < n && sum[1] + arr[r] <= k) ++sum[arr[r++]];
+    maxx = max<int>(maxx, sum[0]);
+    --sum[arr[l++]];
   }
   cout << maxx;
 
