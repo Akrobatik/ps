a34dc60fdc963f29bcd57229a94959ad3fe7e3a5
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-07-02 02:53:52 +0900

    15802 - 타노스

---
+++ BaekJoon/15000/15802.cpp
@@ -0,0 +1,17 @@
+// Title : 타노스
+// Link  : https://www.acmicpc.net/problem/15802 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  cout << "1";
+
+  return 0;
+}
