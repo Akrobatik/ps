3d76c8741bfcb2d4d5664f8d5325188637d02587
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-07-01 14:01:41 +0900

    2490 - 윷놀이

---
+++ BaekJoon/02000/2490.cpp
@@ -0,0 +1,25 @@
+// Title : 윷놀이
+// Link  : https://www.acmicpc.net/problem/2490 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  for (int i = 0; i < 3; i++) {
+    int cnt = 0;
+    for (int j = 0; j < 4; j++) {
+      int x;
+      cin >> x;
+      cnt += x;
+    }
+    cout << "DCBAE"[cnt] << "\n";
+  }
+
+  return 0;
+}
