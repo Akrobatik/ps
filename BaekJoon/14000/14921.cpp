f4b768b47b215dc3ded25d44ff9385d3817cf4f3
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-07-01 10:38:59 +0900

    14921 - 용액 합성하기

---
+++ BaekJoon/14000/14921.cpp
@@ -0,0 +1,28 @@
+// Title : 용액 합성하기
+// Link  : https://www.acmicpc.net/problem/14921 
+// Time  : 12 ms
+// Memory: 2412 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  cin >> n;
+  vector<int> arr(n);
+  for (auto& e : arr) cin >> e;
+
+  int minn = INT_MAX;
+  for (int i = 0; i < n - 1; i++) {
+    auto it = lower_bound(arr.begin() + i + 1, arr.end(), -arr[i]);
+    if (it != arr.end() && abs(minn) > abs(arr[i] + *it)) minn = arr[i] + *it;
+    if (--it != arr.begin() + i && abs(minn) > abs(arr[i] + *it)) minn = arr[i] + *it;
+  }
+  cout << minn;
+
+  return 0;
+}
