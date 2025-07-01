6812ec41e1ef71644ddd228035e50cdf075daf4f
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-07-01 10:09:59 +0900

    1083 - 소트

---
+++ BaekJoon/01000/1083.cpp
@@ -0,0 +1,42 @@
+// Title : 소트
+// Link  : https://www.acmicpc.net/problem/1083 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  cin >> n;
+  vector<int> arr(n);
+  for (auto& e : arr) cin >> e;
+  int s;
+  cin >> s;
+
+  auto Find = [&](int i) {
+    int res = -1;
+    for (int j = 1; j <= s && i + j < n; j++) {
+      int k = i + j;
+      if (arr[i] >= arr[k]) continue;
+      if (res == -1 || arr[k] > arr[res]) res = k;
+    }
+    return res;
+  };
+
+  for (int i = 0; i < n && s > 0; i++) {
+    int j = Find(i);
+    while (i < j) {
+      swap(arr[j - 1], arr[j]);
+      --j, --s;
+    }
+  }
+
+  for (auto e : arr) cout << e << " ";
+
+  return 0;
+}
