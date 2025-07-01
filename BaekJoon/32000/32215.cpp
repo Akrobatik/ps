392ee650496e352720584b6071581148aa6bccca
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-07-01 09:16:09 +0900

    32215 - 코드마스터 2024

---
+++ BaekJoon/32000/32215.cpp
@@ -0,0 +1,20 @@
+// Title : 코드마스터 2024
+// Link  : https://www.acmicpc.net/problem/32215 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n, m, k;
+  cin >> n >> m >> k;
+
+  cout << (k + (n != k)) * m;
+
+  return 0;
+}
