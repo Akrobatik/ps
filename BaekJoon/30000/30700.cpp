a51f7ba480354acca0a6fe8f7c018fc484f7c2a2
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-07-02 18:09:00 +0900

    30700 - KOREA 문자열 만들기

---
+++ BaekJoon/30000/30700.cpp
@@ -0,0 +1,23 @@
+// Title : KOREA 문자열 만들기
+// Link  : https://www.acmicpc.net/problem/30700 
+// Time  : 0 ms
+// Memory: 2024 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  string s;
+  cin >> s;
+  int cnt = 0;
+  for (auto c :s) {
+    cnt += (c == "KOREA"[cnt % 5]);
+  }
+  cout << cnt;
+
+  return 0;
+}
