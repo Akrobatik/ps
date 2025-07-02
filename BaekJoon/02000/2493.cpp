704a42e9d7ba20ea1c3445c13edeeed5e97114f3
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-07-02 23:00:54 +0900

    2493 - 탑

---
+++ BaekJoon/02000/2493.cpp
@@ -0,0 +1,27 @@
+// Title : 탑
+// Link  : https://www.acmicpc.net/problem/2493 
+// Time  : 88 ms
+// Memory: 7176 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  cin >> n;
+  vector<int> arr(n);
+  for (auto& e : arr) cin >> e;
+
+  vector<int> stk;
+  for (int i = 0; i < n; i++) {
+    while (!stk.empty() && arr[stk.back()] < arr[i]) stk.pop_back();
+    cout << (stk.empty() ? 0 : stk.back() + 1) << " ";
+    stk.push_back(i);
+  }
+
+  return 0;
+}
