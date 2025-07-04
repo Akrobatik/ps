4b63646bf601b714ee1747363f284a22f2b56a85
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-07-04 13:44:14 +0900

    1091 - 카드 섞기

---
+++ BaekJoon/01000/1091.cpp
@@ -0,0 +1,41 @@
+// Title : 카드 섞기
+// Link  : https://www.acmicpc.net/problem/1091 
+// Time  : 24 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  cin >> n;
+  vector<int> arr(n), axr(n), brr(n);
+  for (auto& e : arr) cin >> e;
+  for (auto& e : brr) cin >> e;
+
+  auto Check = [&]() {
+    for (int i = 0; i < n; i++) {
+      if (i % 3 != arr[i]) return false;
+    }
+    return true;
+  };
+
+  for (int i = 0; i < 510510; i++) {
+    if (Check()) {
+      cout << i;
+      return 0;
+    }
+
+    for (int j = 0; j < n; j++) {
+      axr[brr[j]] = arr[j];
+    }
+    swap(arr, axr);
+  }
+  cout << "-1";
+
+  return 0;
+}
