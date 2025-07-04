ca11e15cc00c29befbad083ee35023fd8c2621b3
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-07-04 23:51:07 +0900

    5717 - 상근이의 친구들

---
+++ BaekJoon/05000/5717.cpp
@@ -0,0 +1,20 @@
+// Title : 상근이의 친구들
+// Link  : https://www.acmicpc.net/problem/5717 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int a, b;
+  while (cin >> a >> b && a && b) {
+    cout << a + b << "\n";
+  }
+
+  return 0;
+}
