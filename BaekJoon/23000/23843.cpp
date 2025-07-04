93b94fce1e602c9a7939020b7251fe6966b6e63b
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-07-04 12:32:25 +0900

    23843 - 콘센트

---
+++ BaekJoon/23000/23843.cpp
@@ -0,0 +1,33 @@
+// Title : 콘센트
+// Link  : https://www.acmicpc.net/problem/23843 
+// Time  : 0 ms
+// Memory: 2176 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n, m;
+  cin >> n >> m;
+  vector<int> arr(n);
+  for (auto& e : arr) cin >> e;
+  sort(arr.begin(), arr.end(), greater<int>());
+
+  priority_queue<int, vector<int>, greater<int>> pq;
+  for (int i = 0; i < m; i++) pq.push(0);
+  for (int i = 0; i < n; i++) {
+    auto x = pq.top();
+    pq.pop();
+    pq.push(x + arr[i]);
+  }
+
+  int x;
+  while (!pq.empty()) x = pq.top(), pq.pop();
+  cout << x;
+
+  return 0;
+}
