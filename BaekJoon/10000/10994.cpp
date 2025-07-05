f3098a4df71e05fcf52269052ab4080c59d615fb
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-07-06 02:41:17 +0900

    10994 - 별 찍기 - 19

---
+++ BaekJoon/10000/10994.cpp
@@ -0,0 +1,35 @@
+// Title : 별 찍기 - 19
+// Link  : https://www.acmicpc.net/problem/10994 
+// Time  : 0 ms
+// Memory: 2284 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<string> board;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  cin >> n;
+  int h = n * 4 - 3, w = h;
+  vector<string> board(h, string(w, ' '));
+
+  int half = h >> 1;
+  for (int i = 0; i < n; i++) {
+    int sz = i << 1;
+    for (int j = -sz; j <= sz; j++) {
+      board[half + j][half - sz] = board[half - sz][half + j] = board[half + j][half + sz] = board[half + sz][half + j] = '*';
+    }
+  }
+
+  for (auto& s : board) {
+    while (s.back() == ' ') s.pop_back();
+    cout << s << "\n";
+  }
+
+  return 0;
+}
