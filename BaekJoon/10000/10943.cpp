c14a91b19c340d297305e3c239f712cb173f55bb
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-07-06 02:49:12 +0900

    10943 - 랜덤 게임~

---
+++ BaekJoon/10000/10943.cpp
@@ -0,0 +1,18 @@
+// Title : 랜덤 게임~
+// Link  : https://www.acmicpc.net/problem/10943 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  mt19937 gen(random_device{}());
+  cout << gen() % 10 + 1;
+
+  return 0;
+}
