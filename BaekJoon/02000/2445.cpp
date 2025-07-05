5a24d8135c8a651fe5d1c2152c00137964013e18
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-07-05 22:15:13 +0900

    2445 - 별 찍기 - 8

---
+++ BaekJoon/02000/2445.cpp
@@ -0,0 +1,29 @@
+// Title : 별 찍기 - 8
+// Link  : https://www.acmicpc.net/problem/2445 
+// Time  : 0 ms
+// Memory: 2152 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  cin >> n;
+  int sz = (n << 1) - 1;
+
+  vector<string> board(sz, string(sz + 1, ' '));
+  for (int i = 0; i < sz; i++) {
+    int limit = min<int>(i + 1, sz - i);
+    for (int j = 0; j < limit; j++) {
+      board[i][j] = board[i][sz - j] = '*';
+    }
+  }
+
+  for (auto& s : board) cout << s << "\n";
+
+  return 0;
+}
