ab1c8aaae3b0b777b714108327717d2ada3ac8f2
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-07-05 22:15:03 +0900

    2522 - 별 찍기 - 12

---
+++ BaekJoon/02000/2522.cpp
@@ -0,0 +1,29 @@
+// Title : 별 찍기 - 12
+// Link  : https://www.acmicpc.net/problem/2522 
+// Time  : 0 ms
+// Memory: 2152 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  cin >> n;
+  int sz = (n << 1) - 1;
+
+  vector<string> board(sz, string(n, ' '));
+  for (int i = 0; i < sz; i++) {
+    int limit = min<int>(i + 1, sz - i);
+    for (int j = 0; j < limit; j++) {
+      board[i][n - j - 1] = '*';
+    }
+  }
+
+  for (auto& s : board) cout << s << "\n";
+
+  return 0;
+}
