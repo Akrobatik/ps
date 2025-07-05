d8b03e3d50a65e4e7387a3de203a1f4cce302720
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-07-05 22:14:42 +0900

    10991 - 별 찍기 - 16

---
+++ BaekJoon/10000/10991.cpp
@@ -0,0 +1,32 @@
+// Title : 별 찍기 - 16
+// Link  : https://www.acmicpc.net/problem/10991 
+// Time  : 0 ms
+// Memory: 2152 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  cin >> n;
+  int sz = (n << 1) - 1;
+
+  vector<string> board(n, string(sz, ' '));
+  for (int i = 0; i < n; i++) {
+    int limit = min<int>(i + 1, sz - i);
+    for (int j = limit - 1; j >= 0; j -= 2) {
+      board[i][n - j - 1] = board[i][n + j - 1] = '*';
+    }
+  }
+
+  for (auto& s : board) {
+    while (s.back() == ' ') s.pop_back();
+    cout << s << "\n";
+  }
+
+  return 0;
+}
