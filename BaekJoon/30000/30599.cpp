c7e120e3ab7ae9e2a3a889770f0a8e384fd3c379
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-07-05 13:47:42 +0900

    30599 - Divisibility Trick

---
+++ BaekJoon/30000/30599.cpp
@@ -0,0 +1,21 @@
+// Title : Divisibility Trick
+// Link  : https://www.acmicpc.net/problem/30599 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int d;
+  cin >> d;
+  for (int i = 0; i < d; i++) {
+    cout << d;
+  }
+
+  return 0;
+}
