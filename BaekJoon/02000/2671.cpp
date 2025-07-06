591730151b181c68bb7d2bb036191144f3d9e969
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-07-06 21:07:23 +0900

    2671 - 잠수함식별

---
+++ BaekJoon/02000/2671.cpp
@@ -0,0 +1,21 @@
+// Title : 잠수함식별
+// Link  : https://www.acmicpc.net/problem/2671 
+// Time  : 0 ms
+// Memory: 2172 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  string s;
+  cin >> s;
+
+  regex re("^(100+1+|01)+$");
+  cout << (regex_match(s, re) ? "SUBMARINE" : "NOISE");
+
+  return 0;
+}
