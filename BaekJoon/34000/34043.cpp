5bae28036154c3cc8a2e9626e2fc4f3848e9d8e5
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-07-07 03:15:13 +0900

    34043 - 수라도

---
+++ BaekJoon/34000/34043.cpp
@@ -0,0 +1,20 @@
+// Title : 수라도
+// Link  : https://www.acmicpc.net/problem/34043 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  cin >> n;
+
+  cout << ((n <= 3 && (n & 1) == 1) ? "NO" : "YES");
+
+  return 0;
+}
