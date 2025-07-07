1b757e8a61b03612599dec9378853285bf2066be
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-07-07 23:08:59 +0900

    15894 - 수학은 체육과목 입니다

---
+++ BaekJoon/15000/15894.cpp
@@ -0,0 +1,19 @@
+// Title : 수학은 체육과목 입니다
+// Link  : https://www.acmicpc.net/problem/15894 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int64_t n;
+  cin >> n;
+  cout << n * 4;
+
+  return 0;
+}
