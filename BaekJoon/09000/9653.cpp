cbd15e5cc3d572cd8cf8db988201e6858dd20c7e
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-07-07 14:01:43 +0900

    9653 - 스타워즈 로고

---
+++ BaekJoon/09000/9653.cpp
@@ -0,0 +1,30 @@
+// Title : 스타워즈 로고
+// Link  : https://www.acmicpc.net/problem/9653 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+constexpr const char* kStr =
+R"(    8888888888  888    88888
+   88     88   88 88   88  88
+    8888  88  88   88  88888
+       88 88 888888888 88   88
+88888888  88 88     88 88    888888
+
+88  88  88   888    88888    888888
+88  88  88  88 88   88  88  88
+88 8888 88 88   88  88888    8888
+ 888  888 888888888 88  88      88
+  88  88  88     88 88   88888888)";
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  cout << kStr;
+
+  return 0;
+}
