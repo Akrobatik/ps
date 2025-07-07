3c4a0f4a447b4fa5588f323c05669e3a62cbaf63
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-07-07 23:12:13 +0900

    9063 - 대지

---
+++ BaekJoon/09000/9063.cpp
@@ -0,0 +1,29 @@
+// Title : 대지
+// Link  : https://www.acmicpc.net/problem/9063 
+// Time  : 8 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int minx = 10000, maxx = -10000;
+  int miny = 10000, maxy = -10000;
+
+  int n;
+  cin >> n;
+  while (n--) {
+    int x, y;
+    cin >> x >> y;
+    minx = min<int>(minx, x), maxx = max<int>(maxx, x);
+    miny = min<int>(miny, y), maxy = max<int>(maxy, y);
+  }
+
+  cout << (int64_t)(maxx - minx) * (maxy - miny);
+
+  return 0;
+}
