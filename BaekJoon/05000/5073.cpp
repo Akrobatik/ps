8d642a69d0e10c773a3d0c79fd7b59d8dc15acb0
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-07-07 23:52:00 +0900

    5073 - 삼각형과 세 변

---
+++ BaekJoon/05000/5073.cpp
@@ -0,0 +1,38 @@
+// Title : 삼각형과 세 변
+// Link  : https://www.acmicpc.net/problem/5073 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int a, b, c;
+  while (cin >> a >> b >> c && a && b && c) {
+    int x[] = {a, b, c};
+    sort(x, x + 3);
+
+    if (x[0] + x[1] <= x[2]) {
+      cout << "Invalid\n";
+      continue;
+    }
+
+    if (x[0] == x[2]) {
+      cout << "Equilateral\n";
+      continue;
+    }
+
+    if (x[0] == x[1] || x[1] == x[2]) {
+      cout << "Isosceles\n";
+      continue;
+    }
+
+    cout << "Scalene\n";
+  }
+
+  return 0;
+}
