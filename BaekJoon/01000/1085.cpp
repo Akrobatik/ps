d517284bf0d30a973c658ecc4242c7623c9a93f9
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-07-07 23:00:08 +0900

    1085 - 직사각형에서 탈출

---
+++ BaekJoon/01000/1085.cpp
@@ -0,0 +1,19 @@
+// Title : 직사각형에서 탈출
+// Link  : https://www.acmicpc.net/problem/1085 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int x, y, w, h;
+  cin >> x >> y >> w >> h;
+  cout << min<int>({x, w - x, y, h - y});
+
+  return 0;
+}
