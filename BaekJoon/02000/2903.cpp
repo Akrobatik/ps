143d00722a3e1e829d5eb91e4b0d7f4e4639fe06
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-07-07 22:33:49 +0900

    2903 - 중앙 이동 알고리즘

---
+++ BaekJoon/02000/2903.cpp
@@ -0,0 +1,21 @@
+// Title : 중앙 이동 알고리즘
+// Link  : https://www.acmicpc.net/problem/2903 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int x = 2;
+  int n;
+  cin >> n;
+  while (n--) x = x * 2 - 1;
+  cout << x * x;
+
+  return 0;
+}
