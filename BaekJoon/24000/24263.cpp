18f4acba98c4689df33df69d41e67fcc89337de3
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-07-07 23:55:49 +0900

    24263 - 알고리즘 수업 - 알고리즘의 수행 시간 2

---
+++ BaekJoon/24000/24263.cpp
@@ -0,0 +1,19 @@
+// Title : 알고리즘 수업 - 알고리즘의 수행 시간 2
+// Link  : https://www.acmicpc.net/problem/24263 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  cin >> n;
+  cout << n << "\n1";
+
+  return 0;
+}
