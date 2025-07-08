544e8a7b78081d45f3ee8945c7e8e49ea15c1f73
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-07-09 03:59:26 +0900

    12789 - 도키도키 간식드리미

---
+++ BaekJoon/12000/12789.cpp
@@ -0,0 +1,31 @@
+// Title : 도키도키 간식드리미
+// Link  : https://www.acmicpc.net/problem/12789 
+// Time  : 0 ms
+// Memory: 2152 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  cin >> n;
+  vector<int> arr(n), stk;
+  for (auto& e : arr) cin >> e;
+
+  for (int i = 1, j = 0; i <= n; i++) {
+    if (!stk.empty() && stk.back() == i) {
+      stk.pop_back();
+      continue;
+    }
+    while (j < n && arr[j] != i) stk.push_back(arr[j++]);
+    ++j;
+  }
+
+  cout << (stk.empty() ? "Nice" : "Sad");
+
+  return 0;
+}
