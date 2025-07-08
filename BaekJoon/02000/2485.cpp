f81f39549e9b61c3c7077de1825df81f05b60225
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-07-08 20:30:57 +0900

    2485 - 가로수

---
+++ BaekJoon/02000/2485.cpp
@@ -0,0 +1,33 @@
+// Title : 가로수
+// Link  : https://www.acmicpc.net/problem/2485 
+// Time  : 12 ms
+// Memory: 2412 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int GCD(int a, int b) {
+  while (b) a %= b, swap(a, b);
+  return a;
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  cin >> n;
+  vector<int> arr(n);
+  for (auto& e : arr) cin >> e;
+  int g = arr[1] - arr[0];
+  for (int i = 2; i < n; i++) {
+    int d = arr[i] - arr[i - 1];
+    g = GCD(d, g);
+  }
+
+  int cnt = (arr[n - 1] - arr[0]) / g - n + 1;
+  cout << cnt;
+
+  return 0;
+}
