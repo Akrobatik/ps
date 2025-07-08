6230e0e0a82072e38ac2952035036a00ae446d4d
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-07-09 04:31:54 +0900

    24723 - 녹색거탑

---
+++ BaekJoon/24000/24723.cpp
@@ -0,0 +1,19 @@
+// Title : 녹색거탑
+// Link  : https://www.acmicpc.net/problem/24723 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  cin >> n;
+  cout << (1 << n);
+
+  return 0;
+}
