e94da69902f88a9231b9bdbf8ca58eda718ba395
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-07-08 20:25:38 +0900

    1735 - 분수 합

---
+++ BaekJoon/01000/1735.cpp
@@ -0,0 +1,27 @@
+// Title : 분수 합
+// Link  : https://www.acmicpc.net/problem/1735 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int GCD(int a, int b) {
+  while (b) a %= b, swap(a, b);
+  return a;
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int p1, q1, p2, q2;
+  cin >> p1 >> q1 >> p2 >> q2;
+  int np = p1 * q2 + p2 * q1, nq = q1 * q2;
+  int g = GCD(np, nq);
+  np /= g, nq /= g;
+  cout << np << " " << nq;
+
+  return 0;
+}
