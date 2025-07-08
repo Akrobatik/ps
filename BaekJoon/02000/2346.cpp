6903b50440ba0e4aa7c330478927015f6e575999
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-07-09 04:24:59 +0900

    2346 - 풍선 터뜨리기

---
+++ BaekJoon/02000/2346.cpp
@@ -0,0 +1,37 @@
+// Title : 풍선 터뜨리기
+// Link  : https://www.acmicpc.net/problem/2346 
+// Time  : 0 ms
+// Memory: 2152 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  cin >> n;
+  vector<int> arr(n);
+  for (auto& e : arr) cin >> e;
+
+  list<int> ls;
+  for (int i = 0; i < n; i++) ls.push_back(i);
+
+  auto it = ls.begin();
+  for (;;) {
+    int x = *it;
+    cout << x + 1 << " ";
+    if (it = ls.erase(it); it == ls.end()) it = ls.begin(); 
+    if (it == ls.end()) break;
+    int d = arr[x], l = ls.size();
+    if (d > 0) --d;
+    if ((d %= l) < 0) d += l;
+    while (d--) {
+      if (++it == ls.end()) it = ls.begin();
+    }
+  }
+
+  return 0;
+}
