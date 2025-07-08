76949dbb0ece6a90589162aebbeb7d1f6f870325
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-07-09 03:52:43 +0900

    13909 - 창문 닫기

---
+++ BaekJoon/13000/13909.cpp
@@ -0,0 +1,29 @@
+// Title : 창문 닫기
+// Link  : https://www.acmicpc.net/problem/13909 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  cin >> n;
+
+  int lo = 1, hi = 45826;
+  while (lo + 1 < hi) {
+    int mid = (lo + hi) >> 1;
+    if (mid * mid <= n) {
+      lo = mid;
+    } else {
+      hi = mid;
+    }
+  }
+  cout << lo;
+
+  return 0;
+}
