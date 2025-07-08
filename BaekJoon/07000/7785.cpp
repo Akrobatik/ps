e34f3a1195793a1ed086a214a9085339f859c1c0
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-07-08 19:56:58 +0900

    7785 - 회사에 있는 사람

---
+++ BaekJoon/07000/7785.cpp
@@ -0,0 +1,29 @@
+// Title : 회사에 있는 사람
+// Link  : https://www.acmicpc.net/problem/7785 
+// Time  : 76 ms
+// Memory: 9948 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  set<string, greater<string>> hs;
+  int n;
+  cin >> n;
+  while (n--) {
+    string s, cmd;
+    cin >> s >> cmd;
+    if (cmd[0] == 'e') {
+      hs.insert(s);
+    } else {
+      hs.erase(s);
+    }
+  }
+  for (auto& s : hs) cout << s << "\n";
+
+  return 0;
+}
