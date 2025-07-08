814d0a10f691da83475e476cc17dfa2e8d301a6e
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-07-08 20:21:02 +0900

    1305 - 광고

---
+++ BaekJoon/01000/1305.cpp
@@ -0,0 +1,55 @@
+// Title : 광고
+// Link  : https://www.acmicpc.net/problem/1305 
+// Time  : 8 ms
+// Memory: 8056 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct KMP {
+  void Init(string_view sv) {
+    int n = sv.size();
+    pat = sv;
+    table.clear(), table.resize(n, 0);
+    int idx = 0;
+    for (int i = 1; i < n; i++) {
+      while (idx && sv[i] != sv[idx]) idx = table[idx - 1];
+      if (sv[i] == sv[idx]) table[i] = ++idx;
+    }
+  }
+
+  vector<int> Search(string_view sv) {
+    vector<int> res;
+    int n = sv.size(), np = table.size();
+    int idx = 0;
+    for (int i = 0; i < n; i++) {
+      while (idx && sv[i] != pat[idx]) idx = table[idx - 1];
+      if (sv[i] == pat[idx]) {
+        if (++idx == np) {
+          res.push_back(i - np + 1);
+          idx = table[idx - 1];
+        }
+      }
+    }
+    return res;
+  }
+
+  string pat;
+  vector<int> table;
+};
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  string s;
+  cin >> n >> s;
+
+  KMP kmp;
+  kmp.Init(s);
+  cout << n - kmp.table[n - 1];
+
+  return 0;
+}
