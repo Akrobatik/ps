90fe662308425217be59007f5477771096ea5a8c
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-07-08 19:26:55 +0900

    25305 - 커트라인

---
+++ BaekJoon/25000/25305.cpp
@@ -0,0 +1,22 @@
+// Title : 커트라인
+// Link  : https://www.acmicpc.net/problem/25305 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n, k;
+  cin >> n >> k;
+  vector<int> arr(n);
+  for (auto& e : arr) cin >> e;
+  sort(arr.begin(), arr.end(), greater<int>());
+  cout << arr[k - 1];
+
+  return 0;
+}
