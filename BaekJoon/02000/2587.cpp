e62ae0bd5f79d2f4fa3cfe415638021c8f386a2a
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-07-08 19:25:43 +0900

    2587 - 대표값2

---
+++ BaekJoon/02000/2587.cpp
@@ -0,0 +1,21 @@
+// Title : 대표값2
+// Link  : https://www.acmicpc.net/problem/2587 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int arr[5];
+  for (auto& e : arr) cin >> e;
+  sort(arr, arr + 5);
+  cout << accumulate(arr, arr + 5, 0) / 5 << "\n"
+       << arr[2];
+
+  return 0;
+}
