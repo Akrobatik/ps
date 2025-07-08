484b6ab3a358330300021f2ba793ac1dfe65408c
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-07-09 04:33:04 +0900

    15439 - 베라의 패션

---
+++ BaekJoon/15000/15439.cpp
@@ -0,0 +1,19 @@
+// Title : 베라의 패션
+// Link  : https://www.acmicpc.net/problem/15439 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  cin >> n;
+  cout << n * (n - 1);
+
+  return 0;
+}
