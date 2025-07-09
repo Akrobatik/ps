332c876a9775ec695196d25a08c9d8941f671e11
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-07-09 20:42:20 +0900

    1904 - 01타일

--- BaekJoon/01000/1904.cpp
+++ BaekJoon/01000/1904.cpp
@@ -1,27 +1,42 @@
 // Title : 01타일
 // Link  : https://www.acmicpc.net/problem/1904 
-// Time  : 4 ms
-// Memory: 5928 KB
+// Time  : 0 ms
+// Memory: 2020 KB
 
 #include <bits/stdc++.h>
 
 using namespace std;
 
-constexpr int kMod = 15746;
+struct Matrix {
+  friend Matrix operator*(const Matrix& m1, const Matrix& m2) {
+    Matrix m;
+    m.m[0][0] = (m1.m[0][0] * m2.m[0][0] + m1.m[0][1] * m2.m[1][0]) % 15746;
+    m.m[0][1] = (m1.m[0][0] * m2.m[0][1] + m1.m[0][1] * m2.m[1][1]) % 15746;
+    m.m[1][0] = (m1.m[1][0] * m2.m[0][0] + m1.m[1][1] * m2.m[1][0]) % 15746;
+    m.m[1][1] = (m1.m[1][0] * m2.m[0][1] + m1.m[1][1] * m2.m[1][1]) % 15746;
+    return m;
+  }
+
+  int64_t m[2][2];
+};
+
+Matrix Exp(int64_t n, const Matrix& m) {
+  if (n == 1) return m;
+
+  auto exp = Exp(n / 2, m);
+  auto mp = exp * exp;
+  if (n % 2) mp = mp * m;
+  return mp;
+}
 
 int main() {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
 
-  int n;
+  int64_t n;
   cin >> n;
-  vector<int> arr(n + 1);
-  arr[0] = arr[1] = 1;
-  for (int i = 2; i <= n; i++) {
-    arr[i] = arr[i - 2] + arr[i - 1];
-    if (arr[i] >= kMod) arr[i] -= kMod;
-  }
-  cout << arr[n];
+  auto m = Exp(n + 1, {{{1, 1}, {1, 0}}});
+  cout << m.m[0][1] << endl;
 
   return 0;
 }
