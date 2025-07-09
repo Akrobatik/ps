a6867f69e6301e0d3377e99e0e2a7289014c4a14
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-07-09 17:15:29 +0900

    4779 - 칸토어 집합

---
+++ BaekJoon/04000/4779.cpp
@@ -0,0 +1,31 @@
+// Title : 칸토어 집합
+// Link  : https://www.acmicpc.net/problem/4779 
+// Time  : 0 ms
+// Memory: 2660 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  while (cin >> n) {
+    int m = 1;
+    while (n--) m *= 3;
+    
+    string s(m, ' ');
+    s[0] = '-';
+    int cur = 1;
+    while (cur != m) {
+      int nxt = cur * 3;
+      copy(s.begin(), s.begin() + cur, s.begin() + nxt - cur);
+      cur = nxt;
+    }
+    cout << s << "\n";
+  }
+
+  return 0;
+}
