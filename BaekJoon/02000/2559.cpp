2521d09b2d3cdf91bfd346ce2dc06a4b4c54bf2a
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-07-09 19:09:44 +0900

    2559 - 수열

---
+++ BaekJoon/02000/2559.cpp
@@ -0,0 +1,30 @@
+// Title : 수열
+// Link  : https://www.acmicpc.net/problem/2559 
+// Time  : 8 ms
+// Memory: 2412 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n, k;
+  cin >> n >> k;
+  vector<int> arr(n + 1);
+  for (int i = 0; i < n; i++) {
+    int x;
+    cin >> x;
+    arr[i + 1] = arr[i] + x;
+  }
+
+  int maxx = INT_MIN;
+  for (int i = k; i <= n; i++) {
+    maxx = max<int>(maxx, arr[i] - arr[i - k]);
+  }
+  cout << maxx;
+
+  return 0;
+}
