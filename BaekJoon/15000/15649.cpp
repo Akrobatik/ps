793f57a356a4b81d833290a846c6bba7de411b73
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-07-09 17:37:38 +0900

    15649 - N과 M (1)

---
+++ BaekJoon/15000/15649.cpp
@@ -0,0 +1,40 @@
+// Title : N과 M (1)
+// Link  : https://www.acmicpc.net/problem/15649 
+// Time  : 20 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int arr[8];
+bool used[9];
+
+void Traverse(int idx, int maxx, int limit) {
+  if (idx == limit) {
+    for (int i = 0; i < limit; i++) {
+      cout << arr[i] << " ";
+    }
+    cout << "\n";
+    return;
+  }
+
+  for (int i = 1; i <= maxx; i++) {
+    if (used[i]) continue;
+    used[i] = true;
+    arr[idx] = i;
+    Traverse(idx + 1, maxx, limit);
+    used[i] = false;
+  }
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n, k;
+  cin >> n >> k;
+  Traverse(0, n, k);
+
+  return 0;
+}
