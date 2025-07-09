7b9b7fe5c3f62a028546ff370ffbeeb2dbc012c8
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-07-09 20:34:20 +0900

    24416 - 알고리즘 수업 - 피보나치 수 1

--- BaekJoon/24000/24416.cpp
+++ BaekJoon/24000/24416.cpp
@@ -1,15 +1,32 @@
 // Title : 알고리즘 수업 - 피보나치 수 1
 // Link  : https://www.acmicpc.net/problem/24416 
-// Time  : 180 ms
+// Time  : 0 ms
 // Memory: 2020 KB
 
 #include <bits/stdc++.h>
 
 using namespace std;
 
-int Fib1(int n) {
-  if (n == 1 || n == 2) return 1;
-  return Fib1(n - 1) + Fib1(n - 2);
+struct Matrix {
+  friend Matrix operator*(const Matrix& m1, const Matrix& m2) {
+    Matrix m;
+    m.m[0][0] = (m1.m[0][0] * m2.m[0][0] + m1.m[0][1] * m2.m[1][0]);
+    m.m[0][1] = (m1.m[0][0] * m2.m[0][1] + m1.m[0][1] * m2.m[1][1]);
+    m.m[1][0] = (m1.m[1][0] * m2.m[0][0] + m1.m[1][1] * m2.m[1][0]);
+    m.m[1][1] = (m1.m[1][0] * m2.m[0][1] + m1.m[1][1] * m2.m[1][1]);
+    return m;
+  }
+
+  int64_t m[2][2];
+};
+
+Matrix Exp(int64_t n, const Matrix& m) {
+  if (n == 1) return m;
+
+  auto exp = Exp(n / 2, m);
+  auto mp = exp * exp;
+  if (n % 2) mp = mp * m;
+  return mp;
 }
 
 int main() {
@@ -18,7 +35,8 @@ int main() {
 
   int n;
   cin >> n;
-  cout << Fib1(n) << " " << max<int>(n - 2, 1);
+  auto m = Exp(n, {{{1, 1}, {1, 0}}});
+  cout << m.m[0][1] << " " << max<int>(n - 2, 1);
 
   return 0;
 }
