621eb4cd941f40359c54f77bd1f8a300e754824c
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-07-11 15:51:09 +0900

    33963 - 돈복사

---
+++ BaekJoon/33000/33963.cpp
@@ -0,0 +1,23 @@
+// Title : 돈복사
+// Link  : https://www.acmicpc.net/problem/33963 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int64_t n;
+  cin >> n;
+  int sz = to_string(n).size();
+
+  int cnt = 0;
+  while (sz == to_string(n).size()) n <<= 1, ++cnt;
+  cout << cnt - 1;
+
+  return 0;
+}
