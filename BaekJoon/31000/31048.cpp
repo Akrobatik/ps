86bc90715659d045c32c6f3a5741adda3edb397e
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-07-11 17:50:10 +0900

    31048 - Last Factorial Digit

---
+++ BaekJoon/31000/31048.cpp
@@ -0,0 +1,25 @@
+// Title : Last Factorial Digit
+// Link  : https://www.acmicpc.net/problem/31048 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int arr[6] = {1, 1, 2, 6, 4, 0};
+
+  int t;
+  cin >> t;
+  while (t--) {
+    int n;
+    cin >> n;
+    cout << arr[min<int>(n, 5)] << "\n";
+  }
+
+  return 0;
+}
