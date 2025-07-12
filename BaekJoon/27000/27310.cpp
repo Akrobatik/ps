daf900d4c8776a665ba67ac7afefd240db5b31c3
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-07-13 00:27:35 +0900

    27310 - :chino_shock:

---
+++ BaekJoon/27000/27310.cpp
@@ -0,0 +1,29 @@
+// Title : :chino_shock:
+// Link  : https://www.acmicpc.net/problem/27310 
+// Time  : 0 ms
+// Memory: 2024 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  string s;
+  cin >> s;
+  int sum = 0;
+  for (auto c : s) {
+    if (c == '_') {
+      sum += 6;
+    } else if (c == ':') {
+      sum += 2;
+    } else {
+      sum += 1;
+    }
+  }
+  cout << sum;
+
+  return 0;
+}
