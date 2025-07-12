d45d1a42aaeff2a3d2f7a1847b6b240b3ce4fffc
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-07-13 00:19:30 +0900

    28239 - 배고파(Easy)

---
+++ BaekJoon/28000/28239.cpp
@@ -0,0 +1,48 @@
+// Title : 배고파(Easy)
+// Link  : https://www.acmicpc.net/problem/28239 
+// Time  : 4 ms
+// Memory: 2160 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  vector<uint64_t> arr;
+  for (int i = 0; i <= 62; i++) {
+    uint64_t x = 1ULL << i;
+    for (int j = i; j <= 62; j++) {
+      uint64_t y = 1ULL << j;
+      arr.push_back(x + y);
+    }
+  }
+  sort(arr.begin(), arr.end());
+
+  int t;
+  cin >> t;
+  while (t--) {
+    uint64_t m;
+    cin >> m;
+    if (m <= 2) {
+      cout << "0 0\n";
+      continue;
+    }
+
+    auto it = lower_bound(arr.begin(), arr.end(), m);
+    uint64_t rv = *it, lv = *--it;
+    uint64_t v = (m - lv <= rv - m ? lv : rv);
+    uint64_t lsb = v & (-v);
+    uint64_t msb = v ^ lsb;
+    if (msb == 0) {
+      int x = countr_zero(lsb) - 1;
+      cout << x << " " << x << "\n";
+    } else {
+      cout << countr_zero(lsb) << " " << countr_zero(msb) << "\n";
+    }
+  }
+
+  return 0;
+}
