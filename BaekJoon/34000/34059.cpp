eb9c17668fbeda3aca9f0923a878be8de9f9eff0
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-07-14 03:24:55 +0900

    34059 - 해안선

--- BaekJoon/34000/34059.cpp
+++ BaekJoon/34000/34059.cpp
@@ -1,42 +1,125 @@
 // Title : 해안선
 // Link  : https://www.acmicpc.net/problem/34059 
-// Time  : 16 ms
-// Memory: 17644 KB
+// Time  : 0 ms
+// Memory: 2020 KB
 
 #include <bits/stdc++.h>
 
 using namespace std;
 
-template <int MAXN, int64_t MOD>
-  requires(MAXN >= 0, MOD >= 3)
-struct Combination {
-  constexpr Combination() {
-    fact[0] = 1;
-    for (int i = 1; i <= MAXN; i++) fact[i] = (fact[i - 1] * i) % MOD;
-    inv[MAXN] = Power(fact[MAXN], MOD - 2);
-    for (int i = MAXN; i > 0; i--) inv[i - 1] = (inv[i] * i) % MOD;
-  }
+template <int32_t MOD>
+struct ModInt32 {
+  using CT = typename std::conditional<
+      (MOD <= std::numeric_limits<int32_t>::max() / 2),
+      int32_t,
+      int64_t>::type;
 
-  int64_t operator()(int n, int r) const {
-    return (((fact[n] * inv[r]) % MOD) * inv[n - r]) % MOD;
-  }
+  constexpr ModInt32() : val(0) {}
+  constexpr ModInt32(int32_t x) : val((x %= MOD) < 0 ? x + MOD : x) {}
+  constexpr ModInt32(int64_t x) : val((x %= MOD) < 0 ? x + MOD : x) {}
+  constexpr ModInt32(__int128_t x) : val((x %= MOD) < 0 ? x + MOD : x) {}
 
-  static constexpr int64_t Power(int64_t n, int exp) {
-    int64_t res = 1;
+  constexpr ModInt32 Pow(int64_t exp) const {
+    int64_t n = val, x = 1;
     while (exp) {
-      if (exp & 1) res = (res * n) % MOD;
-      n = (n * n) % MOD;
+      if (exp & 1) x = x * n % MOD;
+      n = n * n % MOD;
       exp >>= 1;
     }
+
+    ModInt32 res;
+    res.val = x;
     return res;
   }
 
-  int64_t fact[MAXN + 1], inv[MAXN + 1];
+  constexpr ModInt32 Inv() const {
+    return Pow(MOD - 2);
+  }
+
+  constexpr ModInt32& operator++() {
+    if (++val == MOD) val = 0;
+    return *this;
+  }
+
+  constexpr ModInt32 operator++(int) {
+    ModInt32 tmp(*this);
+    operator++();
+    return tmp;
+  }
+
+  constexpr ModInt32& operator+=(const ModInt32& other) {
+    CT x = (CT)val + other.val;
+    if (x >= MOD) x -= MOD;
+    val = x;
+    return *this;
+  }
+
+  constexpr ModInt32& operator-=(const ModInt32& other) {
+    CT x = (CT)val + (MOD - other.val);
+    if (x >= MOD) x -= MOD;
+    val = x;
+    return *this;
+  }
+
+  constexpr ModInt32& operator*=(const ModInt32& other) {
+    val = (int64_t)val * other.val % MOD;
+    return *this;
+  }
+
+  constexpr ModInt32& operator/=(const ModInt32& other) {
+    *this *= other.Inv();
+    return *this;
+  }
+
+  constexpr ModInt32 operator-() const {
+    ModInt32 res;
+    if (val) res.val = MOD - val;
+    return res;
+  }
+
+  constexpr ModInt32 operator+(const ModInt32& rhs) const {
+    return ModInt32(*this) += rhs;
+  }
+
+  constexpr ModInt32 operator-(const ModInt32& rhs) const {
+    return ModInt32(*this) -= rhs;
+  }
+
+  constexpr ModInt32 operator*(const ModInt32& rhs) const {
+    return ModInt32(*this) *= rhs;
+  }
+
+  constexpr ModInt32 operator/(const ModInt32& rhs) const {
+    return ModInt32(*this) /= rhs;
+  }
+
+  constexpr bool operator!() const {
+    return val == 0;
+  }
+
+  friend istream& operator>>(istream& is, ModInt32& num) {
+    is >> num.val;
+    if ((num.val %= MOD) < 0) num.val += MOD;
+    return is;
+  }
+
+  friend ostream& operator<<(ostream& os, const ModInt32& num) {
+    os << num.val;
+    return os;
+  }
+
+  int32_t val;
 };
 
-constexpr int kMax = 1e6;
 constexpr int kMod = 1e9 + 7;
-const Combination<kMax, kMod> kComb;
+using ModInt = ModInt32<kMod>;
+
+ModInt nCr(int n, int r) {
+  if (r > n - r) r = n - r;
+  ModInt x = 1, y = 1;
+  for (int i = 0; i < r; i++) x *= n - i, y *= i + 1;
+  return x / y;
+}
 
 int main() {
   ios::sync_with_stdio(false);
@@ -47,10 +130,7 @@ int main() {
   if (a > b) swap(a, b);
 
   int x = a - 2, y = n - b, z = b - a - 1;
-  int64_t ans = kComb(x + y, x) * kComb.Power(2, z) % kMod;
-  if (b - a == 1) {
-    if ((ans += kComb.Power(2, n - 3)) >= kMod) ans -= kMod; 
-  }
+  ModInt p = 2, ans = nCr(x + y, x) * p.Pow(z) + (b - a == 1 ? p.Pow(n - 3) : 0);
   cout << ans;
 
   return 0;
