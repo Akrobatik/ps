3a437f221d5ff5b2baea75a2f1369405c37cc3ca
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-07-15 21:19:39 +0900

    23235 - The Fastest Sorting Algorithm In The World

---
+++ BaekJoon/23000/23235.cpp
@@ -0,0 +1,23 @@
+// Title : The Fastest Sorting Algorithm In The World
+// Link  : https://www.acmicpc.net/problem/23235 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  int t = 0;
+  while (cin >> n && n) {
+    int x;
+    while (n--) cin >> x;
+    cout << "Case " << ++t << ": Sorting... done!\n";
+  }
+
+  return 0;
+}
