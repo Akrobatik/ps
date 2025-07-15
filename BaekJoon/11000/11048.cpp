6b57b0dc09ea42816fbef2ab9130c5954331aea5
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-07-16 03:30:43 +0900

    11048 - 이동하기

---
+++ BaekJoon/11000/11048.cpp
@@ -0,0 +1,28 @@
+// Title : 이동하기
+// Link  : https://www.acmicpc.net/problem/11048 
+// Time  : 60 ms
+// Memory: 5936 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int memo[1001][1001];
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n, m;
+  cin >> n >> m;
+  for (int i = 1; i <= n; i++) {
+    for (int j = 1; j <= m; j++) {
+      int x;
+      cin >> x;
+      memo[i][j] = max<int>(memo[i - 1][j], memo[i][j - 1]) + x;
+    }
+  }
+  cout << memo[n][m];
+
+  return 0;
+}
