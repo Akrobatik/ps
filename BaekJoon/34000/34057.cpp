eeaca0cc14c99a5408c841d0b44cda1327faee13
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-07-16 21:55:20 +0900

    34057 - 시장조성하기

---
+++ BaekJoon/34000/34057.cpp
@@ -0,0 +1,48 @@
+// Title : 시장조성하기
+// Link  : https://www.acmicpc.net/problem/34057 
+// Time  : 880 ms
+// Memory: 88036 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using tup = tuple<int64_t, int64_t, int64_t>;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  cin >> n;
+  vector<tup> arr(n);
+  int64_t sa = 0, sb = 0;
+  for (auto& [a, b, x] : arr) {
+    cin >> a >> b >> x;
+    sa += a, sb += b;
+    a = sa, b = sb;
+  }
+
+  sort(arr.begin(), arr.end(), [](const tup& lhs, const tup& rhs) {
+    auto [la, lb, lx] = lhs;
+    auto [ra, rb, rx] = rhs;
+    return la > ra || (la == ra && lb < rb);
+  });
+
+  int64_t sum = 0;
+  map<int64_t, int64_t> memo;
+  for (auto [a, b, x] : arr) {
+    int64_t rem = x;
+    auto it = memo.upper_bound(b);
+    bool pass = (it == memo.end()) && !(a <= 0 && 0 <= b);
+    while (rem && it != memo.end()) {
+      int64_t cut = min<int64_t>(rem, it->second);
+      sum -= cut, rem -= cut;
+      if ((it->second -= cut) == 0) it = memo.erase(it);
+    }
+    if (!pass) memo[b] += x, sum += x;
+  }
+  cout << sum;
+
+  return 0;
+}
