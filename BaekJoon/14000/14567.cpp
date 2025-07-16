d1f97600fe4e8c79c8b813effb4c12c41f6c1ae2
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-07-16 09:15:05 +0900

    14567 - 선수과목 (Prerequisite)

--- BaekJoon/14000/14567.cpp
+++ BaekJoon/14000/14567.cpp
@@ -1,7 +1,7 @@
 // Title : 선수과목 (Prerequisite)
 // Link  : https://www.acmicpc.net/problem/14567 
 // Time  : 92 ms
-// Memory: 4956 KB
+// Memory: 4948 KB
 
 #include <bits/stdc++.h>
 
@@ -15,30 +15,15 @@ int main() {
 
   int n, m;
   cin >> n >> m;
-  vector<int> deg(n + 1);
   while (m--) {
     int a, b;
     cin >> a >> b;
     edges[a].push_back(b);
-    ++deg[b];
   }
 
-  queue<int> q;
   vector<int> ans(n + 1, 1);
-
   for (int i = 1; i <= n; i++) {
-    if (deg[i]) continue;
-    q.push(i);
-  }
-
-  while (!q.empty()) {
-    int cur = q.front();
-    q.pop();
-
-    for (auto nxt : edges[cur]) {
-      ans[nxt] = max<int>(ans[nxt], ans[cur] + 1);
-      if (--deg[nxt] == 0) q.push(nxt);
-    }
+    for (auto nxt : edges[i]) ans[nxt] = max<int>(ans[nxt], ans[i] + 1);
   }
 
   for (int i = 1; i <= n; i++) cout << ans[i] << " ";
