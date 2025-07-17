727766a8e9c5b54f97a7f343444c37dacb17e2ce
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-07-18 04:31:50 +0900

    34066 - 반짝임이 있는 곳

--- BaekJoon/34000/34066.cpp
+++ BaekJoon/34000/34066.cpp
@@ -1,13 +1,13 @@
 // Title : 반짝임이 있는 곳
 // Link  : https://www.acmicpc.net/problem/34066 
-// Time  : 128 ms
-// Memory: 16428 KB
+// Time  : 84 ms
+// Memory: 5936 KB
 
 #include <bits/stdc++.h>
 
 using namespace std;
 
-vector<int> edges[200001];
+using tup = tuple<int, int, int>;
 
 int main() {
   ios::sync_with_stdio(false);
@@ -18,18 +18,13 @@ int main() {
   vector<int> arr(n + 1);
   for (int i = 1; i <= n; i++) cin >> arr[i];
 
-  for (int i = 1; i < n; i++) {
+  vector<tup> edges(n - 1);
+  for (int i = 0; i < n - 1; i++) {
     int u, v;
     cin >> u >> v;
-    edges[u].push_back(v);
-    edges[v].push_back(u);
+    edges[i] = {min<int>(arr[u], arr[v]), u, v};
   }
-
-  vector<int> sorted(n);
-  iota(sorted.begin(), sorted.end(), 1);
-  sort(sorted.begin(), sorted.end(), [&](int lhs, int rhs) {
-    return arr[lhs] > arr[rhs];
-  });
+  sort(edges.begin(), edges.end(), greater<tup>());
 
   vector<int> memo(n + 1);
   iota(memo.begin(), memo.end(), 0);
@@ -44,32 +39,15 @@ int main() {
 
   auto Union = [&](int a, int b) {
     a = Find(a), b = Find(b);
+    bool ok = (arr[a] == arr[b]);
+    if (arr[a] > arr[b]) swap(a, b);
     memo[b] = a;
+    return ok;
   };
 
-  int cnt = 0, idx = 0;
-  vector<bool> check(n + 1), tmp(n + 1);
-  while (idx < n) {
-    int nxt = idx;
-    while (nxt < n && arr[sorted[idx]] == arr[sorted[nxt]]) check[sorted[nxt++]] = true;
-    for (int i = idx; i < nxt; i++) {
-      int u = sorted[i];
-      for (auto v : edges[u]) {
-        if (!check[v]) continue;
-        Union(u, v);
-      }
-    }
-    for (int i = idx; i < nxt; i++) {
-      int id = Find(sorted[i]);
-      if (tmp[id]) continue;
-      tmp[id] = true;
-      ++cnt;
-    }
-    for (int i = idx; i < nxt; i++) {
-      int id = Find(sorted[i]);
-      tmp[id] = false;
-    }
-    idx = nxt;
+  int cnt = n;
+  for (auto [w, u, v] : edges) {
+    cnt -= Union(u, v);
   }
   cout << cnt;
 
