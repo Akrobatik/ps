3e0260b7696161efcfc36fd7ab85b5c3865e666f
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-07-18 03:19:43 +0900

    34069 - 자리 바꾸기

---
+++ BaekJoon/34000/34069.cpp
@@ -0,0 +1,47 @@
+// Title : 자리 바꾸기
+// Link  : https://www.acmicpc.net/problem/34069 
+// Time  : 44 ms
+// Memory: 3080 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n, m;
+  cin >> n >> m;
+  if (n * m & 1) {
+    cout << "No";
+    return 0;
+  }
+
+  vector<vector<int>> mat(n, vector<int>(m));
+  for (auto& vec : mat) {
+    for (auto& e : vec) cin >> e;
+  }
+
+  if (~n & 1) {
+    for (int i = 0; i < n; i += 2) {
+      for (int j = 0; j < m; j++) {
+        swap(mat[i][j], mat[i + 1][j]);
+      }
+    }
+  } else {
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < m; j += 2) {
+        swap(mat[i][j], mat[i][j + 1]);
+      }
+    }
+  }
+
+  cout << "Yes\n";
+  for (auto& vec : mat) {
+    for (auto e : vec) cout << e << " ";
+    cout << "\n";
+  }
+
+  return 0;
+}
