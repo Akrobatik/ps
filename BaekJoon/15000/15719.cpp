9a48aad2d46c73f277946877d252256db1175688
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-07-19 03:02:45 +0900

    15719 - 중복된 숫자

---
+++ BaekJoon/15000/15719.cpp
@@ -0,0 +1,29 @@
+// Title : 중복된 숫자
+// Link  : https://www.acmicpc.net/problem/15719 
+// Time  : 1588 ms
+// Memory: 11664 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  bool check[10000001] = {};
+
+  int n;
+  cin >> n;
+  while (n--) {
+    int x;
+    cin >> x;
+    if (check[x]) {
+      cout << x;
+      return 0;
+    }
+    check[x] = true;
+  }
+
+  return 0;
+}
