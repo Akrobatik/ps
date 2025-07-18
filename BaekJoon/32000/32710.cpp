0477ccd7a0654293674505f949d3a0fe9593dd8b
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-07-19 02:59:46 +0900

    32710 - 구구단표

---
+++ BaekJoon/32000/32710.cpp
@@ -0,0 +1,27 @@
+// Title : 구구단표
+// Link  : https://www.acmicpc.net/problem/32710 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  bool ok[101] = {};
+  for (int i = 2; i <= 9; i++) {
+    for (int j = 1; j <= 9; j++) {
+      ok[i * j] = true;
+    }
+  }
+  ok[1] = true;
+  
+  int n;
+  cin >> n;
+  cout << ok[n];
+
+  return 0;
+}
