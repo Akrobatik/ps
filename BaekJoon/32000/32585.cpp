1997fdcf52794ba662597cb1c89a4299d073e29b
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-07-19 06:52:13 +0900

    32585 - Building Pyramids

---
+++ BaekJoon/32000/32585.cpp
@@ -0,0 +1,25 @@
+// Title : Building Pyramids
+// Link  : https://www.acmicpc.net/problem/32585 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  cin >> n;
+
+  int64_t sum = 0, val = 0;
+  for (int i = 1; i <= n; i++) {
+    val += i;
+    sum += val;
+  }
+  cout << sum;
+
+  return 0;
+}
