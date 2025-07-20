51bf2c33d776f258f6da78dc5af93b8ba4eade6e
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-07-20 22:28:32 +0900

    12963 - 달리기

---
+++ BaekJoon/12000/12963.cpp
@@ -0,0 +1,68 @@
+// Title : 달리기
+// Link  : https://www.acmicpc.net/problem/12963 
+// Time  : 4 ms
+// Memory: 2156 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+constexpr int kMod = 1e9 + 7;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n, m;
+  cin >> n >> m;
+
+  vector<pair<int, int>> edges(m);
+  for (auto& [u, v] : edges) cin >> u >> v;
+
+  vector<int> table(m + 1);
+  table[0] = 1;
+  for (int i = 1; i < m; i++) table[i] = (int64_t)table[i - 1] * 3 % kMod;
+
+  vector<int> memo(n);
+
+  auto Find = [&](int id) {
+    while (id != memo[id]) {
+      int par = memo[id];
+      id = memo[id] = memo[par];
+    }
+    return id;
+  };
+
+  auto Union = [&](int a, int b) {
+    a = Find(a), b = Find(b);
+    if (a != b) {
+      if (a > b) swap(a, b);
+      memo[b] = a;
+    }
+    return a == 0 && Find(n - 1) == 0;
+  };
+
+  int ans = 0;
+  vector<bool> used(m);
+  for (;;) {
+    iota(memo.begin(), memo.end(), 0);
+
+    bool out = true;
+    for (int i = m - 1; i >= 0; i--) {
+      if (used[i]) continue;
+
+      auto [u, v] = edges[i];
+      if (Union(u, v)) {
+        used[i] = true;
+        if ((ans += table[i]) >= kMod) ans -= kMod;
+        out = false;
+        break;
+      }
+    }
+
+    if (out) break;
+  }
+  cout << ans;
+
+  return 0;
+}
