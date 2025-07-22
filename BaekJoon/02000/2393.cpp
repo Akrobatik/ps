aa5f3ecfdb2aa231a600af1245b7d6447ea081a6
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-07-23 02:04:25 +0900

    2393 - Rook

---
+++ BaekJoon/02000/2393.cpp
@@ -0,0 +1,32 @@
+// Title : Rook
+// Link  : https://www.acmicpc.net/problem/2393 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+constexpr const char* kStr = 
+R"(  ___  ___  ___
+  | |__| |__| |
+  |           |
+   \_________/
+    \_______/
+     |     |
+     |     |
+     |     |
+     |     |
+     |_____|
+  __/       \__
+ /             \
+/_______________\)";
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  cout << kStr;
+
+  return 0;
+}
