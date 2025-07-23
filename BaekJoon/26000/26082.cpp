fd93f16fb7a11bf86fb79ba66f786b88598ad52c
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-07-24 02:42:13 +0900

    26082 - WARBOY

---
+++ BaekJoon/26000/26082.cpp
@@ -0,0 +1,19 @@
+// Title : WARBOY
+// Link  : https://www.acmicpc.net/problem/26082 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int a, b, c;
+  cin >> a >> b >> c;
+  cout << (b / a) * c * 3;
+
+  return 0;
+}
