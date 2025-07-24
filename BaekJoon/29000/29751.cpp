bfd0efa4cbb8788e81bbdd20a3b914b9113ec7b7
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-07-24 22:25:21 +0900

    29751 - 삼각형

---
+++ BaekJoon/29000/29751.cpp
@@ -0,0 +1,20 @@
+// Title : 삼각형
+// Link  : https://www.acmicpc.net/problem/29751 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int w, h;
+  cin >> w >> h;
+  int wh = w * h;
+  cout << (wh >> 1) << ((wh & 1) ? ".5" : ".0");
+
+  return 0;
+}
