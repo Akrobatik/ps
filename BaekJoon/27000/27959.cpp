e3477791eff16c7fa5368a8a4eae9f78ff0988b4
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-07-25 17:39:02 +0900

    27959 - 초코바

---
+++ BaekJoon/27000/27959.cpp
@@ -0,0 +1,19 @@
+// Title : 초코바
+// Link  : https://www.acmicpc.net/problem/27959 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n, m;
+  cin >> n >> m;
+  cout << (n * 100 >= m ? "Yes" : "No");
+
+  return 0;
+}
