c1170a8d023665468e8be7c86cb23851cd753332
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-07-26 20:50:10 +0900

    32384 - 사랑은 고려대입니다

---
+++ BaekJoon/32000/32384.cpp
@@ -0,0 +1,21 @@
+// Title : 사랑은 고려대입니다
+// Link  : https://www.acmicpc.net/problem/32384 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  cin >> n;
+  while (n--) {
+    cout << "LoveisKoreaUniversity ";
+  }
+
+  return 0;
+}
