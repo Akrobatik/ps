8d4df8e4bced28873a9613f54a37f2f5b9b3d17b
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-07-28 02:47:00 +0900

    28235 - 코드마스터 2023

---
+++ BaekJoon/28000/28235.cpp
@@ -0,0 +1,32 @@
+// Title : 코드마스터 2023
+// Link  : https://www.acmicpc.net/problem/28235 
+// Time  : 0 ms
+// Memory: 2024 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  string s;
+  cin >> s;
+  switch (s[0]) {
+    case 'S': {
+      cout << "HIGHSCHOOL";
+    } break;
+    case 'C': {
+      cout << "MASTER";
+    } break;
+    case '2': {
+      cout <<  "0611";
+    } break;
+    case 'A': {
+      cout << "CONTEST";
+    } break;
+  }
+
+  return 0;
+}
