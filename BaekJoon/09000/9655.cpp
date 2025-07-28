a271a10fae8625b11e529b7b75d3414d18be01c2
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-07-28 19:27:35 +0900

    9655 - 돌 게임

---
+++ BaekJoon/09000/9655.cpp
@@ -0,0 +1,19 @@
+// Title : 돌 게임
+// Link  : https://www.acmicpc.net/problem/9655 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  cin >> n;
+  cout << (n % 2 == 0 ? "CY" : "SK");
+
+  return 0;
+}
