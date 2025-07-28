739af00f120730a8879e6248ffffeb787035966d
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-07-28 10:44:39 +0900

    34077 - 폴카의 수학 공부

---
+++ BaekJoon/34000/34077.cpp
@@ -0,0 +1,40 @@
+// Title : 폴카의 수학 공부
+// Link  : https://www.acmicpc.net/problem/34077 
+// Time  : 0 ms
+// Memory: 2168 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int t;
+  cin >> t;
+  while (t--) {
+    int n;
+    string s;
+    cin >> n >> s;
+    auto pos = s.find('-');
+    if (pos == string::npos) {
+      cout << "YES\n";
+      continue;
+    }
+    ++pos;
+
+    while (pos < s.size() && '0' <= s[pos] && s[pos] <= '9') ++pos;
+
+    bool ok = true;
+    for (int i = pos; i < s.size(); i++) {
+      if ('1' <= s[i] && s[i] <= '9') {
+        ok = false;
+        break;
+      }
+    }
+    cout << (ok ? "YES\n" : "NO\n");
+  }
+
+  return 0;
+}
