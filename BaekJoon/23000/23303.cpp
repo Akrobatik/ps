d7b66c438c88a3ab399230044e5efbc0b4cf4768
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-07-31 00:38:42 +0900

    23303 - 이 문제는 D2 입니다.

---
+++ BaekJoon/23000/23303.cpp
@@ -0,0 +1,23 @@
+// Title : 이 문제는 D2 입니다.
+// Link  : https://www.acmicpc.net/problem/23303 
+// Time  : 0 ms
+// Memory: 2168 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  string s;
+  getline(cin, s);
+  for (auto& c : s) {
+    if (isalpha(c) && c < 'a') c ^= 32;
+  }
+
+  cout << (s.find("d2") != string::npos ? "D2" : "unrated");
+
+  return 0;
+}
