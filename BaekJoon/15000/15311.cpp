975a95d0a237cea3d8640c9adfb1da93662b4261
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-08-01 15:34:30 +0900

    15311 - 약 팔기

---
+++ BaekJoon/15000/15311.cpp
@@ -0,0 +1,19 @@
+// Title : 약 팔기
+// Link  : https://www.acmicpc.net/problem/15311 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  cout << "2000\n";
+  for (int i = 0; i < 1000; i++) cout << "1 ";
+  for (int i = 0; i < 1000; i++) cout << "1000 ";
+
+  return 0;
+}
