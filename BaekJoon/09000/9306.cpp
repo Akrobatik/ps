420152ade85f500fa3c2edda6861f2c2ee2c3db1
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-08-01 16:59:08 +0900

    9306 - Practice: Roll Call

---
+++ BaekJoon/09000/9306.cpp
@@ -0,0 +1,23 @@
+// Title : Practice: Roll Call
+// Link  : https://www.acmicpc.net/problem/9306 
+// Time  : 0 ms
+// Memory: 2380 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int t;
+  cin >> t;
+  for (int i = 1; i <= t; i++) {
+    string s1, s2;
+    cin >> s1 >> s2;
+    cout << format("Case {}: {}, {}\n", i, s2, s1);
+  }
+
+  return 0;
+}
