70f56f65fce2afa00d349cbaacf2686b12e6da6d
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-08-01 14:59:22 +0900

    34089 - Wildcard and Query

--- BaekJoon/34000/34089.cpp
+++ BaekJoon/34000/34089.cpp
@@ -1,6 +1,6 @@
 // Title : Wildcard and Query
 // Link  : https://www.acmicpc.net/problem/34089 
-// Time  : 200 ms
+// Time  : 192 ms
 // Memory: 86952 KB
 
 #include <bits/stdc++.h>
@@ -158,118 +158,6 @@ struct LCP {
  private:
   vector<int> buf;
 };
-
-template <int32_t MOD>
-struct ModInt32 {
-  using CT = typename std::conditional<
-      (MOD <= std::numeric_limits<int32_t>::max() / 2),
-      int32_t,
-      int64_t>::type;
-
-  constexpr ModInt32() : val(0) {}
-  constexpr ModInt32(int32_t x) : val((x %= MOD) < 0 ? x + MOD : x) {}
-  constexpr ModInt32(int64_t x) : val((x %= MOD) < 0 ? x + MOD : x) {}
-  constexpr ModInt32(__int128_t x) : val((x %= MOD) < 0 ? x + MOD : x) {}
-
-  constexpr ModInt32 Pow(int64_t exp) const {
-    int64_t n = val, x = 1;
-    while (exp) {
-      if (exp & 1) x = x * n % MOD;
-      n = n * n % MOD;
-      exp >>= 1;
-    }
-
-    ModInt32 res;
-    res.val = x;
-    return res;
-  }
-
-  constexpr ModInt32 Inv() const {
-    return Pow(MOD - 2);
-  }
-
-  constexpr ModInt32& operator++() {
-    if (++val == MOD) val = 0;
-    return *this;
-  }
-
-  constexpr ModInt32 operator++(int) {
-    ModInt32 tmp(*this);
-    operator++();
-    return tmp;
-  }
-
-  constexpr ModInt32& operator+=(const ModInt32& other) {
-    CT x = (CT)val + other.val;
-    if (x >= MOD) x -= MOD;
-    val = x;
-    return *this;
-  }
-
-  constexpr ModInt32& operator-=(const ModInt32& other) {
-    CT x = (CT)val + (MOD - other.val);
-    if (x >= MOD) x -= MOD;
-    val = x;
-    return *this;
-  }
-
-  constexpr ModInt32& operator*=(const ModInt32& other) {
-    val = (int64_t)val * other.val % MOD;
-    return *this;
-  }
-
-  constexpr ModInt32& operator/=(const ModInt32& other) {
-    *this *= other.Inv();
-    return *this;
-  }
-
-  constexpr ModInt32 operator-() const {
-    ModInt32 res;
-    if (val) res.val = MOD - val;
-    return res;
-  }
-
-  constexpr ModInt32 operator+(const ModInt32& rhs) const {
-    return ModInt32(*this) += rhs;
-  }
-
-  constexpr ModInt32 operator-(const ModInt32& rhs) const {
-    return ModInt32(*this) -= rhs;
-  }
-
-  constexpr ModInt32 operator*(const ModInt32& rhs) const {
-    return ModInt32(*this) *= rhs;
-  }
-
-  constexpr ModInt32 operator/(const ModInt32& rhs) const {
-    return ModInt32(*this) /= rhs;
-  }
-
-  constexpr bool operator!() const {
-    return val == 0;
-  }
-
-  friend istream& operator>>(istream& is, ModInt32& num) {
-    is >> num.val;
-    if ((num.val %= MOD) < 0) num.val += MOD;
-    return is;
-  }
-
-  friend ostream& operator<<(ostream& os, const ModInt32& num) {
-    os << num.val;
-    return os;
-  }
-
-  int32_t val;
-};
-
-constexpr int kMod1 = 1e9 + 7;
-constexpr int kMod2 = 1e9 + 9;
-constexpr int kBase1 = 373;
-constexpr int kBase2 = 331;
-
-using M1 = ModInt32<kMod1>;
-using M2 = ModInt32<kMod2>;
 
 constexpr int kMax = 1 << 19;
 vector<int> tree[kMax << 1];
