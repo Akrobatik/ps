d3970f1b8d1a45c75f7e5b6f0018f02b03a77bf4
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-08-06 17:20:04 +0900

    30821 - 별자리가 될 수 있다면

---
+++ BaekJoon/30000/30821.cpp
@@ -0,0 +1,24 @@
+// Title : 별자리가 될 수 있다면
+// Link  : https://www.acmicpc.net/problem/30821 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  cin >> n;
+  int64_t x = 1;
+  for (int i = 0; i < 5; i++) {
+    x *= n - i;
+    x /= i + 1;
+  }
+  cout << x;
+
+  return 0;
+}
