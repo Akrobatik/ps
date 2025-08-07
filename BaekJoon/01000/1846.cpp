eb645240fce233886d3eb632ea27b3a1565d6aeb
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-08-08 06:08:19 +0900

    1846 - 장기

---
+++ BaekJoon/01000/1846.cpp
@@ -0,0 +1,30 @@
+// Title : 장기
+// Link  : https://www.acmicpc.net/problem/1846 
+// Time  : 4 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  cin >> n;
+  if (n == 3) {
+    cout << "-1";
+    return 0;
+  }
+
+  int h1 = n >> 1, h2 = h1 + 1;
+  cout << h1 << "\n";
+  for (int i = 1; i <= n; i++) {
+    if (i == h1 || i == h2) continue;
+    cout << i << "\n";
+  }
+  cout << h2;
+
+  return 0;
+}
