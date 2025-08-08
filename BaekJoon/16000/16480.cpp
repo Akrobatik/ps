1eeb63fa4b7f0e58a0d5463ab2bc8c07fedc2cb8
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-08-08 23:26:26 +0900

    16480 - 외심과 내심은 사랑입니다

---
+++ BaekJoon/16000/16480.cpp
@@ -0,0 +1,19 @@
+// Title : 외심과 내심은 사랑입니다
+// Link  : https://www.acmicpc.net/problem/16480 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int64_t a, b;
+  cin >> a >> b;
+  cout << (a - b) * (a - b) - b * b;
+
+  return 0;
+}
