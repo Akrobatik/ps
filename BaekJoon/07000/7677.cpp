51de15aac26674317182cc01e841d7294e9b00bb
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-08-13 12:06:17 +0800

    7677 - Fibonacci

---
+++ BaekJoon/07000/7677.cpp
@@ -0,0 +1,72 @@
+// Title : Fibonacci
+// Link  : https://www.acmicpc.net/problem/7677 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct Matrix {
+
+  friend Matrix operator*(const Matrix& m1, const Matrix& m2) {
+
+    Matrix m;
+
+    m.m[0][0] = (m1.m[0][0] * m2.m[0][0] + m1.m[0][1] * m2.m[1][0]) % 10000;
+
+    m.m[0][1] = (m1.m[0][0] * m2.m[0][1] + m1.m[0][1] * m2.m[1][1]) % 10000;
+
+    m.m[1][0] = (m1.m[1][0] * m2.m[0][0] + m1.m[1][1] * m2.m[1][0]) % 10000;
+
+    m.m[1][1] = (m1.m[1][0] * m2.m[0][1] + m1.m[1][1] * m2.m[1][1]) % 10000;
+
+    return m;
+
+  }
+
+  int64_t m[2][2];
+
+};
+
+Matrix Exp(int64_t n, const Matrix& m) {
+
+  if (n == 1) return m;
+
+  auto exp = Exp(n / 2, m);
+
+  auto mp = exp * exp;
+
+  if (n % 2) mp = mp * m;
+
+  return mp;
+
+}
+
+int main() {
+
+  ios::sync_with_stdio(false);
+
+  cin.tie(nullptr);
+
+  int n;
+
+  while (cin >> n && n != -1) {
+
+    if (n == 0) {
+
+      cout << "0\n";
+
+    } else {
+
+      auto m = Exp(n, {{{1, 1}, {1, 0}}});
+
+      cout << m.m[0][1] << "\n";
+
+    }
+
+  }
+
+  return 0;
+
+}
