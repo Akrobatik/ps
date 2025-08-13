f139fa53ed2ca86635793295857f33966ed5e638
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-08-13 12:06:31 +0800

    28426 - 더하기와 나누기

---
+++ BaekJoon/28000/28426.cpp
@@ -0,0 +1,23 @@
+// Title : 더하기와 나누기
+// Link  : https://www.acmicpc.net/problem/28426 
+// Time  : 8 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+    
+  int n;
+  cin >> n;
+    
+  cout << "2 ";
+  for (int i = 1; i < n; i++) {
+    cout << i * 4 << " ";
+  }
+    
+  return 0;
+}
