a3486f623311ce72fdab417daddf32752a9dcf0b
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-08-13 15:25:35 +0800

    34130 - Yet Another Stone Game

---
+++ BaekJoon/34000/34130.cpp
@@ -0,0 +1,47 @@
+// Title : Yet Another Stone Game
+// Link  : https://www.acmicpc.net/problem/34130 
+// Time  : 44 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int t;
+  cin >> t;
+  while (t--) {
+    int n, k;
+    cin >> n >> k;
+
+    int cnt = 0, minn = INT_MAX;
+    for (int i = 0; i < n; i++) {
+      int x;
+      cin >> x;
+      cnt += (x & 1);
+      minn = min<int>(minn, x);
+    }
+
+    if (n == k) {
+      cout << (cnt > 0 ? "First\n" : "Second\n");
+      continue;
+    }
+
+    if (n == k + 1) {
+      cout << (1 <= cnt && cnt <= k ? "First\n" : "Second\n");
+      continue;
+    }
+
+    if (k == 1) {
+      cout << (cnt % 2 == 1 ? "First\n" : "Second\n");
+      continue;
+    }
+
+    cout << (cnt > 0 && (n != cnt + 1 || minn % 2 == 1) ? "First\n" : "Second\n");
+  }
+
+  return 0;
+}
