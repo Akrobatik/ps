690835b6927d9f67fa69400937b3622bc4095b11
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-08-15 00:11:35 +0800

    34124 - $A$의 배수

---
+++ BaekJoon/34000/34124.cpp
@@ -0,0 +1,23 @@
+// Title : $A$의 배수
+// Link  : https://www.acmicpc.net/problem/34124 
+// Time  : 264 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int t;
+  cin >> t;
+  while (t--) {
+    int n, a;
+    cin >> n >> a;
+    cout << ((n & 1) || n == 2 ? "O\n" : "I\n");
+  }
+
+  return 0;
+}
