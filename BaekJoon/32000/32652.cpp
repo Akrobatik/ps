64b20a7b5b0589243415c755926903e9dcf0112a
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-08-17 23:41:36 +0800

    32652 - 아카라카 2

---
+++ BaekJoon/32000/32652.cpp
@@ -0,0 +1,25 @@
+// Title : 아카라카 2
+// Link  : https://www.acmicpc.net/problem/32652 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  cin >> n;
+
+  string s = "AKA";
+  s.reserve(n * 4 + 3);
+  while (n--) {
+    s.append("RAKA");
+  }
+  cout << s;
+
+  return 0;
+}
