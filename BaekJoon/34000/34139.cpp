578ad43447152be2fb775c44ccf2d6b0b12cbfe4
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-08-18 07:46:56 +0800

    34139 - 의식의 광장

---
+++ BaekJoon/34000/34139.cpp
@@ -0,0 +1,34 @@
+// Title : 의식의 광장
+// Link  : https://www.acmicpc.net/problem/34139 
+// Time  : 80 ms
+// Memory: 4376 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n, m;
+  cin >> n >> m;
+  vector<int> arr(m);
+  for (auto& e : arr) cin >> e >> e;
+
+  vector<int> axr(m);
+  iota(axr.begin(), axr.end(), 0);
+  sort(axr.begin(), axr.end(), [&](int lhs, int rhs) {
+    return arr[lhs] < arr[rhs];
+  });
+
+  vector<int> ans(m);
+  for (int i = 0; i < m; i++) {
+    ans[axr[i]] = i + 1;
+  }
+
+  cout << "YES\n";
+  for (auto e : ans) cout << e << " ";
+
+  return 0;
+}
