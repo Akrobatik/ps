63bbac5a04e90963c2a90ae261837da4af633acc
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-08-20 00:28:45 +0900

    study

--- template/segment_tree.cpp
+++ template/segment_tree.cpp
@@ -2,28 +2,65 @@
 
 using namespace std;
 
-struct SegmentTree {
+template <typename V, typename OP, auto IV>
+  requires requires(V a, V b) {
+    { OP{}(a, b) } -> convertible_to<V>;
+    requires(is_invocable_r_v<V, decltype(IV)> || convertible_to<decltype(IV), V>);
+  }
+struct SegTree {
   void Init(int n) {
-    nmax = has_single_bit((uint32_t)n) ? n : (1 << (32 - countl_zero((uint32_t)n)));
-    tree.clear(), tree.resize(nmax << 1);
+    nmax = bit_ceil(static_cast<unsigned>(n));
+    tree.assign(nmax << 1, IValue());
   }
 
-  void Update(int idx, int value) {
-    int node = idx + nmax;
-    int delta = value - tree[node];
-    tree[node] = value;
-    while ((node >>= 1)) tree[node] += delta;
+  void Set(int idx, const V& val) {
+    tree[idx + nmax] = val;
   }
 
-  int Query(int l, int r) {
-    int res = 0;
-    for (l += nmax, r += nmax + 1; l < r; l >>= 1, r >>= 1) {
-      if (l & 1) res += tree[l++];
-      if (r & 1) res += tree[--r];
+  void Build() {
+    for (int i = nmax - 1; i > 0; --i) {
+      tree[i] = OP{}(tree[i << 1], tree[i << 1 | 1]);
     }
-    return res;
+  }
+
+  void Update(int idx, const V& val) {
+    int node = idx + nmax;
+    tree[node] = val;
+    while (node >>= 1) {
+      tree[node] = OP{}(tree[node << 1], tree[node << 1 | 1]);
+    }
+  }
+
+  template <typename T, typename F>
+    requires requires(V a, T b) {
+      { F{}(a, b) } -> convertible_to<V>;
+    }
+  void Update(int idx, const T& delta, F func) {
+    int node = idx + nmax;
+    tree[node] = func(tree[node], delta);
+    while (node >>= 1) {
+      tree[node] = OP{}(tree[node << 1], tree[node << 1 | 1]);
+    }
+  }
+
+  V Query(int l, int r) const {
+    V lv = IValue(), rv = IValue();
+    for (l += nmax, r += nmax + 1; l < r; l >>= 1, r >>= 1) {
+      if (l & 1) lv = OP{}(lv, tree[l++]);
+      if (r & 1) rv = OP{}(tree[--r], rv);
+    }
+    return OP{}(lv, rv);
   }
 
   int nmax;
-  vector<int> tree;
+  vector<V> tree;
+
+ private:
+  static constexpr V IValue() {
+    if constexpr (is_invocable_r_v<V, decltype(IV)>) {
+      return invoke(IV);
+    } else {
+      return IV;
+    }
+  }
 };
