b5677a42be3dac099f110e1779645c45bece2104
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-08-21 00:03:02 +0900

    27440 - 1로 만들기 3

---
+++ BaekJoon/27000/27440.cpp
@@ -0,0 +1,45 @@
+// Title : 1로 만들기 3
+// Link  : https://www.acmicpc.net/problem/27440 
+// Time  : 4 ms
+// Memory: 3192 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int64_t n;
+  cin >> n;
+
+  queue<int64_t> q;
+  unordered_set<int64_t> hs;
+
+  auto Push = [&](int64_t x) {
+    if (hs.contains(x)) return;
+    hs.insert(x);
+    q.push(x);
+  };
+
+  Push(n);
+  for (int i = 0;; i++) {
+    int nq = q.size();
+    while (nq--) {
+      int64_t x = q.front();
+      q.pop();
+
+      if (x == 1) {
+        cout << i;
+        return 0;
+      }
+
+      if (x % 3 == 0) Push(x / 3);
+      if (x % 2 == 0) Push(x / 2);
+      Push(x - 1);
+    }
+  }
+
+  return 0;
+}
