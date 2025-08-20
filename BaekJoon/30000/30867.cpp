8382b4cd1829017405a32c4172a47276fd8e7569
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-08-21 07:40:46 +0900

    30867 - 과제가 너무 많아

---
+++ BaekJoon/30000/30867.cpp
@@ -0,0 +1,36 @@
+// Title : 과제가 너무 많아
+// Link  : https://www.acmicpc.net/problem/30867 
+// Time  : 4 ms
+// Memory: 2648 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n, m;
+  string s;
+  cin >> n >> m >> s;
+
+  int idx = 0;
+  while (idx < n) {
+    while (idx < n && s[idx] != 'w' && s[idx] != 'h') ++idx;
+    int nxt = idx + 1;
+    while (nxt < n && (s[nxt] == 'w' || s[nxt] == 'h')) ++nxt;
+
+    int cnt = 0;
+    for (int i = idx; i < nxt; i++) {
+      if (s[i] != 'h') continue;
+      int d = i - (idx + cnt++);
+      swap(s[i - min<int>(d, m)], s[i]);
+    }
+
+    idx = nxt;
+  }
+  cout << s;
+
+  return 0;
+}
