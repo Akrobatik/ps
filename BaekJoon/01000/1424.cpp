55b489a7fb26b6bf31192af24356f15ea88742c3
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-08-21 12:09:02 +0900

    1424 - 새 앨범

---
+++ BaekJoon/01000/1424.cpp
@@ -0,0 +1,36 @@
+// Title : 새 앨범
+// Link  : https://www.acmicpc.net/problem/1424 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n, l, c;
+  cin >> n >> l >> c;
+
+  auto Check = [&](int x) {
+    return (l + 1) * x - 1 <= c;
+  };
+
+  int lo = 1, hi = n + 1;
+  while (lo + 1 < hi) {
+    int mid = (lo + hi) >> 1;
+    if (Check(mid)) {
+      lo = mid;
+    } else {
+      hi = mid;
+    }
+  }
+
+  int r = lo % 13;
+  if (r == 0 || (r == 1 && n % lo != 0)) --lo;
+  cout << (n + lo - 1) / lo;
+
+  return 0;
+}
