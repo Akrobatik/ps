92482009a34c95f6fa8e63c585f3af232e9ee499
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-08-22 09:54:11 +0900

    1989 - 부분배열 고르기 2

---
+++ BaekJoon/01000/1989.cpp
@@ -0,0 +1,70 @@
+// Title : 부분배열 고르기 2
+// Link  : https://www.acmicpc.net/problem/1989 
+// Time  : 20 ms
+// Memory: 4508 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  cin >> n;
+
+  int ml, mr;
+  int64_t maxx = -1;
+  vector<int64_t> sum(n + 2);
+  vector<int> minn(n + 2);
+  for (int i = 1; i <= n; i++) {
+    int x;
+    cin >> x;
+    minn[i] = sum[i] = x;
+    int64_t cur = (int64_t)x * x;
+    if (maxx < cur) maxx = cur, ml = mr = i;
+  }
+
+  vector<int> memo(n + 2), sz(n + 2, 1);
+  iota(memo.begin(), memo.end(), 0);
+
+  vector<bool> used(n + 2);
+
+  auto Find = [&](int id) {
+    while (id != memo[id]) {
+      int par = memo[id];
+      id = memo[id] = memo[par];
+    }
+    return id;
+  };
+
+  auto Union = [&](int a, int b) {
+    a = Find(a), b = Find(b);
+    if (a == b) return;
+    if (a > b) swap(a, b);
+    memo[b] = a;
+    sz[a] += sz[b];
+    minn[a] = min<int>(minn[a], minn[b]);
+    sum[a] += sum[b];
+    int64_t cur = sum[a] * minn[a];
+    if (maxx < cur) maxx = cur, ml = a, mr = a + sz[a] - 1;
+  };
+
+  vector<int> cands(n);
+  iota(cands.begin(), cands.end(), 1);
+  sort(cands.begin(), cands.end(), [&](int lhs, int rhs) {
+    return sum[lhs] > sum[rhs];
+  });
+
+  for (auto i : cands) {
+    used[i] = true;
+    if (used[i - 1]) Union(i, i - 1);
+    if (used[i + 1]) Union(i, i + 1);
+  }
+
+  cout << maxx << "\n"
+       << ml << " " << mr;
+
+  return 0;
+}
