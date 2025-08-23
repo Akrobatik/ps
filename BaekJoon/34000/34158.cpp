3fc8cc3fab20c07c5f0ea5f61ae2ab795dc8c8c0
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-08-24 05:21:10 +0900

    34158 - Game with Segment Tree

---
+++ BaekJoon/34000/34158.cpp
@@ -0,0 +1,42 @@
+// Title : Game with Segment Tree
+// Link  : https://www.acmicpc.net/problem/34158 
+// Time  : 88 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int k, g;
+  cin >> k >> g;
+
+  int64_t nmax = 1LL << (k - 1);
+
+  auto Grundy = [&](int x) {
+    int h = __builtin_ctz(x) + 1;
+    return h & -h;
+  };
+
+  auto Check = [&](int64_t l, int64_t r) {
+    int x = 0;
+
+    l += nmax - 1, r += nmax;
+    for (int i = 1; l < r; l >>= 1, r >>= 1) {
+      if (l & 1) x ^= Grundy(i), ++l;
+      if (r & 1) x ^= Grundy(i), --r;
+    }
+    return x == 0;
+  };
+
+  while (g--) {
+    int64_t l, r;
+    cin >> l >> r;
+    cout << (Check(l, r) ? "mj1000j\n" : "swlee0202\n");
+  }
+
+  return 0;
+}
