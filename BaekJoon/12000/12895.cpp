89f903386c61795bb3ab316d8d948bbf174fa954
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-08-23 12:33:30 +0900

    12895 - 화려한 마을

--- BaekJoon/12000/12895.cpp
+++ BaekJoon/12000/12895.cpp
@@ -1,58 +1,129 @@
 // Title : 화려한 마을
 // Link  : https://www.acmicpc.net/problem/12895 
-// Time  : 76 ms
-// Memory: 4084 KB
+// Time  : 68 ms
+// Memory: 5108 KB
 
 #include <bits/stdc++.h>
 
 using namespace std;
 
-struct LazySegmentTree {
-  void Init(int n) {
-    nmax = has_single_bit((uint32_t)n) ? n : (1 << (32 - countl_zero((uint32_t)n)));
-    tree.clear(), tree.resize(nmax << 1);
-    lazy.clear(), lazy.resize(nmax << 1);
+template <typename V, typename L, typename OP, typename APPLY, typename COMPO>
+  requires requires(V va, V vb, L la, L lb, int sz) {
+    { OP{}(va, vb) } -> convertible_to<V>;
+    { APPLY{}(va, la, sz) } -> convertible_to<V>;
+    { COMPO{}(la, lb) } -> convertible_to<L>;
+  }
+struct LazySegTree {
+  void Init(int n, const V& ival, const L& ilzy) {
+    nmax = bit_ceil((uint32_t)n);
+    nlog = countr_zero((uint32_t)nmax);
+    iv = ival, il = ilzy;
+    tree.assign(nmax << 1, iv);
+    lazy.assign(nmax << 1, il);
+    sz.resize(nmax << 1);
+    sz[1] = nmax;
+    for (int i = 1; i < nmax; i++) {
+      sz[i << 1] = sz[i << 1 | 1] = sz[i] >> 1;
+    }
   }
 
-  void Update(int l, int r, int x) {
-    Update(1, 0, nmax - 1, l, r, x);
+  void Set(int idx, const V& val) {
+    tree[idx + nmax] = val;
   }
 
-  int Query(int l, int r) {
-    return __builtin_popcount(Query(1, 0, nmax - 1, l, r));
+  void Build() {
+    for (int i = nmax - 1; i > 0; i--) {
+      tree[i] = OP{}(tree[i << 1], tree[i << 1 | 1]);
+    }
   }
 
-  int nmax;
-  vector<int> tree, lazy;
+  void Update(int l, int r, const L& lzy) {
+    l += nmax, r += nmax + 1;
+    for (int i = nlog; i > 0; i--) {
+      if (Check(l, i)) Push(l >> i);
+      if (Check(r, i)) Push((r - 1) >> i);
+    }
+
+    for (int ll = l, rr = r; ll < rr; ll >>= 1, rr >>= 1) {
+      if (ll & 1) Apply(ll++, lzy);
+      if (rr & 1) Apply(--rr, lzy);
+    }
+
+    for (int i = 1; i <= nlog; i++) {
+      if (Check(l, i)) Pull(l >> i);
+      if (Check(r, i)) Pull((r - 1) >> i);
+    }
+  }
+
+  V Query(int idx) {
+    int node = idx + nmax;
+    for (int i = nlog; i > 0; i--) Push(node >> i);
+    return tree[node];
+  }
+
+  V Query(int l, int r) {
+    V lv = iv, rv = iv;
+
+    l += nmax, r += nmax + 1;
+    for (int i = nlog; i > 0; i--) {
+      if (Check(l, i)) Push(l >> i);
+      if (Check(r, i)) Push((r - 1) >> i);
+    }
+
+    for (; l < r; l >>= 1, r >>= 1) {
+      if (l & 1) lv = OP{}(lv, tree[l++]);
+      if (r & 1) rv = OP{}(tree[--r], rv);
+    }
+    return OP{}(lv, rv);
+  }
 
  private:
-  void Push(int node, int b, int e) {
-    if (!lazy[node]) return;
-    tree[node] = 1 << lazy[node];
-    if (b != e) {
-      lazy[node << 1] = lazy[node];
-      lazy[(node << 1) + 1] = lazy[node];
-    }
-    lazy[node] = 0;
+  int nmax, nlog;
+  V iv;
+  L il;
+  vector<V> tree;
+  vector<L> lazy;
+  vector<int> sz;
+
+  bool Check(int x, int shift) {
+    int y = (x >> shift) << shift;
+    return x != y;
   }
 
-  void Update(int node, int b, int e, int l, int r, int x) {
-    bool fit = (l <= b && e <= r);
-    if (fit) lazy[node] = x;
-    Push(node, b, e);
-    if (l > e || b > r || fit) return;
-    int mid = (b + e) >> 1;
-    Update(node << 1, b, mid, l, r, x);
-    Update((node << 1) + 1, mid + 1, e, l, r, x);
-    tree[node] = tree[node << 1] | tree[(node << 1) + 1];
+  void Apply(int node, const L& lzy) {
+    tree[node] = APPLY{}(tree[node], lzy, sz[node]);
+    if (node < nmax) lazy[node] = COMPO{}(lazy[node], lzy);
   }
 
-  int Query(int node, int b, int e, int l, int r) {
-    Push(node, b, e);
-    if (l > e || b > r) return 0;
-    if (l <= b && e <= r) return tree[node];
-    int mid = (b + e) >> 1;
-    return Query(node << 1, b, mid, l, r) | Query((node << 1) + 1, mid + 1, e, l, r);
+  void Push(int node) {
+    Apply(node << 1, lazy[node]);
+    Apply(node << 1 | 1, lazy[node]);
+    lazy[node] = il;
+  }
+
+  void Pull(int node) {
+    tree[node] = OP{}(tree[node << 1], tree[node << 1 | 1]);
+  }
+};
+
+using Node = int;
+using Lazy = int;
+
+struct FOp {
+  Node operator()(const Node& a, const Node& b) const {
+    return a | b;
+  }
+};
+
+struct FApply {
+  Node operator()(const Node& a, const Lazy& b, int sz) const {
+    return b ? b : a;
+  }
+};
+
+struct FCompo {
+  Lazy operator()(const Lazy& a, const Lazy& b) const {
+    return b ? b : a;
   }
 };
 
@@ -63,9 +134,9 @@ int main() {
   int n, t, q;
   cin >> n >> t >> q;
 
-  LazySegmentTree seg;
-  seg.Init(n + 1);
-  seg.Update(1, n, 1);
+  LazySegTree<Node, Lazy, FOp, FApply, FCompo> seg;
+  seg.Init(n + 1, 0, 0);
+  seg.Update(1, n, 1 << 1);
 
   while (q--) {
     char cmd;
@@ -75,9 +146,9 @@ int main() {
     if (cmd == 'C') {
       int z;
       cin >> z;
-      seg.Update(x, y, z);
+      seg.Update(x, y, 1 << z);
     } else {
-      cout << seg.Query(x, y) << "\n";
+      cout << __builtin_popcount(seg.Query(x, y)) << "\n";
     }
   }
 
