c6c70887b0eef618e4fffd00e245f5d15f4d7a20
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-08-24 16:16:00 +0900

    34157 - Split the SSHS 5

--- BaekJoon/34000/34157.cpp
+++ BaekJoon/34000/34157.cpp
@@ -1,7 +1,7 @@
 // Title : Split the SSHS 5
 // Link  : https://www.acmicpc.net/problem/34157 
-// Time  : 92 ms
-// Memory: 14352 KB
+// Time  : 120 ms
+// Memory: 17440 KB
 
 #include <bits/stdc++.h>
 
@@ -111,15 +111,142 @@ struct ModInt32 {
   int32_t val;
 };
 
+template <typename V, typename L, typename OP, typename APPLY, typename COMPO>
+  requires requires(V va, V vb, L la, L lb, int sz) {
+    { OP{}(va, vb) } -> convertible_to<V>;
+    { APPLY{}(va, la, sz) } -> convertible_to<V>;
+    { COMPO{}(la, lb) } -> convertible_to<L>;
+  }
+struct LazySegTree {
+  void Init(int n, const V& ival, const L& ilzy) {
+    nmax = bit_ceil((uint32_t)n);
+    nlog = countr_zero((uint32_t)nmax);
+    iv = ival, il = ilzy;
+    tree.assign(nmax << 1, iv);
+    lazy.assign(nmax << 1, il);
+    sz.resize(nmax << 1);
+    sz[1] = nmax;
+    for (int i = 1; i < nmax; i++) {
+      sz[i << 1] = sz[i << 1 | 1] = sz[i] >> 1;
+    }
+  }
+
+  void Set(int idx, const V& val) {
+    tree[idx + nmax] = val;
+  }
+
+  void Build() {
+    for (int i = nmax - 1; i > 0; i--) {
+      tree[i] = OP{}(tree[i << 1], tree[i << 1 | 1]);
+    }
+  }
+
+  void Update(int l, int r, const L& lzy) {
+    l += nmax, r += nmax + 1;
+    for (int i = nlog; i > 0; i--) {
+      if (Check(l, i)) Push(l >> i);
+      if (Check(r, i)) Push((r - 1) >> i);
+    }
+
+    for (int ll = l, rr = r; ll < rr; ll >>= 1, rr >>= 1) {
+      if (ll & 1) Apply(ll++, lzy);
+      if (rr & 1) Apply(--rr, lzy);
+    }
+
+    for (int i = 1; i <= nlog; i++) {
+      if (Check(l, i)) Pull(l >> i);
+      if (Check(r, i)) Pull((r - 1) >> i);
+    }
+  }
+
+  V Query(int idx) {
+    int node = idx + nmax;
+    for (int i = nlog; i > 0; i--) Push(node >> i);
+    return tree[node];
+  }
+
+  V Query(int l, int r) {
+    V lv = iv, rv = iv;
+
+    l += nmax, r += nmax + 1;
+    for (int i = nlog; i > 0; i--) {
+      if (Check(l, i)) Push(l >> i);
+      if (Check(r, i)) Push((r - 1) >> i);
+    }
+
+    for (; l < r; l >>= 1, r >>= 1) {
+      if (l & 1) lv = OP{}(lv, tree[l++]);
+      if (r & 1) rv = OP{}(tree[--r], rv);
+    }
+    return OP{}(lv, rv);
+  }
+
+  void PushAll() {
+    for (int i = 1; i < nmax; i++) {
+      Push(i);
+    }
+  }
+
+  //  private:
+  int nmax, nlog;
+  V iv;
+  L il;
+  vector<V> tree;
+  vector<L> lazy;
+  vector<int> sz;
+
+  bool Check(int x, int shift) {
+    int y = (x >> shift) << shift;
+    return x != y;
+  }
+
+  void Apply(int node, const L& lzy) {
+    tree[node] = APPLY{}(tree[node], lzy, sz[node]);
+    if (node < nmax) lazy[node] = COMPO{}(lazy[node], lzy);
+  }
+
+  void Push(int node) {
+    Apply(node << 1, lazy[node]);
+    Apply(node << 1 | 1, lazy[node]);
+    lazy[node] = il;
+  }
+
+  void Pull(int node) {
+    tree[node] = OP{}(tree[node << 1], tree[node << 1 | 1]);
+  }
+};
+
 constexpr int kMax = 1e5;
 
 constexpr int kMod = 998244353;
 using ModInt = ModInt32<kMod>;
 
+using Node = ModInt;
+using Lazy = ModInt;
+
+struct FOp {
+  Node operator()(const Node& a, const Node& b) const {
+    return a + b;
+  }
+};
+
+struct FApply {
+  Node operator()(const Node& a, const Lazy& b, int sz) const {
+    return a * b;
+  }
+};
+
+struct FCompo {
+  Lazy operator()(const Lazy& a, const Lazy& b) const {
+    return a * b;
+  }
+};
+
+LazySegTree<Node, Lazy, FOp, FApply, FCompo> seg;
 vector<int> edges[kMax + 1];
 vector<int> bombs[kMax + 1];
 int in[kMax + 1], out[kMax + 1], nidx;
-ModInt prob[kMax + 1];
+ModInt inv[kMax + 1];
 vector<int> stk;
 
 void Build(int cur, int par) {
@@ -132,23 +259,20 @@ void Build(int cur, int par) {
 }
 
 void Traverse(int cur, int par) {
-  auto& bomb = bombs[cur];
-  int idx = 0, bsz = bomb.size();
-  ModInt dv = ModInt(bsz).Inv();
-
   stk.clear();
-  while (idx < bsz) {
-    int u = bomb[idx++];
+  int bsz = bombs[cur].size();
+  for (auto u : bombs[cur]) {
     if (!(in[cur] < in[u] && in[u] <= out[cur])) continue;
-
     while (!stk.empty() && out[stk.back()] < in[u]) stk.pop_back();
     stk.push_back(u);
-    prob[u] *= dv * (bsz - (int)stk.size());
+
+    int sz = stk.size();
+    int x = bsz - sz;
+    seg.Update(in[u], out[u], ModInt(x) * inv[x + 1]);
   }
 
   for (auto nxt : edges[cur]) {
     if (nxt == par) continue;
-    prob[nxt] *= prob[cur];
     Traverse(nxt, cur);
   }
 }
@@ -159,6 +283,7 @@ int main() {
 
   int n;
   cin >> n;
+  seg.Init(n + 1, 1, 1);
 
   for (int i = 1; i <= n; i++) {
     int sz;
@@ -184,14 +309,15 @@ int main() {
     });
   }
 
-  ModInt one = 1;
+  inv[0] = 1;
   for (int i = 1; i <= n; i++) {
-    prob[i] = one;
+    inv[i] = ModInt(i).Inv();
   }
   Traverse(1, 0);
 
+  seg.PushAll();
   for (int i = 2; i <= n; i++) {
-    cout << prob[i] << "\n";
+    cout << seg.tree[in[i] + seg.nmax] << "\n";
   }
 
   return 0;
