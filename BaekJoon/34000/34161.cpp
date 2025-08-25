e627c614d12d19998966110f03e09f1b79a089c6
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-08-25 21:18:31 +0900

    34161 - OO0OO

---
+++ BaekJoon/34000/34161.cpp
@@ -0,0 +1,19 @@
+// Title : OO0OO
+// Link  : https://www.acmicpc.net/problem/34161 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  for (int i = 0; i < 10000; i++) {
+    cout << "-1\n";
+  }
+
+  return 0;
+}
