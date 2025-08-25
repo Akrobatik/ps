b77945e799953930df80914ab44cd8a78052b718
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-08-26 00:59:39 +0900

    14740 - Highway Track

---
+++ BaekJoon/14000/14740.cpp
@@ -0,0 +1,43 @@
+// Title : Highway Track
+// Link  : https://www.acmicpc.net/problem/14740 
+// Time  : 96 ms
+// Memory: 5928 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  cin >> n;
+  int m = n << 1;
+  vector<int64_t> arr(n + 1);
+  for (int i = 1; i <= n; i++) {
+    cin >> arr[i];
+  }
+  for (int i = 1; i <= n; i++) {
+    int x;
+    cin >> x;
+    arr[i] -= x;
+  }
+
+  for (int i = 1; i <= n; i++) {
+    arr[i] += arr[i - 1];
+  }
+
+  int cnt = 0;
+  int64_t minn = INT64_MAX;
+  for (int i = 1; i <= n; i++) {
+    if (minn > arr[i]) {
+      minn = arr[i], cnt = 1;
+    } else if (minn == arr[i]) {
+      ++cnt;
+    }
+  }
+  cout << cnt;
+
+  return 0;
+}
