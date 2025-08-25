142a8158458d9d744360cfaeca7b681e246a3b68
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-08-26 00:05:37 +0900

    14925 - 목장 건설하기

--- BaekJoon/14000/14925.cpp
+++ BaekJoon/14000/14925.cpp
@@ -1,6 +1,6 @@
 // Title : 목장 건설하기
 // Link  : https://www.acmicpc.net/problem/14925 
-// Time  : 60 ms
+// Time  : 16 ms
 // Memory: 5996 KB
 
 #include <bits/stdc++.h>
@@ -14,35 +14,18 @@ int main() {
   int n, m;
   cin >> n >> m;
 
-  vector<vector<int>> board(n + 1, vector<int>(m + 1));
+  int maxx = 0;
+  vector<vector<int>> memo(n + 1, vector<int>(m + 1));
   for (int i = 1; i <= n; i++) {
     for (int j = 1; j <= m; j++) {
-      int x;
-      cin >> x;
-      board[i][j] = board[i - 1][j] + board[i][j - 1] - board[i - 1][j - 1] + x;
+      char c;
+      cin >> c;
+      if (c != '0') continue;
+      memo[i][j] = min<int>({memo[i][j - 1], memo[i - 1][j], memo[i - 1][j - 1]}) + 1;
+      maxx = max<int>(maxx, memo[i][j]);
     }
   }
-
-  auto Check = [&](int x) {
-    for (int i = x; i <= n; i++) {
-      for (int j = x; j <= m; j++) {
-        int cur = board[i][j] - board[i][j - x] - board[i - x][j] + board[i - x][j - x];
-        if (cur == 0) return true;
-      }
-    }
-    return false;
-  };
-
-  int lo = 0, hi = min<int>(n, m) + 1;
-  while (lo + 1 < hi) {
-    int mid = (lo + hi) >> 1;
-    if (Check(mid)) {
-      lo = mid;
-    } else {
-      hi = mid;
-    }
-  }
-  cout << lo;
+  cout << maxx;
 
   return 0;
 }
