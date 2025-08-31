4753fb970b03db0749cc83062c2d2fb5ced9c68b
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-08-31 23:38:13 +0900

    8370 - Plane

---
+++ BaekJoon/08000/8370.cpp
@@ -0,0 +1,19 @@
+// Title : Plane
+// Link  : https://www.acmicpc.net/problem/8370 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int a, b, c, d;
+  cin >> a >> b >> c >> d;
+  cout << a * b + c * d;
+
+  return 0;
+}
