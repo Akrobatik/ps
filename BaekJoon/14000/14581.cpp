5765c0cf97c6692399bb8f49014fc86083db97e2
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-09-01 22:19:32 +0900

    14581 - 팬들에게 둘러싸인 홍준

---
+++ BaekJoon/14000/14581.cpp
@@ -0,0 +1,19 @@
+// Title : 팬들에게 둘러싸인 홍준
+// Link  : https://www.acmicpc.net/problem/14581 
+// Time  : 0 ms
+// Memory: 2380 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  string s;
+  cin >> s;
+  cout << format(":fan::fan::fan:\n:fan::{}::fan:\n:fan::fan::fan:", s);
+
+  return 0;
+}
