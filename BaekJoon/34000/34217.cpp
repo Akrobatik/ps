1ca577d5c0b102935597015af75bfd242d1f33ba
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-09-07 23:15:27 +0900

    34217 - 찾아오시는 길

---
+++ BaekJoon/34000/34217.cpp
@@ -0,0 +1,21 @@
+// Title : 찾아오시는 길
+// Link  : https://www.acmicpc.net/problem/34217 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int a, b, c, d;
+  cin >> a >> b >> c >> d;
+
+  int ac = a + c, bd = b + d;
+  cout << (ac < bd ? "Hanyang Univ." : (ac > bd ? "Yongdap" : "Either"));
+
+  return 0;
+}
