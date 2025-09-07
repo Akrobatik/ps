6d1398e6512d56e92126513072d4ff6d2b8b3ad4
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-09-07 23:11:26 +0900

    34236 - 숭고한에 어서오세요

---
+++ BaekJoon/34000/34236.cpp
@@ -0,0 +1,19 @@
+// Title : 숭고한에 어서오세요
+// Link  : https://www.acmicpc.net/problem/34236 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n, a, b;
+  cin >> n >> a >> b;
+  cout << a + (b - a) * n;
+
+  return 0;
+}
