b0d690bd03f134c5ac188f90e500ff0f0541b8cd
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-09-09 16:13:43 +0900

    34311 - QWERTY

---
+++ BaekJoon/34000/34311.cpp
@@ -0,0 +1,28 @@
+// Title : QWERTY
+// Link  : https://www.acmicpc.net/problem/34311 
+// Time  : 0 ms
+// Memory: 2024 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+constexpr const char* kTable = "qwertyuiopasdfghjklzxcvbnm";
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  string s;
+  getline(cin, s);
+  getline(cin, s);
+  for (auto c : s) {
+    if ('a' <= c && c <= 'z') {
+      cout << kTable[c - 'a'];
+    } else {
+      cout << c;
+    }
+  }
+
+  return 0;
+}
