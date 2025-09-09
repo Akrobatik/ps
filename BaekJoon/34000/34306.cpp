a501ce8849ca5f2306e4090d56aef6451cdc667d
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-09-10 03:14:59 +0900

    34306 - M-Climb Road

---
+++ BaekJoon/34000/34306.cpp
@@ -0,0 +1,21 @@
+// Title : M-Climb Road
+// Link  : https://www.acmicpc.net/problem/34306 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int w, n;
+  cin >> w >> n;
+  w *= 5280;
+
+  cout << w / n;
+
+  return 0;
+}
