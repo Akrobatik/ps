9dbf003a280e8f4571906b7382754bc0babbd3a5
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-09-09 22:37:03 +0900

    8363 - Balloons

--- BaekJoon/08000/8363.cpp
+++ BaekJoon/08000/8363.cpp
@@ -1,7 +1,7 @@
 // Title : Balloons
 // Link  : https://www.acmicpc.net/problem/8363 
 // Time  : 64 ms
-// Memory: 6720 KB
+// Memory: 5152 KB
 
 #include <bits/stdc++.h>
 
@@ -14,27 +14,20 @@ int main() {
   int n, m;
   cin >> n >> m;
 
-  vector<int> arr(n);
-  for (auto& e : arr) cin >> e;
-  sort(arr.begin(), arr.end());
+  vector<int64_t> arr(n + 1), brr(m + 1);
+  for (int i = 1; i <= n; i++) cin >> arr[i];
+  for (int i = 1; i <= m; i++) cin >> brr[i];
+  sort(arr.begin() + 1, arr.end());
+  sort(brr.begin() + 1, brr.end(), greater<int>());
 
-  vector<int> brr(m);
-  for (auto& e : brr) cin >> e;
-  sort(brr.begin(), brr.end(), greater<int>());
-
-  vector<int64_t> axr(n + 1), bxr(m + 1);
-  for (int i = 0; i < n; i++) axr[i + 1] = axr[i] + arr[i];
-  for (int i = 0; i < m; i++) bxr[i + 1] = bxr[i] + brr[i];
+  for (int i = 0; i < n; i++) arr[i + 1] += arr[i];
+  for (int i = 0; i < m; i++) brr[i + 1] += brr[i];
 
   bool ok = true;
   int idx = 0;
-  for (int i = 1; i <= m; i++) {
-    while (idx < n && arr[idx] < i) ++idx;
-    int64_t y = axr[idx] + (int64_t)(n - idx) * i;
-    if (y < bxr[i]) {
-      ok = false;
-      break;
-    }
+  for (int i = 1; ok && i <= m; i++) {
+    while (idx < n && arr[idx + 1] - arr[idx] < i) ++idx;
+    ok = (arr[idx] + (int64_t)(n - idx) * i >= brr[i]);
   }
   cout << (ok ? "TAK" : "NIE");
 
