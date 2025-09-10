60357b066f53b11b42d8927c7170b776399a1566
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-09-10 15:50:23 +0900

    14550 - 마리오 파티

---
+++ BaekJoon/14000/14550.cpp
@@ -0,0 +1,42 @@
+// Title : 마리오 파티
+// Link  : https://www.acmicpc.net/problem/14550 
+// Time  : 0 ms
+// Memory: 2152 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+constexpr int kInf = INT_MAX >> 1;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n, s, t;
+  while (cin >> n >> s >> t) {
+    vector<int> arr(n + 2);
+    for (int i = 1; i <= n; i++) {
+      cin >> arr[i];
+    }
+
+    vector<vector<int>> memo(t + 1, vector<int>(n + 2, -kInf));
+    memo[0][0] = 0;
+
+    for (int i = 1; i <= n + 1; i++) {
+      for (int j = 1; i - j >= 0 && j <= s; j++) {
+        for (int k = 0; k < t; k++) {
+          memo[k + 1][i] = max<int>(memo[k + 1][i], memo[k][i - j] + arr[i]);
+        }
+      }
+    }
+
+    int maxx = -kInf;
+    for (int i = 0; i <= t; i++) {
+      maxx = max<int>(maxx, memo[i][n + 1]);
+    }
+    cout << maxx << "\n";
+  }
+
+  return 0;
+}
