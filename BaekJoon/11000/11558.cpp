c672b8a36b60c2c85cc0ef0c43066cc1e971fef9
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-09-10 19:38:03 +0900

    11558 - The Game of Death

---
+++ BaekJoon/11000/11558.cpp
@@ -0,0 +1,35 @@
+// Title : The Game of Death
+// Link  : https://www.acmicpc.net/problem/11558 
+// Time  : 4 ms
+// Memory: 2152 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int t;
+  cin >> t;
+  while (t--) {
+    int n;
+    cin >> n;
+    vector<int> arr(n + 1);
+    vector<bool> used(n + 1);
+    for (int i = 1; i <= n; i++) cin >> arr[i];
+
+    int ans = 0, idx = 0;
+    int cur = 1;
+    while (!used[cur] && cur != n) {
+      ++idx;
+      used[cur] = true;
+      cur = arr[cur];
+      if (cur == n) ans = idx;
+    }
+    cout << ans << "\n";
+  }
+
+  return 0;
+}
