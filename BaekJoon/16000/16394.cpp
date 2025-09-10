5aab70aa369fd109dedea1fe0c737e62b9166a58
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-09-10 19:09:17 +0900

    16394 - 홍익대학교

---
+++ BaekJoon/16000/16394.cpp
@@ -0,0 +1,19 @@
+// Title : 홍익대학교
+// Link  : https://www.acmicpc.net/problem/16394 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  cin >> n;
+  cout << n - 1946;
+
+  return 0;
+}
