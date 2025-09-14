c22015016277dd8928355668d979b6bf8739053f
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-09-14 13:56:36 +0900

    34346 - 대각선

---
+++ BaekJoon/34000/34346.cpp
@@ -0,0 +1,19 @@
+// Title : 대각선
+// Link  : https://www.acmicpc.net/problem/34346 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  cin >> n;
+  cout << 2 - (n & 1);
+
+  return 0;
+}
