c7a15c4b3a7e6020b79e67b0cab4b290e7eb0b03
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-09-15 18:44:04 +0900

    34328 - 대칭 XOR

---
+++ BaekJoon/34000/34328.cpp
@@ -0,0 +1,39 @@
+// Title : 대칭 XOR
+// Link  : https://www.acmicpc.net/problem/34328 
+// Time  : 24 ms
+// Memory: 3232 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  cin >> n;
+
+  if (n & 1) {
+    cout << (n == 1 ? "1" : "-1");
+    return 0;
+  }
+
+  int key = n | 1;
+  deque<int> dq;
+  vector<bool> used(n + 1);
+  for (int i = 1; i <= n; i++) {
+    if (used[i]) continue;
+    int l = i, r = key ^ i;
+    if (r > n) {
+      cout << "-1";
+      return 0;
+    }
+    used[l] = used[r] = true;
+    dq.push_front(l), dq.push_back(r);
+  }
+
+  for (auto e : dq) cout << e << " ";
+
+  return 0;
+}
