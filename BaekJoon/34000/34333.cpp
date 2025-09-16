28cf9da66128bcff077a1dbc6c59285fa386726c
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-09-16 21:47:51 +0900

    34333 - @Override

--- BaekJoon/34000/34333.cpp
+++ BaekJoon/34000/34333.cpp
@@ -3,3 +3,189 @@
 // Time  : 804 ms
 // Memory: 80688 KB
 
+#include <bits/stdc++.h>
+
+using namespace std;
+
+template <typename V, typename L, typename OP, typename APPLY, typename COMPO>
+  requires requires(V va, V vb, L la, L lb, int sz) {
+    { OP{}(va, vb) } -> convertible_to<V>;
+    { APPLY{}(va, la, sz) } -> convertible_to<V>;
+    { COMPO{}(la, lb) } -> convertible_to<L>;
+  }
+struct LazySegTree {
+  void Init(int n, const V& ival, const L& ilzy) {
+    nmax = bit_ceil((uint32_t)n);
+    nlog = countr_zero((uint32_t)nmax);
+    iv = ival, il = ilzy;
+    tree.assign(nmax << 1, iv);
+    lazy.assign(nmax << 1, il);
+    sz.resize(nmax << 1);
+    sz[1] = nmax;
+    for (int i = 1; i < nmax; i++) {
+      sz[i << 1] = sz[i << 1 | 1] = sz[i] >> 1;
+    }
+  }
+
+  void Set(int idx, const V& val) {
+    tree[idx + nmax] = val;
+  }
+
+  void Build() {
+    for (int i = nmax - 1; i > 0; i--) {
+      tree[i] = OP{}(tree[i << 1], tree[i << 1 | 1]);
+    }
+  }
+
+  void Update(int l, int r, const L& lzy) {
+    l += nmax, r += nmax + 1;
+    for (int i = nlog; i > 0; i--) {
+      if (Check(l, i)) Push(l >> i);
+      if (Check(r, i)) Push((r - 1) >> i);
+    }
+
+    for (int ll = l, rr = r; ll < rr; ll >>= 1, rr >>= 1) {
+      if (ll & 1) Apply(ll++, lzy);
+      if (rr & 1) Apply(--rr, lzy);
+    }
+
+    for (int i = 1; i <= nlog; i++) {
+      if (Check(l, i)) Pull(l >> i);
+      if (Check(r, i)) Pull((r - 1) >> i);
+    }
+  }
+
+  V Query(int idx) {
+    int node = idx + nmax;
+    for (int i = nlog; i > 0; i--) Push(node >> i);
+    return tree[node];
+  }
+
+  V Query(int l, int r) {
+    V lv = iv, rv = iv;
+
+    l += nmax, r += nmax + 1;
+    for (int i = nlog; i > 0; i--) {
+      if (Check(l, i)) Push(l >> i);
+      if (Check(r, i)) Push((r - 1) >> i);
+    }
+
+    for (; l < r; l >>= 1, r >>= 1) {
+      if (l & 1) lv = OP{}(lv, tree[l++]);
+      if (r & 1) rv = OP{}(tree[--r], rv);
+    }
+    return OP{}(lv, rv);
+  }
+
+ private:
+  int nmax, nlog;
+  V iv;
+  L il;
+  vector<V> tree;
+  vector<L> lazy;
+  vector<int> sz;
+
+  bool Check(int x, int shift) {
+    int y = (x >> shift) << shift;
+    return x != y;
+  }
+
+  void Apply(int node, const L& lzy) {
+    tree[node] = APPLY{}(tree[node], lzy, sz[node]);
+    if (node < nmax) lazy[node] = COMPO{}(lazy[node], lzy);
+  }
+
+  void Push(int node) {
+    Apply(node << 1, lazy[node]);
+    Apply(node << 1 | 1, lazy[node]);
+    lazy[node] = il;
+  }
+
+  void Pull(int node) {
+    tree[node] = OP{}(tree[node << 1], tree[node << 1 | 1]);
+  }
+};
+
+struct Node {
+  Node() : maxx(INT_MIN), sum(0) {}
+  Node(int x, int64_t v) : maxx(x), sum(v) {}
+
+  int maxx;
+  int64_t sum;
+};
+
+using Lazy = int;
+
+struct FOp {
+  Node operator()(const Node& a, const Node& b) const {
+    Node res;
+    res.maxx = max<int>(a.maxx, b.maxx);
+    res.sum = a.sum + b.sum;
+    return res;
+  }
+};
+
+struct FApply {
+  Node operator()(const Node& a, const Lazy& b, int sz) const {
+    if (b == INT_MIN) return a;
+    return Node(b, (int64_t)b * sz);
+  }
+};
+
+struct FCompo {
+  Lazy operator()(const Lazy& a, const Lazy& b) const {
+    if (b == INT_MIN) return a;
+    return b;
+  }
+};
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n, q;
+  cin >> n >> q;
+
+  vector<int> arr(n + 1);
+  for (int i = 1; i <= n; i++) {
+    cin >> arr[i];
+  }
+
+  vector<vector<int>> g(n + 1);
+  for (int i = 1; i < n; i++) {
+    int u, v;
+    cin >> u >> v;
+    g[u].push_back(v);
+    g[v].push_back(u);
+  }
+
+  LazySegTree<Node, Lazy, FOp, FApply, FCompo> seg;
+  seg.Init(n + 1, Node(), INT_MIN);
+
+  vector<int> in(n + 1), out(n + 1);
+  int idx = 0;
+  [&](this auto&& self, int cur, int par, int maxx) -> void {
+    in[cur] = ++idx;
+    seg.Set(idx, Node(maxx, arr[cur]));
+    maxx = max<int>(maxx, arr[cur]);
+    for (auto nxt : g[cur]) {
+      if (nxt == par) continue;
+      self(nxt, cur, maxx);
+    }
+    out[cur] = idx;
+  }(1, 0, INT_MIN);
+  seg.Build();
+
+  while (q--) {
+    int cmd, i;
+    cin >> cmd >> i;
+    if (cmd == 1) {
+      int val = seg.Query(in[i]).maxx;
+      seg.Update(in[i], out[i], val);
+    } else {
+      cout << seg.Query(in[i], out[i]).sum << "\n";
+    }
+  }
+
+  return 0;
+}
