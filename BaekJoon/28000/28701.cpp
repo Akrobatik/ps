b9dae9683f6f9ede4968cd26f0061082975c9f30
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-09-17 07:57:00 +0900

    28701 - 세제곱의 합

---
+++ BaekJoon/28000/28701.cpp
@@ -0,0 +1,22 @@
+// Title : 세제곱의 합
+// Link  : https://www.acmicpc.net/problem/28701 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int64_t n;
+  cin >> n;
+  int64_t x = n * (n + 1) / 2;
+  cout << x << "\n"
+       << x * x << "\n"
+       << x * x << "\n";
+
+  return 0;
+}
