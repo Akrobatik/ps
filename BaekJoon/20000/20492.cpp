f1d820fcb53e8f5ab0da974231f8aefd37512195
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-09-17 07:49:20 +0900

    20492 - 세금

---
+++ BaekJoon/20000/20492.cpp
@@ -0,0 +1,20 @@
+// Title : 세금
+// Link  : https://www.acmicpc.net/problem/20492 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  cin >> n;
+  n /= 1000;
+  cout << n * 780 << " " << n * 956;
+
+  return 0;
+}
