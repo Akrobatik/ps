3731234de072b24430c425dabc4a22762f0c9362
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-09-17 07:52:19 +0900

    11728 - 배열 합치기

---
+++ BaekJoon/11000/11728.cpp
@@ -0,0 +1,22 @@
+// Title : 배열 합치기
+// Link  : https://www.acmicpc.net/problem/11728 
+// Time  : 556 ms
+// Memory: 9836 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n, m;
+  cin >> n >> m;
+  vector<int> arr(n + m);
+  for (auto& e : arr) cin >> e;
+  sort(arr.begin(), arr.end());
+  for (auto e : arr) cout << e << " ";
+
+  return 0;
+}
