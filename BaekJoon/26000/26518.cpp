a508fd9522c78d6200ae6d810cb138c88e3fe673
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-09-18 12:28:55 +0900

    26518 - 수열의 극한값

---
+++ BaekJoon/26000/26518.cpp
@@ -0,0 +1,21 @@
+// Title : 수열의 극한값
+// Link  : https://www.acmicpc.net/problem/26518 
+// Time  : 0 ms
+// Memory: 2032 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int64_t b, c;
+  cin >> b >> c;
+
+  cout << setprecision(6) << fixed
+       << ((long double)b + sqrt((long double)(b * b + c * 4))) / 2.0L;
+
+  return 0;
+}
