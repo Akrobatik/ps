f02ac0f19a244c7b71c73727653fd52201330e13
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-09-18 13:14:28 +0900

    15553 - 난로

---
+++ BaekJoon/15000/15553.cpp
@@ -0,0 +1,33 @@
+// Title : 난로
+// Link  : https://www.acmicpc.net/problem/15553 
+// Time  : 20 ms
+// Memory: 2412 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n, k, old;
+  cin >> n >> k >> old;
+
+  vector<int> arr(n - 1);
+  for (int i = 0; i < n - 1; i++) {
+    int cur;
+    cin >> cur;
+    arr[i] = cur - old - 1;
+    old = cur;
+  }
+  sort(arr.begin(), arr.end());
+
+  int sum = n, rem = n - k;
+  for (int i = 0; i < rem; i++) {
+    sum += arr[i];
+  }
+  cout << sum;
+
+  return 0;
+}
