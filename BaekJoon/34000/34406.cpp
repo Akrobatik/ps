9710e7b6c3839940feacaaedc9ce79a78e15dbae
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-09-22 07:21:43 +0900

    34406 - x와 배수와 XOR (Easy)

---
+++ BaekJoon/34000/34406.cpp
@@ -0,0 +1,29 @@
+// Title : x와 배수와 XOR (Easy)
+// Link  : https://www.acmicpc.net/problem/34406 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+constexpr int kTable[2] = {1 << 30 | 1, 1 << 30};
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int t;
+  cin >> t;
+  while (t--) {
+    int64_t x;
+    cin >> x;
+
+    int n = x ? 2 : 1;
+    cout << n << "\n";
+    while (n--) cout << kTable[n] << " ";
+    cout << "\n";
+  }
+
+  return 0;
+}
