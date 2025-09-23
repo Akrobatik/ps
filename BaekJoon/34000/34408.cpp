c1fee623cfa909cae1b9af5c0f422674976ca885
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-09-23 16:54:38 +0900

    34408 - 문자열 분해기

---
+++ BaekJoon/34000/34408.cpp
@@ -0,0 +1,28 @@
+// Title : 문자열 분해기
+// Link  : https://www.acmicpc.net/problem/34408 
+// Time  : 0 ms
+// Memory: 2168 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  string s, t;
+  cin >> s >> t;
+
+  int cnt[256] = {};
+  for (auto c : s) ++cnt[c];
+
+  bool ok = true;
+  for (auto c : t) {
+    ok &= (--cnt[c] >= 0);
+  }
+
+  cout << (ok ? "OK" : "NEED FIX");
+
+  return 0;
+}
