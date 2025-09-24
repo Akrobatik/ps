98d27b673f3a2ecb3ba08ccb0eb0cc21d185d8ef
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-09-25 02:31:12 +0900

    31052 - Relocation

---
+++ BaekJoon/31000/31052.cpp
@@ -0,0 +1,31 @@
+// Title : Relocation
+// Link  : https://www.acmicpc.net/problem/31052 
+// Time  : 28 ms
+// Memory: 2360 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n, q;
+  cin >> n >> q;
+
+  vector<int> arr(n + 1);
+  for (int i = 1; i <= n; i++) cin >> arr[i];
+
+  while (q--) {
+    int cmd, a, b;
+    cin >> cmd >> a >> b;
+    if (cmd == 1) {
+      arr[a] = b;
+    } else {
+      cout << abs(arr[a] - arr[b]) << "\n";
+    }
+  }
+
+  return 0;
+}
