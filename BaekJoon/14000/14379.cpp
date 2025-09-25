f2db57b64d98af59ad7024e7c4f9c80acdaaaa3e
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-09-25 23:16:35 +0900

    14379 - BFFs (Small)

---
+++ BaekJoon/14000/14379.cpp
@@ -0,0 +1,68 @@
+// Title : BFFs (Small)
+// Link  : https://www.acmicpc.net/problem/14379 
+// Time  : 0 ms
+// Memory: 2024 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int t;
+  cin >> t;
+  for (int tc = 1; tc <= t; tc++) {
+    int n;
+    cin >> n;
+
+    vector<int> arr(n + 1), deg(n + 1);
+    for (int i = 1; i <= n; i++) {
+      cin >> arr[i];
+      ++deg[arr[i]];
+    }
+
+    queue<int> q;
+    for (int i = 1; i <= n; i++) {
+      if (deg[i]) continue;
+      q.push(i);
+    }
+
+    vector<int> memo(n + 1, 1);
+    while (!q.empty()) {
+      int cur = q.front();
+      q.pop();
+
+      int nxt = arr[cur];
+      memo[nxt] = max<int>(memo[nxt], memo[cur] + 1);
+      if (--deg[nxt] == 0) q.push(nxt);
+    }
+
+    int maxx = 0;
+    vector<bool> visited(n + 1);
+    for (int i = 1; i <= n; i++) {
+      if (visited[i] || deg[i] == 0) continue;
+
+      int cur = i, len = 0;
+      do {
+        ++len;
+        visited[cur] = true;
+        cur = arr[cur];
+      } while (cur != i);
+
+      maxx = max<int>(maxx, len);
+    }
+
+    int two = 0;
+    for (int i = 1; i <= n; i++) {
+      if (arr[i] > i || arr[arr[i]] != i) continue;
+      two += memo[i] + memo[arr[i]];
+    }
+    maxx = max<int>(maxx, two);
+
+    cout << "Case #" << tc << ": " << maxx << "\n";
+  }
+
+  return 0;
+}
