16fd02e33ffe8a592c3c5b823e964723f0bf7d98
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-09-26 08:39:17 +0900

    34407 - x와 배수와 XOR (Hard)

--- BaekJoon/34000/34407.cpp
+++ BaekJoon/34000/34407.cpp
@@ -1,7 +1,7 @@
 // Title : x와 배수와 XOR (Hard)
 // Link  : https://www.acmicpc.net/problem/34407 
-// Time  : 284 ms
-// Memory: 3488 KB
+// Time  : 124 ms
+// Memory: 2468 KB
 
 #include <bits/stdc++.h>
 
@@ -27,26 +27,38 @@ int main() {
     int bw = bit_width((uint32_t)x);
     int minn = 1 << bw, mask = minn - 1;
 
-    unordered_map<int, int> memo;
-    for (int b = 1; b < minn; b <<= 1) {
-      unordered_map<int, int> mnxt;
+    vector<int> memo;
+    for (int b = 2; b < minn; b <<= 1) {
+      vector<int> mnxt;
       int lm = b - 1, um = mask ^ lm;
 
       auto Push = [&](int v) {
         int y = (x * v) & mask;
-        int l = y & lm, u = y & um;
+        int l = y & lm;
         if (l & x) return;
         if (!(x & y)) minn = min<int>(minn, v);
-
-        if (mnxt.contains(u) && mnxt[u] <= v) return;
-        mnxt[u] = v;
+        mnxt.push_back(v);
       };
 
       Push(b);
-      for (auto [k, v] : memo) {
-        Push(v);
-        Push(v + b);
+      for (auto e : memo) {
+        Push(e);
+        Push(e + b);
       }
+
+      sort(mnxt.begin(), mnxt.end(), [&](int lhs, int rhs) {
+        int lu = (x * lhs) & um;
+        int ru = (x * rhs) & um;
+        if (lu == ru) return lhs < rhs;
+        return lu < ru;
+      });
+
+      auto it = unique(mnxt.begin(), mnxt.end(), [&](int lhs, int rhs) {
+        int lu = (x * lhs) & um;
+        int ru = (x * rhs) & um;
+        return lu == ru;
+      });
+      mnxt.erase(it, mnxt.end());
 
       swap(memo, mnxt);
     }
