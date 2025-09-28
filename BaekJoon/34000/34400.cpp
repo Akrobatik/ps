6bbc2ad4fe91542e4bf99514e17325d343d69eff
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-09-29 05:45:20 +0900

    34400 - 민규의 서카디안 리듬

---
+++ BaekJoon/34000/34400.cpp
@@ -0,0 +1,24 @@
+// Title : 민규의 서카디안 리듬
+// Link  : https://www.acmicpc.net/problem/34400 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int t;
+  cin >> t;
+  while (t--) {
+    int x;
+    cin >> x;
+    x %= 25;
+    cout << (0 <= x && x < 17 ? "ONLINE\n" : "OFFLINE\n");
+  }
+
+  return 0;
+}
