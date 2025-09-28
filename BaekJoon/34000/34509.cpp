79324dc2ddfada43615e3e07763e641a4619689d
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-09-28 21:33:06 +0900

    34509 - 2, 4, 6, 8

---
+++ BaekJoon/34000/34509.cpp
@@ -0,0 +1,17 @@
+// Title : 2, 4, 6, 8
+// Link  : https://www.acmicpc.net/problem/34509 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  cout << "42";
+
+  return 0;
+}
