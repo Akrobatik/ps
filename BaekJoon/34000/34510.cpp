9a03b2e29d8d0e945f0f20923513c1a5de4981f8
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-09-28 21:32:39 +0900

    34510 - 초콜릿 우유가 좋아

---
+++ BaekJoon/34000/34510.cpp
@@ -0,0 +1,22 @@
+// Title : 초콜릿 우유가 좋아
+// Link  : https://www.acmicpc.net/problem/34510 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int64_t h1, h2, h3, n;
+  cin >> h1 >> h2 >> h3 >> n;
+
+  int64_t h = h3 * n + h2 * ((n + 1) >> 1);
+  if (n & 1) h += h1;
+  cout << h;
+
+  return 0;
+}
