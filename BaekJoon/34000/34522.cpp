bebed0bc5ca405c16481ef2746429a4fdf36d88f
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-09-28 21:27:53 +0900

    34522 - NYPC <CODE BATTLE/>: Leaf Hack

---
+++ BaekJoon/34000/34522.cpp
@@ -0,0 +1,17 @@
+// Title : NYPC <CODE BATTLE/>: Leaf Hack
+// Link  : https://www.acmicpc.net/problem/34522 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  cout << "Yeti";
+
+  return 0;
+}
