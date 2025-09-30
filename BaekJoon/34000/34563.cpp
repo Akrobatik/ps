5d32fbf798cb274599da9837cd08c58f98ce9861
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-09-30 09:00:38 +0900

    34563 - 궁핍한 모그

---
+++ BaekJoon/34000/34563.cpp
@@ -0,0 +1,23 @@
+// Title : 궁핍한 모그
+// Link  : https://www.acmicpc.net/problem/34563 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int t;
+  cin >> t;
+  while (t--) {
+    int n, m;
+    cin >> n >> m;
+    cout << n + m - 1 << "\n";
+  }
+
+  return 0;
+}
