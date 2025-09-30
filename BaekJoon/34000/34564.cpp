fca53c06d46577b13bd506bb5c268cf20830454c
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-09-30 15:15:45 +0900

    34564 - 참을 수 없는 머슥

---
+++ BaekJoon/34000/34564.cpp
@@ -0,0 +1,19 @@
+// Title : 참을 수 없는 머슥
+// Link  : https://www.acmicpc.net/problem/34564 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int64_t n, k;
+  cin >> n >> k;
+  cout << "0 " << k << " " << n - k << " 0";
+
+  return 0;
+}
