7fcdd182acd8950da52945ca1881a067199cec60
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-10-01 12:16:10 +0900

    28987 - Загадка древних Ассасинов

---
+++ BaekJoon/28000/28987.cpp
@@ -0,0 +1,47 @@
+// Title : Загадка древних Ассасинов
+// Link  : https://www.acmicpc.net/problem/28987 
+// Time  : 4 ms
+// Memory: 2300 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  string s;
+  cin >> s;
+
+  int cnt[10] = {};
+  for (auto c : s) {
+    ++cnt[c - '0'];
+  }
+
+  int sum = 0;
+  for (int i = 0; i < 10; i++) {
+    sum += cnt[i] * i;
+  }
+
+  while (sum % 3) {
+    int rem = sum % 3;
+    int idx = -1;
+    for (int x : {rem, rem ^ 3}) {
+      for (int i = x; i < 10; i += 3) {
+        if (cnt[i] == 0) continue;
+        idx = i;
+        break;
+      }
+      if (idx != -1) break;
+    }
+
+    --cnt[idx], sum -= idx;
+  }
+
+  for (int i = 9; i >= 0; i--) {
+    while (cnt[i]--) cout << (char)(i + '0');
+  }
+
+  return 0;
+}
