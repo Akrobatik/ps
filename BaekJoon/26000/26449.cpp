4fd5723e7e1827a3e3e17899c7b66d3833bcaeff
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-10-01 12:08:59 +0900

    26449 - Harvest

--- BaekJoon/26000/26449.cpp
+++ BaekJoon/26000/26449.cpp
@@ -1,40 +1,11 @@
 // Title : Harvest
 // Link  : https://www.acmicpc.net/problem/26449 
-// Time  : 244 ms
-// Memory: 43796 KB
+// Time  : 36 ms
+// Memory: 2940 KB
 
 #include <bits/stdc++.h>
 
 using namespace std;
-
-constexpr int kMax = 1 << 19;
-vector<int> tree[kMax << 1];
-
-void Update(int l, int r, int x) {
-  for (l += kMax, r += kMax + 1; l < r; l >>= 1, r >>= 1) {
-    if (l & 1) tree[l++].push_back(x);
-    if (r & 1) tree[--r].push_back(x);
-  }
-}
-
-vector<int> Query(int idx) {
-  vector<int> res;
-  int sz = 0;
-  for (int i = idx + kMax; i > 0; i >>= 1) {
-    sz += tree[i].size();
-  }
-
-  res.reserve(sz);
-  for (int i = idx + kMax; i > 0; i >>= 1) {
-    if (tree[i].empty()) continue;
-
-    int old = res.size();
-    res.resize(old + tree[i].size());
-    copy(tree[i].begin(), tree[i].end(), res.begin() + old);
-    tree[i].clear();
-  }
-  return res;
-}
 
 int main() {
   ios::sync_with_stdio(false);
@@ -42,47 +13,23 @@ int main() {
 
   int n;
   cin >> n;
-  vector<int> memo;
   vector<pair<int, int>> arr(n);
-  for (auto& [l, r] : arr) {
-    cin >> l >> r;
-    memo.push_back(l);
-    memo.push_back(r);
-  }
+  for (auto& [l, r] : arr) cin >> l >> r;
+
+  sort(arr.begin(), arr.end());
 
   int k;
   cin >> k;
-  vector<int> brr(k);
-  for (auto& e : brr) {
-    cin >> e;
-    memo.push_back(e);
-  }
-
-  sort(memo.begin(), memo.end());
-  memo.erase(unique(memo.begin(), memo.end()), memo.end());
-
-  for (auto& [l, r] : arr) {
-    l = lower_bound(memo.begin(), memo.end(), l) - memo.begin();
-    r = lower_bound(memo.begin(), memo.end(), r) - memo.begin();
-  }
-
-  for (auto& e : brr) {
-    e = lower_bound(memo.begin(), memo.end(), e) - memo.begin();
-  }
-
-  for (int i = 0; i < n; i++) {
-    auto [l, r] = arr[i];
-    Update(l, r, i);
-  }
-
-  int cnt = 0;
-  vector<bool> vis(n);
-  for (auto e : brr) {
-    auto res = Query(e);
-    for (auto e : res) {
-      if (vis[e]) continue;
-      vis[e] = true;
-      ++cnt;
+  vector<int> memo;
+  int idx = 0, cnt = 0;
+  while (k--) {
+    int x;
+    cin >> x;
+    while (idx < n && arr[idx].first <= x) memo.push_back(arr[idx++].second);
+    while (!memo.empty()) {
+      int y = memo.back();
+      memo.pop_back();
+      cnt += (y >= x);
     }
   }
   cout << cnt;
