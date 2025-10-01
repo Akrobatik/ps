4e284fef9fb6cb359c73068b5c6b415264ef7fc2
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-10-01 11:54:02 +0900

    32684 - 장기

---
+++ BaekJoon/32000/32684.cpp
@@ -0,0 +1,30 @@
+// Title : 장기
+// Link  : https://www.acmicpc.net/problem/32684 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+constexpr int kTable[] = {13, 7, 5, 3, 3, 2};
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int a = 0, b = 0;
+  for (int i = 0; i < 6; i++) {
+    int x;
+    cin >> x;
+    a += kTable[i] * x;
+  }
+  for (int i = 0; i < 6; i++) {
+    int x;
+    cin >> x;
+    b += kTable[i] * x;
+  }
+  cout << (a > b + 1 ? "cocjr0208" : "ekwoo");
+
+  return 0;
+}
