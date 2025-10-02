aabd2946e0d923fe4452a92bac62da88a7409953
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-10-02 10:05:28 +0900

    34558 - Prime Median

---
+++ BaekJoon/34000/34558.cpp
@@ -0,0 +1,47 @@
+// Title : Prime Median
+// Link  : https://www.acmicpc.net/problem/34558 
+// Time  : 48 ms
+// Memory: 3892 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+constexpr int kMax = 1e6;
+
+bool seive[kMax + 1];
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  vector<int> primes;
+  for (int i = 2; i <= kMax; i++) {
+    if (!seive[i]) primes.push_back(i);
+    for (auto p : primes) {
+      if (i * p > kMax) break;
+      seive[i * p] = true;
+      if (i % p == 0) break;
+    }
+  }
+
+  int n;
+  cin >> n;
+  while (n--) {
+    int a, b;
+    cin >> a >> b;
+
+    auto lit = lower_bound(primes.begin(), primes.end(), a);
+    auto rit = upper_bound(primes.begin(), primes.end(), b);
+
+    int sz = rit - lit;
+    if (sz & 1) {
+      int half = sz >> 1;
+      cout << lit[half] << "\n";
+    } else {
+      cout << "-1\n";
+    }
+  }
+
+  return 0;
+}
