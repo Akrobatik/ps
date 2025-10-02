32520ddd348145a782291fab8eefd3ca7004e6fd
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-10-02 10:06:08 +0900

    34552 - 디딤돌 장학금

---
+++ BaekJoon/34000/34552.cpp
@@ -0,0 +1,32 @@
+// Title : 디딤돌 장학금
+// Link  : https://www.acmicpc.net/problem/34552 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int table[11];
+  for (auto& e : table) cin >> e;
+
+  int sum = 0;
+
+  int n;
+  cin >> n;
+  while (n--) {
+    int b, s;
+    long double fl;
+    cin >> b >> fl >> s;
+    int l = (int)(fl * 100.0L);
+
+    sum += (s >= 17 && l >= 200 ? table[b] : 0);
+  }
+  cout << sum;
+
+  return 0;
+}
