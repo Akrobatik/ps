03cb9c6b027b4d7d83c6357832d62574f4db209b
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-10-02 13:50:57 +0900

    34562 - 터치 앤 리턴

--- BaekJoon/34000/34562.cpp
+++ BaekJoon/34000/34562.cpp
@@ -1,85 +1,84 @@
 // Title : 터치 앤 리턴
 // Link  : https://www.acmicpc.net/problem/34562 
-// Time  : 440 ms
-// Memory: 174140 KB
+// Time  : 228 ms
+// Memory: 88124 KB
 
 #include <bits/stdc++.h>
 
 using namespace std;
 
-constexpr int64_t kInf = 1e18;
-
 int main() {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
 
-  int64_t n, m, k;
+  int n, m, k;
   cin >> n >> m >> k;
 
-  vector<vector<int64_t>> mat(n, vector<int64_t>(n, kInf));
+  vector<vector<int>> mat(n, vector<int>(n, k + 1));
   for (int i = 0; i < n; i++) mat[i][i] = 0;
   while (m--) {
-    int64_t u, v, w;
+    int u, v, w;
     cin >> u >> v >> w;
     --u, --v;
-    mat[u][v] = min<int64_t>(mat[u][v], w);
-    mat[v][u] = min<int64_t>(mat[v][u], w);
+    mat[u][v] = min<int>(mat[u][v], w);
+    mat[v][u] = min<int>(mat[v][u], w);
   }
 
   for (int l = 1; l < n; l++) {
     for (int i = 0; i < n; i++) {
       for (int j = 0; j < n; j++) {
-        mat[i][j] = min<int64_t>(mat[i][j], mat[i][l] + mat[l][j]);
+        mat[i][j] = min<int>(mat[i][j], mat[i][l] + mat[l][j]);
       }
     }
   }
 
   int64_t mb = 1LL << n;
-  vector<vector<int64_t>> dist(n, vector<int64_t>(mb, kInf));
+  vector<int> incl, excl;
+  vector<vector<int>> dist(n, vector<int>(mb, k + 1));
   dist[0][1] = 0;
   for (int i = 0; i < mb; i++) {
     if (~i & 1) continue;
+
+    incl.clear(), excl.clear();
     for (int j = 0; j < n; j++) {
-      if (~i & (1 << j)) continue;
+      if (i & (1 << j)) {
+        incl.push_back(j);
+      } else {
+        excl.push_back(j);
+      }
+    }
 
-      int64_t cur = dist[j][i];
-      if (cur == kInf) continue;
-
-      for (int l = 0; l < n; l++) {
-        if (i & (1 << l)) continue;
-        if (mat[j][l] == kInf) continue;
-        dist[l][i | (1 << l)] = min<int64_t>(dist[l][i | (1 << l)], cur + mat[j][l]);
+    for (auto j : incl) {
+      for (auto l : excl) {
+        dist[l][i | (1 << l)] = min<int>(dist[l][i | (1 << l)], dist[j][i] + mat[j][l]);
       }
     }
   }
 
-  vector<int64_t> mind(n, kInf);
+  vector<int> mind(n, k + 1);
   for (int i = 0; i < mb; i++) {
     if (~i & 1) continue;
 
     int pc = popcount((uint32_t)i) - 1;
     for (int j = 0; j < n; j++) {
       if (~i & (1 << j)) continue;
-
-      int64_t d = dist[j][i], w = mat[j][0];
-      if (d == kInf || w == kInf) continue;
-      mind[pc] = min<int64_t>(mind[pc], d + w);
+      mind[pc] = min<int>(mind[pc], dist[j][i] + mat[j][0]);
     }
   }
 
-  vector<pair<int64_t, int64_t>> arr;
+  vector<pair<int, int>> arr;
   for (int i = 1; i < n; i++) {
     if (mind[i] > k) continue;
     while (!arr.empty() && arr.back().second >= mind[i]) arr.pop_back();
     arr.push_back({i * i, mind[i]});
   }
 
-  vector<int64_t> memo(k + 1);
+  vector<int> memo(k + 1);
   for (int i = 1; i <= k; i++) {
-    memo[i] = max<int64_t>(memo[i], memo[i - 1]);
+    memo[i] = max<int>(memo[i], memo[i - 1]);
     for (auto [r, w] : arr) {
       if (w > i) break;
-      memo[i] = max<int64_t>(memo[i], memo[i - w] + r);
+      memo[i] = max<int>(memo[i], memo[i - w] + r);
     }
   }
   cout << memo[k];
