2c1a1bf81d8d026ca103da15b665e3d2982bba4e
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-10-02 10:06:00 +0900

    34553 - 알파벳 점수 계산기

---
+++ BaekJoon/34000/34553.cpp
@@ -0,0 +1,30 @@
+// Title : 알파벳 점수 계산기
+// Link  : https://www.acmicpc.net/problem/34553 
+// Time  : 0 ms
+// Memory: 2300 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  string s;
+  cin >> s;
+  int n = s.size();
+
+  int64_t cur = 1, sum = 1;
+  for (int i = 1; i < n; i++) {
+    if (s[i] > s[i - 1]) {
+      ++cur;
+    } else {
+      cur = 1;
+    }
+    sum += cur;
+  }
+  cout << sum;
+
+  return 0;
+}
