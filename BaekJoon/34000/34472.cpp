55e81238f1d639ad0445c919a5de71bdb13cf64b
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-10-04 20:01:46 +0900

    34472 - 교도소

--- BaekJoon/34000/34472.cpp
+++ BaekJoon/34000/34472.cpp
@@ -1,7 +1,7 @@
 // Title : 교도소
 // Link  : https://www.acmicpc.net/problem/34472 
-// Time  : 752 ms
-// Memory: 6444 KB
+// Time  : 64 ms
+// Memory: 5928 KB
 
 #include <bits/stdc++.h>
 
@@ -18,32 +18,21 @@ int main() {
   for (auto& e : arr) cin >> e;
 
   int64_t m = accumulate(arr.begin(), arr.end(), 0LL) / n;
-
-  auto Cmp = [&](int lhs, int rhs) {
-    return arr[lhs] < arr[rhs];
-  };
-
-  priority_queue<pair<int64_t, int>> pq;
-  for (int i = 0; i < n; i++) {
-    if (arr[i] > m) pq.push({arr[i], i});
-  }
+  for (auto& e : arr) e -= m;
 
   int64_t cnt = 0;
-  while (!pq.empty()) {
-    auto [val, idx] = pq.top();
-    pq.pop();
+  for (int j = 0; j < 2; j++) {
+    for (int i = 0; i < n; i++) {
+      if (arr[i] <= 0) continue;
+      cnt += arr[i];
 
-    if (arr[idx] != val) continue;
+      int nxt = i + 1;
+      if (nxt == n) nxt = 0;
 
-    int64_t cut = val - m;
-    cnt += cut;
-    arr[idx] -= cut;
-
-    int nxt = idx + 1;
-    if (nxt == n) nxt = 0;
-    if ((arr[nxt] += cut) > m) pq.push({arr[nxt], nxt});
+      arr[nxt] += arr[i];
+      arr[i] = 0;
+    }
   }
-
   cout << cnt;
 
   return 0;
