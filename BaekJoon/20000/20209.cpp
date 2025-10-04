ab9dbd25da8db2dc79c0146b91105d7d1c00c8d7
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-10-04 20:58:25 +0900

    20209 - 스트레이트 스위치 게임

---
+++ BaekJoon/20000/20209.cpp
@@ -0,0 +1,52 @@
+// Title : 스트레이트 스위치 게임
+// Link  : https://www.acmicpc.net/problem/20209 
+// Time  : 8 ms
+// Memory: 2088 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n, k;
+  cin >> n >> k;
+
+  vector<int> arr(n + 1);
+  for (int i = 1; i <= n; i++) {
+    cin >> arr[i];
+  }
+
+  vector<vector<int>> g(k);
+  for (int i = 0; i < k; i++) {
+    int x;
+    cin >> x;
+    g[i].resize(x);
+    for (auto& e : g[i]) cin >> e;
+  }
+
+  int minn = INT_MAX;
+  [&](this auto&& self, int idx, int sum) -> void {
+    if (idx == k) {
+      int val = arr[1] % 5;
+      bool ok = true;
+      for (int i = 1; ok && i <= n; i++) {
+        ok = (arr[i] % 5 == val);
+      }
+      if (ok) minn = min<int>(minn, sum);
+      return;
+    }
+
+    for (int i = 0; i < 5; i++) {
+      for (auto e : g[idx]) arr[e] += (idx + 1) * i;
+      self(idx + 1, sum + i);
+      for (auto e : g[idx]) arr[e] -= (idx + 1) * i;
+    }
+  }(0, 0);
+
+  cout << (minn != INT_MAX ? minn : -1);
+
+  return 0;
+}
