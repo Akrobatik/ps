7f7f853b453d7af64a204efe5e35ba5a71b5e7cb
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-10-04 20:12:55 +0900

    20205 - 교수님 그림이 깨지는데요?

---
+++ BaekJoon/20000/20205.cpp
@@ -0,0 +1,31 @@
+// Title : 교수님 그림이 깨지는데요?
+// Link  : https://www.acmicpc.net/problem/20205 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n, m;
+  cin >> n >> m;
+  vector<vector<int>> board(n, vector<int>(n));
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n; j++) {
+      cin >> board[i][j];
+    }
+  }
+
+  for (int i = 0; i < n * m; i++) {
+    for (int j = 0; j < n * m; j++) {
+      cout << board[i / m][j / m] << " ";
+    }
+    cout << "\n";
+  }
+
+  return 0;
+}
