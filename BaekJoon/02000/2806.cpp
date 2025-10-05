a3e29038698974964d6cdc31465dfd0d0506482d
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-10-05 18:45:16 +0900

    2806 - DNA 발견

---
+++ BaekJoon/02000/2806.cpp
@@ -0,0 +1,27 @@
+// Title : DNA 발견
+// Link  : https://www.acmicpc.net/problem/2806 
+// Time  : 8 ms
+// Memory: 10984 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  string s;
+  cin >> n >> s;
+
+  vector<array<int, 2>> memo(n + 1);
+  for (int i = 0; i < n; i++) {
+    int a = (s[i] == 'A' ? 0 : 1), b = a ^ 1;
+    memo[i + 1][a] = min<int>(memo[i][a], memo[i][b] + 1);
+    memo[i + 1][b] = memo[i][b] + 1;
+  }
+  cout << min<int>(memo[n][0], memo[n][1] + 1);
+
+  return 0;
+}
