93c65c9c5443a3cc6d6ff58f159858c2b3d89b14
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-10-05 15:58:28 +0900

    2109 - 순회강연

---
+++ BaekJoon/02000/2109.cpp
@@ -0,0 +1,30 @@
+// Title : 순회강연
+// Link  : https://www.acmicpc.net/problem/2109 
+// Time  : 4 ms
+// Memory: 2224 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  cin >> n;
+  vector<pair<int, int>> arr(n);
+  for (auto& [d, p] : arr) cin >> p >> d;
+  sort(arr.begin(), arr.end());
+
+  int64_t sum = 0;
+  priority_queue<int, vector<int>, greater<int>> pq;
+  for (auto [d, p] : arr) {
+    sum += p;
+    pq.push(p);
+    while (pq.size() > d) sum -= pq.top(), pq.pop();
+  }
+  cout << sum;
+
+  return 0;
+}
