db703751e8f0a4732df71eea4a14321936f051fa
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-10-05 16:19:08 +0900

    24956 - 나는 정말 휘파람을 못 불어

---
+++ BaekJoon/24000/24956.cpp
@@ -0,0 +1,138 @@
+// Title : 나는 정말 휘파람을 못 불어
+// Link  : https://www.acmicpc.net/problem/24956 
+// Time  : 0 ms
+// Memory: 2648 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// https://github.com/Akrobatik/ps/blob/main/template/modint.cpp
+template <int32_t MOD>
+struct ModInt32 {
+  using CT = conditional_t<MOD <= numeric_limits<int32_t>::max() / 2, int32_t, int64_t>;
+
+  constexpr ModInt32() : val(0) {}
+  constexpr ModInt32(int32_t x) : val((x %= MOD) < 0 ? x + MOD : x) {}
+  constexpr ModInt32(int64_t x) : val((x %= MOD) < 0 ? x + MOD : x) {}
+  constexpr ModInt32(__int128_t x) : val((x %= MOD) < 0 ? x + MOD : x) {}
+
+  constexpr ModInt32 Pow(int64_t exp) const {
+    int64_t n = val, x = 1;
+    while (exp) {
+      if (exp & 1) x = x * n % MOD;
+      n = n * n % MOD;
+      exp >>= 1;
+    }
+
+    ModInt32 res;
+    res.val = x;
+    return res;
+  }
+
+  constexpr ModInt32 Inv() const {
+    return Pow(MOD - 2);
+  }
+
+  constexpr ModInt32& operator++() {
+    if (++val == MOD) val = 0;
+    return *this;
+  }
+
+  constexpr ModInt32 operator++(int) {
+    ModInt32 tmp(*this);
+    operator++();
+    return tmp;
+  }
+
+  constexpr ModInt32& operator+=(const ModInt32& other) {
+    CT x = (CT)val + other.val;
+    if (x >= MOD) x -= MOD;
+    val = x;
+    return *this;
+  }
+
+  constexpr ModInt32& operator-=(const ModInt32& other) {
+    CT x = (CT)val + (MOD - other.val);
+    if (x >= MOD) x -= MOD;
+    val = x;
+    return *this;
+  }
+
+  constexpr ModInt32& operator*=(const ModInt32& other) {
+    val = (int64_t)val * other.val % MOD;
+    return *this;
+  }
+
+  constexpr ModInt32& operator/=(const ModInt32& other) {
+    *this *= other.Inv();
+    return *this;
+  }
+
+  constexpr ModInt32 operator-() const {
+    ModInt32 res;
+    if (val) res.val = MOD - val;
+    return res;
+  }
+
+  constexpr ModInt32 operator+(const ModInt32& rhs) const {
+    return ModInt32(*this) += rhs;
+  }
+
+  constexpr ModInt32 operator-(const ModInt32& rhs) const {
+    return ModInt32(*this) -= rhs;
+  }
+
+  constexpr ModInt32 operator*(const ModInt32& rhs) const {
+    return ModInt32(*this) *= rhs;
+  }
+
+  constexpr ModInt32 operator/(const ModInt32& rhs) const {
+    return ModInt32(*this) /= rhs;
+  }
+
+  constexpr bool operator!() const {
+    return val == 0;
+  }
+
+  friend istream& operator>>(istream& is, ModInt32& num) {
+    is >> num.val;
+    if ((num.val %= MOD) < 0) num.val += MOD;
+    return is;
+  }
+
+  friend ostream& operator<<(ostream& os, const ModInt32& num) {
+    os << num.val;
+    return os;
+  }
+
+  int32_t val;
+};
+
+constexpr int kMod = 1e9 + 7;
+using ModInt = ModInt32<kMod>;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  string s;
+  cin >> n >> s;
+
+  ModInt sum = 0;
+  ModInt memo[3] = {};
+  for (auto c : s) {
+    if (c == 'W') {
+      memo[0] += 1;
+    } else if (c == 'H') {
+      memo[1] += memo[0];
+    } else if (c == 'E') {
+      sum += sum + memo[2];
+      memo[2] += memo[1];
+    }
+  }
+  cout << sum;
+
+  return 0;
+}
