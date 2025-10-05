96a516f7bb88d5c08135547d98c34736a9128113
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-10-05 22:03:48 +0900

    10978 - 기숙사 재배정

---
+++ BaekJoon/10000/10978.cpp
@@ -0,0 +1,29 @@
+// Title : 기숙사 재배정
+// Link  : https://www.acmicpc.net/problem/10978 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int64_t memo[21] = {1};
+
+  for (int i = 1, j = -1; i <= 20; i++, j = -j) {
+    memo[i] = memo[i - 1] * i + j;
+  }
+
+  int t;
+  cin >> t;
+  while (t--) {
+    int n;
+    cin >> n;
+    cout << memo[n] << "\n";
+  }
+
+  return 0;
+}
