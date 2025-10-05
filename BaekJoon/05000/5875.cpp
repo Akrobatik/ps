1932b910a848764ca721782bb1de85f05b004fb0
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-10-05 16:41:00 +0900

    5875 - 오타

---
+++ BaekJoon/05000/5875.cpp
@@ -0,0 +1,46 @@
+// Title : 오타
+// Link  : https://www.acmicpc.net/problem/5875 
+// Time  : 0 ms
+// Memory: 2300 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  string s;
+  cin >> s;
+  int n = s.size();
+
+  int cur = 0, minn = 0;
+  for (int i = 0; i < n; i++) {
+    cur += (s[i] == '(' ? 1 : -1);
+    minn = min<int>(minn, cur);
+  }
+
+  if (abs(cur) != 2 || minn < -2) {
+    cout << "0";
+    return 0;
+  }
+
+  if (cur == 2) {
+    reverse(s.begin(), s.end());
+    for (auto& c : s) c ^= 1;
+  }
+
+  int cnt = 0;
+  cur = 0;
+  for (int i = 0; i < n; i++) {
+    cur += (s[i] == '(' ? 1 : -1);
+    if (s[i] == ')') ++cnt;
+    if (cur == -1) {
+      cout << cnt;
+      break;
+    }
+  }
+
+  return 0;
+}
