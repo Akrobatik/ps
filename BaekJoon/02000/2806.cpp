053d6892268b2acec41a983c6af52abde575d840
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-10-05 18:48:39 +0900

    2806 - DNA 발견

--- BaekJoon/02000/2806.cpp
+++ BaekJoon/02000/2806.cpp
@@ -1,7 +1,7 @@
 // Title : DNA 발견
 // Link  : https://www.acmicpc.net/problem/2806 
-// Time  : 8 ms
-// Memory: 10984 KB
+// Time  : 4 ms
+// Memory: 3680 KB
 
 #include <bits/stdc++.h>
 
@@ -15,13 +15,16 @@ int main() {
   string s;
   cin >> n >> s;
 
-  vector<array<int, 2>> memo(n + 1);
+  int mem[2][2] = {};
+  int *memo = mem[0], *mnxt = mem[1];
+
   for (int i = 0; i < n; i++) {
     int a = (s[i] == 'A' ? 0 : 1), b = a ^ 1;
-    memo[i + 1][a] = min<int>(memo[i][a], memo[i][b] + 1);
-    memo[i + 1][b] = memo[i][b] + 1;
+    mnxt[a] = min<int>(memo[a], memo[b] + 1);
+    mnxt[b] = memo[b] + 1;
+    swap(memo, mnxt);
   }
-  cout << min<int>(memo[n][0], memo[n][1] + 1);
+  cout << min<int>(memo[0], memo[1] + 1);
 
   return 0;
 }
