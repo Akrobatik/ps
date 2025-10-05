f157f4af86bd0cc77c50618d1999a49baae1647d
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-10-05 18:00:25 +0900

    4811 - 알약

---
+++ BaekJoon/04000/4811.cpp
@@ -0,0 +1,30 @@
+// Title : 알약
+// Link  : https://www.acmicpc.net/problem/4811 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int64_t memo[31] = {};
+  for (int i = 1; i <= 30; i++) {
+    int64_t x = 1, y = i << 1;
+    for (int j = 0; j < i; j++) {
+      x *= y - j;
+      x /= j + 1;
+    }
+    memo[i] = x / (i + 1);
+  }
+
+  int n;
+  while (cin >> n && n) {
+    cout << memo[n] << "\n";
+  }
+
+  return 0;
+}
