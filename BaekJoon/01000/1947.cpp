b140883eba34369031dd50e74305deeebb3f03d1
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-10-05 22:00:35 +0900

    1947 - 선물 전달

---
+++ BaekJoon/01000/1947.cpp
@@ -0,0 +1,27 @@
+// Title : 선물 전달
+// Link  : https://www.acmicpc.net/problem/1947 
+// Time  : 4 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+constexpr int kMod = 1e9;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  cin >> n;
+
+  int64_t cur = 1;
+  for (int i = 1, j = -1; i <= n; i++, j = -j) {
+    cur = (cur * i + j) % kMod;
+    if (cur < 0) cur += kMod;
+  }
+  cout << cur;
+
+  return 0;
+}
