da96b554289d012e9531e1c80d31a3144368915d
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-10-05 13:51:08 +0900

    24533 - 결합

---
+++ BaekJoon/24000/24533.cpp
@@ -0,0 +1,27 @@
+// Title : 결합
+// Link  : https://www.acmicpc.net/problem/24533 
+// Time  : 48 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  cin >> n;
+
+  int64_t sum = 0, a = 0, b = 0;
+  while (n--) {
+    int64_t c, d;
+    cin >> c >> d;
+    sum += a * d + b * c;
+    a += c, b += d;
+  }
+  cout << sum;
+
+  return 0;
+}
