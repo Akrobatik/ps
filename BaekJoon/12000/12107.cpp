dae94669e73fee86daf307eb2091d9b614b7d01d
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-10-07 03:25:37 +0900

    12107 - 약수 지우기 게임 1

---
+++ BaekJoon/12000/12107.cpp
@@ -0,0 +1,19 @@
+// Title : 약수 지우기 게임 1
+// Link  : https://www.acmicpc.net/problem/12107 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  cin >> n;
+  cout << (n == 1 ? "B" : "A");
+
+  return 0;
+}
