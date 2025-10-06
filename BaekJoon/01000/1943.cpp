416dd7dc9c98046510015b5de6891642a7cff428
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-10-07 03:22:02 +0900

    1943 - 동전 분배

---
+++ BaekJoon/01000/1943.cpp
@@ -0,0 +1,33 @@
+// Title : 동전 분배
+// Link  : https://www.acmicpc.net/problem/1943 
+// Time  : 268 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+constexpr int kMax = 5e4;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  while (cin >> n) {
+    bitset<kMax + 1> memo;
+    memo.set(0);
+    int sum = 0;
+    while (n--) {
+      int x, cnt;
+      cin >> x >> cnt;
+      sum += cnt * x;
+      while (cnt--) {
+        memo |= memo << x;
+      }
+    }
+    cout << ((~sum & 1) && memo.test(sum >> 1) ? "1\n" : "0\n");
+  }
+
+  return 0;
+}
