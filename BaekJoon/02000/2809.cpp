2cdb571795a71bd70ac1ffd15d4ff89c6079600a
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-10-08 00:39:14 +0900

    2809 - 아스키 거리

--- BaekJoon/02000/2809.cpp
+++ BaekJoon/02000/2809.cpp
@@ -1,7 +1,7 @@
 // Title : 아스키 거리
 // Link  : https://www.acmicpc.net/problem/2809 
-// Time  : 3928 ms
-// Memory: 510764 KB
+// Time  : 3832 ms
+// Memory: 510760 KB
 
 #include <bits/stdc++.h>
 
@@ -187,130 +187,6 @@ struct SuffixArray {
   vector<int> buf, tmp;
 };
 
-// https://github.com/Akrobatik/ps/blob/main/template/lazy_segment_tree.cpp
-template <typename V, typename L, typename OP, typename APPLY, typename COMPO>
-  requires requires(V va, V vb, L la, L lb, int sz) {
-    { OP{}(va, vb) } -> convertible_to<V>;
-    { APPLY{}(va, la, sz) } -> convertible_to<V>;
-    { COMPO{}(la, lb) } -> convertible_to<L>;
-    { la == lb } -> convertible_to<bool>;
-  }
-struct LazySegTree {
-  void Init(int n, const V& ival, const L& ilzy) {
-    nmax = bit_ceil((uint32_t)n);
-    nlog = countr_zero((uint32_t)nmax);
-    iv = ival, il = ilzy;
-    tree.assign(nmax << 1, iv);
-    lazy.assign(nmax << 1, il);
-    sz.resize(nmax << 1);
-    sz[1] = nmax;
-    for (int i = 1; i < nmax; i++) {
-      sz[i << 1] = sz[i << 1 | 1] = sz[i] >> 1;
-    }
-  }
-
-  void Set(int idx, const V& val) {
-    tree[idx + nmax] = val;
-  }
-
-  void Build() {
-    for (int i = nmax - 1; i > 0; i--) {
-      tree[i] = OP{}(tree[i << 1], tree[i << 1 | 1]);
-    }
-  }
-
-  void Update(int l, int r, const L& lzy) {
-    l += nmax, r += nmax + 1;
-    for (int i = nlog; i > 0; i--) {
-      if (Check(l, i)) Push(l >> i);
-      if (Check(r, i)) Push((r - 1) >> i);
-    }
-
-    for (int ll = l, rr = r; ll < rr; ll >>= 1, rr >>= 1) {
-      if (ll & 1) Apply(ll++, lzy);
-      if (rr & 1) Apply(--rr, lzy);
-    }
-
-    for (int i = 1; i <= nlog; i++) {
-      if (Check(l, i)) Pull(l >> i);
-      if (Check(r, i)) Pull((r - 1) >> i);
-    }
-  }
-
-  V Query(int idx) {
-    int node = idx + nmax;
-    for (int i = nlog; i > 0; i--) Push(node >> i);
-    return tree[node];
-  }
-
-  V Query(int l, int r) {
-    V lv = iv, rv = iv;
-
-    l += nmax, r += nmax + 1;
-    for (int i = nlog; i > 0; i--) {
-      if (Check(l, i)) Push(l >> i);
-      if (Check(r, i)) Push((r - 1) >> i);
-    }
-
-    for (; l < r; l >>= 1, r >>= 1) {
-      if (l & 1) lv = OP{}(lv, tree[l++]);
-      if (r & 1) rv = OP{}(tree[--r], rv);
-    }
-    return OP{}(lv, rv);
-  }
-
- private:
-  int nmax, nlog;
-  V iv;
-  L il;
-  vector<V> tree;
-  vector<L> lazy;
-  vector<int> sz;
-
-  bool Check(int x, int shift) {
-    int y = (x >> shift) << shift;
-    return x != y;
-  }
-
-  void Apply(int node, const L& lzy) {
-    tree[node] = APPLY{}(tree[node], lzy, sz[node]);
-    if (node < nmax) lazy[node] = COMPO{}(lazy[node], lzy);
-  }
-
-  void Push(int node) {
-    if (lazy[node] == il) return;
-    Apply(node << 1, lazy[node]);
-    Apply(node << 1 | 1, lazy[node]);
-    lazy[node] = il;
-  }
-
-  void Pull(int node) {
-    tree[node] = OP{}(tree[node << 1], tree[node << 1 | 1]);
-  }
-};
-
-using Node = int;
-using Lazy = int;
-
-struct FOp {
-  Node operator()(const Node& a, const Node& b) const {
-    return a + b;
-  }
-};
-
-struct FApply {
-  Node operator()(const Node& a, const Lazy& b, int sz) const {
-    if (b) return 0;
-    return a;
-  }
-};
-
-struct FCompo {
-  Lazy operator()(const Lazy& a, const Lazy& b) const {
-    return a | b;
-  }
-};
-
 int main() {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
@@ -338,13 +214,6 @@ int main() {
   auto& sa = solver.sa;
   auto& lcp = solver.lcp;
 
-  LazySegTree<Node, Lazy, FOp, FApply, FCompo> seg;
-  seg.Init(n, 0, 0);
-  for (int i = 0; i < n; i++) {
-    seg.Set(i, 1);
-  }
-  seg.Build();
-
   int ns = s.size();
   vector<int> stk;
   for (int i = 0; i < ns; i++) {
@@ -352,14 +221,18 @@ int main() {
     while (!stk.empty() && len < stk.back()) stk.pop_back();
 
     if (pos < n) {
-      int x = (stk.empty() ? 0 : stk.back());
-      seg.Update(pos, pos + x - 1, 1);
+      lens[pos] = (stk.empty() ? 0 : stk.back());
     } else if (lens[pos]) {
       stk.push_back(lens[pos]);
     }
   }
 
-  cout << seg.Query(0, n - 1);
+  int cnt = 0, cur = 0;
+  for (int i = 0; i < n; i++) {
+    cur = max<int>(cur, i + lens[i]);
+    cnt += (cur == i);
+  }
+  cout << cnt;
 
   return 0;
 }
