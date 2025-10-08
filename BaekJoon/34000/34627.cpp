ce98e6727b9a81dff0640b1610b97ee0a260866d
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-10-09 03:18:02 +0900

    34627 - Catching the Krug

---
+++ BaekJoon/34000/34627.cpp
@@ -0,0 +1,27 @@
+// Title : Catching the Krug
+// Link  : https://www.acmicpc.net/problem/34627 
+// Time  : 4 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void Solve() {
+  int n, ay, ax, by, bx;
+  cin >> n >> ay >> ax >> by >> bx;
+  if (ay > by) ay = n - ay, by = n - by;
+  if (ax > bx) ax = n - ax, bx = n - bx;
+  cout << max<int>(ay != by ? by : 0, ax != bx ? bx : 0) << "\n";
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int t;
+  cin >> t;
+  while (t--) Solve();
+
+  return 0;
+}
