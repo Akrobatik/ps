93ebcadd415b435d45b469085b0fa15e6686199f
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-10-09 01:53:37 +0900

    9372 - 상근이의 여행

---
+++ BaekJoon/09000/9372.cpp
@@ -0,0 +1,27 @@
+// Title : 상근이의 여행
+// Link  : https://www.acmicpc.net/problem/9372 
+// Time  : 16 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int t;
+  cin >> t;
+  while (t--) {
+    int n, m;
+    cin >> n >> m;
+    while (m--) {
+      int u, v;
+      cin >> u >> v;
+    }
+    cout << n - 1 << "\n";
+  }
+
+  return 0;
+}
