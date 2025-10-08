2335fc9ad473cc103c0dd3b9e9fcaf538870f0e3
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-10-09 02:15:07 +0900

    34210 - A + B Queries

---
+++ BaekJoon/34000/34210.cpp
@@ -0,0 +1,16 @@
+// Title : A + B Queries
+// Link  : https://www.acmicpc.net/problem/34210 
+// Time  : 36 ms
+// Memory: 6720 KB
+
+#include "aplusb.h"
+
+std::vector<int> AA, BB;
+
+void initialize(std::vector<int> A, std::vector<int> B) {
+  AA = move(A), BB = move(B);
+}
+
+int answer_question(int i, int j) {
+  return AA[i] + BB[j];
+}
