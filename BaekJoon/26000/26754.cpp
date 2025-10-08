a4766f9b2a93b28317c5c695f60056f0ed7df1c6
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-10-08 16:13:36 +0900

    26754 - Programy

---
+++ BaekJoon/26000/26754.cpp
@@ -0,0 +1,32 @@
+// Title : Programy
+// Link  : https://www.acmicpc.net/problem/26754 
+// Time  : 368 ms
+// Memory: 9840 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  cin >> n;
+  vector<int> arr(n);
+  for (auto& e : arr) cin >> e;
+  sort(arr.begin(), arr.end(), greater<int>());
+
+  int m;
+  cin >> m;
+  vector<int> brr(m);
+  for (auto& e : brr) cin >> e;
+  sort(brr.begin(), brr.end());
+
+  for (auto e : brr) {
+    if (!arr.empty() && arr.back() <= e) arr.pop_back();
+  }
+  cout << n - arr.size();
+
+  return 0;
+}
