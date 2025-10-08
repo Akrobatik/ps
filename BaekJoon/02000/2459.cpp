7150e73c033ccba35c987ae7bc5283fc45dc1f21
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-10-08 15:42:35 +0900

    2459 - 철사 자르기

---
+++ BaekJoon/02000/2459.cpp
@@ -0,0 +1,57 @@
+// Title : 철사 자르기
+// Link  : https://www.acmicpc.net/problem/2459 
+// Time  : 16 ms
+// Memory: 3684 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n, k;
+  cin >> n >> k;
+  vector<pair<int, int>> arr{{2, 2}};
+  for (int i = 0; i < k; i++) {
+    int x, y;
+    cin >> x >> y;
+    arr.push_back({x << 1, y << 1});
+  }
+
+  int l;
+  cin >> l;
+  l = l << 1 | 1;
+
+  int64_t sum = 0, sss = 0;
+  vector<int64_t> axr;
+  int na = arr.size();
+  for (int i = 0; i < na; i++) {
+    auto [x1, y1] = arr[i];
+    auto [x2, y2] = arr[(i + 1) % na];
+
+    if (x1 < l && l < x2) {
+      sum += l - x1;
+      axr.push_back(sum);
+      sum = x2 - l;
+    } else if (x2 < l && l < x1) {
+      sum += x1 - l;
+      axr.push_back(sum);
+      sum = l - x2;
+    } else {
+      sum += abs(x2 - x1) + abs(y2 - y1);
+    }
+  }
+
+  if (!axr.empty()) {
+    axr[0] += sum;
+  } else {
+    axr.push_back(sum);
+  }
+
+  int64_t ans = *max_element(axr.begin(), axr.end());
+  cout << (ans >> 1);
+
+  return 0;
+}
