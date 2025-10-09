1bb0ee3cb74386dedcdae5d79709d4daa7fbdea9
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-10-09 16:50:37 +0900

    20944 - 팰린드롬 척화비

---
+++ BaekJoon/20000/20944.cpp
@@ -0,0 +1,19 @@
+// Title : 팰린드롬 척화비
+// Link  : https://www.acmicpc.net/problem/20944 
+// Time  : 0 ms
+// Memory: 3000 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  cin >> n;
+  cout << string(n, 'a');
+
+  return 0;
+}
