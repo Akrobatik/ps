787b453bb15f39f600a2d1f25d2702d79515b463
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-10-09 16:58:16 +0900

    25288 - 영어 시험

---
+++ BaekJoon/25000/25288.cpp
@@ -0,0 +1,20 @@
+// Title : 영어 시험
+// Link  : https://www.acmicpc.net/problem/25288 
+// Time  : 0 ms
+// Memory: 2024 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  string s;
+  cin >> n >> s;
+  while (n--) cout << s;
+
+  return 0;
+}
