30aa5572b481eec7368662c1e8426f86b2e5ed74
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-10-09 16:52:15 +0900

    30618 - donstructive

---
+++ BaekJoon/30000/30618.cpp
@@ -0,0 +1,29 @@
+// Title : donstructive
+// Link  : https://www.acmicpc.net/problem/30618 
+// Time  : 12 ms
+// Memory: 2944 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  cin >> n;
+
+  deque<int> dq;
+  for (int i = n; i >= 1; i--) {
+    if (i & 1) {
+      dq.push_back(i);
+    } else {
+      dq.push_front(i);
+    }
+  }
+
+  for (auto e : dq) cout << e << " ";
+
+  return 0;
+}
