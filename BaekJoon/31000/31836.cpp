63c2a9e0adc58099ccc16c211a08f50287f1bce0
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-10-09 17:03:10 +0900

    31836 - 피보나치 기념품

---
+++ BaekJoon/31000/31836.cpp
@@ -0,0 +1,48 @@
+// Title : 피보나치 기념품
+// Link  : https://www.acmicpc.net/problem/31836 
+// Time  : 0 ms
+// Memory: 2156 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  cin >> n;
+
+  vector<int> arr, brr;
+
+  int r = n % 3;
+  if (r == 2) {
+    arr.push_back(1), brr.push_back(2);
+    for (int i = 1; i + 2 <= n; i++) {
+      if (i % 3) {
+        arr.push_back(i + 2);
+      } else {
+        brr.push_back(i + 2);
+      }
+    }
+  } else {
+    n = (n / 3) * 3;
+    for (int i = 1; i <= n; i++) {
+      if (i % 3) {
+        arr.push_back(i);
+      } else {
+        brr.push_back(i);
+      }
+    }
+  }
+
+  cout << arr.size() << "\n";
+  for (auto e : arr) cout << e << " ";
+  cout << "\n";
+  cout << brr.size() << "\n";
+  for (auto e : brr) cout << e << " ";
+  cout << "\n";
+
+  return 0;
+}
