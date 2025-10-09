4fdf7c94f731271cc7f92f60651ef4a10ab1c007
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-10-09 16:53:54 +0900

    28065 - SW 수열 구하기

---
+++ BaekJoon/28000/28065.cpp
@@ -0,0 +1,23 @@
+// Title : SW 수열 구하기
+// Link  : https://www.acmicpc.net/problem/28065 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  cin >> n;
+
+  int l = 1, r = n;
+  for (int i = 0; i < n; i++) {
+    cout << ((i & 1) ? r-- : l++) << " ";
+  }
+
+  return 0;
+}
