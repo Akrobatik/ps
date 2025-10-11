178f8d0e4f661f5da16f538c4f0366826ed34b5f
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-10-11 17:35:51 +0900

    27312 - 운영진에게 설정 짜기는 어려워

---
+++ BaekJoon/27000/27312.cpp
@@ -0,0 +1,34 @@
+// Title : 운영진에게 설정 짜기는 어려워
+// Link  : https://www.acmicpc.net/problem/27312 
+// Time  : 60 ms
+// Memory: 36952 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int m, n, q;
+  cin >> m >> n >> q;
+  vector<int> arr(n), ans(n);
+  for (int i = 0; i < n; i++) {
+    cin >> arr[i];
+    ans[i] = arr[i];
+  }
+
+  for (int i = 0; i < q; i++) {
+    cout << "? " << (i % m) + 1 << " " << i + 1 << endl;
+    int x;
+    cin >> x;
+    ans[i] = (x < arr[i] ? x + 1 : 1);
+  }
+
+  cout << "!";
+  for (auto e : ans) cout << " " << e;
+  cout << endl;
+
+  return 0;
+}
