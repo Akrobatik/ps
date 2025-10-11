63c455f6cbc76a96e99a4f3f14a163f4056c678f
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-10-11 22:52:11 +0900

    11438 - LCA 2

--- BaekJoon/11000/11438.cpp
+++ BaekJoon/11000/11438.cpp
@@ -1,43 +1,64 @@
-// Title : LCA 2
-// Link  : https://www.acmicpc.net/problem/11438
-// Time  : 84 ms
-// Memory: 15092 KB
+// Title : LCA 2
+// Link  : https://www.acmicpc.net/problem/11438 
+// Time  : 64 ms
+// Memory: 15816 KB
 
 #include <bits/stdc++.h>
 
 using namespace std;
 
-int heights[100001];
-int parents[100001][18];
-vector<int> edges[100001];
+struct LCA {
+  void Init(const vector<vector<int>>& g, int rt) {
+    int n = g.size();
+    ord.resize(n + 1);
+    par.resize(n + 1);
+    asc.resize(n + 1);
+    dep.resize(n + 1);
+    in.resize(n + 1);
+    head.resize(n + 1);
 
-int LCA(int a, int b) {
-  if (heights[a] > heights[b]) swap(a, b);
-
-  uint32_t delta = heights[b] - heights[a];
-  while (delta) {
-    int msb = bit_width(delta) - 1;
-    b = parents[b][msb];
-    delta ^= (1 << msb);
-  }
-
-  while (a != b) {
-    if (parents[a][0] == parents[b][0]) {
-      a = parents[a][0];
-      break;
-    } else {
-      for (int i = 1; i <= 17; i++) {
-        if (parents[a][i] == parents[b][i]) {
-          a = parents[a][i - 1];
-          b = parents[b][i - 1];
-          break;
-        }
+    int idx = 0;
+    auto DFS = [&](auto&& self, int u) -> void {
+      int cur = ++idx;
+      ord[idx] = u;
+      in[u] = cur;
+      for (auto v : g[u]) {
+        if (v == par[u]) continue;
+        par[v] = u;
+        dep[v] = dep[u] + 1;
+        self(self, v);
+        head[in[v]] = u;
+        if ((in[u] & -in[u]) < (in[v] & -in[v])) in[u] = in[v];
       }
+    };
+
+    par[rt] = asc[rt] = dep[rt] = head[rt] = 0;
+    DFS(DFS, rt);
+
+    for (int i = 1; i <= idx; i++) {
+      int u = ord[i], p = par[u];
+      asc[u] = asc[p] | (in[u] & -in[u]);
     }
   }
 
-  return a;
-}
+  int Query(int u, int v) {
+    if (in[u] ^ in[v]) {
+      int x = asc[u] & asc[v] & -bit_floor((uint32_t)(in[u] ^ in[v]));
+      if (asc[u] ^ x) {
+        int k = bit_floor((uint32_t)(asc[u] ^ x));
+        u = head[(in[u] & -k) | k];
+      }
+      if (asc[v] ^ x) {
+        int k = bit_floor((uint32_t)(asc[v] ^ x));
+        v = head[(in[v] & -k) | k];
+      }
+    }
+    return dep[u] < dep[v] ? u : v;
+  }
+
+ private:
+  vector<int> ord, par, asc, dep, in, head;
+};
 
 int main() {
   ios::sync_with_stdio(false);
@@ -45,41 +66,24 @@ int main() {
 
   int n;
   cin >> n;
+
+  vector<vector<int>> g(n + 1);
   for (int i = 1; i < n; i++) {
     int u, v;
     cin >> u >> v;
-    edges[u].push_back(v);
-    edges[v].push_back(u);
+    g[u].push_back(v);
+    g[v].push_back(u);
   }
 
-  queue<pair<int, int>> q;
-  q.push({1, 0});
-  for (int i = 0; !q.empty(); i++) {
-    int nq = q.size();
-    while (nq--) {
-      auto [cur, parent] = q.front();
-      q.pop();
-
-      heights[cur] = i;
-      parents[cur][0] = parent;
-      for (auto child : edges[cur]) {
-        if (child != parent) q.push({child, cur});
-      }
-    }
-  }
-
-  for (int h = 1; h <= 17; h++) {
-    for (int i = 1; i <= n; i++) {
-      parents[i][h] = parents[parents[i][h - 1]][h - 1];
-    }
-  }
+  LCA lca;
+  lca.Init(g, 1);
 
   int m;
   cin >> m;
   while (m--) {
-    int a, b;
-    cin >> a >> b;
-    cout << LCA(a, b) << "\n";
+    int u, v;
+    cin >> u >> v;
+    cout << lca.Query(u, v) << "\n";
   }
 
   return 0;
