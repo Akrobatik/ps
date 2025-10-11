482229458687281ad15b8395f25d1b89b97cccb7
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-10-11 14:55:00 +0900

    1867 - 돌멩이 제거

---
+++ BaekJoon/01000/1867.cpp
@@ -0,0 +1,45 @@
+// Title : 돌멩이 제거
+// Link  : https://www.acmicpc.net/problem/1867 
+// Time  : 0 ms
+// Memory: 2220 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n, k;
+  cin >> n >> k;
+
+  vector<vector<int>> g(n + 1);
+  for (int i = 0; i < k; i++) {
+    int u, v;
+    cin >> u >> v;
+    g[u].push_back(v);
+  }
+
+  int cnt = 0;
+  vector<int> cover(n + 1);
+  vector<bool> vis;
+  for (int i = 1; i <= n; i++) {
+    vis.assign(n + 1, false);
+
+    cnt += [&](this auto&& self, int cur) -> int {
+      for (auto nxt : g[cur]) {
+        if (vis[nxt]) continue;
+        vis[nxt] = true;
+        if (!cover[nxt] || self(cover[nxt])) {
+          cover[nxt] = cur;
+          return 1;
+        }
+      }
+      return 0;
+    }(i);
+  }
+  cout << cnt;
+
+  return 0;
+}
