39bb64f81c5a0fc52355d1f385ce8705a35707a4
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-10-11 17:04:21 +0900

    30917 - A+B - 10 (제1편)

---
+++ BaekJoon/30000/30917.cpp
@@ -0,0 +1,28 @@
+// Title : A+B - 10 (제1편)
+// Link  : https://www.acmicpc.net/problem/30917 
+// Time  : 4 ms
+// Memory: 13676 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  auto Find = [](char c) {
+    for (int i = 1; i <= 9; i++) {
+      cout << "? " << c << " " << i << endl;
+      int res;
+      cin >> res;
+      if (res) return i;
+    }
+    assert(0);
+  };
+
+  int a = Find('A'), b = Find('B');
+  cout << "! " << a + b << endl;
+
+  return 0;
+}
