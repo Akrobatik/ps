d1d48bc1e71a7cf64e7ceb244e20ddff625d1af7
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-10-11 17:12:03 +0900

    23306 - binary는 호남선

---
+++ BaekJoon/23000/23306.cpp
@@ -0,0 +1,26 @@
+// Title : binary는 호남선
+// Link  : https://www.acmicpc.net/problem/23306 
+// Time  : 4 ms
+// Memory: 13820 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  cin >> n;
+
+  int x, y;
+  cout << "? 1" << endl;
+  cin >> x;
+  cout << "? " << n << endl;
+  cin >> y;
+
+  cout << "! " << ((x < y) - (x > y));
+
+  return 0;
+}
