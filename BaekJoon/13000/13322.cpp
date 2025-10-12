963666cc12a671b429b410b60e6528fc19df9e19
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-10-12 16:08:30 +0900

    13322 - 접두사 배열

---
+++ BaekJoon/13000/13322.cpp
@@ -0,0 +1,21 @@
+// Title : 접두사 배열
+// Link  : https://www.acmicpc.net/problem/13322 
+// Time  : 8 ms
+// Memory: 2300 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  string s;
+  cin >> s;
+  for (int i = 0; i < s.size(); i++) {
+    cout << i << "\n";
+  }
+
+  return 0;
+}
