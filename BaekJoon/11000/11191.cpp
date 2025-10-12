de7645e5a2a595214267efc6644c0e9360051e69
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-10-12 21:53:55 +0900

    11191 - Xor Maximization

---
+++ BaekJoon/11000/11191.cpp
@@ -0,0 +1,41 @@
+// Title : Xor Maximization
+// Link  : https://www.acmicpc.net/problem/11191 
+// Time  : 44 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int64_t memo[60] = {};
+
+  int n;
+  cin >> n;
+  while (n--) {
+    int64_t x;
+    cin >> x;
+    for (int i = 59; i >= 0; i--) {
+      if ((x >> i) & 1) {
+        if (memo[i]) {
+          x ^= memo[i];
+        } else {
+          memo[i] = x;
+          break;
+        }
+      }
+    }
+  }
+
+  int64_t ans = 0;
+  for (int i = 59; i >= 0; i--) {
+    if (!memo[i]) continue;
+    ans = max<int64_t>(ans, ans ^ memo[i]);
+  }
+  cout << ans;
+
+  return 0;
+}
