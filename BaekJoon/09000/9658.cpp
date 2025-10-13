6cfcac7df458e66355c9781985e11b30bdf6f527
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-10-13 11:14:06 +0900

    9658 - 돌 게임 4

---
+++ BaekJoon/09000/9658.cpp
@@ -0,0 +1,29 @@
+// Title : 돌 게임 4
+// Link  : https://www.acmicpc.net/problem/9658 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  cin >> n;
+
+  int memo[1001] = {1};
+  for (int i = 1; i <= n; i++) {
+    for (int j : {1, 3, 4}) {
+      if (i - j >= 0 && !memo[i - j]) {
+        memo[i] = 1;
+        break;
+      }
+    }
+  }
+  cout << (memo[n] ? "SK" : "CY");
+
+  return 0;
+}
