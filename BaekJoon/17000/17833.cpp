9175b1832b97dba7492b3470079815c277a221df
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-10-15 16:42:47 +0900

    17833 - 홍익대학교 지하캠퍼스

---
+++ BaekJoon/17000/17833.cpp
@@ -0,0 +1,58 @@
+// Title : 홍익대학교 지하캠퍼스
+// Link  : https://www.acmicpc.net/problem/17833 
+// Time  : 316 ms
+// Memory: 66400 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+constexpr int64_t kInf = 1e18;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n, r, d, m;
+  cin >> n >> r >> d >> m;
+
+  vector<vector<pair<int, int>>> g(n + 1);
+  while (m--) {
+    int h, t, e1, e2;
+    cin >> h >> t >> e1 >> e2;
+    if (e1 > e2) swap(e1, e2);
+    int dt = e2 - e1;
+    int mg = h - e2;
+
+    for (int u = e1; u + dt + mg <= n; u++) {
+      int v = u + dt;
+      g[u].push_back({v, t});
+      g[v].push_back({u, t});
+    }
+  }
+
+  priority_queue<pair<int64_t, int>, vector<pair<int64_t, int>>, greater<pair<int64_t, int>>> pq;
+  vector<int64_t> dist(n + 1, kInf);
+
+  auto Push = [&](int x, int64_t ds) {
+    if (dist[x] <= ds) return;
+    dist[x] = ds;
+    pq.push({ds, x});
+  };
+
+  Push(r, 0);
+  while (!pq.empty()) {
+    auto [ds, cur] = pq.top();
+    pq.pop();
+
+    if (dist[cur] != ds) continue;
+    if (cur == d) break;
+
+    for (auto [nxt, w] : g[cur]) {
+      Push(nxt, ds + w);
+    }
+  }
+  cout << (dist[d] != kInf ? dist[d] : -1);
+
+  return 0;
+}
