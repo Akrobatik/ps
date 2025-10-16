64dfb2a3090df6ddb3f73b9d20d91868e411eb61
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-10-16 21:22:13 +0900

    10070 - 벽

---
+++ BaekJoon/10000/10070.cpp
@@ -0,0 +1,196 @@
+// Title : 벽
+// Link  : https://www.acmicpc.net/problem/10070 
+// Time  : 624 ms
+// Memory: 91772 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// https://github.com/Akrobatik/ps/blob/main/template/lazy_segment_tree.cpp
+template <typename V, typename L, typename OP, typename APPLY, typename COMPO>
+  requires requires(V va, V vb, L la, L lb, int sz) {
+    { OP{}(va, vb) } -> convertible_to<V>;
+    { APPLY{}(va, la, sz) } -> convertible_to<V>;
+    { COMPO{}(la, lb) } -> convertible_to<L>;
+    { la == lb } -> convertible_to<bool>;
+  }
+struct LazySegTree {
+  void Init(int n, const V& ival, const L& ilzy) {
+    nmax = bit_ceil((uint32_t)n);
+    nlog = countr_zero((uint32_t)nmax);
+    iv = ival, il = ilzy;
+    tree.assign(nmax << 1, iv);
+    lazy.assign(nmax << 1, il);
+    sz.resize(nmax << 1);
+    sz[1] = nmax;
+    for (int i = 1; i < nmax; i++) {
+      sz[i << 1] = sz[i << 1 | 1] = sz[i] >> 1;
+    }
+  }
+
+  void Set(int idx, const V& val) {
+    tree[idx + nmax] = val;
+  }
+
+  void Build() {
+    for (int i = nmax - 1; i > 0; i--) {
+      tree[i] = OP{}(tree[i << 1], tree[i << 1 | 1]);
+    }
+  }
+
+  void Update(int l, int r, const L& lzy) {
+    l += nmax, r += nmax + 1;
+    for (int i = nlog; i > 0; i--) {
+      if (Check(l, i)) Push(l >> i);
+      if (Check(r, i)) Push((r - 1) >> i);
+    }
+
+    for (int ll = l, rr = r; ll < rr; ll >>= 1, rr >>= 1) {
+      if (ll & 1) Apply(ll++, lzy);
+      if (rr & 1) Apply(--rr, lzy);
+    }
+
+    for (int i = 1; i <= nlog; i++) {
+      if (Check(l, i)) Pull(l >> i);
+      if (Check(r, i)) Pull((r - 1) >> i);
+    }
+  }
+
+  V Query(int idx) {
+    int node = idx + nmax;
+    for (int i = nlog; i > 0; i--) Push(node >> i);
+    return tree[node];
+  }
+
+  V Query(int l, int r) {
+    V lv = iv, rv = iv;
+
+    l += nmax, r += nmax + 1;
+    for (int i = nlog; i > 0; i--) {
+      if (Check(l, i)) Push(l >> i);
+      if (Check(r, i)) Push((r - 1) >> i);
+    }
+
+    for (; l < r; l >>= 1, r >>= 1) {
+      if (l & 1) lv = OP{}(lv, tree[l++]);
+      if (r & 1) rv = OP{}(tree[--r], rv);
+    }
+    return OP{}(lv, rv);
+  }
+
+  vector<int> PushAll(int l, int r) {
+    vector<int> res(r - l + 1);
+    for (int i = 1; i < nmax; i++) {
+      Push(i);
+    }
+    for (int i = l; i <= r; i++) {
+      res[i - l] = tree[i + nmax].minn;
+    }
+    return res;
+  }
+
+ private:
+  int nmax, nlog;
+  V iv;
+  L il;
+  vector<V> tree;
+  vector<L> lazy;
+  vector<int> sz;
+
+  bool Check(int x, int shift) {
+    int y = (x >> shift) << shift;
+    return x != y;
+  }
+
+  void Apply(int node, const L& lzy) {
+    tree[node] = APPLY{}(tree[node], lzy, sz[node]);
+    if (node < nmax) lazy[node] = COMPO{}(lazy[node], lzy);
+  }
+
+  void Push(int node) {
+    if (lazy[node] == il) return;
+    Apply(node << 1, lazy[node]);
+    Apply(node << 1 | 1, lazy[node]);
+    lazy[node] = il;
+  }
+
+  void Pull(int node) {
+    tree[node] = OP{}(tree[node << 1], tree[node << 1 | 1]);
+  }
+};
+
+constexpr int kMax = 1e5;
+
+struct Node {
+  Node() : minn(0), maxx(kMax) {}
+  Node(int x) : minn(x), maxx(x) {}
+
+  int minn, maxx;
+};
+
+struct Lazy {
+  Lazy() : minn(0), maxx(kMax) {}
+  Lazy(int a, int b) : minn(a), maxx(b) {}
+
+  int minn, maxx;
+
+  constexpr bool operator==(const Lazy& other) const {
+    return minn == other.minn && maxx == other.maxx;
+  }
+};
+
+struct FOp {
+  Node operator()(const Node& a, const Node& b) const {
+    Node res;
+    res.minn = min<int>(a.minn, b.minn);
+    res.maxx = max<int>(a.maxx, b.maxx);
+    return res;
+  }
+};
+
+struct FApply {
+  Node operator()(const Node& a, const Lazy& b, int sz) const {
+    Node res;
+    res.minn = clamp<int>(a.minn, b.minn, b.maxx);
+    res.maxx = clamp<int>(a.maxx, b.minn, b.maxx);
+    return res;
+  }
+};
+
+struct FCompo {
+  Lazy operator()(const Lazy& a, const Lazy& b) const {
+    Lazy res;
+    res.minn = clamp<int>(a.minn, b.minn, b.maxx);
+    res.maxx = clamp<int>(a.maxx, b.minn, b.maxx);
+    return res;
+  }
+};
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n, m;
+  cin >> n >> m;
+
+  LazySegTree<Node, Lazy, FOp, FApply, FCompo> seg;
+  seg.Init(n, Node(), Lazy());
+  for (int i = 0; i < n; i++) seg.Set(i, 0);
+  seg.Build();
+
+  while (m--) {
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
+    if (a == 1) {
+      seg.Update(b, c, Lazy(d, kMax));
+    } else {
+      seg.Update(b, c, Lazy(0, d));
+    }
+  }
+
+  auto res = seg.PushAll(0, n - 1);
+  for (auto e : res) cout << e << "\n";
+
+  return 0;
+}
