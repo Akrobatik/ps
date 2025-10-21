fa8cfd4c7e48ae423cdea758ffc193b4676498f1
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-10-22 01:19:53 +0900

    33310 - Minimal Coprime

---
+++ BaekJoon/33000/33310.cpp
@@ -0,0 +1,23 @@
+// Title : Minimal Coprime
+// Link  : https://www.acmicpc.net/problem/33310 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int t;
+  cin >> t;
+  while (t--) {
+    int l, r;
+    cin >> l >> r;
+    cout << max<int>(r - l, l == 1) << "\n";
+  }
+
+  return 0;
+}
