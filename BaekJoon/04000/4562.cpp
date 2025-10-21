cccbaa601ddce6e73b0ed7b654cdb7f71dd1a53b
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-10-22 06:34:35 +0900

    4562 - No Brainer

---
+++ BaekJoon/04000/4562.cpp
@@ -0,0 +1,23 @@
+// Title : No Brainer
+// Link  : https://www.acmicpc.net/problem/4562 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int t;
+  cin >> t;
+  while (t--) {
+    int64_t x, y;
+    cin >> x >> y;
+    cout << (x >= y ? "MMM BRAINS\n" : "NO BRAINS\n");
+  }
+
+  return 0;
+}
