7b5c478954ef4c867f55f16f95cd368693b72164
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-10-23 17:18:31 +0900

    1264 - 모음의 개수

---
+++ BaekJoon/01000/1264.cpp
@@ -0,0 +1,28 @@
+// Title : 모음의 개수
+// Link  : https://www.acmicpc.net/problem/1264 
+// Time  : 0 ms
+// Memory: 2024 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+constexpr const char kMask[] = "aeiou";
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  string s;
+  while (getline(cin, s) && s != "#") {
+    int cnt = 0;
+    for (auto c : s) {
+      for (int i = 0; i < 5; i++) {
+        if (isalpha(c)) cnt += (tolower(c) == kMask[i]);
+      }
+    }
+    cout << cnt << "\n";
+  }
+
+  return 0;
+}
