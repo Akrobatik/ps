487c0313589694e67a460d5df3e38a089a68493b
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-10-24 23:15:43 +0900

    2420 - 사파리월드

---
+++ BaekJoon/02000/2420.cpp
@@ -0,0 +1,19 @@
+// Title : 사파리월드
+// Link  : https://www.acmicpc.net/problem/2420 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int64_t n, m;
+  cin >> n >> m;
+  cout << abs(n - m);
+
+  return 0;
+}
