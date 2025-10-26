50e0559594acbe4f1a98ccdd513593d180b556e3
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-10-26 14:01:43 +0900

    1977 - 완전제곱수

---
+++ BaekJoon/01000/1977.cpp
@@ -0,0 +1,37 @@
+// Title : 완전제곱수
+// Link  : https://www.acmicpc.net/problem/1977 
+// Time  : 0 ms
+// Memory: 2032 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int l, r;
+  cin >> l >> r;
+
+  int minn = INT_MAX;
+  int64_t sum = 0;
+  for (int i = l; i <= r; i++) {
+    int s = sqrt(i);
+    while (s * s < i) ++s;
+
+    if (s * s == i) {
+      minn = min<int>(minn, i);
+      sum += i;
+    }
+  }
+
+  if (minn == INT_MAX) {
+    cout << "-1";
+  } else {
+    cout << sum << "\n"
+         << minn;
+  }
+
+  return 0;
+}
