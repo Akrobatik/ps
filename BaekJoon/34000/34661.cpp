d360fdfd2389be71a041ae5958189c12464ac79e
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-10-27 11:46:39 +0900

    34661 - 고령 멜빙 축제

---
+++ BaekJoon/34000/34661.cpp
@@ -0,0 +1,27 @@
+// Title : 고령 멜빙 축제
+// Link  : https://www.acmicpc.net/problem/34661 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int t;
+  cin >> t;
+  while (t--) {
+    int n, m;
+    cin >> n >> m;
+
+    int nm = n * m, cnt = 0;
+    char c;
+    while (nm--) cin >> c, cnt += (c == '.');
+    cout << ((cnt & 1) ? "sewon\n" : "pizza\n");
+  }
+
+  return 0;
+}
