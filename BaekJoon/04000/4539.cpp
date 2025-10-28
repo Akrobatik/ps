ac09541a76e69e9ed7eceb8172cf2dc045798b0b
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-10-29 03:36:42 +0900

    4539 - 반올림

---
+++ BaekJoon/04000/4539.cpp
@@ -0,0 +1,28 @@
+// Title : 반올림
+// Link  : https://www.acmicpc.net/problem/4539 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int t;
+  cin >> t;
+  while (t--) {
+    int64_t x;
+    cin >> x;
+    for (int64_t d = 10;; d *= 10) {
+      if (x < d) break;
+      int64_t r = x % d;
+      x = x - r + ((r << 1) >= d ? d : 0);
+    }
+    cout << x << "\n";
+  }
+
+  return 0;
+}
