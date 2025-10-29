43e88589c7164b92a29766cf61b62727faeadbcf
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-10-30 03:49:56 +0900

    6679 - 싱기한 네자리 숫자

---
+++ BaekJoon/06000/6679.cpp
@@ -0,0 +1,32 @@
+// Title : 싱기한 네자리 숫자
+// Link  : https://www.acmicpc.net/problem/6679 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+constexpr int kBase[] = {10, 12, 16};
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  for (int i = 1000; i < 10000; i++) {
+    int x = 0;
+    bool ok = true;
+    for (auto b : kBase) {
+      int v = i, s = 0;
+      while (v > 0) s += v % b, v /= b;
+      if (x && x != s) {
+        ok = false;
+        break;
+      }
+      x = s;
+    }
+    if (ok) cout << i << "\n";
+  }
+
+  return 0;
+}
