9348dd8e72fbf859795791b175d4082cd91b9fc3
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-10-30 05:05:10 +0900

    1954 - 화학실험

---
+++ BaekJoon/01000/1954.cpp
@@ -0,0 +1,50 @@
+// Title : 화학실험
+// Link  : https://www.acmicpc.net/problem/1954 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  cin >> n;
+
+  priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+  for (int i = 0; i < n; i++) {
+    int a, b;
+    cin >> a >> b;
+    pq.push({a + b, a});
+  }
+
+  int m;
+  cin >> m;
+
+  if (n > m) {
+    cout << "0";
+    return 0;
+  }
+  m -= n;
+
+  while (m--) {
+    auto [s, a] = pq.top();
+    pq.pop();
+
+    pq.push({s + a, a});
+  }
+
+  bool ok = true;
+  int x = pq.top().first;
+  for (int i = 0; ok && i < n; i++) {
+    auto [s, a] = pq.top();
+    pq.pop();
+    ok = (s == x);
+  }
+  cout << (ok ? x : 0);
+
+  return 0;
+}
