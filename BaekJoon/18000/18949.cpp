25c94e7eb0e89474a7b73c5954b12c8623b5a968
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-10-30 12:48:35 +0900

    18949 - Intersection of Parabolas

---
+++ BaekJoon/18000/18949.cpp
@@ -0,0 +1,19 @@
+// Title : Intersection of Parabolas
+// Link  : https://www.acmicpc.net/problem/18949 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int64_t a;
+  cin >> a;
+  cout << a * 4 - 1 << ".6666666667";
+
+  return 0;
+}
