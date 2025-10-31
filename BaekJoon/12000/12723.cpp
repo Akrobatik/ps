71646cd4b6f4c1bd7f5eb1a6ab6c2b38d0c4858d
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-10-31 20:22:24 +0900

    12723 - Minimum Scalar Product (Small)

---
+++ BaekJoon/12000/12723.cpp
@@ -0,0 +1,33 @@
+// Title : Minimum Scalar Product (Small)
+// Link  : https://www.acmicpc.net/problem/12723 
+// Time  : 0 ms
+// Memory: 2024 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int t;
+  cin >> t;
+  for (int tc = 1; tc <= t; tc++) {
+    int n;
+    cin >> n;
+    vector<int> arr(n), brr(n);
+    for (auto& e : arr) cin >> e;
+    for (auto& e : brr) cin >> e;
+    sort(arr.begin(), arr.end());
+    sort(brr.begin(), brr.end(), greater<int>());
+
+    int64_t sum = 0;
+    for (int i = 0; i < n; i++) {
+      sum += (int64_t)arr[i] * brr[i];
+    }
+    cout << "Case #" << tc << ": " << sum << "\n";
+  }
+
+  return 0;
+}
