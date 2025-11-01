510a09de924c0d8a5d9b64c2827b27b85079a6b8
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-11-01 10:29:10 +0900

    32775 - 가희와 4시간의 벽 1

---
+++ BaekJoon/32000/32775.cpp
@@ -0,0 +1,19 @@
+// Title : 가희와 4시간의 벽 1
+// Link  : https://www.acmicpc.net/problem/32775 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int a, b;
+  cin >> a >> b;
+  cout << (a <= b ? "high speed rail" : "flight");
+
+  return 0;
+}
