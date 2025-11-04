6f60d8d8e1d577f26c9d819bc0f344bfcfd8d59c
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-11-04 15:15:49 +0900

    34648 - Armageddon

--- BaekJoon/34000/34648.cpp
+++ BaekJoon/34000/34648.cpp
@@ -1,141 +1,45 @@
 // Title : Armageddon
 // Link  : https://www.acmicpc.net/problem/34648 
-// Time  : 8 ms
+// Time  : 12 ms
 // Memory: 2020 KB
 
 #include <bits/stdc++.h>
 
 using namespace std;
 
-// https://github.com/Akrobatik/ps/blob/main/template/modint.cpp
-template <int32_t MOD>
-struct ModInt32 {
-  using CT = conditional_t<MOD <= numeric_limits<int32_t>::max() / 2, int32_t, int64_t>;
-
-  constexpr ModInt32() : val(0) {}
-  constexpr ModInt32(int32_t x) : val((x %= MOD) < 0 ? x + MOD : x) {}
-  constexpr ModInt32(int64_t x) : val((x %= MOD) < 0 ? x + MOD : x) {}
-  constexpr ModInt32(__int128_t x) : val((x %= MOD) < 0 ? x + MOD : x) {}
-
-  constexpr ModInt32 Pow(int64_t exp) const {
-    int64_t n = val, x = 1;
-    while (exp) {
-      if (exp & 1) x = x * n % MOD;
-      n = n * n % MOD;
-      exp >>= 1;
-    }
-
-    ModInt32 res;
-    res.val = x;
-    return res;
-  }
-
-  constexpr ModInt32 Inv() const {
-    return Pow(MOD - 2);
-  }
-
-  constexpr ModInt32& operator++() {
-    if (++val == MOD) val = 0;
-    return *this;
-  }
-
-  constexpr ModInt32 operator++(int) {
-    ModInt32 tmp(*this);
-    operator++();
-    return tmp;
-  }
-
-  constexpr ModInt32& operator+=(const ModInt32& other) {
-    CT x = (CT)val + other.val;
-    if (x >= MOD) x -= MOD;
-    val = x;
-    return *this;
-  }
-
-  constexpr ModInt32& operator-=(const ModInt32& other) {
-    CT x = (CT)val + (MOD - other.val);
-    if (x >= MOD) x -= MOD;
-    val = x;
-    return *this;
-  }
-
-  constexpr ModInt32& operator*=(const ModInt32& other) {
-    val = (int64_t)val * other.val % MOD;
-    return *this;
-  }
-
-  constexpr ModInt32& operator/=(const ModInt32& other) {
-    *this *= other.Inv();
-    return *this;
-  }
-
-  constexpr ModInt32 operator-() const {
-    ModInt32 res;
-    if (val) res.val = MOD - val;
-    return res;
-  }
-
-  constexpr ModInt32 operator+(const ModInt32& rhs) const {
-    return ModInt32(*this) += rhs;
-  }
-
-  constexpr ModInt32 operator-(const ModInt32& rhs) const {
-    return ModInt32(*this) -= rhs;
-  }
-
-  constexpr ModInt32 operator*(const ModInt32& rhs) const {
-    return ModInt32(*this) *= rhs;
-  }
-
-  constexpr ModInt32 operator/(const ModInt32& rhs) const {
-    return ModInt32(*this) /= rhs;
-  }
-
-  constexpr bool operator!() const {
-    return val == 0;
-  }
-
-  friend istream& operator>>(istream& is, ModInt32& num) {
-    is >> num.val;
-    if ((num.val %= MOD) < 0) num.val += MOD;
-    return is;
-  }
-
-  friend ostream& operator<<(ostream& os, const ModInt32& num) {
-    os << num.val;
-    return os;
-  }
-
-  int32_t val;
-};
-
 constexpr int kMod = 1e9 + 7;
 
-using ModInt = ModInt32<kMod>;
-constexpr ModInt kInv4 = 250000002;
+int64_t Pow(int64_t a, int64_t exp) {
+  int64_t res = 1;
+  while (exp) {
+    if (exp & 1) res = res * a % kMod;
+    a = a * a % kMod;
+    exp >>= 1;
+  }
+  return res;
+}
 
 int main() {
-  ios::sync_with_stdio(false);
-  cin.tie(nullptr);
-
   int64_t p, q, n;
   cin >> p >> q >> n;
 
-  for (int i = 1; i <= min<int>(n, 2); i++) {
-    cout << (i == 1 ? 0 : 1) << " ";
+  int64_t limit = n;
+  if (p > q) limit = min<int64_t>(limit, ((q << 1) / (p - q) + 1) << 1);
+
+  int64_t x = 0, y = 0, xs = 0, ys = 0;
+  for (int i = 1; i <= limit; i++) {
+    if (i & 1) {
+      xs += ++x;
+    } else {
+      ys += ++y;
+    }
+    cout << (xs * ys) % kMod << ' ';
   }
 
-  int64_t x = 1, y = 1;
-  ModInt zs = 1, zm = ModInt(p) / ModInt(q);
-  for (int i = 3; i <= n; i++) {
-    if (x > y) swap(x, y);
-    int64_t pp = x + 2, qq = x;
-    if (pp * q > p * qq) {
-      ++x;
-    } else {
-      zs *= zm;
-    }
-    cout << zs * ModInt(x * (x + 1)) * ModInt(y * (y + 1)) * kInv4 << " ";
+  int64_t zs = xs * ys % kMod, zm = p * Pow(q, kMod - 2) % kMod;
+  for (int i = limit + 1; i <= n; i++) {
+    zs = zs * zm % kMod;
+    cout << zs << ' ';
   }
 
   return 0;
