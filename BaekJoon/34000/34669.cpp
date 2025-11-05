58293ebcad39cf37be52c1502cf4236843613e4c
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-11-05 12:30:15 +0900

    34669 - 가희와 노선 건설 놀이 2

---
+++ BaekJoon/34000/34669.cpp
@@ -0,0 +1,23 @@
+// Title : 가희와 노선 건설 놀이 2
+// Link  : https://www.acmicpc.net/problem/34669 
+// Time  : 32 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int t;
+  cin >> t;
+  while (t--) {
+    int64_t a, b;
+    cin >> a >> b;
+    cout << a * b << " " << a / b << "\n";
+  }
+
+  return 0;
+}
