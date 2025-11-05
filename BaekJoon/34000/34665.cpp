38d4844707ad6af3691cc10d027834e9f879836f
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-11-05 12:29:40 +0900

    34665 - 가희와 교통 요금

---
+++ BaekJoon/34000/34665.cpp
@@ -0,0 +1,19 @@
+// Title : 가희와 교통 요금
+// Link  : https://www.acmicpc.net/problem/34665 
+// Time  : 0 ms
+// Memory: 2024 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  string s1, s2;
+  cin >> s1 >> s2;
+  cout << (s1 == s2 ? 0 : 1550);
+
+  return 0;
+}
