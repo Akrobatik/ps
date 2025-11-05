fc56a6b85fecba7f134c7569bfa0abea41fd3328
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-11-05 12:30:04 +0900

    34667 - 가희와 철도역

---
+++ BaekJoon/34000/34667.cpp
@@ -0,0 +1,19 @@
+// Title : 가희와 철도역
+// Link  : https://www.acmicpc.net/problem/34667 
+// Time  : 0 ms
+// Memory: 2024 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  string s;
+  cin >> s;
+  cout << s[0] << s;
+
+  return 0;
+}
