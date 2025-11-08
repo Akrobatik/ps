e42159c6a7baea9b04dd3d4a9760930f8bcf53f5
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-11-09 01:37:04 +0900

    34721 - 역사를 걸으면 동국이 보이고

---
+++ BaekJoon/34000/34721.cpp
@@ -0,0 +1,19 @@
+// Title : 역사를 걸으면 동국이 보이고
+// Link  : https://www.acmicpc.net/problem/34721 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  cin >> n;
+  while (n--) cout << "I love DGU\n";
+
+  return 0;
+}
