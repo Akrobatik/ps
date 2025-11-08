8c414a707cd8d990916ded1750d39db330d905bf
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-11-09 01:37:16 +0900

    34722 - 출제자가 몇 명

---
+++ BaekJoon/34000/34722.cpp
@@ -0,0 +1,25 @@
+// Title : 출제자가 몇 명
+// Link  : https://www.acmicpc.net/problem/34722 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  cin >> n;
+  int cnt = 0;
+  while (n--) {
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
+    cnt += (a >= 1000 || b >= 1600 || c >= 1500 || (1 <= d && d <= 30));
+  }
+  cout << cnt;
+
+  return 0;
+}
