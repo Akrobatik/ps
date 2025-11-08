730ef1d7e82b1cfcf14139796c483ac7f98e4490
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-11-09 01:37:32 +0900

    34724 - 종양 찾기

---
+++ BaekJoon/34000/34724.cpp
@@ -0,0 +1,40 @@
+// Title : 종양 찾기
+// Link  : https://www.acmicpc.net/problem/34724 
+// Time  : 8 ms
+// Memory: 3100 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+constexpr pair<int, int> kDelta[] = {
+    {-1, -1}, {-1, 0}, {0, -1}, {0, 0}};
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n, m;
+  cin >> n >> m;
+  vector<string> mat(n);
+  for (auto& s : mat) {
+    cin >> s;
+    for (auto& c : s) c ^= '0';
+  }
+
+  for (int i = 1; i < n; i++) {
+    for (int j = 1; j < m; j++) {
+      int cnt = 0;
+      for (auto [dy, dx] : kDelta) {
+        cnt += mat[i + dy][j + dx];
+      }
+      if (cnt == 4) {
+        cout << "1";
+        return 0;
+      }
+    }
+  }
+  cout << "0";
+
+  return 0;
+}
