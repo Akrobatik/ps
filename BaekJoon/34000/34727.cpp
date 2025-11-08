4fa030a4480f9e4389ff6cd371820baeb1dcd2ff
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-11-09 01:37:53 +0900

    34727 - 큐브 채우기

---
+++ BaekJoon/34000/34727.cpp
@@ -0,0 +1,23 @@
+// Title : 큐브 채우기
+// Link  : https://www.acmicpc.net/problem/34727 
+// Time  : 16 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int t;
+  cin >> t;
+  while (t--) {
+    int n;
+    cin >> n;
+    cout << (n & 1) << "\n";
+  }
+
+  return 0;
+}
