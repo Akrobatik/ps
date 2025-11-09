870d98044999fed7371d94ea0920b0eb075d3352
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-11-09 23:15:13 +0900

    34748 - K Network Stations

--- BaekJoon/34000/34748.cpp
+++ BaekJoon/34000/34748.cpp
@@ -1,7 +1,7 @@
 // Title : K Network Stations
 // Link  : https://www.acmicpc.net/problem/34748 
-// Time  : 44 ms
-// Memory: 10948 KB
+// Time  : 68 ms
+// Memory: 16308 KB
 
 #include <bits/stdc++.h>
 
@@ -11,7 +11,7 @@ int main() {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
 
-  int n, k;
+  int64_t n, k;
   cin >> n >> k;
 
   vector<vector<pair<int, int>>> g(n + 1);
@@ -22,51 +22,36 @@ int main() {
     g[v].push_back({u, w});
   }
 
+  vector<int64_t> cnt(n + 1), wsum(n + 1), vsum(n + 1);
+  [&](this auto&& self, int cur, int par) -> void {
+    cnt[cur] = 1;
+    for (auto [nxt, w] : g[cur]) {
+      if (nxt == par) continue;
+      self(nxt, cur);
+      cnt[cur] += cnt[nxt];
+      wsum[cur] += wsum[nxt] + cnt[nxt] * w;
+      vsum[cur] += vsum[nxt] + cnt[nxt] * (n - cnt[nxt]) * w;
+    }
+  }(1, 0);
+
   if (k == 1) {
-    int64_t all = 0;
-    [&](this auto&& self, int cur, int par) -> int {
-      int res = 1;
-      for (auto [nxt, w] : g[cur]) {
-        if (nxt == par) continue;
-        int val = self(nxt, cur);
-        all += (int64_t)val * (n - val) * w;
-        res += val;
-      }
-      return res;
-    }(1, 0);
-
-    cout << all;
+    cout << vsum[1];
   } else {
-    int64_t maxx = 0, mu, mv, cu, cv;
-    [&](this auto&& self, int cur, int par) -> int {
-      int res = 1;
+    int64_t minn = INT64_MAX;
+
+    [&](this auto&& self, int cur, int par, int64_t v1, int64_t v2) -> void {
       for (auto [nxt, w] : g[cur]) {
         if (nxt == par) continue;
-        int val = self(nxt, cur);
+        int64_t nv1 = v1 + cnt[nxt] * w, nv2 = v2 + (n - cnt[nxt]) * w;
+        self(nxt, cur, nv1, nv2);
 
-        int64_t lrv = (int64_t)val * (n - val) * w;
-        if (maxx < lrv) maxx = lrv, mu = nxt, mv = cur, cu = val, cv = n - val;
-        res += val;
+        int64_t av = vsum[nxt] - wsum[nxt] * (n - cnt[nxt]);
+        int64_t bv = vsum[1] - vsum[nxt] - cnt[nxt] * (wsum[1] - wsum[nxt] + nv2 - nv1);
+        minn = min<int64_t>(minn, max<int64_t>(av, bv));
       }
-      return res;
-    }(1, 0);
+    }(1, 0, 0, 0);
 
-    auto Calc = [&](this auto&& self, int u, int p, int s) -> int {
-      int64_t all = 0;
-      [&](this auto&& self, int cur, int par) -> int {
-        int res = 1;
-        for (auto [nxt, w] : g[cur]) {
-          if (nxt == par) continue;
-          int val = self(nxt, cur);
-          all += (int64_t)val * (s - val) * w;
-          res += val;
-        }
-        return res;
-      }(u, p);
-      return all;
-    };
-
-    cout << max<int64_t>(Calc(mu, mv, cu), Calc(mv, mu, cv));
+    cout << minn;
   }
 
   return 0;
