42cd445fe21241d8aa91583148af2c67bc1817c7
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-11-14 00:32:56 +0900

    29111 - Последовательности

---
+++ BaekJoon/29000/29111.cpp
@@ -0,0 +1,167 @@
+// Title : Последовательности
+// Link  : https://www.acmicpc.net/problem/29111 
+// Time  : 4 ms
+// Memory: 4108 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int nmax;
+
+struct DLX {
+  void Init(int cols, span<vector<int>> covers) {
+    int total = cols + 1;
+    for (auto& e : covers) total += e.size();
+
+    id.clear(), id.resize(total);
+    size.clear(), size.resize(cols + 1);
+    head.clear(), head.resize(total);
+    up.clear(), up.resize(total);
+    down.clear(), down.resize(total);
+    left.clear(), left.resize(total);
+    right.clear(), right.resize(total);
+
+    // Root
+    left[0] = cols;
+    right[0] = 1;
+
+    // Columns
+    int l = 0, r = 2;
+    for (int i = 1; i <= cols; i++) {
+      id[i] = l;
+      size[i] = 0;
+      head[i] = up[i] = down[i] = i;
+      left[i] = l++;
+      right[i] = r++;
+    }
+    right[cols] = 0;
+
+    // Covers
+    int cur = cols + 1;
+    int rows = covers.size();
+    for (int rid = 0; rid < rows; rid++) {
+      int n = covers[rid].size();
+      if (!n) continue;
+
+      for (int _cid : covers[rid]) {
+        int cid = _cid + 1;
+        if (down[cid] == cid) down[cid] = cur;
+        ++size[cid];
+        id[cur] = rid;
+        down[up[cid]] = cur;
+        up[cur] = up[cid];
+        up[cid] = cur;
+        head[cur] = down[cur] = cid;
+        left[cur] = cur - 1;
+        right[cur] = cur + 1;
+        ++cur;
+      }
+      left[cur - n] += n;
+      right[cur - 1] -= n;
+    }
+
+    // Cover childless columns
+    for (int i = 1; i <= cols; i++) {
+      if (size[i]) continue;
+      right[left[i]] = right[i];
+      left[right[i]] = left[i];
+    }
+  }
+
+  void CoverColumn(int col) {
+    right[left[col]] = right[col];
+    left[right[col]] = left[col];
+    for (int row = down[col]; row != col; row = down[row]) {
+      for (int node = right[row]; node != row; node = right[node]) {
+        down[up[node]] = down[node];
+        up[down[node]] = up[node];
+        --size[head[node]];
+      }
+    }
+  }
+
+  void UncoverColumn(int col) {
+    for (int row = up[col]; row != col; row = up[row]) {
+      for (int node = left[row]; node != row; node = left[node]) {
+        ++size[head[node]];
+        down[up[node]] = node;
+        up[down[node]] = node;
+      }
+    }
+    right[left[col]] = col;
+    left[right[col]] = col;
+  }
+
+  int FindMinColumn() {
+    int min_col = right[0];
+    int minn = size[min_col];
+    for (int col = right[min_col]; minn != 1 && col; col = right[col]) {
+      if (minn > size[col]) min_col = col, minn = size[col];
+    }
+    return min_col;
+  }
+
+  bool Search() {
+    if (solution.size() == nmax) return true;
+    int col = FindMinColumn();
+    CoverColumn(col);
+    for (int row = down[col]; row != col; row = down[row]) {
+      solution.push_back(id[row]);
+      for (int node = right[row]; node != row; node = right[node]) CoverColumn(head[node]);
+      if (Search()) return true;
+      for (int node = left[row]; node != row; node = left[node]) UncoverColumn(head[node]);
+      solution.pop_back();
+    }
+    UncoverColumn(col);
+    return false;
+  }
+
+  vector<int> id, size;
+  vector<int> head, up, down, left, right;
+  vector<int> solution;
+};
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  cin >> n;
+  nmax = n;
+
+  int cols = n * 3 + 1;
+  int rows = n * (n + 1) * 2;
+
+  auto PV = [&](int id) -> pair<int, int> {
+    int p = id / (n + 1);
+    int v = id % (n + 1) + 1;
+    return {p, v};
+  };
+
+  vector<vector<int>> covers(rows);
+  for (int i = 0; i < rows; i++) {
+    auto& cvr = covers[i];
+    auto [p, v] = PV(i);
+    if (p + v < (n << 1)) {
+      cvr.push_back(p);
+      cvr.push_back(p + v);
+      cvr.push_back((n << 1) + v - 1);
+    }
+  }
+
+  DLX dlx;
+  dlx.Init(cols, covers);
+  if (dlx.Search()) {
+    vector<int> arr(n << 1);
+    for (auto id : dlx.solution) {
+      auto [p, v] = PV(id);
+      arr[p] = arr[p + v] = v;
+    }
+    for (auto e : arr) cout << e << " ";
+  } else {
+    cout << "-1";
+  }
+
+  return 0;
+}
