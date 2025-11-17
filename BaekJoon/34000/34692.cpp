84ae4c716d58e27da881570e2134997ef094c860
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-11-17 14:59:36 +0900

    34692 - 아 마이마이 하고 싶다

---
+++ BaekJoon/34000/34692.cpp
@@ -0,0 +1,34 @@
+// Title : 아 마이마이 하고 싶다
+// Link  : https://www.acmicpc.net/problem/34692 
+// Time  : 0 ms
+// Memory: 2156 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int64_t n, m, k;
+  cin >> n >> m >> k;
+
+  priority_queue<int64_t, vector<int64_t>, greater<int64_t>> pq;
+  while (m--) pq.push(0);
+
+  while (n--) {
+    int64_t x;
+    cin >> x;
+
+    x += pq.top();
+    pq.pop();
+
+    pq.push(x);
+  }
+
+  int64_t x = pq.top();
+  cout << (x <= k ? "WAIT" : "GO");
+
+  return 0;
+}
