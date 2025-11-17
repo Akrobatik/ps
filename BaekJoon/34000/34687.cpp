763edfd11b04edfff4a2604db93095cfc8556749
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-11-17 19:53:42 +0900

    34687 - 라면 끓여 먹자 야호

---
+++ BaekJoon/34000/34687.cpp
@@ -0,0 +1,19 @@
+// Title : 라면 끓여 먹자 야호
+// Link  : https://www.acmicpc.net/problem/34687 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int64_t n, m;
+  cin >> n >> m;
+  cout << (n * 81 <= m * 100 ? "yaho" : "no");
+
+  return 0;
+}
