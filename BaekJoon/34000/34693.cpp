567e53e05f43f9b4157327a4a30d9e85188a6b32
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-11-17 14:59:46 +0900

    34693 - 마법소녀 코이

---
+++ BaekJoon/34000/34693.cpp
@@ -0,0 +1,30 @@
+// Title : 마법소녀 코이
+// Link  : https://www.acmicpc.net/problem/34693 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int t;
+  cin >> t;
+  while (t--) {
+    int64_t x;
+    cin >> x;
+
+    int64_t y = (x + 1) >> 1;
+    if (x & 1) {
+      cout << y + 2 << " - " << y + 1 << " - 2\n";
+    } else {
+      int64_t y = x >> 1;
+      cout << y + 1 << " - " << y << " - 1\n";
+    }
+  }
+
+  return 0;
+}
