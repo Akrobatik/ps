a0343324f2b4f226ee9b351e339bb2fbc39d75f5
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-11-17 14:59:23 +0900

    34691 - 대전과학고등학교를 사랑하십니까?

---
+++ BaekJoon/34000/34691.cpp
@@ -0,0 +1,32 @@
+// Title : 대전과학고등학교를 사랑하십니까?
+// Link  : https://www.acmicpc.net/problem/34691 
+// Time  : 0 ms
+// Memory: 2024 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  string s;
+  while (cin >> s) {
+    switch (s[0]) {
+      case 'a': {
+        cout << "Panthera tigris\n";
+      } break;
+
+      case 't': {
+        cout << "Pinus densiflora\n";
+      } break;
+
+      case 'f': {
+        cout << "Forsythia koreana\n";
+      } break;
+    }
+  }
+
+  return 0;
+}
