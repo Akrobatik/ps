0f17cbba5b74b24b5b35883f768ed6240ae1d0e3
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-11-19 17:59:26 +0900

    3256 - 비행기

---
+++ BaekJoon/03000/3256.cpp
@@ -0,0 +1,51 @@
+// Title : 비행기
+// Link  : https://www.acmicpc.net/problem/3256 
+// Time  : 4 ms
+// Memory: 2152 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using tup = tuple<int, int, int>;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  cin >> n;
+
+  list<tup> ls;
+  for (int i = 0; i < n; i++) {
+    int x;
+    cin >> x;
+    ls.push_back({x, 0, 5});
+  }
+
+  for (int t = 0;; t++) {
+    auto it = ls.begin();
+    while (it != ls.end()) {
+      auto& [g, p, r] = *it;
+      if (g != p) {
+        if (it == ls.begin() || get<1>(*prev(it)) > p + 1) {
+          ++p;
+        }
+        ++it;
+      } else {
+        if (--r == 0) {
+          it = ls.erase(it);
+        } else {
+          ++it;
+        }
+      }
+    }
+
+    if (ls.empty()) {
+      cout << t;
+      break;
+    }
+  }
+
+  return 0;
+}
