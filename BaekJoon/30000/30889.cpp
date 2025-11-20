87c201a2e32abbc480b8749f14a112f21b620c24
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-11-20 11:05:32 +0900

    30889 - 좌석 배치도

---
+++ BaekJoon/30000/30889.cpp
@@ -0,0 +1,29 @@
+// Title : 좌석 배치도
+// Link  : https://www.acmicpc.net/problem/30889 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  vector<string> mat(10, string(20, '.'));
+
+  int n;
+  cin >> n;
+  while (n--) {
+    char cy;
+    int x;
+    cin >> cy >> x, --x;
+    int y = cy - 'A';
+    mat[y][x] = 'o';
+  }
+
+  for (auto& s : mat) cout << s << "\n";
+
+  return 0;
+}
