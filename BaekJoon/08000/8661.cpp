292f3044c2da21f8ec1f7ce011ee3a363f2fdf5f
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-11-21 15:47:19 +0900

    8661 - Stopki

---
+++ BaekJoon/08000/8661.cpp
@@ -0,0 +1,22 @@
+// Title : Stopki
+// Link  : https://www.acmicpc.net/problem/8661 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int64_t x, k, a;
+  cin >> x >> k >> a;
+
+  int64_t ka = k + a;
+  x %= ka;
+  cout << (x < k);
+
+  return 0;
+}
