edd4f72dc033dc2d74b0d56021e5f7186dd9dcf0
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-11-25 01:11:08 +0900

    34750 - 추석은 언제나 좋아

---
+++ BaekJoon/34000/34750.cpp
@@ -0,0 +1,29 @@
+// Title : 추석은 언제나 좋아
+// Link  : https://www.acmicpc.net/problem/34750 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int64_t n;
+  cin >> n;
+
+  int64_t m = n / 20;
+  if (n >= 1000000) {
+    cout << m * 4 << " " << m * 16;
+  } else if (n >= 500000) {
+    cout << m * 3 << " " << m * 17;
+  } else if (n >= 100000) {
+    cout << m * 2 << " " << m * 18;
+  } else {
+    cout << m * 1 << " " << m * 19;
+  }
+
+  return 0;
+}
