f6f6615590bd1ab436167e3c8743c41d09022fb6
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-11-26 03:53:05 +0900

    34762 - 꿈

---
+++ BaekJoon/34000/34762.cpp
@@ -0,0 +1,34 @@
+// Title : 꿈
+// Link  : https://www.acmicpc.net/problem/34762 
+// Time  : 16 ms
+// Memory: 3156 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n, k, m;
+  cin >> n >> k >> m;
+
+  vector<int> arr(m);
+  for (auto& e : arr) cin >> e;
+
+  vector<pair<int, int>> axr;
+  for (int i = 1; i < m; i++) {
+    int a = arr[i - 1], b = arr[i];
+    if (a + 1 < b) axr.push_back({a + 1, b - 1});
+  }
+  if (arr.back() != n) axr.push_back({arr.back() + 1, n});
+
+  bool ok = true;
+  for (auto [l, r] : axr) {
+    ok &= (r - l + 1 > k);
+  }
+  cout << (ok ? "YES" : "NO");
+
+  return 0;
+}
