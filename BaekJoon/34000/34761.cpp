1977e8b1fe0a71cd23d8a0cfd396d79fbc1a952f
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-11-26 03:53:14 +0900

    34761 - 같은 배열

---
+++ BaekJoon/34000/34761.cpp
@@ -0,0 +1,36 @@
+// Title : 같은 배열
+// Link  : https://www.acmicpc.net/problem/34761 
+// Time  : 56 ms
+// Memory: 4564 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  cin >> n;
+
+  vector<int> arr(n), brr(n << 1);
+  for (auto& e : arr) cin >> e;
+  for (auto& e : brr) cin >> e;
+
+  bool ok = true;
+  for (int i = 0; ok && i < n; i++) {
+    ok = (arr[i] == brr[i]);
+  }
+
+  vector<int8_t> table(n + 1);
+  for (auto e : arr) table[e] = 1;
+
+  for (int i = 0; ok && i < n; i++) {
+    ok = table[brr[i + n]];
+  }
+
+  cout << (ok ? "YES" : "NO");
+
+  return 0;
+}
