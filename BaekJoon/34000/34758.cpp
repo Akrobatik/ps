b86470d7e1a21c254c022ac801daeb9af3f0ab08
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-11-26 03:53:56 +0900

    34758 - KUPC에 어서 오세요

---
+++ BaekJoon/34000/34758.cpp
@@ -0,0 +1,23 @@
+// Title : KUPC에 어서 오세요
+// Link  : https://www.acmicpc.net/problem/34758 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int x, y, t;
+  cin >> x >> y >> t;
+  while (t--) {
+    int xx, yy;
+    cin >> xx >> yy;
+    cout << max<int>((x != xx) + (y != yy) - 1, 0) << "\n";
+  }
+
+  return 0;
+}
