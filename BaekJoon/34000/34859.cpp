1ad33191be1bb71e60d201d40c9f5a9969dceb8a
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-11-30 06:40:01 +0900

    34859 - KHU와 DKU

---
+++ BaekJoon/34000/34859.cpp
@@ -0,0 +1,36 @@
+// Title : KHU와 DKU
+// Link  : https://www.acmicpc.net/problem/34859 
+// Time  : 44 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int t;
+  cin >> t;
+  while (t--) {
+    int n, d, h, k, u;
+    cin >> n >> d >> h >> k >> u;
+
+    cout << "YES\n";
+    if (h <= n && d <= n) {
+      while (d--) cout << 'D';
+      while (k--) cout << 'K';
+      while (u--) cout << 'U';
+      while (h--) cout << 'H';
+    } else {
+      while (k--) cout << 'K';
+      while (d--) cout << 'D';
+      while (h--) cout << 'H';
+      while (u--) cout << 'U';
+    }
+    cout << "\n";
+  }
+
+  return 0;
+}
