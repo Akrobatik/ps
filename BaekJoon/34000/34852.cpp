a69ae37a3fff9f481a11bed5957c03a54eeef819
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-11-29 17:50:35 +0900

    34852 - 아이스크림 접기

---
+++ BaekJoon/34000/34852.cpp
@@ -0,0 +1,21 @@
+// Title : 아이스크림 접기
+// Link  : https://www.acmicpc.net/problem/34852 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int64_t n;
+  cin >> n;
+
+  long double x = n;
+  cout << setprecision(6) << fixed << x * x * (9.0L - 5.0L * sqrt(3.0L)) / 3.0L;
+
+  return 0;
+}
