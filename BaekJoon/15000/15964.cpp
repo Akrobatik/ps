096bbb1eaa9522762a84eba3f72f3c086872a0df
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-12-01 08:50:06 +0900

    15964 - 이상한 기호

---
+++ BaekJoon/15000/15964.cpp
@@ -0,0 +1,19 @@
+// Title : 이상한 기호
+// Link  : https://www.acmicpc.net/problem/15964 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int64_t a, b;
+  cin >> a >> b;
+  cout << (a + b) * (a - b);
+
+  return 0;
+}
