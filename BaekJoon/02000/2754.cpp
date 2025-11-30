73190a153107d433ce96e242d237835fe9e9ed71
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-12-01 08:54:05 +0900

    2754 - 학점계산

---
+++ BaekJoon/02000/2754.cpp
@@ -0,0 +1,28 @@
+// Title : 학점계산
+// Link  : https://www.acmicpc.net/problem/2754 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  char a;
+  cin >> a;
+
+  int x = ('A' <= a && a <= 'D' ? 69 - a : 0) * 10;
+  if (x) {
+    char b;
+    cin >> b;
+    if (b == '+') x += 3;
+    if (b == '-') x -= 3;
+  }
+
+  cout << (x / 10) << "." << (x % 10);
+
+  return 0;
+}
