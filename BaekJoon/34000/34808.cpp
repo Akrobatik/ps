d55d3b821b8088e75b2d4f19e335b346e7c013fe
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-12-01 02:33:30 +0900

    34808 - 건조한 조건

---
+++ BaekJoon/34000/34808.cpp
@@ -0,0 +1,35 @@
+// Title : 건조한 조건
+// Link  : https://www.acmicpc.net/problem/34808 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int64_t n, m, k;
+  cin >> n >> m >> k;
+
+  if (min<int64_t>(n, m) != 1) {
+    cout << "-1";
+    return 0;
+  }
+
+  if (max<int64_t>(n, m) > 2 && k == 0) {
+    cout << "-1";
+    return 0;
+  }
+
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < m; j++) {
+      cout << k * (i + j) << " ";
+    }
+    cout << "\n";
+  }
+
+  return 0;
+}
