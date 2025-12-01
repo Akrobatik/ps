247fd29168b1e53aced95d78902ae78e7098271c
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-12-01 09:44:49 +0900

    34875 - 자벌레 세기

--- BaekJoon/34000/34875.cpp
+++ BaekJoon/34000/34875.cpp
@@ -1,7 +1,7 @@
 // Title : 자벌레 세기
 // Link  : https://www.acmicpc.net/problem/34875 
-// Time  : 60 ms
-// Memory: 5536 KB
+// Time  : 56 ms
+// Memory: 6708 KB
 
 #include <bits/stdc++.h>
 
@@ -14,19 +14,19 @@ int main() {
   int n;
   cin >> n;
 
-  vector<int> cnt(n + 1);
+  vector<int64_t> val(n + 1);
   vector<pair<int, int>> adj(n - 1);
   for (auto& [u, v] : adj) {
     cin >> u >> v;
-    ++cnt[u], ++cnt[v];
+    ++val[u], ++val[v];
+  }
+  for (int i = 1; i <= n; i++) {
+    val[i] = ((val[i] - 1) * (val[i] - 2)) >> 1;
   }
 
   int64_t ans = 0;
   for (auto [u, v] : adj) {
-    int64_t uc = cnt[u], vc = cnt[v];
-    int64_t ux = (uc - 1) * (uc - 2) / 2;
-    int64_t vx = (vc - 1) * (vc - 2) / 2;
-    ans += ux * vx;
+    ans += val[u] * val[v];
   }
   cout << ans;
 
