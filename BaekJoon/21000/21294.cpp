af23d4997761471d32e80622c02f928fc47ce5ad
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-12-01 10:14:46 +0900

    21294 - 와 쿼리

---
+++ BaekJoon/21000/21294.cpp
@@ -0,0 +1,23 @@
+// Title : 와 쿼리
+// Link  : https://www.acmicpc.net/problem/21294 
+// Time  : 72 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int q;
+  cin >> q;
+  while (q--) {
+    int x;
+    cin >> x;
+    if (x == 2) cout << "\n";
+  }
+
+  return 0;
+}
