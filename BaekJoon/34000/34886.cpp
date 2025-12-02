ac86dda56e0b1e4f278504d429a8275643e3a2fd
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-12-02 13:57:20 +0900

    34886 - 인버전 과제

---
+++ BaekJoon/34000/34886.cpp
@@ -0,0 +1,31 @@
+// Title : 인버전 과제
+// Link  : https://www.acmicpc.net/problem/34886 
+// Time  : 8 ms
+// Memory: 13808 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n, m;
+  cin >> n >> m;
+
+  int l = 1, cur = m;
+  for (int i = 1; cur && i <= n; i++) {
+    cout << l << " " << i << endl;
+    int nxt;
+    cin >> nxt;
+    if (cur > nxt) {
+      cur = nxt;
+      l = i + 1;
+    } else {
+      l = i;
+    }
+  }
+
+  return 0;
+}
