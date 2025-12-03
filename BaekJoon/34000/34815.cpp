09d3fb1bc5ae4dbba665f9886d328405c9e0f4f3
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-12-03 20:30:40 +0900

    34815 - $K+1$의 배수

---
+++ BaekJoon/34000/34815.cpp
@@ -0,0 +1,20 @@
+// Title : $K+1$의 배수
+// Link  : https://www.acmicpc.net/problem/34815 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int64_t n, k;
+  cin >> n >> k;
+
+  cout << (n == k && n % 2 == 1 ? "NO" : "YES");
+
+  return 0;
+}
