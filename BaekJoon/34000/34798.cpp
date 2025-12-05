f7dd37b885c70d34c013e32bf857de3a637bc8b4
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-12-06 02:34:06 +0900

    34798 - Missed Alarm

---
+++ BaekJoon/34000/34798.cpp
@@ -0,0 +1,19 @@
+// Title : Missed Alarm
+// Link  : https://www.acmicpc.net/problem/34798 
+// Time  : 0 ms
+// Memory: 2024 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  string s1, s2;
+  cin >> s1 >> s2;
+  cout << (s1 > s2 ? "NO" : "YES");
+
+  return 0;
+}
