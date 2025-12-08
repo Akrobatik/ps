4b0a946095d9db21152d957755eacdedc7495677
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-12-08 18:41:25 +0900

    32986 - 나는 건포도가 싫어요

---
+++ BaekJoon/32000/32986.cpp
@@ -0,0 +1,23 @@
+// Title : 나는 건포도가 싫어요
+// Link  : https://www.acmicpc.net/problem/32986 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int64_t arr[3];
+  for (auto& e : arr) cin >> e;
+  sort(arr, arr + 3);
+
+  int64_t ans = (arr[0] - 1) >> 1;
+  if (arr[0] == 3 && arr[1] == 3) ans = min<int64_t>(ans, (arr[2] - 2) >> 1);
+  cout << ans;
+
+  return 0;
+}
