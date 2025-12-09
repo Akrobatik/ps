9310351283fc051b5ec03b342bb8b8c36aa54edf
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-12-09 17:10:53 +0900

    34849 - 이중 반복문

---
+++ BaekJoon/34000/34849.cpp
@@ -0,0 +1,19 @@
+// Title : 이중 반복문
+// Link  : https://www.acmicpc.net/problem/34849 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int64_t n;
+  cin >> n;
+  cout << (n <= 10000 ? "Accepted" : "Time limit exceeded");
+
+  return 0;
+}
