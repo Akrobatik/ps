98ae9d0fe627b450b7626c20519575d293545e9f
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-12-11 23:02:45 +0900

    34813 - 공통교육과정

---
+++ BaekJoon/34000/34813.cpp
@@ -0,0 +1,35 @@
+// Title : 공통교육과정
+// Link  : https://www.acmicpc.net/problem/34813 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  char c;
+  cin >> c;
+  switch (c) {
+    case 'F': {
+      cout << "Foundation";
+    } break;
+
+    case 'C': {
+      cout << "Claves";
+    } break;
+
+    case 'V': {
+      cout << "Veritas";
+    } break;
+
+    case 'E': {
+      cout << "Exploration";
+    } break;
+  }
+
+  return 0;
+}
