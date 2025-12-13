508ece82a5af375150deeeae58550116b5f73112
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-12-13 12:24:28 +0900

    16440 - 제이크와 케이크

---
+++ BaekJoon/16000/16440.cpp
@@ -0,0 +1,39 @@
+// Title : 제이크와 케이크
+// Link  : https://www.acmicpc.net/problem/16440 
+// Time  : 0 ms
+// Memory: 2648 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  string s;
+  cin >> n >> s;
+
+  int half = n >> 1, quad = n >> 2, cnt = 0;
+  for (int i = 0; i < half; i++) {
+    cnt += (s[i] == 's');
+  }
+
+  if (cnt == quad) {
+    cout << "1\n"
+         << half;
+    return 0;
+  }
+
+  for (int i = 0; i < half; i++) {
+    cnt += (s[i + half] == 's') - (s[i] == 's');
+    if (cnt == quad) {
+      cout << "2\n"
+           << i + 1 << " " << i + half + 1;
+      return 0;
+    }
+  }
+
+  return 0;
+}
