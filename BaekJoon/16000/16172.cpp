8d1bf8b209d2da8192ee599fc00e5a93a5687f60
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-12-13 09:03:21 +0900

    16172 - 나는 친구가 적다 (Large)

---
+++ BaekJoon/16000/16172.cpp
@@ -0,0 +1,24 @@
+// Title : 나는 친구가 적다 (Large)
+// Link  : https://www.acmicpc.net/problem/16172 
+// Time  : 8 ms
+// Memory: 3088 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  string s1, s2, s3;
+  cin >> s1 >> s2;
+
+  for (auto c : s1) {
+    if (isalpha(c)) s3.push_back(c);
+  }
+
+  cout << (strstr(s3.data(), s2.data()) != nullptr);
+
+  return 0;
+}
