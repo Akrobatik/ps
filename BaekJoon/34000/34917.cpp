8287607a599e37506570bf469c2f34fadd9eb87c
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-12-15 18:20:38 +0900

    34917 - M

---
+++ BaekJoon/34000/34917.cpp
@@ -0,0 +1,29 @@
+// Title : M
+// Link  : https://www.acmicpc.net/problem/34917 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int t;
+  cin >> t;
+  while (t--) {
+    int n;
+    cin >> n;
+    int half = n >> 1;
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < n; j++) {
+        cout << ((j == 0 || j + 1 == n) || (i <= half && abs(half - j) == half - i) ? '#' : '.');
+      }
+      cout << '\n';
+    }
+  }
+
+  return 0;
+}
