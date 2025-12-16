c6ef1934c77d2c6e8f09365657af60e4165285c1
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-12-16 09:47:50 +0900

    34840 - 인버전의 기댓값

---
+++ BaekJoon/34000/34840.cpp
@@ -0,0 +1,29 @@
+// Title : 인버전의 기댓값
+// Link  : https://www.acmicpc.net/problem/34840 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  cin >> n;
+
+  if (n == 1) {
+    cout << "0";
+    return 0;
+  }
+
+  long double e = 0.5L;
+  for (int i = 2; i < n; i++) {
+    e = e * (long double)(i + 1) / (long double)(i - 1);
+  }
+  cout << setprecision(9) << fixed << e;
+
+  return 0;
+}
