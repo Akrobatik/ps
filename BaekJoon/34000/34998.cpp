c7427cee62b9a5521427a981524a121cacec1eac
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-12-23 20:09:51 +0800

    34998 - 1, 2, 3, 많다!

---
+++ BaekJoon/34000/34998.cpp
@@ -0,0 +1,23 @@
+// Title : 1, 2, 3, 많다!
+// Link  : https://www.acmicpc.net/problem/34998 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+int main(){
+    auto F=[&](char c){if(c=='!')return 10;
+                       return c-'0';
+                       };
+    int t;
+    std::cin>>t;
+    while(t--){
+        int n;char c;
+        std::cin>>n>>c;
+        int x=F(c);
+        for(int i=0;i<n;i++){
+            std::cin>>c>>c;
+            x+=F(c);
+            }
+        std::cout<<(char)(x>9?'!':x+'0')<<'\n';
+        }}
+        
