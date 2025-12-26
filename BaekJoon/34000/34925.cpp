ffa2d7db56b07ed543a4b06e80b450961cb96309
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-12-26 15:53:10 +0800

    34925 - 실크의 노래

---
+++ BaekJoon/34000/34925.cpp
@@ -0,0 +1,25 @@
+// Title : 실크의 노래
+// Link  : https://www.acmicpc.net/problem/34925 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int64_t h, s;
+  cin >> h >> s;
+
+  if (h <= 2) {
+    cout << "1";
+  } else {
+    h += s * (h >= 5 ? 3 : 2);
+    cout << (h + 1) / 2;
+  }
+
+  return 0;
+}
