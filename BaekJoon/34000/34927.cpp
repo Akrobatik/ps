b9f0ac6fe4ccccb2a837a5bf4c82dbf1386349de
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-12-26 15:59:00 +0800

    34927 - 박스 쌓기

---
+++ BaekJoon/34000/34927.cpp
@@ -0,0 +1,30 @@
+// Title : 박스 쌓기
+// Link  : https://www.acmicpc.net/problem/34927 
+// Time  : 56 ms
+// Memory: 3192 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  cin >> n;
+  vector<int> arr(n);
+  for (auto& e : arr) cin >> e;
+  sort(arr.begin(), arr.end());
+
+  int64_t acc = 0, cnt = 0;
+  for (auto e : arr) {
+    if (acc <= e) {
+      acc += e;
+      ++cnt;
+    }
+  }
+  cout << cnt;
+
+  return 0;
+}
