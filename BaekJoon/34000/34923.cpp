03f7b85a3736ab74925b63697a64357e1a1b1cc8
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-12-26 15:49:57 +0800

    34923 - 꼬마 미타

---
+++ BaekJoon/34000/34923.cpp
@@ -0,0 +1,27 @@
+// Title : 꼬마 미타
+// Link  : https://www.acmicpc.net/problem/34923 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int Read() {
+  int hh, mm;
+  char c;
+  cin >> hh >> c >> mm;
+  return (hh - 1) * 60 + mm;
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int x = Read(), y = Read();
+  if (x > y) swap(x, y);
+  int dt = min<int>(y - x, x + 720 - y);
+  cout << dt * 6;
+
+  return 0;
+}
