da6d0532dd7b61d85085afa8322962f752941c7d
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-12-26 15:34:52 +0800

    34921 - 덕후

---
+++ BaekJoon/34000/34921.cpp
@@ -0,0 +1,21 @@
+// Title : 덕후
+// Link  : https://www.acmicpc.net/problem/34921 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int a, t;
+  cin >> a >> t;
+
+  int x = 10 + 2 * (25 - a + t);
+  cout << max<int>(x, 0);
+
+  return 0;
+}
