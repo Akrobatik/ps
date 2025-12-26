9a8b5144e5938fb01b5f9f5538df5df56d4d2136
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-12-26 15:57:09 +0800

    34926 - 공룡 게임

---
+++ BaekJoon/34000/34926.cpp
@@ -0,0 +1,28 @@
+// Title : 공룡 게임
+// Link  : https://www.acmicpc.net/problem/34926 
+// Time  : 8 ms
+// Memory: 4148 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n, k;
+  string s;
+  cin >> n >> k >> s;
+
+  vector<int8_t> memo(n);
+  memo[0] = 1;
+  for (int i = 1; i < n; i++) {
+    if (s[i] == '_' && (memo[i - 1] || (i - k >= 0 && memo[i - k]))) {
+      memo[i] = 1;
+    }
+  }
+  cout << (memo[n - 1] ? "YES" : "NO");
+
+  return 0;
+}
