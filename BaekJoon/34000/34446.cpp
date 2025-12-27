bd85a7fe5cf6813c937c08aba5bda95e535d1e20
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-12-28 03:31:20 +0800

    34446 - E-Days Ore Cart Pull

---
+++ BaekJoon/34000/34446.cpp
@@ -0,0 +1,19 @@
+// Title : E-Days Ore Cart Pull
+// Link  : https://www.acmicpc.net/problem/34446 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int64_t a, b, c;
+  cin >> a >> b >> c;
+  cout << a * 2;
+
+  return 0;
+}
