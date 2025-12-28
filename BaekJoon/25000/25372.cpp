29abe6f2527e87ed1a0762ceb0f28d59478ae881
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-12-29 03:19:23 +0800

    25372 - 성택이의 은밀한 비밀번호

---
+++ BaekJoon/25000/25372.cpp
@@ -0,0 +1,24 @@
+// Title : 성택이의 은밀한 비밀번호
+// Link  : https://www.acmicpc.net/problem/25372 
+// Time  : 0 ms
+// Memory: 2024 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int t;
+  cin >> t;
+  while (t--) {
+    string s;
+    cin >> s;
+    int n = s.size();
+    cout << (6 <= n && n <= 9 ? "yes\n" : "no\n");
+  }
+
+  return 0;
+}
