6587a0eaae8559aea1b8aba3b066ddd7652a5e74
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-12-29 07:47:22 +0800

    26766 - Serca

---
+++ BaekJoon/26000/26766.cpp
@@ -0,0 +1,33 @@
+// Title : Serca
+// Link  : https://www.acmicpc.net/problem/26766 
+// Time  : 44 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+constexpr const char kStr[] =
+    R"( @@@   @@@ 
+@   @ @   @
+@    @    @
+@         @
+ @       @ 
+  @     @  
+   @   @   
+    @ @    
+     @     )";
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  cin >> n;
+
+  while (n--) {
+    cout << kStr << "\n";
+  }
+
+  return 0;
+}
