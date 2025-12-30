e7f03ed597302145715a2f049ca6fc47ab86ee30
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-12-31 07:38:06 +0800

    16076 - 휴식이 필요해

---
+++ BaekJoon/16000/16076.cpp
@@ -0,0 +1,17 @@
+// Title : 휴식이 필요해
+// Link  : https://www.acmicpc.net/problem/16076 
+// Time  : 0 ms
+// Memory: 0 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  cout << "2025-12-31";
+
+  return 0;
+}
