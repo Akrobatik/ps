5f293934c2ab73fb4616a6f980b4ab4b15ab784f
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-12-30 22:33:54 +0800

    32929 - UOS 문자열

---
+++ BaekJoon/32000/32929.cpp
@@ -0,0 +1,19 @@
+// Title : UOS 문자열
+// Link  : https://www.acmicpc.net/problem/32929 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  cin >> n, --n;
+  cout << "UOS"[n % 3];
+
+  return 0;
+}
