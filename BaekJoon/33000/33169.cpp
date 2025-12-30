ffa4157eca75471de83f24bbdb02f56be7d26bcc
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2025-12-31 07:33:52 +0800

    33169 - 所持金 (Money On Me)

---
+++ BaekJoon/33000/33169.cpp
@@ -0,0 +1,19 @@
+// Title : 所持金 (Money On Me)
+// Link  : https://www.acmicpc.net/problem/33169 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int a, b;
+  cin >> a >> b;
+  cout << (a * 1000 + b * 10000);
+
+  return 0;
+}
