954f3f59424bbabfd5846e6df60ce3a7db7f9bbc
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2026-01-04 09:23:04 +0800

    35071 - Circular Shortcut

---
+++ BaekJoon/35000/35071.cpp
@@ -0,0 +1,21 @@
+// Title : Circular Shortcut
+// Link  : https://www.acmicpc.net/problem/35071 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+constexpr long double kPI = 3.14159265358979L;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  long double x;
+  cin >> x;
+  cout << setprecision(9) << fixed << (kPI / 2.0L - 1.0L) * x;
+
+  return 0;
+}
