c135c326ef254ed8e6fec017009b37b70fdf93cd
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2026-01-06 06:30:19 +0900

    35060 - 겨울 축제 2

---
+++ BaekJoon/35000/35060.cpp
@@ -0,0 +1,14 @@
+// Title : 겨울 축제 2
+// Link  : https://www.acmicpc.net/problem/35060 
+// Time  : 8 ms
+// Memory: 2160 KB
+
+#include "festival.h"
+//
+#include <bits/stdc++.h>
+
+int answer(int N, int K, int i, std::vector<int> v) {
+  int64_t acc = 0;
+  for (auto e : v) acc += e;
+  return K - acc % K;
+}
