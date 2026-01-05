211b72ca8e37b55903bb33ba65cf5f6300c4177f
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2026-01-06 06:42:54 +0900

    34970 - 더 좋은 대화 수단

---
+++ BaekJoon/34000/34970.cpp
@@ -0,0 +1,37 @@
+// Title : 더 좋은 대화 수단
+// Link  : https://www.acmicpc.net/problem/34970 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+constexpr int kArr[] = {1, 3, 2};
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  cin >> n;
+
+  for (int i = 0; i < n; i++) {
+    cout << kArr[i % 3] << " \n"[i + 1 == n];
+  }
+
+  int q = n / 3, r = n % 3;
+  int cnt = q * 2 + (r ? 2 : 1);
+  cout << cnt << "\n";
+  for (int i = 0; i < q; i++) {
+    int st = i * 3 + 1;
+    cout << st << " " << st + 1 << "\n"
+         << st + 1 << " " << st + 1 << "\n";
+  }
+
+  int en = n - (r == 2 ? 1 : 0);
+  cout << "1 " << en << "\n";
+  if (r) cout << en << " " << en;
+
+  return 0;
+}
