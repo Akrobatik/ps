386583cd59e5480115b5a7ff121d7f7c2bc86c66
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2026-01-08 01:08:12 +0900

    35097 - 2025

---
+++ BaekJoon/35000/35097.cpp
@@ -0,0 +1,30 @@
+// Title : 2025
+// Link  : https://www.acmicpc.net/problem/35097 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  vector<int> arr(101);
+  for (int i = 1; i <= 100; i++) {
+    arr[i] = arr[i - 1] + i;
+  }
+
+  vector<int> axr(101);
+  for (int i = 1; i <= 100; i++) {
+    axr[i] = arr[i] * arr[i];
+  }
+
+  int n;
+  while (cin >> n && n) {
+    cout << axr[n] << "\n";
+  }
+
+  return 0;
+}
