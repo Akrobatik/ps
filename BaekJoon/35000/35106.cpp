27c6ffcf088cbbd49cf17c5907dec2bd5bfa45a9
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2026-01-13 01:31:32 +0900

    35106 - 릴레이 가위바위보 게임

---
+++ BaekJoon/35000/35106.cpp
@@ -0,0 +1,32 @@
+// Title : 릴레이 가위바위보 게임
+// Link  : https://www.acmicpc.net/problem/35106 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  cin >> n;
+
+  int cnt[4] = {};
+  for (int i = 1; i <= n * 3; i++) {
+    int x;
+    cin >> x;
+    ++cnt[x];
+  }
+
+  for (int i = 1; i <= 3; i++) {
+    if (cnt[i] == n - 1) cout << i << "\n";
+  }
+  for (int i = 1; i <= 3; i++) {
+    if (cnt[i] == n + 1) cout << i << "\n";
+  }
+
+  return 0;
+}
