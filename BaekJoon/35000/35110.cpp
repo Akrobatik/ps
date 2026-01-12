ed894c502c4d36a011f07760bf3a6770c3faa180
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2026-01-13 01:31:59 +0900

    35110 - Swap, then record

---
+++ BaekJoon/35000/35110.cpp
@@ -0,0 +1,38 @@
+// Title : Swap, then record
+// Link  : https://www.acmicpc.net/problem/35110 
+// Time  : 52 ms
+// Memory: 3592 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  cin >> n;
+
+  vector<int> arr(n);
+  for (auto& e : arr) cin >> e;
+
+  auto memo = arr;
+  sort(memo.begin(), memo.end());
+
+  for (auto& e : arr) {
+    e = lower_bound(memo.begin(), memo.end(), e) - memo.begin();
+  }
+
+  int64_t sum = 0;
+  int idx = 0;
+  while (idx < n) {
+    int nxt = idx, bnd = arr[idx];
+    while (nxt < bnd) bnd = max<int>(bnd, arr[++nxt]);
+    sum += memo[nxt] - memo[idx];
+    idx = nxt + 1;
+  }
+  cout << sum;
+
+  return 0;
+}
