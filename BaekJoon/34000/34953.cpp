6422b17cde28c4d3bb4b77bc6c7e580b60865e39
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2026-01-12 18:21:10 +0900

    34953 - SSHS 문자열

---
+++ BaekJoon/34000/34953.cpp
@@ -0,0 +1,22 @@
+// Title : SSHS 문자열
+// Link  : https://www.acmicpc.net/problem/34953 
+// Time  : 8 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  cin >> n;
+
+  for (int i = 0; i < n; i++) {
+    cout << "SSH"[i % 3];
+  }
+
+  return 0;
+}
