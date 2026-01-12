a5552b050ff9855ad36e658b53d796b650cfec62
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2026-01-13 01:31:52 +0900

    35109 - ^&^

---
+++ BaekJoon/35000/35109.cpp
@@ -0,0 +1,34 @@
+// Title : ^&^
+// Link  : https://www.acmicpc.net/problem/35109 
+// Time  : 0 ms
+// Memory: 2024 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  string s;
+  cin >> s;
+
+  if (s == "10") {
+    cout << "0";
+    return 0;
+  }
+
+  int cnt = count(s.begin(), s.end(), '1');
+  if (s.back() == '1') {
+    cout << s;
+  } else if (cnt + 1 == s.size() && s.back() == '0') {
+    s[s.size() - 2] = '0';
+    cout << s;
+  } else {
+    for (auto& c : s) c = '1';
+    cout << s;
+  }
+
+  return 0;
+}
