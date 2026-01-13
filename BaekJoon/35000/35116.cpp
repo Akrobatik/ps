99b30e1e1249fa70d33367a83a0e3d71d9378529
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2026-01-13 17:47:57 +0900

    35116 - tree xor

---
+++ BaekJoon/35000/35116.cpp
@@ -0,0 +1,34 @@
+// Title : tree xor
+// Link  : https://www.acmicpc.net/problem/35116 
+// Time  : 8 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  cin >> n;
+
+  if (n == 2) {
+    cout << "1 2";
+    return 0;
+  }
+
+  auto Print = [](int u, int v) {
+    if (u > v) swap(u, v);
+    cout << u + 1 << " " << v + 1 << "\n";
+  };
+
+  int half = n >> 1;
+  for (int t = 1; t < n; t++) {
+    for (int i = 1; i <= half; i++) Print(i, n - i);
+    for (int i = 1; i <= half; i++) Print((t + i) % n, (n + t - i) % n);
+  }
+
+  return 0;
+}
