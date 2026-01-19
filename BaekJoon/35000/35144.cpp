899b3a767b4089b8d5690c7133d04dd99e02006b
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2026-01-19 17:28:04 +0900

    35144 - $\exists99$

---
+++ BaekJoon/35000/35144.cpp
@@ -0,0 +1,32 @@
+// Title : $\exists99$
+// Link  : https://www.acmicpc.net/problem/35144 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int64_t GCD(int64_t a, int64_t b) {
+  while (b) a %= b, swap(a, b);
+  return a;
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int64_t a, b;
+  cin >> a >> b;
+  if (a > b) swap(a, b);
+
+  if (a == b) {
+    cout << "INF";
+  } else {
+    int64_t p = a * a, q = b - a;
+    int64_t g = GCD(p, q);
+    cout << p / g << " " << q / g << "\n";
+  }
+
+  return 0;
+}
