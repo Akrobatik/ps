72f04f314e69c96e41d5e297e4786ba357acf451
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2026-01-19 17:07:49 +0900

    35146 - $02493$

---
+++ BaekJoon/35000/35146.cpp
@@ -0,0 +1,25 @@
+// Title : $02493$
+// Link  : https://www.acmicpc.net/problem/35146 
+// Time  : 316 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int t;
+  cin >> t;
+  while (t--) {
+    int x;
+    cin >> x;
+
+    int n = 1, m = x, a = 10, b = 10;
+    cout << n << " " << m << " " << a << " " << b << "\n";
+  }
+
+  return 0;
+}
