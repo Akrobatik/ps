81a541299f2db65639487215f2f3890a8fd5c097
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2026-01-26 17:04:34 +0700

    35162 - KCPC 스티커 만들기

---
+++ BaekJoon/35000/35162.cpp
@@ -0,0 +1,56 @@
+// Title : KCPC 스티커 만들기
+// Link  : https://www.acmicpc.net/problem/35162 
+// Time  : 8 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int t;
+  cin >> t;
+  while (t--) {
+    int k, c, p;
+    cin >> k >> c >> p;
+
+    if (k == 0) {
+      if (p == 0) {
+        cout << (c >= 2 ? "Y\n" : "N\n");
+      } else if (p == 1) {
+        cout << (c != 0 ? "Y\n" : "N\n");
+      } else {
+        cout << "Y\n";
+      }
+    } else if (k == 1) {
+      if (p == 0) {
+        cout << (c != 0 ? "Y\n" : "N\n");
+      } else if (p == 1) {
+        cout << (c != 0 ? "Y\n" : "N\n");
+      } else {
+        cout << "Y\n";
+      }
+    } else if (k == 2) {
+      if (p == 0) {
+        cout << (c >= 2 ? "Y\n" : "N\n");
+      } else if (p == 1) {
+        cout << (c != 0 ? "Y\n" : "N\n");
+      } else {
+        cout << "Y\n";
+      }
+    } else {
+      if (p == 0) {
+        cout << (c >= 2 ? "Y\n" : "N\n");
+      } else if (p == 1) {
+        cout << (c != 0 ? "Y\n" : "N\n");
+      } else {
+        cout << "Y\n";
+      }
+    }
+  }
+
+  return 0;
+}
