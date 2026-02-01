b8dfe515f70ac409d22ae34a885f260f2b10a7a5
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2026-02-01 19:19:33 +0900

    32758 - N+1 행사

---
+++ BaekJoon/32000/32758.cpp
@@ -0,0 +1,27 @@
+// Title : N+1 행사
+// Link  : https://www.acmicpc.net/problem/32758 
+// Time  : 28 ms
+// Memory: 2412 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  cin >> n;
+
+  vector<int> arr(n);
+  for (auto& e : arr) cin >> e;
+
+  for (int i = 0; i < n; i++) {
+    int64_t x;
+    cin >> x;
+    cout << (x ? x - (x - 1) / arr[i] : 0) << " ";
+  }
+
+  return 0;
+}
