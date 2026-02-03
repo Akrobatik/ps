99adfedf430bd61bc57c2ee2e95bb4607433d93a
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2026-02-03 19:02:05 +0900

    35247 - Itsy Bits

---
+++ BaekJoon/35000/35247.cpp
@@ -0,0 +1,21 @@
+// Title : Itsy Bits
+// Link  : https://www.acmicpc.net/problem/35247 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  uint64_t n;
+  cin >> n;
+
+  int bw = bit_ceil((uint32_t)bit_width(n));
+  cout << bw << (bw > 1 ? " bits" : " bit");
+
+  return 0;
+}
