d4ee3a36e2b6cdb3e25d09e2abe477f8cb898041
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2026-02-03 19:01:52 +0900

    35245 - Get Good

---
+++ BaekJoon/35000/35245.cpp
@@ -0,0 +1,35 @@
+// Title : Get Good
+// Link  : https://www.acmicpc.net/problem/35245 
+// Time  : 4 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int t;
+  cin >> t;
+  while (t--) {
+    int64_t n, a, b, x, y;
+    cin >> n >> a >> b >> x >> y;
+
+    int64_t cut = min<int64_t>(n, x);
+    int64_t ans = a * cut;
+    n -= cut;
+
+    int64_t cyc = x + y;
+    int64_t cv = max<int64_t>(a * x, b * cyc);
+
+    ans += (n / cyc) * cv;
+    n %= cyc;
+
+    int64_t add = max<int64_t>(a * max<int>(n - y, 0), b * n);
+    cout << ans + add << "\n";
+  }
+
+  return 0;
+}
