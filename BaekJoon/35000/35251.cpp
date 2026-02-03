f0807e0532b37df4852c14f5010c7d68cb541342
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2026-02-03 19:02:26 +0900

    35251 - Motorway Stops

---
+++ BaekJoon/35000/35251.cpp
@@ -0,0 +1,33 @@
+// Title : Motorway Stops
+// Link  : https://www.acmicpc.net/problem/35251 
+// Time  : 8 ms
+// Memory: 2412 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  cin >> n;
+
+  vector<int> arr(n);
+  for (auto& e : arr) cin >> e;
+
+  int maxx = 0;
+  for (int i = 1; i < n; i++) {
+    maxx = max<int>(maxx, arr[i] - arr[i - 1]);
+  }
+
+  int minn = INT_MAX;
+  for (int i = 1; i + 1 < n; i++) {
+    int val = max<int>(maxx, arr[i + 1] - arr[i - 1]);
+    minn = min<int>(minn, val);
+  }
+  cout << minn;
+
+  return 0;
+}
