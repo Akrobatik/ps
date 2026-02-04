d537672a19b642e8d280e4279615280699747fb5
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2026-02-04 20:56:14 +0900

    35230 - Dungeon Equilibrium

---
+++ BaekJoon/35000/35230.cpp
@@ -0,0 +1,31 @@
+// Title : Dungeon Equilibrium
+// Link  : https://www.acmicpc.net/problem/35230 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  cin >> n;
+
+  vector<int> cnt(n + 1);
+  for(int i = 0; i < n; i++) {
+    int x;
+    cin >> x;
+    ++cnt[x];
+  }
+
+  int cut = 0;
+  for (int i = 0; i <= n; i++) {
+    cut += cnt[i] - (cnt[i] >= i ? i : 0);
+  }
+  cout << cut;
+
+  return 0;
+}
