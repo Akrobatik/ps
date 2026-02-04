f30f6f77d9c5172bbf010139111d24baae407375
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2026-02-04 20:56:20 +0900

    35195 - Dralinpome

---
+++ BaekJoon/35000/35195.cpp
@@ -0,0 +1,24 @@
+// Title : Dralinpome
+// Link  : https://www.acmicpc.net/problem/35195 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int cnt[256] = {};
+  char c;
+  while (cin >> c) ++cnt[c];
+
+  int odd = 0;
+  for (int i = 'a'; i <= 'z'; i++) odd += cnt[i] & 1;
+
+  cout << (odd <= 1 ? "yes" : "no");
+
+  return 0;
+}
