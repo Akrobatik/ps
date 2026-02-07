99e9c27a0578bce9e659b5d8cc304729d5d24891
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2026-02-07 15:33:53 +0900

    30030 - 스위트콘 가격 구하기

---
+++ BaekJoon/30000/30030.cpp
@@ -0,0 +1,20 @@
+// Title : 스위트콘 가격 구하기
+// Link  : https://www.acmicpc.net/problem/30030 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  cin >> n;
+
+  cout << (n / 11) * 10;
+
+  return 0;
+}
