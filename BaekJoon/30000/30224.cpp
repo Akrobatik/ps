019c0777b7716ba3cbabef91f6f4e03648e276bf
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2026-02-10 22:31:52 +0900

    30224 - Lucky 7

---
+++ BaekJoon/30000/30224.cpp
@@ -0,0 +1,23 @@
+// Title : Lucky 7
+// Link  : https://www.acmicpc.net/problem/30224 
+// Time  : 0 ms
+// Memory: 2024 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  string s;
+  cin >> s;
+
+  int x = 0;
+  x |= (stoi(s) % 7 == 0);
+  x |= (s.find('7') != string::npos ? 2 : 0);
+  cout << x;
+
+  return 0;
+}
