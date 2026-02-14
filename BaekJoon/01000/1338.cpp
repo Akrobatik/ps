2d228828fe82088c4818af6e6c6aa44808d73987
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2026-02-15 02:50:30 +0900

    1338 - 알 수 없는 번호

---
+++ BaekJoon/01000/1338.cpp
@@ -0,0 +1,36 @@
+// Title : 알 수 없는 번호
+// Link  : https://www.acmicpc.net/problem/1338 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int64_t l, r, x, y;
+  cin >> l >> r >> x >> y;
+  if (l > r) swap(l, r);
+
+  if (!(0 <= y && y < abs(x))) {
+    cout << "Unknwon Number";
+    return 0;
+  }
+
+  int64_t v = l % x;
+  if (v < 0) v += x;
+  v = l + (v <= y ? y - v : x + y - v);
+
+  if (!(l <= v && v <= r)) {
+    cout << "Unknwon Number";
+  } else if (l <= (v + x) && (v + x) <= r) {
+    cout << "Unknwon Number";
+  } else {
+    cout << v;
+  }
+
+  return 0;
+}
