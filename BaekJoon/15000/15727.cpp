2846e0318620d0df3b38a15aa56b34c86adb71a7
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2026-02-28 03:22:49 +0900

    15727 - 조별과제를 하려는데 조장이 사라졌다

---
+++ BaekJoon/15000/15727.cpp
@@ -0,0 +1,19 @@
+// Title : 조별과제를 하려는데 조장이 사라졌다
+// Link  : https://www.acmicpc.net/problem/15727 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  cin >> n;
+  cout << (n + 4) / 5;
+
+  return 0;
+}
