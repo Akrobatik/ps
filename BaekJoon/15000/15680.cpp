c4c9dbf78ab648f2bfd886577c265d5951647eeb
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2026-03-02 01:11:59 +0900

    15680 - 연세대학교

---
+++ BaekJoon/15000/15680.cpp
@@ -0,0 +1,19 @@
+// Title : 연세대학교
+// Link  : https://www.acmicpc.net/problem/15680 
+// Time  : 0 ms
+// Memory: 2020 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  cin >> n;
+  cout << (!n ? "YONSEI" : "Leading the Way to the Future");
+
+  return 0;
+}
