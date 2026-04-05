3aa094165f1734f6e4764eaca90473d38eaec268
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2026-04-06 00:40:37 +0900

    35338 - It's Mooin' Time IV

---
+++ BaekJoon/35000/35338.cpp
@@ -0,0 +1,32 @@
+// Title : It's Mooin' Time IV
+// Link  : https://www.acmicpc.net/problem/35338 
+// Time  : 4 ms
+// Memory: 2648 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int t, k;
+  cin >> t >> k;
+  while (t--) {
+    int n;
+    string s;
+    cin >> n >> s;
+
+    int key = 0;
+    for (auto& c : views::reverse(s)) {
+      c ^= key;
+      if (c == 'O') key ^= 2;
+    }
+
+    cout << "YES\n";
+    if (k) cout << s << "\n";
+  }
+
+  return 0;
+}
