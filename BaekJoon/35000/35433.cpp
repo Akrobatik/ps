0ffb5d834807283bddcb8d2c2a0343f1f6c03dbe
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2026-04-06 02:11:24 +0900

    35433 - 고속 프리렌 변환

--- BaekJoon/35000/35433.cpp
+++ BaekJoon/35000/35433.cpp
@@ -1,18 +1,20 @@
 // Title : 고속 프리렌 변환
 // Link  : https://www.acmicpc.net/problem/35433 
-// Time  : 652 ms
-// Memory: 80148 KB
+// Time  : 276 ms
+// Memory: 11928 KB
 
 #include <bits/stdc++.h>
 
 using namespace std;
 
-#define int int64_t
-void Solve() {
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
   int n, x;
   cin >> n >> x;
 
-  int mul = 1, div = 1, mlen = n + 1;
+  int64_t mul = 1, div = 1, mlen = n + 1;
   vector<int> arr(n);
   for (int i = 0; i < n; i++) {
     int a, l;
@@ -21,9 +23,8 @@ void Solve() {
     arr[i] = l;
   }
 
-  vector<int> digits(mlen);
-
   int m = 1 << n;
+  vector<pair<int64_t, int64_t>> axr(m);
   for (int i = 0; i < m; i++) {
     int len = 0, sign = 1;
     for (int j = 0, k = i; j < n; j++, k >>= 1) {
@@ -34,30 +35,27 @@ void Solve() {
       }
     }
 
-    digits[len] += mul * sign;
+    axr[i] = {len, mul * sign};
   }
+  sort(axr.begin(), axr.end(), greater<pair<int64_t, int64_t>>());
 
-  int carry = 0, flip = n & 1;
+  string digits(mlen, '\0');
+  int64_t carry = 0, flip = n & 1;
   for (int i = 0; i < mlen; i++) {
-    int val = digits[i] + carry;
-    int rem = val % x;
+    int64_t val = carry;
+    while (!axr.empty() && axr.back().first == i) val += axr.back().second, axr.pop_back();
+    int64_t rem = val % x;
     if (rem < 0) rem += x;
 
-    digits[i] = (flip ? (rem ? x - rem : 0) : rem);
-    carry = (val - digits[i] * div) / x;
+    int64_t d = flip ? (rem ? x - rem : 0) : rem;
+    digits[i] = d + '0';
+    carry = (val - div * d) / x;
   }
 
-  while (!digits.empty() && digits.back() == 0) digits.pop_back();
+  while (!digits.empty() && digits.back() == '0') digits.pop_back();
+  reverse(digits.begin(), digits.end());
 
-  for (auto e : views::reverse(digits)) cout << e;
-}
-#undef int
-
-int main() {
-  ios::sync_with_stdio(false);
-  cin.tie(nullptr);
-
-  Solve();
+  cout << digits;
 
   return 0;
 }
