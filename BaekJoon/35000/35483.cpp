51df6ecf3188c6807ac6d2cc2f2d15feabab2c2a
Akrobatik <82329914+Akrobatik@users.noreply.github.com>
2026-04-07 22:27:02 +0900

    35483 - 루미상관 수

---
+++ BaekJoon/35000/35483.cpp
@@ -0,0 +1,44 @@
+// Title : 루미상관 수
+// Link  : https://www.acmicpc.net/problem/35483 
+// Time  : 88 ms
+// Memory: 7172 KB
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  set<int> st;
+  for (int i = 1; i < 10; i++) {
+    st.insert(i);
+  }
+
+  for (int i = 1; i < 10000; i++) {
+    int x = 1;
+    while (x * 10 <= i) x *= 10;
+    x *= 10;
+
+    st.insert(i * x + i);
+    for (int j = 0; j < 10; j++) {
+      st.insert((i * 10 + j) * x + i);
+    }
+  }
+
+  int t;
+  cin >> t;
+  while (t--) {
+    int n;
+    cin >> n;
+
+    int cnt = 0;
+    for (auto e : st) {
+      cnt += st.contains(n - e);
+    }
+    cout << cnt << "\n";
+  }
+
+  return 0;
+}
